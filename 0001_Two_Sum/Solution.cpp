606e75469e10e899bfb321935944d7848d6dc488
neilhuang <neilhuang@synology.com>
2021-09-13 22:54:49 +0800

    <LeetCode202109> 1. Two Sum

---
+++ 0001_Two_Sum/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::cout;
+using std::endl;
+using std::unordered_map;
+
+vector<int> Solution::twoSum(vector<int>& nums, int target) {
+	// assume nums number bigger than 2
+	unordered_map<int, int> indices_map;
+
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		if (indices_map.find(target-nums[i]) != indices_map.end()) {
+			return {i, indices_map[target-nums[i]]};
+		} else {
+			indices_map[nums[i]] = i;
+		}
+	}
+	return {0,0};
+}
---
+++ 0001_Two_Sum/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,2,4};
+	Solution* test = new Solution();
+	vector<int> result = test->twoSum(input, 6);
+	for (const auto iter : result) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
