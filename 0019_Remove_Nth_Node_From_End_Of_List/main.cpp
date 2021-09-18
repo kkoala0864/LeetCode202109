28841ce9b078d986045b23a0a6a40d8f80af5f95
neilhuang <neilhuang@synology.com>
2021-09-18 12:12:16 +0800

    <LeetCode202109> 19. Remove Nth Node From End of List

---
+++ 0019_Remove_Nth_Node_From_End_Of_List/main.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	ListNode *five = new ListNode(5);
+	ListNode *four = new ListNode(4, five);
+	ListNode *three = new ListNode(3, four);
+	ListNode *two = new ListNode(2, three);
+	ListNode *one = new ListNode(1, two);
+	//ListNode *one = new ListNode(1);
+	Solution* test = new Solution();
+	ListNode* result = test->removeNthFromEnd(one, 2);
+	while (result != nullptr) {
+		cout << result->val << " ";
+		result = result->next;
+	}
+	cout << endl;
+    return 0;
+}
+
