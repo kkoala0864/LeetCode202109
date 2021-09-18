0c6a1af1ddc6fb67bb99c8641f4e1cada49c3f21
neilhuang <neilhuang@synology.com>
2021-09-18 15:35:04 +0800

    <LeetCode202109> 23. Merge k Sorted Lists

---
+++ 0023_Merge_k_Sorted_Lists/Solution.cpp
@@ -0,0 +1,77 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::queue;
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+	if (l1 == nullptr) return l2;
+	if (l2 == nullptr) return l1;
+	ListNode* iter1 = l1;
+	ListNode* iter2 = l2;
+	ListNode* head = nullptr;
+	if (iter1->val <= iter2->val) {
+		head = iter1;
+		iter1 = iter1->next;
+	} else {
+		head = iter2;
+		iter2 = iter2->next;
+	}
+	ListNode* resultIter = head;
+	while (iter1 && iter2) {
+		if (iter1->val < iter2->val) {
+			resultIter->next = iter1;
+			while (iter1->next != nullptr && iter1->next->val < iter2->val) {
+				iter1 = iter1->next;
+			}
+			resultIter = iter1;
+			iter1 = iter1->next;
+		} else if (iter2->val < iter1->val) {
+			resultIter->next = iter2;
+			while (iter2->next != nullptr && iter2->next->val < iter1->val) {
+				iter2 = iter2->next;
+			}
+			resultIter = iter2;
+			iter2 = iter2->next;
+		} else {
+			resultIter->next = iter2;
+			while (iter2->next != nullptr && iter2->next->val == iter2->val) {
+				iter2 = iter2->next;
+			}
+			resultIter = iter2;
+			iter2 = iter2->next;
+			resultIter->next = iter1;
+			while (iter1->next != nullptr && iter1->next->val == iter1->val) {
+				iter1 = iter1->next;
+			}
+			resultIter = iter1;
+			iter1 = iter1->next;
+		}
+	}
+
+	if (iter1) {
+		resultIter->next = iter1;
+	} else {
+		resultIter->next = iter2;
+	}
+	return head;
+}
+
+
+ListNode* Solution::mergeKLists(vector<ListNode*>& lists) {
+	if (lists.empty()) return nullptr;
+	queue<ListNode*> que;
+	for (const auto iter : lists) {
+		que.emplace(iter);
+	}
+
+	while (que.size() > 1) {
+		ListNode* list1 = que.front();
+		que.pop();
+		ListNode* list2 = que.front();
+		que.pop();
+		ListNode* tmp = mergeTwoLists(list1, list2);
+		que.emplace(tmp);
+	}
+	return que.front();
+}
---
+++ 0023_Merge_k_Sorted_Lists/Solution.h
@@ -0,0 +1,22 @@
+#include <vector>
+
+using std::vector;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+    public :
+        Solution() {}
+	ListNode* mergeKLists(vector<ListNode*>& lists);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
