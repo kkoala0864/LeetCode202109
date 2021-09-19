6e49d639a987d1696dec41632bb381bebd9ac886
neilhuang <neilhuang@synology.com>
2021-09-19 18:37:24 +0800

    <LeetCode202109> 39. Combination Sum

---
+++ 0039_Combination_Sum/Solution.cpp
@@ -0,0 +1,26 @@
+#include <Solution.h>
+#include <iostream>
+
+void recur(vector<int>& candidates, int target, vector<int>& tmpResult, vector<vector<int>>& result) {
+	if (0 > target) return;
+	if (0 == target) {
+		if (!tmpResult.empty()) result.emplace_back(tmpResult);
+		return;
+	}
+	if (0 < target && candidates.empty()) return;
+
+	tmpResult.emplace_back(candidates.back());
+	recur(candidates, target - candidates.back(), tmpResult, result);
+	tmpResult.pop_back();
+	int backVal = candidates.back();
+	candidates.pop_back();
+	recur(candidates, target, tmpResult, result);
+	candidates.push_back(backVal);
+}
+
+vector<vector<int>> Solution::combinationSum(vector<int>& candidates, int target) {
+	vector<int> tmpResult;
+	vector<vector<int>> result;
+	recur(candidates, target, tmpResult, result);
+	return result;
+}
---
+++ 0039_Combination_Sum/main.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {2, 3, 5};
+	Solution* test = new Solution();
+	vector<vector<int>> result = test->combinationSum(input, 8);
+	for (const auto iter : result) {
+		for ( const auto iter2 : iter ) {
+			cout << iter2 << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
