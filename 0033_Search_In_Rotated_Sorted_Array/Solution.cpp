9435dc07b3359a8d33176b505bd16c83a74f1cc3
neilhuang <neilhuang@synology.com>
2021-09-19 12:04:25 +0800

    <LeetCode202109> 33. Search in Rotated Sorted Array

---
+++ 0033_Search_In_Rotated_Sorted_Array/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+
+//[ 4, 5, 6, 7, 0 ,1 ,2 ]
+using std::cout;
+using std::endl;
+
+int Solution::search(vector<int>& nums, int target) {
+	int mid = -1, left  = 0 , right = nums.size() - 1;
+	while (left <= right) {
+		cout << left << " : " << right << endl;
+		mid = (left + right) >> 1;
+		if (nums[mid] == target) return mid;
+
+		if (nums[left] <= nums[mid]) {
+			if (target <= nums[mid] && target >= nums[left]) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		} else {
+			if (target >= nums[mid] && target <= nums[right]) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+	}
+	return -1;
+}
---
+++ 0033_Search_In_Rotated_Sorted_Array/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {4,5,6,7,0,1,2};
+	Solution* test = new Solution();
+	cout << test->search(input, 0) << endl;
+	return 0;
+}
+
