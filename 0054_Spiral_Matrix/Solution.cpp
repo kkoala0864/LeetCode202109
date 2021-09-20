23646f97c054b994827b7be5cc06cb8cb28fb246
neilhuang <neilhuang@synology.com>
2021-09-20 14:11:48 +0800

    <LeetCode202109> 54. Spiral Matrix

---
+++ 0054_Spiral_Matrix/Solution.cpp
@@ -0,0 +1,49 @@
+#include <Solution.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+vector<int> Solution::spiralOrder(vector<vector<int>>& matrix) {
+	int m = matrix.size();
+	int n = matrix[0].size();
+	int size = m*n;
+	vector<int> result;
+	int x_lower = 0, x_upper = m, y_lower = 0, y_upper = n;
+	int x = 0, y = 0, count = 0;
+
+	while (true) {
+		for (; y < y_upper ; ++y) {
+			result.emplace_back(matrix[x][y]);
+			++count;
+		}
+		if (count >= size) break;
+		--y_upper;
+		--y;
+		++x;
+		for (; x < x_upper ; ++x) {
+			result.emplace_back(matrix[x][y]);
+			++count;
+		}
+		if (count >= size) break;
+		--x_upper;
+		--x;
+		--y;
+		for (; y >= y_lower ; --y) {
+			result.emplace_back(matrix[x][y]);
+			++count;
+		}
+		if (count >= size) break;
+		++y_lower;
+		++x_lower;
+		++y;
+		--x;
+		for (; x >= x_lower ; --x) {
+			result.emplace_back(matrix[x][y]);
+			++count;
+		}
+		if (count >= size) break;
+		++x;
+		++y;
+	}
+	return result;
+}
---
+++ 0054_Spiral_Matrix/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+	Solution* test = new Solution();
+	vector<int> result = test->spiralOrder(input);
+	for (const auto iter : result) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
