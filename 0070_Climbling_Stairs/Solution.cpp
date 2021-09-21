e801a6f039019d9c79b4294c68fa901d4d9796d1
neilhuang <neilhuang@synology.com>
2021-09-21 16:34:45 +0800

    <LeetCode202109> 70. Climbing Stairs

---
+++ 0070_Climbling_Stairs/Solution.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+using std::vector;
+int Solution::climbStairs(int n) {
+	vector<int> number(n + 1, 0);
+	number[0] = 1;
+	number[1] = 1;
+	for (int i = 2 ; i <= n ; ++i) {
+		number[i] = number[i-1] + number[i-2];
+	}
+	return number[n];
+}
---
+++ 0070_Climbling_Stairs/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+    Solution* test = new Solution();
+    cout << test->climbStairs(45) << endl;
+    return 0;
+}
+
