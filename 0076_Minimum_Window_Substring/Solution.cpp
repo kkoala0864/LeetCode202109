cf4e45373a175663d3b3564152666060f50e2576
neilhuang <neilhuang@synology.com>
2021-09-23 16:49:06 +0800

    <LeetCode202109> 76. Minimum Window Substring

---
+++ 0076_Minimum_Window_Substring/Solution.cpp
@@ -0,0 +1,40 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+#include <climits>
+
+using std::unordered_map;
+string Solution::minWindow(string s, string t) {
+	string result;
+	int count = t.size(), minSize = INT_MAX;
+	int size = s.size();
+	unordered_map<char, int> record;
+	for (const auto& iter : t) ++record[iter];
+
+	int left = 0;
+	while(left < size && record.find(s[left]) == record.end()) ++left;
+
+	int right = left - 1;
+	while (left < size && right < size) {
+		if (count > 0) {
+			++right;
+			if (right < size && record.find(s[right]) != record.end()) {
+				--record[s[right]];
+				if (record[s[right]] >= 0) --count;
+			}
+		} else {
+			if ((0 == count) && record.find(s[left]) != record.end()){
+				if ((right - left + 1) < minSize) {
+					minSize = right - left + 1;
+					result = s.substr(left, minSize);
+				}
+			}
+			if (left < size && record.find(s[left]) != record.end()) {
+				++record[s[left]];
+				if (record[s[left]] > 0) ++count;
+			}
+			++left;
+		}
+	}
+	return result;
+}
---
+++ 0076_Minimum_Window_Substring/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string s("ADOBECODEBANC");
+	string t("ABC");
+	Solution* test = new Solution();
+	cout << test->minWindow(s, t) << endl;
+	return 0;
+}
+
