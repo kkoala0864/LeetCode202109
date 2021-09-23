c51eeca95ec40b46b58fdb42913d6536b3618c0b
neilhuang <neilhuang@synology.com>
2021-09-23 21:38:36 +0800

    <LeetCode202109> 100. Same Tree

---
+++ 0100_Same_Tree/Solution.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+	if (p == nullptr && q == nullptr) return true;
+	if ((p == nullptr && q != nullptr ) ||
+            (p != nullptr && q == nullptr) ||
+	    (p->val != q->val)) return false;
+
+	return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
