d38b239c24150a00bcf9ee9ccfe4fa806766a5cb
neilhuang <neilhuang@synology.com>
2021-09-24 11:45:18 +0800

    <LeetCode202109> 104. Maximum Depth of Binary Tree

---
+++ 0104_Maximum_Depth_Of_Binary_Tree/Solution.cpp
@@ -0,0 +1,10 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::maxDepth(TreeNode* root) {
+	if (!root) return 0;
+	else return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+}
