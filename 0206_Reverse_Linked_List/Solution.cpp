7101246cc42a19922fe5d585178b826846e2b14d
neilhuang <neilhuang@synology.com>
2021-09-27 20:39:02 +0800

    <LeetCode202109> 206. Reverse Linked List

---
+++ 0206_Reverse_Linked_List/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::reverseList(ListNode* head) {
+	if (!head) return head;
+
+	ListNode* iter = head;
+	ListNode* last = nullptr;
+	ListNode* next = nullptr;
+
+	while (iter) {
+		next = iter->next;
+		iter->next = last;
+		last = iter;
+		iter = next;
+	}
+
+	return last;
+}
---
+++ 0206_Reverse_Linked_List/main.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	ListNode* five = new ListNode(5, nullptr);
+	ListNode* four = new ListNode(4, five);
+	ListNode* three = new ListNode(3, four);
+	ListNode* two = new ListNode(2, three);
+	ListNode* one = new ListNode(1, two);
+    Solution* test = new Solution();
+    ListNode* ret = test->reverseList(one);
+    ListNode* iter = ret;
+    while (iter) {
+	cout << iter->val << " ";
+	iter = iter->next;
+    }
+	cout << endl;
+    return 0;
+}
+
