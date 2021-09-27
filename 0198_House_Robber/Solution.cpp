4751c7d80bc0d926236215ec145dda970a9d128d
neilhuang <neilhuang@synology.com>
2021-09-27 19:56:03 +0800

    <LeetCode202109> 198. House Robber

---
+++ 0198_House_Robber/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+using std::swap;
+
+int Solution::rob(vector<int>& nums) {
+	int prev = 0, cur = nums[0];
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		prev = max(prev+nums[i], cur);
+		swap(prev, cur);
+	}
+	return cur;
+}
