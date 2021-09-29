49341a18cb35467959ce2e24bbf92bc3e8bd74b6
neilhuang <neilhuang@synology.com>
2021-09-29 20:18:18 +0800

    <LeetCode202109> 217. Contains Duplicate

---
+++ 0217_Contains_Duplicate/Solution.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+
+bool Solution::containsDuplicate(vector<int>& nums) {
+	unordered_map<int, bool> uMap;
+	for (const auto& iter : nums) {
+		if (uMap.find(iter) != uMap.end()) return true;
+		uMap[iter] = true;
+	}
+	return false;
+}
