47d5a4d66e58b0d040999c93f196c768d2606297
neilhuang <neilhuang@synology.com>
2021-09-29 20:44:02 +0800

    <LeetCode202109> 230. Kth Smallest Element in a BST

---
+++ 0230_Kth_Smallest_Element_In_A_BST/Solution.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+void traverse(TreeNode* node, int& k, int& result) {
+	if (!node || 0 == k) return;
+	traverse(node->left, k, result);
+	if (0 == k) return;
+	if (0 == --k) result = node->val;
+	traverse(node->right, k, result);
+}
+
+int Solution::kthSmallest(TreeNode* root, int k) {
+	int result = 0;
+	traverse(root, k, result);
+	return result;
+}
