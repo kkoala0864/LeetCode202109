de72fc5103ce5ec16153f1774fb5c52ab7021976
neilhuang <neilhuang@synology.com>
2021-09-29 21:02:33 +0800

    <LeetCode202109> 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ 0235_Lowest_Common_Ancestor_Of_A_Binary_Search_Tree/Solution.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::swap;
+
+TreeNode* Solution::lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+	if (p->val > q->val) swap(p,q);
+	if (root->val == p->val || root->val == q->val) return root;
+	if (root->val > p->val && root->val < q->val) return root;
+
+	if (root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+	else return lowestCommonAncestor(root->right, p, q);
+}
---
+++ 0235_Lowest_Common_Ancestor_Of_A_Binary_Search_Tree/Solution.h
@@ -0,0 +1,17 @@
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+    public :
+        Solution() {}
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
