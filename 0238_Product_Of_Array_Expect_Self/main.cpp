974f7a2f6d5eee3d93dae03778017a951095bd8d
neilhuang <neilhuang@synology.com>
2021-09-30 20:48:34 +0800

    <LeetCode202109> 238. Product of Array Except Self

---
+++ 0238_Product_Of_Array_Expect_Self/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,3,4};
+	Solution* test = new Solution();
+	vector<int> result = test->productExceptSelf(input);
+	for (const auto& iter : result) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
