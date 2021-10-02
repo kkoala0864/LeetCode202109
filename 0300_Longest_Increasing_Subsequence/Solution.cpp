cf081448fc26b30e5f20072ef5a12da816fd5cd7
neilhuang <neilhuang@synology.com>
2021-10-02 14:31:11 +0800

    <LeetCode202109> 300. Longest Increasing Subsequence

---
+++ 0300_Longest_Increasing_Subsequence/Solution.cpp
@@ -0,0 +1,41 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::vector;
+
+/*
+int Solution::lengthOfLIS(vector<int>& nums) {
+	vector<int> dp(nums.size(), 1);
+	int maxLen = INT_MIN;
+
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		for (int j = i ; j >= 0 ; --j) {
+			if (nums[i] > nums[j]) dp[i] = max(dp[i],  1 + dp[j]);
+		}
+	}
+
+	for (const auto& iter : dp) maxLen = max(maxLen, iter);
+	return maxLen;
+}*/
+
+int low_bound(const vector<int>& dp, int start, int end, int val) {
+	while (start < end) {
+		int mid = (end + start) / 2;
+		if (dp[mid] > val) end = mid;
+		else if (dp[mid] < val) start = mid + 1;
+		else return mid;
+	}
+	return start;
+}
+
+int Solution::lengthOfLIS(vector<int>& nums) {
+	if (nums.empty()) return 0;
+	vector<int> dp;
+	dp.emplace_back(nums[0]);
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		int idx = low_bound(dp, 0, dp.size(), nums[i]);
+		if (idx == dp.size()) dp.emplace_back(nums[i]);
+		else dp[idx] = nums[i];
+	}
+	return dp.size();
+}
---
+++ 0300_Longest_Increasing_Subsequence/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {10,9,2,5,3,7,101,18};
+	Solution* test = new Solution();
+	cout << test->lengthOfLIS(input) << endl;
+	return 0;
+}
+
