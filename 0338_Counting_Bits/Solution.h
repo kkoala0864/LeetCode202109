f20236cd68d0c2c489fab4d932f6a65812da1b2b
neilhuang <neilhuang@synology.com>
2021-10-04 00:28:23 +0800

    <LeetCode202109> 338. Counting Bits

---
+++ 0338_Counting_Bits/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> countBits(int n);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
