1ab2b9b5c136eec2c1e3a5beeaeb8be92a3f82c5
neilhuang <neilhuang@synology.com>
2021-10-04 23:12:35 +0800

    <LeetCode202109> 572. Subtree of Another Tree

---
+++ 0572_Subtree_Of_Another_Tree/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Cmp(TreeNode* node, TreeNode* subnode) {
+	if (!node) {
+		if (!subnode) return true;
+		else return false;
+	} else {
+		if (subnode && node->val == subnode->val) {
+			return Cmp(node->left, subnode->left) && Cmp(node->right, subnode->right);
+		} else return false;
+	}
+}
+
+bool Solution::isSubtree(TreeNode* root, TreeNode* subRoot) {
+	if (!root) return false;
+	if (Cmp(root, subRoot)) return true;
+	return (isSubtree(root->left, subRoot)) || (isSubtree(root->right, subRoot));
+}
