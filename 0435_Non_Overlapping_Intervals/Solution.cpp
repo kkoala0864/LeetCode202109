cb0596c7c1853f05004fb78e934784cb89b80634
neilhuang <neilhuang@synology.com>
2021-10-04 22:33:52 +0800

    <LeetCode202109> 435. Non-overlapping Intervals

---
+++ 0435_Non_Overlapping_Intervals/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+int Solution::eraseOverlapIntervals(vector<vector<int>>& intervals) {
+	auto cmp = [](const vector<int>& lhs, const vector<int>& rhs) { return lhs[0] < rhs[0]; };
+
+	sort(intervals.begin(), intervals.end(), cmp);
+
+	int result(0);
+	for (int i = 1, prev = 0 ; i < intervals.size() ; ++i) {
+		if (intervals[i][0] < intervals[prev][1]) {
+			++result;
+			if (intervals[i][1] < intervals[prev][1]) prev = i;
+		} else {
+			prev = i;
+		}
+	}
+	return result;
+
+}
