da3f6ced02fb15dd846b841e8eda82f3ac89ace8
neilhuang <neilhuang@synology.com>
2021-10-05 19:07:42 +0800

    <LeetCode202109> 323. Number of Connected Components in an Undirected Graph

---
+++ 0323_Number_Of_Connected_Components_In_An_Undirected_Graph/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,1},{1,2},{3,4}};
+	Solution* test = new Solution();
+	cout << test->countComponents(5, input) << endl;
+	return 0;
+}
+
