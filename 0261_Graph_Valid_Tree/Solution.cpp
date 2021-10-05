53feec089215ea3bf181c3971df7d157d385f106
neilhuang <neilhuang@synology.com>
2021-10-05 15:34:44 +0800

    <LeetCode202109> 261. Graph Valid Tree

---
+++ 0261_Graph_Valid_Tree/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+bool Solution::validTree(int n, vector<vector<int>>& edges) {
+	if (edges.size() < n-1) return false;
+	vector<int> edge(n, -1);
+
+	for (const auto& iter : edges) {
+		int v1 = iter[0];
+		int v2 = iter[1];
+		while (edge[v1] != -1) v1 = edge[v1];
+		while (edge[v2] != -1) v2 = edge[v2];
+		if (v1 == v2) return false;
+		else edge[v2] = v1;
+	}
+	return true;
+}
+
---
+++ 0261_Graph_Valid_Tree/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,1},{0,2},{0,3},{1,4}};
+	Solution* test = new Solution();
+	cout << test->validTree(5, input) << endl;
+	return 0;
+}
+
