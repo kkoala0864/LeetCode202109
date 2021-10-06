3d02764f500b4cba9c6471d724c237b2362575e0
neilhuang <neilhuang@synology.com>
2021-10-06 19:27:56 +0800

    <LeetCode202109> 283. Move Zeroes

---
+++ 0283_Move_Zeros/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {0,1,0,3,12};
+	Solution* test = new Solution();
+	test->moveZeroes(input);
+	for (const auto& iter : input) {
+		cout << iter << " ";
+	}
+	cout << endl;
+    return 0;
+}
+
