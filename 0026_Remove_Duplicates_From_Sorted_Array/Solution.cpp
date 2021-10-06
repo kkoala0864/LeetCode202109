e192df68bff154bd4cecb7dec3ebb850648608c5
neilhuang <neilhuang@synology.com>
2021-10-06 17:27:01 +0800

    <LeetCode202109> 26. Remove Duplicates from Sorted Array

---
+++ 0026_Remove_Duplicates_From_Sorted_Array/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+//     n  t
+// [ 1, 2, 2, 3, 4, 5, 5, 6]
+//
+
+int Solution::removeDuplicates(vector<int>& nums) {
+	if (nums.size() < 2) return nums.size();
+	int index = 0;
+	int iter = 1;
+	while (iter < nums.size()) {
+		if (nums[index] != nums[iter]) {
+			++index;
+			nums[index] = nums[iter];
+		}
+		++iter;
+	}
+	return index + 1;
+}
---
+++ 0026_Remove_Duplicates_From_Sorted_Array/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1, 2, 2, 3, 4, 5, 5, 6};
+	Solution* test = new Solution();
+	cout << test->removeDuplicates(input) << endl;
+	return 0;
+}
+
