252fb5ddd39c461015699d5bc3663cac2e1ee5ae
neilhuang <neilhuang@synology.com>
2021-10-06 18:05:16 +0800

    <LeetCode202109> 442. Find All Duplicates in an Array

---
+++ 0442_Find_All_Duplicates_In_An_Array/main.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {4,3,2,7,8,2,3,1};
+	Solution* test = new Solution();
+	vector<int> result = test->findDuplicates(input);
+	for (const auto& iter : result) {
+		cout << iter << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+
