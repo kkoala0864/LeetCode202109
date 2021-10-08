09446e4af31364e39e54a29bd0d261ef673a8bf3
neilhuang <neilhuang@synology.com>
2021-10-08 14:04:36 +0800

    <LeetCode202109> 47. Permutations II

---
+++ 0047_PermutationsII/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+using std::swap;
+void permutation(vector<int>& nums, vector<vector<int>>& result, int idx) {
+	if (idx == nums.size() -1) {
+		result.emplace_back(nums);
+		return;
+	}
+
+	vector<bool> flag(21, false);
+	for (int i = idx ; i < nums.size() ; ++i) {
+		if (!flag[nums[i]+10]) {
+			flag[nums[i]+10] = true;
+			swap(nums[i], nums[idx]);
+			permutation(nums, result, idx+1);
+			swap(nums[i], nums[idx]);
+		}
+	}
+}
+
+vector<vector<int>> Solution::permuteUnique(vector<int>& nums) {
+	vector<vector<int>> result;
+	permutation(nums, result, 0);
+	return result;
+}
---
+++ 0047_PermutationsII/main.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {2, 2, 1, 1};
+	Solution* test = new Solution();
+	vector<vector<int>> result = test->permuteUnique(input);
+	for (const auto& iter : result) {
+		for (const auto& iter2 : iter) {
+			cout << iter2 << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
