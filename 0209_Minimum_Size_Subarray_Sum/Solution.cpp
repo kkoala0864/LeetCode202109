1203e47020f896f535efa288abe281003a67be03
neilhuang <neilhuang@synology.com>
2021-10-09 17:38:34 +0800

    <LeetCode202109> 209. Minimum Size Subarray Sum

---
+++ 0209_Minimum_Size_Subarray_Sum/Solution.cpp
@@ -0,0 +1,37 @@
+#include <Solution.h>
+#include <iostream>
+#include <climits>
+
+using std::min;
+using std::cout;
+using std::endl;
+
+int Solution::minSubArrayLen(int target, vector<int>& nums) {
+	int left = -1, right = 0;
+	int size = nums.size();
+	int result = INT_MAX;
+
+	int product = 0;
+	while (left < size && right < size) {
+		while (right < size) {
+			product += nums[right];
+			if (product >= target) {
+				result = min(result, right - left);
+				++right;
+				break;
+			}
+			++right;
+		}
+
+		while (left < right) {
+			++left;
+			product -= nums[left];
+			if (product >= target) {
+				result = min(result, right - left - 1);
+			} else {
+				break;
+			}
+		}
+	}
+	return result == INT_MAX ? 0 : result;
+}
