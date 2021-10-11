2cb7515bebda43a3e312f5de5a6bc648a533d182
neilhuang <neilhuang@synology.com>
2021-10-11 20:38:19 +0800

    <LeetCode202109> 240. Search a 2D Matrix II

---
+++ 0240_Search_A_2D_MatrixII/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Solution::searchMatrix(vector<vector<int>>& matrix, int target) {
+	int m = matrix.size();
+	int n = matrix[0].size();
+
+	int x = 0, y = n - 1;
+	while (x < m && y >= 0) {
+		if (matrix[x][y] == target) return true;
+		else if (matrix[x][y] > target) --y;
+		else ++x;
+	}
+	return false;
+}
