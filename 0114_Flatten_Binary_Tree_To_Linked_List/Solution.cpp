3f6cb6e11bf9ca8c4d4ac1437706ce947bc6e257
neilhuang <neilhuang@synology.com>
2021-10-11 12:17:54 +0800

    <LeetCode202109> 114. Flatten Binary Tree to Linked List

---
+++ 0114_Flatten_Binary_Tree_To_Linked_List/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+TreeNode* flattenTree(TreeNode* node) {
+	if (!node) return nullptr;
+	if (!node->left && !node->right) return node;
+
+	TreeNode* leftEnd = flattenTree(node->left);
+	TreeNode* rightEnd = flattenTree(node->right);
+	if (leftEnd) {
+		leftEnd->right = node->right;
+		node->right = node->left;
+		node->left = nullptr;
+	}
+	return rightEnd == nullptr ? leftEnd : rightEnd;
+}
+
+void Solution::flatten(TreeNode* root) {
+	flattenTree(root);
+}
