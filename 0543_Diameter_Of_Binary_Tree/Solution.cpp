89eedc3355ced4f0a7343186438750eb5a7327af
neilhuang <neilhuang@synology.com>
2021-10-11 11:38:46 +0800

    <LeetCode202109> 543. Diameter of Binary Tree

---
+++ 0543_Diameter_Of_Binary_Tree/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using std::max;
+
+int diameter(TreeNode* root, int& MaxVal) {
+	if (!root) return 0;
+	int leftHeight = diameter(root->left, MaxVal);
+	int rightHeight = diameter(root->right, MaxVal);
+	MaxVal = max(MaxVal, leftHeight + rightHeight);
+	return max(leftHeight, rightHeight) + 1;
+}
+
+int Solution::diameterOfBinaryTree(TreeNode* root) {
+	int MaxVal = INT_MIN;
+	diameter(root, MaxVal);
+	return MaxVal;
+}
