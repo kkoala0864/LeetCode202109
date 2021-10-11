2c63bc5ae83b9b0e41c657ecac6442f297eae54b
neilhuang <neilhuang@synology.com>
2021-10-11 15:24:10 +0800

    <LeetCode202109> 74. Search a 2D Matrix

---
+++ 0074_Search_A_2D_Matrix/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Solution::searchMatrix(vector<vector<int>>& matrix, int target) {
+	int m = matrix.size();
+	int n = matrix[0].size();
+	int start = 0;
+	int end = m*n - 1;
+	int mid = 0;
+
+	while (start <= end) {
+		int mid = (end + start) / 2;
+		int midx = mid / n;
+		int midy = mid % n;
+		int midVal = matrix[midx][midy];
+		if (midVal == target) return true;
+		else if (midVal > target) {
+			end = mid - 1;
+		} else {
+			start = mid + 1;
+		}
+	}
+	return false;
+}
+
---
+++ 0074_Search_A_2D_Matrix/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+	Solution* test = new Solution();
+	cout << test->searchMatrix(input, 13) << endl;
+	return 0;
+}
+
