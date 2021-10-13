b46fa352cda6156763c360c6f8f1e62b1c9096ff
neilhuang <neilhuang@synology.com>
2021-10-13 21:20:56 +0800

    <LeetCode202109> 205. Isomorphic Strings

---
+++ 0205_Isomorphic_Strings/Solution.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+using std::unordered_map;
+
+bool Solution::isIsomorphic(string s, string t) {
+	unordered_map<char, int> sMap;
+	unordered_map<char, int> tMap;
+
+	for (int i = 0 ; i < s.size() ; ++i) {
+		if (sMap.find(s[i]) == sMap.end()) sMap[s[i]] = i;
+		if (tMap.find(t[i]) == tMap.end()) tMap[t[i]] = i;
+		if (sMap[s[i]] != tMap[t[i]]) return false;
+	}
+	return true;
+}
---
+++ 0205_Isomorphic_Strings/Solution.h
@@ -0,0 +1,14 @@
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	bool isIsomorphic(string s, string t);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
