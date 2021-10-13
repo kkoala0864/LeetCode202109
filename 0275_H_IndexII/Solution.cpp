54f348f299ebb1d6d233576a4d2c4a193601f1e0
neilhuang <neilhuang@synology.com>
2021-10-13 19:46:09 +0800

    <LeetCode202109> 275. H-Index II

---
+++ 0275_H_IndexII/Solution.cpp
@@ -0,0 +1,11 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::hIndex(vector<int>& citations) {
+	int size = citations.size();
+
+	for (int i = 0 ; i < size ; ++i) {
+		if (citations[i] >= (size-i)) return size - i;
+	}
+	return 0;
+}
