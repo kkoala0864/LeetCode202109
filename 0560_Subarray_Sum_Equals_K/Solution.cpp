16f979ee52abc92fdd6f70b8cfca080c53463f10
neilhuang <neilhuang@synology.com>
2021-10-14 15:56:05 +0800

    <LeetCode202109> 560. Subarray Sum Equals K

---
+++ 0560_Subarray_Sum_Equals_K/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+
+int Solution::subarraySum(vector<int>& nums, int k) {
+	unordered_map<int, int> uMap;
+	int sum = 0;
+	int result = 0;
+
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		sum += nums[i];
+		if (sum == k) ++result;
+		if (uMap.find(sum - k) != uMap.end()) result += uMap[sum-k];
+		++uMap[sum];
+	}
+	return result;
+}
