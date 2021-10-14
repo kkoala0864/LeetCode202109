0f0c7dc3762a123db1e94afacc16c830e1b7549b
neilhuang <neilhuang@synology.com>
2021-10-14 17:47:22 +0800

    <LeetCode202109> 160. Intersection of Two Linked Lists

---
+++ 0160_Intersection_Of_Two_Linked_Lists/Solution.cpp
@@ -0,0 +1,39 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::getIntersectionNode(ListNode *headA, ListNode *headB) {
+	int aSize = 0, bSize = 0;
+
+	ListNode* iter = headA;
+	while (iter) {
+		++aSize;
+		iter = iter->next;
+	}
+	iter = headB;
+	while (iter) {
+		++bSize;
+		iter = iter->next;
+	}
+
+	ListNode* moreIter = nullptr;
+	ListNode* lessIter = nullptr;
+	int diff = 0;
+	if (aSize > bSize) {
+		moreIter = headA;
+		lessIter = headB;
+		diff = aSize - bSize;
+	} else {
+		moreIter = headB;
+		lessIter = headA;
+		diff = bSize - aSize;
+	}
+
+	while(diff--) moreIter = moreIter->next;
+
+	while (moreIter) {
+		if (moreIter == lessIter) return moreIter;
+		moreIter = moreIter->next;
+		lessIter = lessIter->next;
+	}
+	return nullptr;
+}
