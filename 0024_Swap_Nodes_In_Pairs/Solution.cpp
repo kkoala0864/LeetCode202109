52e557b9f19d33a4a00fc2eb2812c984cdb19def
neilhuang <neilhuang@synology.com>
2021-10-14 19:22:19 +0800

    <LeetCode202109> 24. Swap Nodes in Pairs

---
+++ 0024_Swap_Nodes_In_Pairs/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::swapPairs(ListNode* head) {
+	if (!head) return head;
+	ListNode* iter = head;
+	ListNode* prev = new ListNode(0, head);
+	ListNode* prevHead = prev;
+	ListNode* next = nullptr;
+	while (iter) {
+		ListNode* first = iter;
+		iter = iter->next;
+		ListNode* second = iter;
+		if (second) {
+			iter = iter->next;
+			second->next = first;
+			first->next = iter;
+			prev->next = second;
+			prev = prev->next->next;
+		}
+	}
+	return prevHead->next;
+}
