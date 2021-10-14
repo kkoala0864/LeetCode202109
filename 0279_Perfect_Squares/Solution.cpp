8f5f280817d2714d32bc07ffa53e3d7870ce6f76
neilhuang <neilhuang@synology.com>
2021-10-14 17:08:52 +0800

    <LeetCode202109> 279. Perfect Squares

---
+++ 0279_Perfect_Squares/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <climits>
+
+using std::vector;
+using std::min;
+
+int Solution::numSquares(int n) {
+	int sqr = sqrt(n);
+	vector<int> dp(n+1, 0);
+
+	for (int i = 1 ; i <= n ; ++i) {
+		int local = INT_MAX;
+		for (int j = 1 ; j * j <= i ; ++j) {
+			if (j*j == i) {
+				local = 1;
+				break;
+			}
+			local = min(local, 1 + dp[i - j*j]);
+		}
+		dp[i] = local;
+	}
+	return dp[n];
+}
