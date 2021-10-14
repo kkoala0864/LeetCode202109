dc23b8a3da0bdc91c7bc20c92fcc11847ffd8753
neilhuang <neilhuang@synology.com>
2021-10-14 21:18:29 +0800

    <LeetCode202109> 106. Construct Binary Tree from Inorder and Postorder Traversal

--- 0106_Construct_Binary_Tree_From_Inorder_And_Postorder_Traversal/Solution.cpp
+++ 0106_Construct_Binary_Tree_From_Inorder_And_Postorder_Traversal/Solution.cpp
@@ -1,19 +1,21 @@
 #include <Solution.h>
 #include <iostream>
+#include <unordered_map>
 
-TreeNode* construct(const vector<int>& inorder, int iStart, int iEnd, const vector<int>& postorder, int pStart, int pEnd) {
+using std::unordered_map;
+
+TreeNode* construct(unordered_map<int, int>& uMap, const vector<int>& inorder, int iStart, int iEnd, const vector<int>& postorder, int pStart, int pEnd) {
 	if (iStart > iEnd) return nullptr;
 	TreeNode* result = new TreeNode(postorder[pEnd]);
 
-	for (int i = iStart ; i <= iEnd ; ++i) {
-		if (inorder[i] == postorder[pEnd]) {
-			result->left = construct(inorder, iStart, i-1, postorder, pStart, pStart + i - iStart - 1);
-			result->right = construct(inorder, i+1, iEnd, postorder, pEnd - iEnd + i, pEnd - 1);
-		}
-	}
+	int i = uMap[postorder[pEnd]];
+	result->left = construct(uMap, inorder, iStart, i-1, postorder, pStart, pStart + i - iStart - 1);
+	result->right = construct(uMap, inorder, i+1, iEnd, postorder, pEnd - iEnd + i, pEnd - 1);
 	return result;
 }
 
 TreeNode* Solution::buildTree(vector<int>& inorder, vector<int>& postorder) {
-	return construct(inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1);
+	unordered_map<int, int> uMap;
+	for (int i = 0 ; i < inorder.size() ; ++i) uMap[inorder[i]] = i;
+	return construct(uMap, inorder, 0, inorder.size() - 1, postorder, 0, postorder.size() - 1);
 }
