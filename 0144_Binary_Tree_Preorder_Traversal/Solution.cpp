7dc7085129984fafb4cbd648ac6191edb774b4f7
neilhuang <neilhuang@synology.com>
2021-10-14 20:14:11 +0800

    <LeetCode202109> 144. Binary Tree Preorder Traversal

--- 0144_Binary_Tree_Preorder_Traversal/Solution.cpp
+++ 0144_Binary_Tree_Preorder_Traversal/Solution.cpp
@@ -1,7 +1,9 @@
 #include <Solution.h>
 #include <iostream>
+#include <stack>
 
-
+using std::stack;
+/*
 void traverse(TreeNode* root, vector<int>& result) {
 	if (!root) return;
 	result.emplace_back(root->val);
@@ -12,5 +14,25 @@ void traverse(TreeNode* root, vector<int>& result) {
 vector<int> Solution::preorderTraversal(TreeNode* root) {
 	vector<int> result;
 	traverse(root, result);
+	return result;
+}
+*/
+vector<int> Solution::preorderTraversal(TreeNode* root) {
+	if (!root) return vector<int>()
+	stack<TreeNode*> st;
+	vector<int> result;
+
+	st.emplace(root);
+	while (!st.empty()) {
+		TreeNode* cur = st.top();
+		st.pop();
+		result.emplace_back(cur->val);
+		if (cur->right) {
+			st.emplace(cur->right);
+		}
+		if (cur->left) {
+			st.emplace(cur->left);
+		}
+	}
 	return result;
 }
