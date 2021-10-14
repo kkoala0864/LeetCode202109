d3ad17b60307ddde6e0e799412f9deb4975c4a3b
neilhuang <neilhuang@synology.com>
2021-10-14 19:08:20 +0800

    <LeetCode202109> 203. Remove Linked List Elements

---
+++ 0203_Remove_Linked_List_Elements/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* removeElements(ListNode* head, int val) {
+	ListNode* newhead = nullptr;
+	ListNode* newIter = nullptr;
+	ListNode* iter = head;
+	while (iter) {
+		if (iter->val != val) {
+			if (!newhead) {
+				newhead = iter;
+				newIter = iter;
+			} else {
+				newIter->next = iter;
+				newIter = newIter->next;
+			}
+		}
+		iter = iter->next;
+	}
+	if (newIter) newIter->next = nullptr;
+	return newhead;
+}
