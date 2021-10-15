17be2147354088e04abab7e648db5b94163f04b7
neilhuang <neilhuang@synology.com>
2021-10-16 00:00:31 +0800

    <LeetCode202109> 109. Convert Sorted List to Binary Search Tree

---
+++ 0109_Convert_Sorted_List_To_Binary_Search_Tree/Solution.cpp
@@ -0,0 +1,24 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+#include <stack>
+
+TreeNode* construct(const vector<int>& nums, int start, int end) {
+	if (start > end) return nullptr;
+	int mid = (start + end) / 2;
+	TreeNode* node = new TreeNode(nums[mid]);
+	node->left = construct(nums, start, mid - 1);
+	node->right = construct(nums, mid+1, end);
+	return node;
+}
+
+TreeNode* Solution::sortedListToBST(ListNode* head) {
+	vector<int> nums;
+	ListNode* iter = head;
+	while (iter) {
+		nums.emplace_back(iter->val);
+		iter = iter->next;
+	}
+
+	return construct(nums, 0, nums.size() - 1);
+}
