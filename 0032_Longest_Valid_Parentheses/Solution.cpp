708c69affdb919e240fb3de29133e242698a9ad5
neilhuang <neilhuang@synology.com>
2021-10-16 20:41:45 +0800

    <LeetCode202109> 32. Longest Valid Parentheses

---
+++ 0032_Longest_Valid_Parentheses/Solution.cpp
@@ -0,0 +1,36 @@
+#include <Solution.h>
+#include <iostream>
+#include <stack>
+#include <algorithm>
+#include <climits>
+
+using std::stack;
+using std::max;
+
+int Solution::longestValidParentheses(string s) {
+	int result = 0;
+	int cnt = 0;
+	int size = s.size();
+	stack<int> st;
+
+	for (int i = 0 ; i < size ; ++i) {
+		if (s[i] == '(') {
+			st.emplace(i);
+		} else {
+			if (!st.empty()) {
+				s[st.top()] = s[i] = '*';
+				st.pop();
+			}
+		}
+	}
+
+	for (int i = 0 ; i < size ; ++i) {
+		if (s[i] == '*') ++cnt;
+		else {
+			result = max(result, cnt);
+			cnt = 0;
+		}
+	}
+	result = max(result, cnt);
+	return result;
+}
---
+++ 0032_Longest_Valid_Parentheses/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input("()(()()");
+	Solution* test = new Solution();
+	cout << test->longestValidParentheses(input) << endl;
+	return 0;
+}
+
