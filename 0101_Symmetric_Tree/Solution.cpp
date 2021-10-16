0ff0857f796c610ffe8d6b8bbc0662ac0d78de54
neilhuang <neilhuang@synology.com>
2021-10-16 17:57:11 +0800

    <LeetCode202109> 101. Symmetric Tree

---
+++ 0101_Symmetric_Tree/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+bool check(TreeNode* left, TreeNode* right) {
+	if (!left && !right) return true;
+	if ((!left && right) || (left && !right)) return false;
+	if (left->val != right->val) return false;
+
+	return check(left->left, right->right) && check(left->right, right->left);
+}
+
+bool Solution::isSymmetric(TreeNode* root) {
+	if (!root) return true;
+	return check(root->left, root->right);
+}
---
+++ 0101_Symmetric_Tree/Solution.h
@@ -0,0 +1,19 @@
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public :
+        Solution() {}
+	bool isSymmetric(TreeNode* root);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
