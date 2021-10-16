6ff176198eaba7e7e70a1496e2e009c83c19a6cf
neilhuang <neilhuang@synology.com>
2021-10-16 11:55:38 +0800

    <LeetCode202109> 111. Minimum Depth of Binary Tree

---
+++ 0111_Minimum_Depth_Of_Binary_Tree/Solution.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+
+int minDepth(TreeNode* root) {
+	if (!root) return 0;
+	if (!root->left && !root->right) return 1;
+	int left = root->left ? minDepth(root->left) : INT_MAX;
+	int right = root->right ? minDepth(root->right) : INT_MAX;
+	return min(left, right) + 1;
+
+}
