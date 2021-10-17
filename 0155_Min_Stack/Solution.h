f9f72a91eaac3c05ea392438be88e4c89c5b5899
neilhuang <neilhuang@synology.com>
2021-10-17 22:53:51 +0800

    <LeetCode202109> 155. Min Stack

---
+++ 0155_Min_Stack/Solution.h
@@ -0,0 +1,37 @@
+#include <map>
+#include <stack>
+
+using std::map;
+using std::stack;
+
+class MinStack {
+    public :
+	MinStack() {}
+
+	void push(int val) {
+		++_map[val];
+		_st.emplace(val);
+	}
+
+	void pop() {
+		int val = _st.top();
+		_st.pop();
+		--_map[val];
+		if (_map[val] == 0) _map.erase(val);
+	}
+
+	int top() {
+		return _st.top();
+	}
+
+	int getMin() {
+		return _map.begin()->first;
+	}
+    private :
+	map<int, int> _map;
+	stack<int> _st;
+        virtual ~MinStack() {}
+        MinStack& operator=(const MinStack& source);
+        MinStack(const MinStack&);
+};
+
---
+++ 0155_Min_Stack/main.cpp
@@ -0,0 +1,11 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    MinStack* test = new MinStack();
+    return 0;
+}
+
