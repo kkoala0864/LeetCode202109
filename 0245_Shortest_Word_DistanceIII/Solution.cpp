76ac74fa8c74146fa01f933749ae0bf67b1893e9
neilhuang <neilhuang@synology.com>
2021-10-19 01:48:47 +0800

    <LeetCode202109> 245. Shortest Word Distance III

---
+++ 0245_Shortest_Word_DistanceIII/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using std::min;
+using std::cout;
+using std::endl;
+
+int Solution::shortestWordDistance(vector<string>& wordsDict, string word1, string word2) {
+	int result = INT_MAX;
+	int i1 = -1, i2 = -1;
+	bool same = word1 == word2;
+	for (int i = 0 ; i < wordsDict.size() ; ++i) {
+		if (wordsDict[i] == word1) {
+			if (same) {
+				if (i1 != -1) result = min(i - i1, result);
+			} else {
+				if (i2 != -1) result = min(i - i2, result);
+			}
+			i1 = i;
+		} else if (wordsDict[i] == word2) {
+			if (!same) {
+				if (i1 != -1) result = min(i - i1, result);
+				i2 = i;
+			}
+		}
+	}
+	return result;
+}
---
+++ 0245_Shortest_Word_DistanceIII/Solution.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int shortestWordDistance(vector<string>& wordsDict, string word1, string word2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
