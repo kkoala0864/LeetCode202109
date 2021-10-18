85e703d0eefa3436f18f7915fdec000e49f90bc8
neilhuang <neilhuang@synology.com>
2021-10-18 20:02:20 +0800

    <LeetCode202109> 326. Power of Three

--- 0326_Power_Of_Three/Solution.cpp
+++ 0326_Power_Of_Three/Solution.cpp
@@ -1,6 +1,6 @@
 #include <Solution.h>
 #include <iostream>
-
+/*
 bool Solution::isPowerOfThree(int n) {
 	if (n <= 0) return false;
 	while ( n > 1 ) {
@@ -8,5 +8,11 @@ bool Solution::isPowerOfThree(int n) {
 		if (n /= 3;
 	}
 	return true;
+}
+*/
+
+bool Solution::isPowerOfThree(int n) {
+	int val = pow(3, 19);
+	return n > 0 ? (val % n) == 0 : false;
 }
 
