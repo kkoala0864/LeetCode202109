bb80196336efa6a290a54509768776ad7d2d2e35
neilhuang <neilhuang@synology.com>
2021-10-21 19:48:03 +0800

    <LeetCode202109> 809. Expressive Words

---
+++ 0370_Range_Addition/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> getModifiedArray(int length, vector<vector<int>>& updates);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 0370_Range_Addition/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> updates = {{1,3,2},{2,4,3},{0,2,-2}};
+	Solution* test = new Solution();
+	vector<int> result = test->getModifiedArray(5, updates);
+	for (const auto& iter : result) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
