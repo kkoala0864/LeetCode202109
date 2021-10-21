99121d829bc655eabca6b2c3f58e808f35d9728f
neilhuang <neilhuang@synology.com>
2021-10-21 21:16:05 +0800

    <LeetCode202109> 809. Expressive Words

---
+++ 0809_Expressive_Words/Solution.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int expressiveWords(string s, vector<string>& words);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 0809_Expressive_Words/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"dinnssoo","ddinso","ddiinnso","ddiinnssoo","ddiinso","dinsoo","ddiinsso","dinssoo","dinso"};
+	Solution* test = new Solution();
+	cout << test->expressiveWords("dddiiiinnssssssoooo", input) << endl;
+	return 0;
+}
+
