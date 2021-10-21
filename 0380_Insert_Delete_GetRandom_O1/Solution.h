f6d2e769a1661548d39baa3bb387c87023137ae7
neilhuang <neilhuang@synology.com>
2021-10-21 16:38:00 +0800

    <LeetCode202109> 380. Insert Delete GetRandom O(1)

---
+++ 0380_Insert_Delete_GetRandom_O1/Solution.h
@@ -0,0 +1,48 @@
+#include <unordered_map>
+#include <cstdlib>
+#include <vector>
+#include <ctime>
+
+using std::vector;
+using std::unordered_map;
+using std::swap;
+
+class RandomizedSet {
+    public :
+	RandomizedSet() {
+		srand(time(NULL));
+	}
+
+	bool insert(int val) {
+		bool result = _map.find(val) == _map.end();
+		if (result) {
+			_vec.emplace_back(val);
+			_map[val] = _vec.size() - 1;
+		}
+		return result;
+	}
+
+	bool remove(int val) {
+		bool result = _map.find(val) != _map.end();
+		if (result) {
+			int tail = _vec.size() - 1;
+			_map[_vec[tail]] = _map[val];
+			swap(_vec[tail], _vec[_map[val]]);
+			_vec.pop_back();
+			_map.erase(val);
+		}
+		return result;
+	}
+
+	int getRandom() {
+		int ran = rand() % _vec.size();
+		return _vec[ran];
+	}
+	private :
+	unordered_map<int, int> _map;
+	vector<int> _vec;
+        virtual ~RandomizedSet() {}
+        RandomizedSet& operator=(const RandomizedSet& source);
+        RandomizedSet(const RandomizedSet&);
+};
+
---
+++ 0380_Insert_Delete_GetRandom_O1/main.cpp
@@ -0,0 +1,11 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    RandomizedSet* test = new RandomizedSet();
+    return 0;
+}
+
