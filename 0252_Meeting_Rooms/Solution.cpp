ea6cb6c9e9144176dfbabd8aa34b758f88b9089e
neilhuang <neilhuang@synology.com>
2021-10-21 19:05:07 +0800

    <LeetCode202109> 252. Meeting Rooms

--- 0252_Meeting_Rooms/Solution.cpp
+++ 0252_Meeting_Rooms/Solution.cpp
@@ -1,8 +1,10 @@
 #include <Solution.h>
 #include <iostream>
 #include <algorithm>
+#include <map>
 
 using std::sort;
+using std::map;
 
 bool Solution::canAttendMeetings(vector<vector<int>>& intervals) {
 	if (intervals.empty()) return true;
@@ -12,3 +14,17 @@ bool Solution::canAttendMeetings(vector<vector<int>>& intervals) {
 	return true;
 }
 
+bool Solution::canAttendMeetings(vector<vector<int>>& intervals) {
+	map<int, int> sMap;
+	for (const auto& interval : intervals) {
+		sMap[interval[0]] += 1;
+		sMap[interval[1]] += -1;
+	}
+
+	int cnt = 0;
+	for (const auto iter : sMap) {
+		cnt += iter.second;
+		if (cnt > 1) return false;
+	}
+	return true;
+}
