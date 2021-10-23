38d23dfc764cbc3197419be74394f43fb29ec0a7
neilhuang <neilhuang@synology.com>
2021-10-23 13:00:43 +0800

    <LeetCode202109> 81. Search in Rotated Sorted Array II

---
+++ 0081_Search_In_Rotated_Sorted_ArrayII/Solution.cpp
@@ -0,0 +1,42 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+bool Solution::search(vector<int>& nums, int target) {
+	int left = 0;
+	int right = nums.size() - 1;
+	int mid = 0;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+
+		if (nums[mid] == target) return true;
+		while (nums[left] == nums[mid] && left < mid) {
+			++left;
+		}
+		while (nums[right] == nums[mid] && right > mid) {
+			--right;
+		}
+		if (nums[left] <= nums[mid]) {
+			if (target >= nums[left] && target <= nums[mid]) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		} else {
+			if (target >= nums[mid] && target <= nums[right]) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+	}
+	return false;
+}
+/*
+bool Solution::search(vector<int>& nums, int target) {
+	unordered_map<int, bool> uMap;
+	for (const auto& num : nums) uMap[num] = true;
+	return uMap.find(target) != uMap.end();
+}
+*/
