09cea5de43fe892269d090c22999cfe4c1ae0b0d
neilhuang <neilhuang@synology.com>
2021-10-23 22:17:15 +0800

    <LeetCode202109> 77. Combinations

---
+++ 0077_Combinations/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+
+void combineRecur(int n, int start, int k, vector<vector<int>>& result, vector<int>& local) {
+	if (k == 1) {
+		for (int i = start ; i <= n ; ++i) {
+			local.emplace_back(i);
+			result.emplace_back(local);
+			local.pop_back();
+		}
+	} else {
+		for (int i = start ; i <= (n - k + 1) ; ++i) {
+			local.emplace_back(i);
+			combineRecur(n, i + 1, k-1, result, local);
+			local.pop_back();
+		}
+	}
+}
+
+vector<vector<int>> Solution::combine(int n, int k) {
+	vector<vector<int>> result;
+	vector<int> local;
+	combineRecur(n, 1, k, result, local);
+	return result;
+}
---
+++ 0077_Combinations/main.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    vector<vector<int>> result = test->combine(4, 2);
+    for (const auto& iter : result) {
+	for (const auto& ele : iter) {
+		cout << ele << " ";
+	}
+	cout << endl;
+    }
+    return 0;
+}
+
