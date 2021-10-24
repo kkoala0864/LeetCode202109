a7e4bead6266d304141966d1558f5f8851291b93
neilhuang <neilhuang@synology.com>
2021-10-24 14:40:12 +0800

    <LeetCode202109> 2048. Next Greater Numerically Balanced Number [contest]

---
+++ 2048_Next_Greater_Numerically_Balanced_Number/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+bool isBalanced(int num) {
+	vector<int> cnt(10, 0);
+	while (num) {
+		int mod = num% 10;
+		++cnt[mod];
+		num /= 10;
+	}
+	for (int i = 0 ; i < 10 ; ++i) {
+		if (cnt[i] && i != cnt[i]) return false;
+	}
+	return true;
+}
+
+int Solution::nextBeautifulNumber(int n) {
+	for (int i = n + 1 ; i < 1300000 ; ++i) {
+		if (isBalanced(i)) return i;
+	}
+	return -1;
+}
---
+++ 2048_Next_Greater_Numerically_Balanced_Number/main.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    cout << test->nextBeautifulNumber(3000) << endl;
+    return 0;
+}
+
