a6be4d28081f5d45aa4709bd08e1baffa39f60be
neilhuang <neilhuang@synology.com>
2021-10-25 11:05:03 +0800

    <LeetCode202109> 280. Wiggle Sort

---
+++ 0280_Wiggle_Sort/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,2,1,5,4,6};
+	Solution* test = new Solution();
+	test->wiggleSort(input);
+	for (const auto& iter : input) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
