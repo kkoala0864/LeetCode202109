943c1ca2276b2e5ac55738ed07de869ed0881688
neilhuang <neilhuang@synology.com>
2021-10-27 00:34:27 +0800

    <LeetCode202109> 116. Populating Next Right Pointers in Each Node

---
+++ 0116_Populating_Next_Right_Pointers_In_Each_Node/Solution.cpp
@@ -0,0 +1,47 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::queue;
+
+
+Node* connect(Node* root) {
+	if ((!root) || (!root->left && !root->right)) return root;
+
+	root->left->next = root->right;
+	Node* leftMostRight = root->left;
+	Node* rightMostLeft = root->right;
+	while (leftMostRight->right) {
+		leftMostRight = leftMostRight->right;
+		rightMostLeft = rightMostLeft->left;
+		leftMostRight->next = rightMostLeft;
+	}
+
+	connect(root->left);
+	connect(root->right);
+	return root;
+}
+
+/*
+Node* connect(Node* root) {
+	if (!root) return nullptr;
+	queue<Node*> cur, next;
+
+	cur.emplace(root);
+
+	while (!cur.empty()) {
+		Node* node = cur.front();
+		cur.pop();
+
+		if (!cur.empty()) node->next = cur.front();
+
+		if (node->left) next.emplace(node->left);
+		if (node->right) next.emplace(node->right);
+
+		if (cur.empty()) {
+			cur = move(next);
+		}
+	}
+	return root;
+}
+*/
