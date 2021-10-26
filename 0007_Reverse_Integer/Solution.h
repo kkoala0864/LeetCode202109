4da986e0396d0dc8f9dfec18f59e4ec9168a084b
neilhuang <neilhuang@synology.com>
2021-10-26 10:15:27 +0800

    <LeetCode202109> 7. Reverse Integer

---
+++ 0007_Reverse_Integer/Solution.h
@@ -0,0 +1,10 @@
+class Solution {
+    public :
+        Solution() {}
+	int reverse(int x);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
