413c4e44bc31b1506c3de88b6c7228973b212bbc
neilhuang <neilhuang@synology.com>
2021-10-27 20:27:44 +0800

    <LeetCode202109> 96. Unique Binary Search Trees

---
+++ 0096_Unique_Binary_Search_Trees/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+using std::vector;
+
+int Solution::numTrees(int n) {
+	vector<int> dp(n+1, 0);
+	dp[1] = 1;
+	dp[0] = 1;
+
+	for (int i = 2 ; i <= n ; ++i) {
+		int local = 0;
+		for (int j = 0 ; j < i ; ++j) {
+			local += (dp[j] * dp[i-j-1]);
+		}
+		dp[i] = local;
+	}
+	return dp[n];
+}
