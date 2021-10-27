f0f63926ffd08ceff8b797629f7974729148e0a8
neilhuang <neilhuang@synology.com>
2021-10-27 20:13:36 +0800

    <LeetCode202109> 117. Populating Next Right Pointers in Each Node II

---
+++ 0117_Populating_Next_Right_Pointers_In_Each_NodeII/Solution.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+
+void processChild(Node* node, Node*& prev, Node*& leftMost) {
+	if (!node) return;
+	if (prev) {
+		prev->next = node;
+	} else {
+		leftMost = node;
+	}
+	prev = node;
+}
+
+Node* Solution::connect(Node* root) {
+	if (!root) return nullptr;
+	Node* leftMost = root;
+	Node* prev = nullptr;
+	Node* cur = nullptr;
+
+	while (leftMost) {
+		prev = nullptr;
+		cur = leftMost;
+
+		leftMost = nullptr;
+		while (cur) {
+			processChild(cur->left, prev, leftMost);
+			processChild(cur->right, prev, leftMost);
+			cur = cur->next;
+		}
+	}
+	return root;
+}
