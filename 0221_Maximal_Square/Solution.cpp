c7d717498656865c185544c9fd0af7176c89bd4a
neilhuang <neilhuang@synology.com>
2021-10-27 21:45:57 +0800

    <LeetCode202109> 221. Maximal Square

---
+++ 0221_Maximal_Square/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+using std::max;
+
+int Solution::maximalSquare(vector<vector<char>>& matrix) {
+	int m = matrix.size();
+	int n = matrix[0].size();
+	int result = 0;
+	vector<vector<int>> dp(m, vector<int>(n, 0));
+	for (int i = 0 ; i < m ; ++i) if (matrix[i][0] == '1') dp[i][0] = 1, result = 1;
+	for (int i = 0 ; i < n ; ++i) if (matrix[0][i] == '1') dp[0][i] = 1, result = 1;
+
+	for (int i = 1 ; i < m ; ++i) {
+		for (int j = 1 ; j < n ; ++j) {
+			if (matrix[i][j] == '1') {
+				dp[i][j] = min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]}) + 1;
+				result = max(result, dp[i][j] * dp[i][j]);
+			}
+		}
+	}
+	return result;
+}
