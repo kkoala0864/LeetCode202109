0ad82b03cf1e5de394e32ec67a2d3de881dbf5da
neilhuang <neilhuang@synology.com>
2021-10-28 21:42:29 +0800

    <LeetCode202109> 256. Paint House

---
+++ 0256_Paint_House/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int minCost(vector<vector<int>>& costs);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 0256_Paint_House/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{17,2,17},{16,16,5},{14,3,19}};
+	Solution* test = new Solution();
+	cout << test->minCost(input) << endl;
+	return 0;
+}
+
