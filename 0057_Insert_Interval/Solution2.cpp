7643c0c7f4442685ab3a5b07bfccd861a4355c53
neilhuang <neilhuang@synology.com>
2021-10-30 17:15:38 +0800

    <LeetCode202109> 57. Insert Interval

---
+++ 0057_Insert_Interval/Solution2.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::min;
+using std::max;
+
+vector<vector<int>> Solution::insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+	vector<vector<int>> result;
+	int idx = 0;
+	while (idx < intervals.size() && intervals[idx][1] < newInterval[0]) {
+		result.emplace_back(intervals[idx++]);
+	}
+
+	while (idx < intervals.size() && intervals[idx][0] <= newInterval[1]) {
+		newInterval[0] = min(newInterval[0], intervals[idx][0]);
+		newInterval[1] = max(newInterval[1], intervals[idx][1]);
+		++idx;
+	}
+
+	result.emplace_back(newInterval);
+	while (idx < intervals.size()) {
+		result.emplace_back(intervals[idx++]);
+	}
+	return result;
+}
