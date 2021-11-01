1ebfd5735f425b7cadd4cf8a536b1c138c839062
neilhuang <neilhuang@synology.com>
2021-11-01 20:51:26 +0800

    <LeetCode202109> 78. Subsets

---
+++ 0078_Subsets/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<vector<int>> subsets(vector<int>& nums);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
