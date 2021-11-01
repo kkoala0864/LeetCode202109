2fef5f21012bbd07ed773d02855539781e141d8f
neilhuang <neilhuang@synology.com>
2021-11-01 14:10:22 +0800

    <LeetCode202109> 130. Surrounded Regions

---
+++ 0130_Surrounded_Regions/main.cpp
@@ -0,0 +1,26 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<char>> input = {{'X','O','O','X','X','X','O','X','O','O'},{'X','O','X','X','X','X','X','X','X','X'},{'X','X','X','X','O','X','X','X','X','X'},{'X','O','X','X','X','O','X','X','X','O'},{'O','X','X','X','O','X','O','X','O','X'},{'X','X','O','X','X','O','O','X','X','X'},{'O','X','X','O','O','X','O','X','X','O'},{'O','X','X','X','X','X','O','X','X','X'},{'X','O','O','X','X','O','X','X','O','O'},{'X','X','X','O','O','X','O','X','X','O'}};
+	Solution* test = new Solution();
+	for (const auto& iter : input) {
+		for (const auto& it : iter) {
+			cout << it << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	test->solve(input);
+	for (const auto& iter : input) {
+		for (const auto& it : iter) {
+			cout << it << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
