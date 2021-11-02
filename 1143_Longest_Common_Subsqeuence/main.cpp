0841440dd99e728ecf17367011dfa93fc05c992a
neilhuang <neilhuang@synology.com>
2021-11-02 10:34:59 +0800

    <LeetCode202109> 1143. Longest Common Subsequence

---
+++ 1143_Longest_Common_Subsqeuence/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input1 = "abcde";
+	string input2 = "ace";
+	Solution* test = new Solution();
+	cout << test->longestCommonSubsequence(input1, input2) << endl;
+	return 0;
+}
+
