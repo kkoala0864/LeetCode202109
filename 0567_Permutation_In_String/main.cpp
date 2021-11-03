8b86ce8e3c47c4be67cd674cbe684ca3dd2304d9
neilhuang <neilhuang@synology.com>
2021-11-03 14:23:37 +0800

    <LeetCode202109> 567. Permutation in String

---
+++ 0567_Permutation_In_String/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string s1("ab");
+	string s2("eidbaooo");
+	Solution* test = new Solution();
+	cout << test->checkInclusion(s1, s2) << endl;
+	return 0;
+}
+
