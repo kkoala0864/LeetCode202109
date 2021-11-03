67fc33ca99e3adcdfc54fbe46d4ff7be786d1b27
neilhuang <neilhuang@synology.com>
2021-11-03 11:21:39 +0800

    <LeetCode202109> 95. Unique Binary Search Trees II

---
+++ 0095_Unique_Binary_Search_TreesII/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<TreeNode*> dfs(int start, int end) {
+	if (start > end) return vector<TreeNode*>(1, nullptr);
+
+	vector<TreeNode*> result;
+	if (start == end) {
+		TreeNode* node = new TreeNode(start);
+		result.emplace_back(node);
+		return result;
+	}
+
+	for (int i = start ; i <= end ; ++i) {
+		vector<TreeNode*> left = dfs(start, i-1);
+		vector<TreeNode*> right = dfs(i+1, end);
+		for (const auto& ls : left) {
+			for (const auto& rs : right) {
+				TreeNode* node = new TreeNode(i, ls, rs);
+				result.emplace_back(node);
+			}
+		}
+	}
+	return result;
+}
+
+vector<TreeNode*> Solution::generateTrees(int n) {
+	return dfs(1, n);
+}
