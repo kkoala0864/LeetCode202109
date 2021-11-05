84b0d67c76c9bd9dfeabb9eb34e68cab3e091fa4
neilhuang <neilhuang@synology.com>
2021-11-05 20:40:59 +0800

    <LeetCode202109> 270. Closest Binary Search Tree Value

---
+++ 0270_Closest_Binary_Search_Tree_Value/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+void dfs(TreeNode* node, const double& target, int& result) {
+	if (!node) return;
+	dfs(node->left, target, result);
+	if (abs(target - (double)node->val) < abs(target - result)) {
+		result = node->val;
+	} else {
+		return;
+	}
+
+	dfs(node->right, target, result);
+}
+
+int Solution::closestValue(TreeNode* root, double target) {
+	int result = INT_MAX;
+	dfs(root, target, result);
+	return result;
+}
