354fb9be50608862f0ab91a2c3b39ea690c4ba47
neilhuang <neilhuang@synology.com>
2021-11-07 10:58:17 +0800

    <LeetCode202109> 260. Single Number III

---
+++ 0260_Single_NumberIII/Solution.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<int> Solution::singleNumber(vector<int>& nums) {
+	long long bitmask = 0;
+	for (const auto& iter : nums) {
+		bitmask ^= iter;
+	}
+
+	long long diff = bitmask & -bitmask;
+	long long x = 0;
+	for (const auto& iter : nums) {
+		if (iter & diff) {
+			x ^= iter;
+		}
+	}
+	return {(int)x, static_cast<int>(x^bitmask)};
+}
---
+++ 0260_Single_NumberIII/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,1,3,2,5};
+	Solution* test = new Solution();
+	vector<int> result = test->singleNumber(input);
+	for (const auto& iter : result) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
