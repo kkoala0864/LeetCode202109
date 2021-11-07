96ead2c095842d714e8b19c58105c85efc45e59e
neilhuang <neilhuang@synology.com>
2021-11-07 12:48:58 +0800

    <LeetCode202109> 19. Remove Nth Node From End of List

--- 0019_Remove_Nth_Node_From_End_Of_List/Solution.cpp
+++ 0019_Remove_Nth_Node_From_End_Of_List/Solution.cpp
@@ -3,39 +3,24 @@
 #include <vector>
 
 using std::vector;
-using std::cout;
-using std::endl;
+
 // Clarify
 // 1. does this linked-list have loop ?
 // 2. if head is nullptr, what should return ? or i can assume this head would always has node
 // 3. how many number of node is this linked list maximum ?
 
 ListNode* Solution::removeNthFromEnd(ListNode* head, int n) {
-	if (head == nullptr) return head;
-	vector<ListNode*> location(30, nullptr);
-
-	int index = 0;
-	location[index++] = head;
-	ListNode* iter = head->next;
-	while (iter != nullptr) {
-		location[index++] = iter;
-		iter = iter->next;
+	ListNode* dummy = new ListNode(0, head);
+	ListNode* first = head;
+	ListNode* second = head;
+	for (int i = 0 ; i < n ; ++i) {
+		first = first->next;
 	}
 
-	int size = index;
-	int removeIdx = size - 1 - n + 1;
-	int front = removeIdx - 1;
-	ListNode* frontPtr = nullptr;
-	int backend = removeIdx + 1;
-	ListNode* backendPtr = nullptr;
-
-	if (backend >= index) backendPtr = nullptr;
-	else backendPtr = location[backend];
-
-	if (front < 0) head = backendPtr;
-	else {
-		frontPtr = location[front];
-		frontPtr->next = backendPtr;
+	while (first->next) {
+		first = first->next;
+		second = second->next;
 	}
-	return head;
+	second->next = second->next->next;
+	return dummy->next;
 }
