0a30ad49ec9f67e85d67fc6ad03faa9a31917cfc
neilhuang <neilhuang@synology.com>
2021-11-07 13:10:20 +0800

    <LeetCode202109> 257. Binary Tree Paths

---
+++ 0257_Binary_Tree_Paths/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+using std::to_string;
+void dfs(TreeNode* node, vector<string>& result, vector<int>& local) {
+	if (!node) return;
+	local.emplace_back(node->val);
+	if (!node->left && !node->right) {
+		string insert("");
+		for (int i = 0 ; i < local.size() ; ++i) {
+			insert += to_string(iter);
+			if (i != local.size() - 1) {
+				insert += "->";
+			}
+		}
+		result.emplace_back(insert);
+	} else {
+		dfs(node->left, result, local);
+		dfs(node->right, result, local);
+	}
+	local.pop_back();
+}
+
+vector<string> Solution::binaryTreePaths(TreeNode* root) {
+	vector<string> result;
+	vector<int> local;
+	dfs(root, result, local);
+	return result;
+}
---
+++ 0257_Binary_Tree_Paths/Solution.h
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public :
+        Solution() {}
+	vector<string> binaryTreePaths(TreeNode* root);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
