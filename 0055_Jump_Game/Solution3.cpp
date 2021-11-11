96fa237be0deb3837dcbba5c8f3f20b0e51d879d
neilhuang <neilhuang@synology.com>
2021-11-11 17:45:04 +0800

    <LeetCode202109> 55. Jump Game

---
+++ 0055_Jump_Game/Solution3.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+using std::max;
+
+bool Solution::canJump(vector<int>& nums) {
+	int maxReach = nums[0];
+	int i = 1;
+	for (; i < nums.size() && i <= maxReach ; ++i) {
+		maxReach = max(maxReach, i + nums[i]);
+	}
+	return i == nums.size();
+}
