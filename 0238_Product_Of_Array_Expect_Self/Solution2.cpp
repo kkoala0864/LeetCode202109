22ff7f0478e2dabf7d53cf324cc21b3045135d3b
neilhuang <neilhuang@synology.com>
2021-11-27 18:28:15 +0800

    <LeetCode202109> 238. Product of Array Except Self

---
+++ 0238_Product_Of_Array_Expect_Self/Solution2.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+//[1, 2, 3, 4]
+//
+//from
+//[1, 2, 6]
+//back
+//[4, 12, 24]
+//
+//nums
+//[24, from[0] * back[1], from[1] * back[0], 6]
+// a b c d
+// 1 a ab abc
+//
+vector<int> Solution::productExceptSelf2(vector<int>& nums) {
+	vector<int> prefixProduct(1,1);
+	int product = 1;
+	for (int i = 0 ; i < nums.size()-1 ; ++i) {
+		product *= nums[i];
+		prefixProduct.emplace_back(product);
+	}
+	product = 1;
+	for (int i = nums.size() - 1 ; i > 0 ; --i) {
+		product *= nums[i];
+		prefixProduct[i-1] *= product;
+	}
+	return prefixProduct;
+}
