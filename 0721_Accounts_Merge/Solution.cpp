e426237a1a4d20655acfef6519f51fb2a58107fc
neilhuang <neilhuang@synology.com>
2021-11-29 20:14:34 +0800

    <LeetCode202109> 721. Accounts Merge

---
+++ 0721_Accounts_Merge/Solution.cpp
@@ -0,0 +1,54 @@
+#include <Solution.h>
+#include <iostream>
+#include <set>
+#include <unordered_map>
+
+using std::set;
+using std::unordered_map;
+
+void find(string& idx, unordered_map<string, string>& map) {
+	while (idx != map[idx]) idx = map[idx];
+}
+
+vector<vector<string>> Solution::accountsMerge(vector<vector<string>>& accounts) {
+	unordered_map<string, unordered_map<string, string>> graph;
+	for (const auto& account : accounts) {
+		string name = account[0];
+		string root = account[1];
+
+		if (graph[name].find(root) == graph[name].end()) graph[name][root] = root;
+		else find(root, graph[name]);
+
+		for (int i = 2 ; i < account.size() ; ++i) {
+			string node = account[i];
+			if (graph[name].find(node) == graph[name].end()) graph[name][node] = root;
+			else {
+				find(node, graph[name]);
+				graph[name][node] = root;
+			}
+		}
+	}
+
+	unordered_map<string, unordered_map<string, set<string>>> uMap;
+	for (auto& people : graph) {
+		string name = people.first;
+		for (auto& iter : people.second) {
+			string root = iter.first;
+			find(root, people.second);
+			uMap[name][root].emplace(iter.first);
+		}
+	}
+
+	vector<vector<string>> result;
+	for (const auto& ele : uMap) {
+		string name = ele.first;
+		for (const auto& mail : ele.second) {
+			vector<string> insert;
+			insert.emplace_back(name);
+			for (const auto& m : mail.second) insert.emplace_back(m);
+			result.emplace_back(insert);
+		}
+	}
+
+	return result;
+}
---
+++ 0721_Accounts_Merge/main.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<string>> input = {{"John","johnsmith@mail.com","john_newyork@mail.com"},{"John","johnsmith@mail.com","john00@mail.com"},{"Mary","mary@mail.com"},{"John","johnnybravo@mail.com"}};
+	Solution* test = new Solution();
+	vector<vector<string>> result = test->accountsMerge(input);
+	for (const auto& person : result) {
+		for (const auto& info : person) {
+			cout << info << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
