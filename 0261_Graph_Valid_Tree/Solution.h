4c32fed908c20e88703ab3311d4d2682a3420fdc
neilhuang <neilhuang@synology.com>
2021-12-03 20:32:25 +0800

    <LeetCode202109> 261. Graph Valid Tree

--- 0261_Graph_Valid_Tree/Solution.h
+++ 0261_Graph_Valid_Tree/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	bool validTree(int n, vector<vector<int>>& edges);
+	bool validTree2(int n, vector<vector<int>>& edges);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0261_Graph_Valid_Tree/main.cpp
+++ 0261_Graph_Valid_Tree/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
 	vector<vector<int>> input = {{0,1},{0,2},{0,3},{1,4}};
 	Solution* test = new Solution();
 	cout << test->validTree(5, input) << endl;
+	cout << test->validTree2(5, input) << endl;
 	return 0;
 }
 
