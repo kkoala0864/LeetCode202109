051d3b64b1ebb49c1b65fc154c3de7c29a436ec6
neilhuang <neilhuang@synology.com>
2021-12-03 20:33:06 +0800

    <LeetCode202109> 322. Coin Change

--- 0322_Coin_Change/Solution.h
+++ 0322_Coin_Change/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int coinChange(vector<int>& coins, int amount);
+	int coinChange2(vector<int>& coins, int amount);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0322_Coin_Change/main.cpp
+++ 0322_Coin_Change/main.cpp
@@ -8,5 +8,6 @@ int main(void) {
 	vector<int> input = {125,146,125,252,226,25,24,308,50};
 	Solution* test = new Solution();
 	cout << test->coinChange(input, 8402) << endl;
+	cout << test->coinChange2(input, 3) << endl;
 	return 0;
 }
