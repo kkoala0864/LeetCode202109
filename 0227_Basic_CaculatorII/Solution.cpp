39bad0e3f6c3912521dda0592fca3b70da016eb3
neilhuang <neilhuang@synology.com>
2021-12-25 13:56:06 +0800

    <LeetCode202109> 227. Basic Calculator II

---
+++ 0227_Basic_CaculatorII/Solution.cpp
@@ -0,0 +1,43 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+int getVal(string& s, int& idx) {
+	string val("");
+	while (idx < s.size() && (s[idx] == ' ' || (s[idx] <= '9' && s[idx] >= '0'))) {
+		if (s[idx] == ' ') {
+			++idx;
+			continue;
+		}
+		val.push_back(s[idx]);
+		++idx;
+	}
+	return stoi(val);
+}
+
+int Solution::calculate(string s) {
+	int idx = 0;
+	vector<int> que(1, getVal(s, idx));
+	while (idx < s.size()) {
+		char oper = s[idx];
+		++idx;
+		int tmpVal = getVal(s, idx);
+		if (oper == '*') {
+			int tmp = que.back() * tmpVal;
+			que.pop_back();
+			que.emplace_back(tmp);
+		} else if (oper == '/') {
+			int tmp = que.back() / tmpVal;
+			que.pop_back();
+			que.emplace_back(tmp);
+		} else if (oper == '-') {
+			que.emplace_back(-tmpVal);
+		} else {
+			que.emplace_back(tmpVal);
+		}
+	}
+	int result = 0;
+	for (const auto& iter : que) result += iter;
+	return result;
+}
---
+++ 0227_Basic_CaculatorII/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "3+2*2";
+	Solution* test = new Solution();
+	cout << test->calculate(input) << endl;
+	return 0;
+}
+
