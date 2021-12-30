d103ecdb84b6cce055bcac143bd7e162d9e9cf53
neilhuang <neilhuang@synology.com>
2021-12-30 09:55:38 +0800

    <LeetCode202109> 1015. Smallest Integer Divisible by K

---
+++ 1015_Smallest_Integer_Divisibe_By_K/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::smallestRepunitDivByK(int k) {
+	if (((k & 1) == 0) || (k % 5 == 0)) return -1;
+	int result = 1;
+	int rest = 1 % k;
+	while (rest > 0) {
+		rest = rest * 10 + 1;
+		rest = rest % k;
+		++result;
+	}
+	return result;
+
+}
---
+++ 1015_Smallest_Integer_Divisibe_By_K/Solution.h
@@ -0,0 +1,10 @@
+class Solution {
+    public :
+        Solution() {}
+	int smallestRepunitDivByK(int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
