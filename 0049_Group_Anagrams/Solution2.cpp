0ef745b2a525365bbbdd0e2a2efb30c299bd204f
neilhuang <neilhuang@synology.com>
2022-01-01 12:21:52 +0800

    <LeetCode202109> 49. Group Anagrams

--- 0049_Group_Anagrams/Solution2.cpp
+++ 0049_Group_Anagrams/Solution2.cpp
@@ -1,21 +1,33 @@
 #include <Solution.h>
 #include <iostream>
 #include <unordered_map>
-#include <algorithm>
 
 using std::unordered_map;
-using std::sort;
+
+string sortStr2(const string& str) {
+	vector<int> record(26, 0);
+	for (const auto& iter : str) {
+		++record[iter-'a'];
+	}
+
+	string result;
+	for (int i = 0 ; i < 26 ; ++i) {
+		if (record[i] > 0) result += string(record[i], i + 'a');
+	}
+	return result;
+}
 
 vector<vector<string>> Solution::groupAnagrams2(vector<string>& strs) {
-	unordered_map<string, vector<string>> group;
+	unordered_map<string, int> group;
 	vector<vector<string>> result;
 	for (const auto& str : strs) {
-		string tmp = str;
-		sort(tmp.begin(), tmp.end());
-		group[tmp].emplace_back(str);
-	}
-	for (const auto& iter : group) {
-		result.emplace_back(iter.second);
+		string tmp = sortStr2(str);
+		if (group.find(tmp) != group.end()) {
+			result[group[tmp]].emplace_back(str);
+		} else {
+			result.push_back({str});
+			group[tmp] = result.size() - 1;
+		}
 	}
 	return result;
 }
