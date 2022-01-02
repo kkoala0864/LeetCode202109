10a16638e05fb4a70f783d4e880ba70b3801b379
neilhuang <neilhuang@synology.com>
2022-01-02 12:34:45 +0800

    <LeetCode202109> 1010. Pairs of Songs With Total Durations Divisible by 60

---
+++ 1010_Pairs_Of_Songs_With_Total_Durations_Divisible_Of_60/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int Solution::numPairsDivisibleBy60(vector<int>& time) {
+	if (time.size() < 2) return 0;
+
+	int cnt = 0;
+	vector<int> uMap(60, 0);
+	for (const auto& iter : time) {
+		int rp = (60 - (iter % 60)) % 60;
+		if (uMap[rp] > 0) cnt += uMap[rp];
+		++uMap[iter % 60];
+	}
+
+	return cnt;
+}
---
+++ 1010_Pairs_Of_Songs_With_Total_Durations_Divisible_Of_60/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int numPairsDivisibleBy60(vector<int>& time);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
