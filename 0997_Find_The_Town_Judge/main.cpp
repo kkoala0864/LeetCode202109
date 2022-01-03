dac180f33915069a69058cd46dfba5f912dd7026
neilhuang <neilhuang@synology.com>
2022-01-03 10:16:20 +0800

    <LeetCode202109> 997. Find the Town Judge

---
+++ 0997_Find_The_Town_Judge/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,3},{2,3},{3,1}};
+	Solution* test = new Solution();
+	cout << test->findJudge(3, input) << endl;
+	return 0;
+}
+
