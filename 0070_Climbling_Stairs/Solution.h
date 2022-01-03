aa2762d17b400784019745a9be773e0c379d4925
neilhuang <neilhuang@synology.com>
2022-01-03 17:10:59 +0800

    <LeetCode202109> 70. Climbing Stairs

--- 0070_Climbling_Stairs/Solution.h
+++ 0070_Climbling_Stairs/Solution.h
@@ -2,6 +2,7 @@ class Solution {
     public :
         Solution() {}
 	int climbStairs(int n);
+	int climbStairs2(int n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0070_Climbling_Stairs/main.cpp
+++ 0070_Climbling_Stairs/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
 
     Solution* test = new Solution();
     cout << test->climbStairs(45) << endl;
+    cout << test->climbStairs2(45) << endl;
     return 0;
 }
 
