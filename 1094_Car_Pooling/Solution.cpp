d7efaa2692b081e535560fbbfa3b57dd37234f39
neilhuang <neilhuang@synology.com>
2022-01-06 10:18:25 +0800

    <LeetCode202109> 1094. Car Pooling

---
+++ 1094_Car_Pooling/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+bool Solution::carPooling(vector<vector<int>>& trips, int capacity) {
+	vector<int> loc(1001, 0);
+	int maxLoc = 0;
+	int minLoc = 1001;
+	for (const auto& iter : trips) {
+		loc[iter[1]] += iter[0];
+		loc[iter[2]] -= iter[0];
+		if (iter[2] > maxLoc) maxLoc = iter[2];
+		if (iter[1] < minLoc) minLoc = iter[1];
+	}
+
+	int seat = 0;
+	for (int i = minLoc ; i <= maxLoc ; ++i) {
+		seat += loc[i];
+		if (seat > capacity) return false;
+	}
+	return true;
+}
