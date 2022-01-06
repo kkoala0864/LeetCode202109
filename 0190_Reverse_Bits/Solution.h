ccac725950e4e86721ac179cc2d78babe498951e
neilhuang <neilhuang@synology.com>
2022-01-06 18:02:21 +0800

    <LeetCode202109> 190. Reverse Bits

--- 0190_Reverse_Bits/Solution.h
+++ 0190_Reverse_Bits/Solution.h
@@ -3,6 +3,7 @@ class Solution {
     public :
         Solution() {}
 	uint32_t reverseBits(uint32_t n);
+	uint32_t reverseBits2(uint32_t n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0190_Reverse_Bits/main.cpp
+++ 0190_Reverse_Bits/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
 	uint32_t i = 4294967293;
 	Solution* test = new Solution();
 	cout << test->reverseBits(i) << endl;
+	cout << test->reverseBits2(i) << endl;
 	return 0;
 }
 
