04d4ca772f5b8916fc6846d00f21195b8ae1cbc9
neilhuang <neilhuang@synology.com>
2022-01-06 17:44:07 +0800

    <LeetCode202109> 153. Find Minimum in Rotated Sorted Array

--- 0153_Find_Minimum_In_Rotated_Sorted_Array/Solution.h
+++ 0153_Find_Minimum_In_Rotated_Sorted_Array/Solution.h
@@ -7,6 +7,7 @@ class Solution {
         Solution() {}
 	int findMin(vector<int>& nums);
 	int findMin2(vector<int>& nums);
+	int findMin3(vector<int>& nums);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0153_Find_Minimum_In_Rotated_Sorted_Array/main.cpp
+++ 0153_Find_Minimum_In_Rotated_Sorted_Array/main.cpp
@@ -5,10 +5,11 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<int> input = {2,1};
+	vector<int> input = {3,1,2};
 	Solution* test = new Solution();
 	cout << test->findMin(input) << endl;
 	cout << test->findMin2(input) << endl;
+	cout << test->findMin3(input) << endl;
 	return 0;
 }
 
