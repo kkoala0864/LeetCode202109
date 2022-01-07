7b616fb5744fc8e58658e63e307e82b49f243009
neilhuang <neilhuang@synology.com>
2022-01-07 16:58:41 +0800

    <LeetCode202109> 217. Contains Duplicate

--- 0217_Contains_Duplicate/Solution.h
+++ 0217_Contains_Duplicate/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	bool containsDuplicate(vector<int>& nums);
+	bool containsDuplicate2(vector<int>& nums);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
---
+++ 0217_Contains_Duplicate/Solution2.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+
+using std::unordered_set;
+
+bool Solution::containsDuplicate2(vector<int>& nums) {
+	unordered_set<int> set;
+	for (const auto& iter : nums) {
+		if (set.find(iter) != set.end()) return true;
+		set.emplace(iter);
+	}
+	return false;
+}
