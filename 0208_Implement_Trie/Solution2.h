f3b887d0b71cf934684ff299d2111eba33a09725
neilhuang <neilhuang@synology.com>
2022-01-07 15:30:50 +0800

    <LeetCode202109> 208. Implement Trie (Prefix Tree)

---
+++ 0208_Implement_Trie/Solution2.h
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Trie {
+    public :
+	bool isEnd;
+	vector<Trie*> next;
+	Trie() {
+		next = vector<Trie*>(26, nullptr);
+		isEnd = false;
+	}
+
+	void insert(string word) {
+		Trie* iter = this;
+		for (const auto& ch : word) {
+			if (!iter->next[ch-'a']) {
+				iter->next[ch-'a'] = new Trie();
+			}
+			iter = iter->next[ch-'a'];
+		}
+		iter->isEnd = true;
+	}
+
+	bool search(string word) {
+		Trie* iter = this;
+		for (const auto& ch : word) {
+			if (!iter->next[ch-'a']) return false;
+			iter = iter->next[ch-'a'];
+		}
+		return iter->isEnd;
+	}
+
+	bool startsWith(string prefix) {
+		Trie* iter = this;
+		for (const auto& ch : prefix) {
+			if (!iter->next[ch-'a']) return false;
+			iter = iter->next[ch-'a'];
+		}
+		return true;
+	}
+    private :
+        virtual ~Trie() {}
+        Trie& operator=(const Trie& source);
+        Trie(const Trie&);
+};
+
