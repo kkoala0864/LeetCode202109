d18c0c0292053b650192d078ff2897393f8f5c54
neilhuang <neilhuang@synology.com>
2022-01-07 15:52:41 +0800

    <LeetCode202109> 211. Design Add and Search Words Data Structure

---
+++ 0211_Design_Add_And_Search_Words_Data_Structure/Solution2.h
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using std::string;
+using std::vector;
+
+class WordDictionary {
+    public :
+	    bool isEnd;
+	    vector<WordDictionary*> next;
+	    WordDictionary() {
+		    next = vector<WordDictionary*>(26, nullptr);
+		    isEnd = false;
+	    }
+
+	    void addWord(string word) {
+		    WordDictionary* iter = this;
+		    for (const auto& ch : word) {
+			if (!iter->next[ch-'a']) iter->next[ch-'a'] = new WordDictionary();
+			iter = iter->next[ch-'a'];
+		    }
+		    iter->isEnd = true;
+	    }
+
+	    bool WildcardSeach(WordDictionary* node, const string& word, int index) {
+		    for (int i = index ; i < word.size() ; ++i) {
+			    if (word[i] == '.') {
+				    for (const auto& iter : node->next) {
+					    if (iter) if (WildcardSeach(iter, word, i+1)) return true;
+				    }
+				    return false;
+			    } else {
+				    if (node->next[word[i] - 'a']) node = node->next[word[i]-'a'];
+				    else return false;
+			    }
+		    }
+		    return node->isEnd;
+	    }
+
+	    bool search(string word) {
+		    return WildcardSeach(this, word, 0);
+	    }
+
+    private :
+        virtual ~WordDictionary() {}
+        WordDictionary& operator=(const WordDictionary& source);
+        WordDictionary(const WordDictionary&);
+};
+
