a57a915b9d6f148ce53be56ea3beed3e422ad8c3
neilhuang <neilhuang@synology.com>
2022-01-10 10:13:37 +0800

    <LeetCode202109> 67. Add Binary

---
+++ 0067_Add_Binary/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+
+string Solution::addBinary(string a, string b) {
+	int carry = 0;
+	string result("");
+	while (!a.empty() && !b.empty()) {
+		int first = a.back() - '0';
+		a.pop_back();
+		int second = b.back() - '0';
+		b.pop_back();
+		result = string(1, (first + second + carry) % 2 + '0') + result;
+		carry = ( first + second + carry ) / 2;
+	}
+	// difference size
+	string rest = a.empty() ? b : a;
+	while (!rest.empty() && carry != 0) {
+		int val = rest.back() - '0';
+		rest.pop_back();
+		result = string(1, (val + carry) % 2 + '0') + result;
+		carry = (val + carry) / 2;
+	}
+
+	if (!rest.empty()) result = rest + result;
+	// carry in handle
+	if (carry != 0) result = "1" + result;
+
+	return result;
+}
---
+++ 0067_Add_Binary/Solution.h
@@ -0,0 +1,14 @@
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	string addBinary(string a, string b);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
