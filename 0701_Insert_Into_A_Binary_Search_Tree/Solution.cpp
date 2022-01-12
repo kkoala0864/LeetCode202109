c6aa9ebf048301a419d1ba6561a77e0a3c4db26e
neilhuang <neilhuang@synology.com>
2022-01-12 10:16:49 +0800

    <LeetCode202109> 701. Insert into a Binary Search Tree

---
+++ 0701_Insert_Into_A_Binary_Search_Tree/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+TreeNode* Solution::insertIntoBST(TreeNode* root, int val) {
+	if (!root) {
+		return new TreeNode(val);
+	}
+
+	if (val > root->val) {
+		root->right = insertIntoBST(root->right, val);
+	} else {
+		root->left = insertIntoBST(root->left, val);
+	}
+	return root;
+}
