68199cbd35ed060d3d12a9b9fb9bc4706ab0b7eb
neilhuang <neilhuang@synology.com>
2022-01-15 13:17:03 +0800

    <LeetCode202109> 1345. Jump Game IV

--- 1345_Jump_GameIV/Solution.cpp
+++ 1345_Jump_GameIV/Solution.cpp
@@ -1,36 +1,48 @@
 #include <Solution.h>
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 #include <climits>
-#include <algorithm>
+#include <queue>
 
 using std::unordered_map;
-using std::min;
-
-void dfs(vector<int>& arr, unordered_map<int, vector<int>>& vIndex, int idx, int cnt, int& result) {
-	if (cnt >= result) return;
-	if (idx == arr.size() - 1) {
-		result = min(result, cnt);
-		return;
-	}
-
-	int ori = arr[idx];
-	arr[idx] = INT_MAX;
-	for (const auto& iter : vIndex[ori]) {
-		if (arr[iter] != INT_MAX) {
-			dfs(arr, vIndex, iter, cnt + 1, result);
-		}
-	}
-	if (idx > 0 && arr[idx-1] != INT_MAX) dfs(arr, vIndex, idx - 1, cnt + 1, result);
-	if (arr[idx + 1] != INT_MAX) dfs(arr, vIndex, idx + 1, cnt + 1, result);
-	arr[idx] = ori;
-}
+using std::queue;
+using std::unordered_set;
 
 int Solution::minJumps(vector<int>& arr) {
 	unordered_map<int, vector<int>> vIndex;
-
+	unordered_set<int> isTraverse;
 	for (int i = arr.size() - 1 ; i >= 0 ; --i) vIndex[arr[i]].emplace_back(i);
-	int result = vIndex.size();
-	dfs(arr, vIndex, 0, 0, result);
+	queue<int> cur, next;
+	cur.emplace(0);
+	int result = 0;
+	while (!cur.empty()) {
+		int idx = cur.front();
+		if (idx == arr.size() - 1) break;
+
+		cur.pop();
+		if (idx > 0 && isTraverse.find(idx-1) == isTraverse.end()) {
+			isTraverse.emplace(idx-1);
+			next.emplace(idx - 1);
+		}
+		if (isTraverse.find(idx+1) == isTraverse.end()) {
+			isTraverse.emplace(idx+1);
+			next.emplace(idx+1);
+		}
+		while (!vIndex[arr[idx]].empty()) {
+			int iter = vIndex[arr[idx]].back();
+			if (isTraverse.find(iter) == isTraverse.end()) {
+				isTraverse.emplace(iter);
+				next.emplace(iter);
+			}
+			vIndex[arr[idx]].pop_back();
+		}
+
+		if (cur.empty()) {
+			++result;
+			cur = move(next);
+		}
+	}
 	return result;
 }
+
