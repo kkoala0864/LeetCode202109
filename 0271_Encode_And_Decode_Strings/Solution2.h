1be9ffb6de93ddb23acd2c24bb9aa4e694097927
neilhuang <neilhuang@synology.com>
2022-01-19 18:18:28 +0800

    <LeetCode202109> 271. Encode and Decode Strings

---
+++ 0271_Encode_And_Decode_Strings/Solution2.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::string;
+using std::stoi;
+using std::to_string;
+
+class Solution {
+    public :
+        Solution() {}
+	// Encodes a list of strings to a single string.
+	string encode(vector<string>& strs) {
+		string result("");
+		for (const auto& str : strs) {
+			int size = str.size();
+			result += to_string(size) + "@" + str;
+		}
+		return result;
+	}
+
+	// Decodes a single string to a list of strings.
+	vector<string> decode(string s) {
+		int i = 0;
+		vector<string> result;
+		while (i < s.size()) {
+			int j = i;
+			while (j < s.size() && s[j] != '@') ++j;
+			string sizeStr = s.substr(i, j - i);
+			int size = stoi(sizeStr);
+			string str = s.substr(j+1, size);
+			result.emplace_back(str);
+			i = j + size + 1;
+		}
+		return result;
+	}
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
