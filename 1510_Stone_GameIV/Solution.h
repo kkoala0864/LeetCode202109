759976ee2bc9b5014708c0cbb03c7f34e88e4dee
neilhuang <neilhuang@synology.com>
2022-01-22 12:10:43 +0800

    <LeetCode202109> 1510. Stone Game IV

---
+++ 1510_Stone_GameIV/Solution.h
@@ -0,0 +1,10 @@
+class Solution {
+    public :
+        Solution() {}
+	bool winnerSquareGame(int n);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
