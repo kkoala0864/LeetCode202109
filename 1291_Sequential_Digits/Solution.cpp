0590c698c44bfa6025d334487225ca688e5c61d0
neilhuang <neilhuang@synology.com>
2022-01-23 14:37:07 +0800

    <LeetCode202109> 1291. Sequential Digits

---
+++ 1291_Sequential_Digits/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<int> Solution::sequentialDigits(int low, int high) {
+	int lowSize = 0;
+	int tmp = low;
+	while (tmp > 0) {
+		tmp /= 10;
+		++lowSize;
+	}
+	int highSize = 0;
+	tmp = high;
+	while (tmp > 0) {
+		tmp /= 10;
+		++highSize;
+	}
+	vector<int> result;
+	for (int size = lowSize ; size <= highSize ; ++size) {
+		for (int base = 1 ; (base + size - 1) < 10 ; ++base) {
+			int incre = base;
+			int tmp = 0;
+			for (int i = 0 ; i < size ; ++i) {
+				tmp = tmp * 10 + incre++;
+			}
+			if (tmp >= low && tmp <= high) result.emplace_back(tmp);
+		}
+	}
+	return result;
+}
---
+++ 1291_Sequential_Digits/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> sequentialDigits(int low, int high);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
