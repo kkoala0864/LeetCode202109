4329c6d3c433b064c4914df049a1724e9b939f16
neilhuang <neilhuang@synology.com>
2022-01-24 11:29:56 +0800

    <LeetCode202109> 520. Detect Capital

---
+++ 0520_Detect_Capital/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+
+bool check(char ch) {
+	return ch >= 'A' && ch <= 'Z';
+}
+
+bool Solution::detectCapitalUse(string word) {
+	if (word.empty()) return false;
+
+	bool prev = true, cur = true;
+	for (int i = 1 ; i < word.size() ; ++i) {
+		if (i == 1) {
+			prev = check(word[1]);
+		} else {
+			cur = check(word[i]);
+			if (cur != prev) return false;
+			prev = cur;
+		}
+	}
+
+	if (word.size() > 1 && !check(word[0]) && check(word[1])) return false;
+
+	return true;
+}
---
+++ 0520_Detect_Capital/Solution.h
@@ -0,0 +1,14 @@
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	bool detectCapitalUse(string word);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
