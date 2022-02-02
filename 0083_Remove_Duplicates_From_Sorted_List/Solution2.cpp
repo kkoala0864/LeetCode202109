c456b9fc214e5da4b7808ec4921e211d980f7e13
neilhuang <neilhuang@synology.com>
2022-02-02 14:34:30 +0800

    <LeetCode202109> 83. Remove Duplicates from Sorted List

---
+++ 0083_Remove_Duplicates_From_Sorted_List/Solution2.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::deleteDuplicates2(ListNode* head) {
+	if (!head) return head;
+	ListNode* iter = nullptr;
+	ListNode* cur = head;
+
+	while (cur) {
+		iter = cur->next;
+		while (iter && iter->val == cur->val) {
+			iter = iter->next;
+		}
+		cur->next = iter;
+		cur = cur->next;
+	}
+	return head;
+}
