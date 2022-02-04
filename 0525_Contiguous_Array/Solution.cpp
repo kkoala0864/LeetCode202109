fe6e475ee06e86c7d80ca3bbc4ff1f156cb3be71
neilhuang <neilhuang@synology.com>
2022-02-04 13:37:35 +0800

    <LeetCode202109> 525. Contiguous Array

---
+++ 0525_Contiguous_Array/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+using std::unordered_map;
+using std::max;
+
+int Solution::findMaxLength(vector<int>& nums) {
+	int sum = 0;
+
+	unordered_map<int, int> map;
+	int result = 0;
+
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		if (nums[i]) ++sum;
+		else --sum;
+
+		if (0 == sum) result = max(result, i + 1);
+		else {
+			if (map.find(sum) == map.end()) {
+				map[sum] = i;
+			} else {
+				result = max(result, i - map[sum]);
+			}
+		}
+	}
+	return result;
+}
