9078857bccdcca0f451cf729e49535e5a6cdc807
neilhuang <neilhuang@synology.com>
2022-02-10 16:54:56 +0800

    <LeetCode202109> 298. Binary Tree Longest Consecutive Sequence

--- 0298_Binary_Tree_Longest_Consecutive_Sequence/Solution.h
+++ 0298_Binary_Tree_Longest_Consecutive_Sequence/Solution.h
@@ -11,6 +11,7 @@ class Solution {
     public :
         Solution() {}
 	int longestConsecutive(TreeNode* root);
+	int longestConsecutive2(TreeNode* root);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
---
+++ 0298_Binary_Tree_Longest_Consecutive_Sequence/Solution2.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+void dfs(TreeNode* node, int local, int& result) {
+	if (!node) {
+		result = max(result, local);
+		return;
+	}
+
+	if (node->left && node->left->val - node->val == 1) {
+		dfs(node->left, local + 1, result);
+	} else {
+		result = max(result, local);
+		dfs(node->left, 1, result);
+	}
+
+	if (node->right && node->right->val - node->val == 1) {
+		dfs(node->right, local + 1, result);
+	} else {
+		result = max(result, local);
+		dfs(node->right, 1, result);
+	}
+}
+
+int Solution::longestConsecutive2(TreeNode* root) {
+	int result = 0;
+	dfs(root, 1, result);
+	return result;
+}
