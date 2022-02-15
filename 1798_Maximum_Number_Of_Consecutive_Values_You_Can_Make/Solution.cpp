cb0969cc82bac0fe153c305925299f681ab25020
neilhuang <neilhuang@synology.com>
2022-02-15 14:58:41 +0800

    <LeetCode202109> 1798. Maximum Number of Consecutive Values You Can Make

---
+++ 1798_Maximum_Number_Of_Consecutive_Values_You_Can_Make/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::getMaximumConsecutive(vector<int>& coins) {
+	vector<int> map(40001, 0);
+
+	int maxCoin = 0;
+	for (const auto& iter : coins) {
+		++map[iter];
+		maxCoin = max(maxCoin, iter);
+	}
+
+	int maxCanReach = 0;
+	for (int i = 1 ; i <= maxCoin ; ++i) {
+		if (map[i] == 0) continue;
+		for (int j = 1 ; j <= map[i] ; ++j) {
+			if (maxCanReach >= ((i * j) - 1)) {
+				maxCanReach += i;
+			} else {
+				return maxCanReach + 1;
+			}
+		}
+	}
+	return maxCanReach + 1;
+}
---
+++ 1798_Maximum_Number_Of_Consecutive_Values_You_Can_Make/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,1,3,3,10};
+	Solution* test = new Solution();
+	cout << test->getMaximumConsecutive(input) << endl;
+	return 0;
+}
+
