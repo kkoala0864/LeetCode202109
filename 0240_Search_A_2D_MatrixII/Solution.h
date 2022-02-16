730cd757ec1c09d600ebe5729d3e08a68d1c0b0b
neilhuang <neilhuang@synology.com>
2022-02-16 16:20:50 +0800

    <LeetCode202109> 240. Search a 2D Matrix II

--- 0240_Search_A_2D_MatrixII/Solution.h
+++ 0240_Search_A_2D_MatrixII/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	bool searchMatrix(vector<vector<int>>& matrix, int target);
+	bool searchMatrix2(vector<vector<int>>& matrix, int target);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
