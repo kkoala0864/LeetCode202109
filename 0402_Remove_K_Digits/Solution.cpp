ccc8db8ec98b50b9076dabfe804ba51052f7ee99
neilhuang <neilhuang@synology.com>
2022-02-18 10:55:19 +0800

    <LeetCode202109> 402. Remove K Digits

---
+++ 0402_Remove_K_Digits/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+string Solution::removeKdigits(string num, int k) {
+	if (num.size() <= k) return "0";
+	string st;
+	for (const auto& iter : num) {
+		while (k > 0 && !st.empty() && st.back() > iter) {
+			--k;
+			st.pop_back();
+		}
+		if (iter == '0' && st.empty()) continue;
+		st.push_back(iter);
+	}
+	while (!st.empty() && k > 0) {
+		st.pop_back();
+		--k;
+	}
+	return st.empty() ? "0" : st;
+}
