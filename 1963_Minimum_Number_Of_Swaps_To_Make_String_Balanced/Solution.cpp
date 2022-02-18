3b62bbe00128a9608a50e33ef25d6a27dfbbe8f8
neilhuang <neilhuang@synology.com>
2022-02-18 16:53:36 +0800

    <LeetCode202109> 1963. Minimum Number of Swaps to Make the String Balanced

---
+++ 1963_Minimum_Number_Of_Swaps_To_Make_String_Balanced/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+int Solution::minSwaps(string s) {
+	int start = -1, end = s.size();
+	int sCnt = 0, eCnt = 0;
+	int result = 0;
+	while (start < end) {
+		while (sCnt >= 0 && start < end) {
+			++start;
+			if (s[start] == '[') ++sCnt;
+			else --sCnt;
+		}
+		while (eCnt >= 0 && start < end) {
+			--end;
+			if (s[end] == ']') ++eCnt;
+			else --eCnt;
+		}
+		if (start != end) {
+			++result;
+			eCnt = 1;
+			sCnt = 1;
+		}
+	}
+	return result;
+}
