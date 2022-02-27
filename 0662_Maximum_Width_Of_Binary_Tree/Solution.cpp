4bd510ac714701eb53da90250ec114b8dc7bd1bb
neilhuang <neilhuang@synology.com>
2022-02-27 17:39:29 +0800

    <LeetCode202109> 662. Maximum Width of Binary Tree

---
+++ 0662_Maximum_Width_Of_Binary_Tree/Solution.cpp
@@ -0,0 +1,34 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using std::queue;
+using std::max;
+using std::pair;
+
+int Solution::widthOfBinaryTree(TreeNode* root) {
+	if (!root) return 0;
+
+	queue<pair<TreeNode*, unsigned int>> que;
+	int result = 0;
+
+	que.push({root, 0});
+	while (!que.empty()) {
+		int mostFront = que.front().second;
+		int curSize = que.size();
+		pair<TreeNode*, unsigned int> cur;
+		for (int i = 0 ; i < curSize ; ++i) {
+			cur = que.front();
+			que.pop();
+			if (cur.first->left) {
+				que.push({cur.first->left, 2 * cur.second});
+			}
+			if (cur.first->right) {
+				que.push({cur.first->right, 2 * cur.second + 1});
+			}
+		}
+		result = max(result, (int)(cur.second - mostFront + 1));
+	}
+	return result;
+}
