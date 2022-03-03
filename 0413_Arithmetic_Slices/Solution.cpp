be59dd3f0d8b13c7de5dc02b3119e7103f401365
neilhuang <neilhuang@synology.com>
2022-03-03 10:13:39 +0800

    <LeetCode202109> 413. Arithmetic Slices

---
+++ 0413_Arithmetic_Slices/Solution.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::numberOfArithmeticSlices(vector<int>& nums) {
+	if (nums.size() < 3) return 0;
+
+	int result = 0;
+	int cnt = 0;
+	for (int i = 2 ; i < nums.size() ; ++i) {
+		if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+			++cnt;
+			result += cnt;
+		} else {
+			cnt = 0;
+		}
+	}
+	return result;
+}
