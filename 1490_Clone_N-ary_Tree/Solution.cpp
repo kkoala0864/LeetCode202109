a60ba62e53b2f547ab9bf1995f6835516cf04fa0
neilhuang <neilhuang@synology.com>
2022-03-04 16:34:41 +0800

    <LeetCode202109> 1490. Clone N-ary Tree

---
+++ 1490_Clone_N-ary_Tree/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+Node* clone(Node* node) {
+	if (!node) return nullptr;
+	vector<Node*> list;
+	for (const auto& iter : node->children) {
+		list.emplace_back(clone(iter));
+	}
+	return new Node(node->val, list);
+}
+
+Node* Solution::cloneTree(Node* root) {
+	return clone(root);
+}
