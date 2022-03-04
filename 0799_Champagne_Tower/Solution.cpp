1310ba0bff713af3f970d8768e4e2a8a6aedae90
neilhuang <neilhuang@synology.com>
2022-03-04 15:21:11 +0800

    <LeetCode202109> 799. Champagne Tower

---
+++ 0799_Champagne_Tower/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+double Solution::champagneTower(int poured, int query_row, int query_glass) {
+	vector<vector<double>> dp(102, vector<double>(102, 0));
+	dp[0][0] = (double) poured;
+	for (int r = 0 ; r <= query_row ; ++r) {
+		for (int c = 0 ; c <= r ; ++c) {
+			double q = (dp[r][c] - 1) / 2;
+			if (q > 0) {
+				dp[r+1][c] += q;
+				dp[r+1][c+1] += q;
+			}
+		}
+	}
+	return dp[query_row][query_glass] > 1 ? 1 : dp[query_row][query_glass];
+}
---
+++ 0799_Champagne_Tower/Solution.h
@@ -0,0 +1,10 @@
+class Solution {
+    public :
+        Solution() {}
+	double champagneTower(int poured, int query_row, int query_glass);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
