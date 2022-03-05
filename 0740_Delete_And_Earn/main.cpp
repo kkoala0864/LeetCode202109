70fc3d9d40ade109b0f0d056a5025f4a883ca76f
neilhuang <neilhuang@synology.com>
2022-03-05 23:53:49 +0800

    <LeetCode202109> 740. Delete and Earn

---
+++ 0740_Delete_And_Earn/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {8,3,4,7,6,6,9,2,5,8,2,4,9,5,9,1,5,7,1,4};
+	Solution* test = new Solution();
+	cout << test->deleteAndEarn(input) << endl;
+	return 0;
+}
+
