141c40fb86f8451ed187f49a4df0f51a9e6eefdc
neilhuang <neilhuang@synology.com>
2022-03-06 14:24:26 +0800

    <LeetCode202109> 1359. Count All Valid Pickup and Delivery Options

---
+++ 1359_Count_All_Valid_Pickup_And_Delivery_Options/Solution.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int Solution::countOrders(int n) {
+	long result = 1;
+	int mod = 1e9 + 7;
+
+	for (int i = 1 ; i <= n ; ++i) {
+		result *= i;
+		result = result * (2 * i - 1);
+		result %= mod;
+	}
+	return result;
+}
