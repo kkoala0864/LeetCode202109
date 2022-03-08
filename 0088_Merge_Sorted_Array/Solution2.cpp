faca9fbe9497378d894232d48e044008a3e6ad59
neilhuang <neilhuang@synology.com>
2022-03-08 16:44:42 +0800

    <LeetCode202109> 88. Merge Sorted Array

---
+++ 0088_Merge_Sorted_Array/Solution2.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+
+void Solution::merge2(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+	int iter = m + n - 1;
+	int i1 = m - 1;
+	int i2 = n - 1;
+	while (i1 >= 0 && i2 >= 0) {
+		if (nums1[i1] > nums2[i2]) {
+			nums1[iter--] = nums1[i1--];
+		} else {
+			nums1[iter--] = nums2[i2--];
+		}
+	}
+	if (i2 >= 0) {
+		while (i2 >= 0) {
+			nums1[iter--] = nums2[i2--];
+		}
+	}
+	return;
+}
--- 0088_Merge_Sorted_Array/main.cpp
+++ 0088_Merge_Sorted_Array/main.cpp
@@ -5,7 +5,14 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-    Solution* test = new Solution();
-    return 0;
+	vector<int> input1 = {1,2,3,0,0,0};
+	vector<int> input2 = {2,5,6};
+	Solution* test = new Solution();
+	test->merge2(input1, 3, input2, 3);
+	for (const auto& iter : input1) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
 }
 
