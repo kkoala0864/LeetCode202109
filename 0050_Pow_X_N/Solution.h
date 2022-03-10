7b7412e2a54947ede41d30f759e822935e54a3df
neilhuang <neilhuang@synology.com>
2022-03-10 14:51:38 +0800

    <LeetCode202109> 50. Pow(x, n)

--- 0050_Pow_X_N/Solution.h
+++ 0050_Pow_X_N/Solution.h
@@ -2,6 +2,7 @@ class Solution {
     public :
         Solution() {}
 	double myPow(double x, int n);
+	double myPow2(double x, int n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0050_Pow_X_N/main.cpp
+++ 0050_Pow_X_N/main.cpp
@@ -7,6 +7,7 @@ using std::endl;
 int main(void) {
     Solution* test = new Solution();
     cout << test->myPow(2.00000, -2) << endl;
+    cout << test->myPow2(2.00000, -2) << endl;
     return 0;
 }
 
