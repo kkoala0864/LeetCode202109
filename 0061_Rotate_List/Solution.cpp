42c56cb56f023700b89719b90856b5a049b4cc8c
neilhuang <neilhuang@synology.com>
2022-03-11 10:10:26 +0800

    <LeetCode202109> 61. Rotate List

---
+++ 0061_Rotate_List/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::rotateRight(ListNode* head, int k) {
+	if (!head) return head;
+	int size = 0;
+	ListNode* iter = head;
+	ListNode* tail = nullptr;
+
+	while (iter) {
+		++size;
+		tail = iter;
+		iter = iter->next;
+	}
+
+	k %= size;
+	if (k == 0 || size == 1) return head;
+	k = (size - k);
+	ListNode* result = head;
+	while (k--) {
+		iter = result;
+		result = result->next;
+	}
+	iter->next = nullptr;
+	tail->next = head;
+	return result;
+}
