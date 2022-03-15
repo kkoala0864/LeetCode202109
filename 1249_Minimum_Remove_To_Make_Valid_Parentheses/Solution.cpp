f21c9dce7cebe98a740df43a0ff0026883ae4b89
neilhuang <neilhuang@synology.com>
2022-03-15 11:41:25 +0800

    <LeetCode202109> 1249. Minimum Remove to Make Valid Parentheses

---
+++ 1249_Minimum_Remove_To_Make_Valid_Parentheses/Solution.cpp
@@ -0,0 +1,39 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+string Solution::minRemoveToMakeValid(string s) {
+	vector<char> st;
+	int cnt = 0;
+
+	while (!s.empty()) {
+		if (s.back() == '(') {
+			if (cnt == 0) {
+				s.pop_back();
+				continue;
+			} else --cnt;
+		} else if (s.back() == ')') {
+			++cnt;
+		}
+		st.emplace_back(s.back());
+		s.pop_back();
+	}
+
+	string result;
+	cnt = 0;
+	while (!st.empty()) {
+		if (st.back() == ')') {
+			if (cnt == 0) {
+				st.pop_back();
+				continue;
+			} else --cnt;
+		} else if (st.back() == '(') {
+			++cnt;
+		}
+		result.push_back(st.back());
+		st.pop_back();
+	}
+	return result;
+}
---
+++ 1249_Minimum_Remove_To_Make_Valid_Parentheses/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input("lee(t(c)o)de)");
+	Solution* test = new Solution();
+	cout << test->minRemoveToMakeValid(input) << endl;
+	return 0;
+}
+
