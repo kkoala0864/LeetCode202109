fc59e1bdf715cd07badee7cc3fbb213518cb86df
neilhuang <neilhuang@synology.com>
2022-03-18 14:26:37 +0800

    <LeetCode202109> 316. Remove Duplicate Letters

---
+++ 0316_Remove_Duplicate_Letters/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input("bcabc");
+	Solution* test = new Solution();
+	cout << test->removeDuplicateLetters(input) << endl;
+	return 0;
+}
+
