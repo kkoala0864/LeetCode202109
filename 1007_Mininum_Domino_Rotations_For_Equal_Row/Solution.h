db9a9818bfc358695f603321fdafda01cb918432
neilhuang <neilhuang@synology.com>
2022-03-20 15:09:58 +0800

    <LeetCode202109> 1007. Minimum Domino Rotations For Equal Row

---
+++ 1007_Mininum_Domino_Rotations_For_Equal_Row/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int minDominoRotations(vector<int>& tops, vector<int>& bottoms);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 1007_Mininum_Domino_Rotations_For_Equal_Row/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> tops = {2,1,2,4,2,2};
+	vector<int> bottoms = {5,2,6,2,3,2};
+	Solution* test = new Solution();
+	cout << test->minDominoRotations(tops, bottoms) << endl;
+	return 0;
+}
+
