580cd6117b99cc23f2b0ddb54f63f33c31ffd33e
neilhuang <neilhuang@synology.com>
2022-03-21 17:59:18 +0800

    <LeetCode202109> 895. Maximum Frequency Stack

---
+++ 0895_Maximum_Frequency_Stack/Solution.h
@@ -0,0 +1,34 @@
+#include <stack>
+#include <unordered_map>
+
+using std::unordered_map;
+using std::stack;
+
+class FreqStack {
+	public :
+		FreqStack() {
+			maxfreq = 0;
+		}
+
+		void push(int val) {
+			++freq[val];
+			if (freq[val] > maxfreq) maxfreq = freq[val];
+			group[freq[val]].emplace(val);
+		}
+
+		int pop() {
+			int x = group[maxfreq].top();
+			group[maxfreq].pop();
+			--freq[x];
+			if (group[maxfreq].size() == 0) --maxfreq;
+			return x;
+		}
+    private :
+		unordered_map<int, int> freq;
+		unordered_map<int, stack<int>> group;
+		int maxfreq;
+		virtual ~FreqStack() {}
+		FreqStack& operator=(const FreqStack& source);
+		FreqStack(const FreqStack&);
+};
+
---
+++ 0895_Maximum_Frequency_Stack/main.cpp
@@ -0,0 +1,11 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    FreqStack* test = new FreqStack();
+    return 0;
+}
+
