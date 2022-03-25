6fedf262f88fec4e4231b89c03b2d14818f61896
neilhuang <neilhuang@synology.com>
2022-03-25 18:25:33 +0800

    <LeetCode202109> 1029. Two City Scheduling

---
+++ 1029_Two_City_Scheduling/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+using std::sort;
+using std::min;
+
+int Solution::twoCitySchedCost(vector<vector<int>>& costs) {
+	auto cmp = [](vector<int>& lhs, vector<int>& rhs) {
+		return (lhs[0] - lhs[1]) < (rhs[0] - rhs[1]);
+	};
+	sort(costs.begin(), costs.end(), cmp);
+	int result = 0;
+	int n = costs.size() / 2;
+	for (int i = 0 ; i < costs.size() ; ++i) {
+		if (i < n) {
+			result += costs[i][0];
+		} else {
+			result += costs[i][1];
+		}
+	}
+	return result;
+}
