5632e8bc9c2fe6583d047db5d4ad195a427d4ae8
neilhuang <neilhuang@synology.com>
2022-03-26 13:30:18 +0800

    <LeetCode202109> 704. Binary Search

---
+++ 0704_Binary_Search/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {-1,0,3,5,9,12};
+	Solution* test = new Solution();
+	cout << test->search(input, 2) << endl;
+	return 0;
+}
+
