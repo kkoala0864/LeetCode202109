e755ac64b3c01fefefa3834e0b53b9cc5e5d6415
neilhuang <neilhuang@synology.com>
2022-03-26 15:06:59 +0800

    <LeetCode202109> 1057. Campus Bikes

--- 1057_Campus_Bikes/Solution.cpp
+++ 1057_Campus_Bikes/Solution.cpp
@@ -1,24 +1,31 @@
 #include <Solution.h>
 #include <iostream>
+#include <algorithm>
+
+using std::min;
+using std::greater;
+using std::pair;
 
 vector<int> Solution::assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
-	vector<vector<vector<int>>> record(2001, vector<vector<int>>());
+	vector<vector<pair<int, int>>> record(2000, vector<pair<int, int>>());
 	vector<int> result(workers.size(), -1);
+	int minDis = 2000;
 	for (int i = 0 ; i < workers.size() ; ++i) {
 		for (int j = 0 ; j < bikes.size() ; ++j) {
 			int distance = abs(workers[i][0] - bikes[j][0]) + abs(workers[i][1] - bikes[j][1]);
 			record[distance].push_back({i, j});
+			minDis = min(minDis, distance);
 		}
 	}
 
 	int cnt = workers.size();
 	vector<bool> bikeFlag(bikes.size(), false);
-	for (int i = 0 ; i < record.size() ; ++i) {
+	for (int i = minDis ; i < record.size() ; ++i) {
 		if (record[i].empty()) continue;
 		for (int j = 0 ; j < record[i].size() ; ++j) {
-			int bikeIdx = record[i][j][1];
+			int bikeIdx = record[i][j].second;
 			if (bikeFlag[bikeIdx]) continue;
-			int workIdx = record[i][j][0];
+			int workIdx = record[i][j].first;
 			if (result[workIdx] == -1) {
 				result[workIdx] = bikeIdx;
 				bikeFlag[bikeIdx] = true;
@@ -29,3 +36,4 @@ vector<int> Solution::assignBikes(vector<vector<int>>& workers, vector<vector<in
 	}
 	return result;
 }
+
