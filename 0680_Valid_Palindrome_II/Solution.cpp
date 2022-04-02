6da76c89fd80357a2c562fd98e0189b31e1f2e9b
neilhuang <neilhuang@synology.com>
2022-04-02 14:42:02 +0800

    <LeetCode202109> 680. Valid Palindrome II

---
+++ 0680_Valid_Palindrome_II/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+
+bool check(const string& s, int i, int j) {
+	while (i <= j) {
+		if (s[i++] != s[j--]) return false;
+	}
+	return true;
+}
+
+bool Solution::validPalindrome(string s) {
+	int start = 0, end = s.size() - 1;
+	while (start <= end) {
+		if (s[start] != s[end]) {
+			return check(s, start, end - 1) || check(s, start + 1, end);
+		}
+		++start;
+		--end;
+	}
+	return true;
+}
