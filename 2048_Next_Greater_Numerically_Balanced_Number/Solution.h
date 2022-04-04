67111e74bb40e9c3216fa7d6b91fe93ff6515aae
neilhuang <neilhuang@synology.com>
2022-04-04 15:05:17 +0800

    <LeetCode202109> 2048. Next Greater Numerically Balanced Number

--- 2048_Next_Greater_Numerically_Balanced_Number/Solution.h
+++ 2048_Next_Greater_Numerically_Balanced_Number/Solution.h
@@ -2,6 +2,7 @@ class Solution {
     public :
         Solution() {}
 	int nextBeautifulNumber(int n);
+	int nextBeautifulNumber2(int n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 2048_Next_Greater_Numerically_Balanced_Number/main.cpp
+++ 2048_Next_Greater_Numerically_Balanced_Number/main.cpp
@@ -7,6 +7,7 @@ using std::endl;
 int main(void) {
     Solution* test = new Solution();
     cout << test->nextBeautifulNumber(3000) << endl;
+    cout << test->nextBeautifulNumber2(3000) << endl;
     return 0;
 }
 
