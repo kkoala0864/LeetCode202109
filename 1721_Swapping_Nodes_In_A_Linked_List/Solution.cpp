8c2a043975c6c6c796d55cf5079a74858bbf970a
neilhuang <neilhuang@synology.com>
2022-04-04 16:47:35 +0800

    <LeetCode202109> 1721. Swapping Nodes in a Linked List

---
+++ 1721_Swapping_Nodes_In_A_Linked_List/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::swap;
+
+ListNode* Solution::swapNodes(ListNode* head, int k) {
+	ListNode* one = head;
+	while (--k) {
+		one = one->next;
+	}
+	ListNode* swapOne = one;
+	ListNode* two = head;
+	while (one->next) {
+		one = one->next;
+		two = two->next;
+	}
+	swap(two->val, swapOne->val);
+	return head;
+}
