a1dbf95b82095cdaf9584cd15054631eb0cda7c8
neilhuang <neilhuang@synology.com>
2022-04-07 17:33:42 +0800

    <LeetCode202109> 1046. Last Stone Weight

---
+++ 1046_Last_Stone_Weight/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::priority_queue;
+
+int Solution::lastStoneWeight(vector<int>& stones) {
+	priority_queue<int> maxHeap;
+	for (const auto& iter : stones) maxHeap.emplace(iter);
+
+	while (maxHeap.size() > 1) {
+		int first = maxHeap.top();
+		maxHeap.pop();
+		int second = maxHeap.top();
+		maxHeap.pop();
+		int next = abs(first - second);
+		if (next > 0) maxHeap.emplace(next);
+	}
+	return maxHeap.empty() ? 0 : maxHeap.top();
+}
---
+++ 1046_Last_Stone_Weight/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {2,7,4,1,8,1};
+	Solution* test = new Solution();
+	cout << test->lastStoneWeight(input) << endl;
+	return 0;
+}
+
