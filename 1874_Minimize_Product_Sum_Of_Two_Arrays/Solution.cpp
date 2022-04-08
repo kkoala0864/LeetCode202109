814ece0a74c9c30f77e2bf9b4703d538206eb3b8
neilhuang <neilhuang@synology.com>
2022-04-08 16:33:24 +0800

    <LeetCode202109> 1874. Minimize Product Sum of Two Arrays

---
+++ 1874_Minimize_Product_Sum_Of_Two_Arrays/Solution.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+int Solution::minProductSum(vector<int>& nums1, vector<int>& nums2) {
+	sort(nums1.begin(), nums1.end());
+	sort(nums2.begin(), nums2.end(), std::greater<int>());
+
+	int result = 0;
+	for (int i = 0 ; i < nums1.size() ; ++i) {
+		result += nums1[i] * nums2[i];
+	}
+	return result;
+}
