9a0985ec58065c971fc714c89cf422cf086cc9c9
neilhuang <neilhuang@synology.com>
2022-04-12 14:40:54 +0800

    <LeetCode202109> 289. Game of Life

---
+++ 0289_Game_Of_Life/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	void gameOfLife(vector<vector<int>>& board);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 0289_Game_Of_Life/main.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,1,0},{0,0,1},{1,1,1},{0,0,0}};
+	Solution* test = new Solution();
+	test->gameOfLife(input);
+	for (const auto& iter : input) {
+		for (const auto& it : iter) {
+			cout << it << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
