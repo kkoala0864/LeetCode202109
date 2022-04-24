67d972c12d0641452b265dbed12ad87e5b764882
neilhuang <neilhuang@synology.com>
2022-04-24 14:04:13 +0800

    <LeetCode202109> 1396. Design Underground System

---
+++ 1396_Design_Underground_System/Solution.h
@@ -0,0 +1,37 @@
+#include <string>
+#include <unordered_map>
+
+using std::string;
+using std::pair;
+using std::unordered_map;
+
+class UndergroundSystem {
+    public :
+	UndergroundSystem() {}
+
+	void checkIn(int id, string stationName, int t) {
+		_inRecord[id] = pair<string, int>(stationName, t);
+	}
+
+	void checkOut(int id, string stationName, int t) {
+		int timeDiff = t - _inRecord[id].second;
+		if (_timeSlice.find(_inRecord[id].first) == _timeSlice.end()
+		 || _timeSlice[_inRecord[id].first].find(stationName) == _timeSlice[_inRecord[id].first].end()) {
+			_timeSlice[_inRecord[id].first][stationName] = pair<int, int>(timeDiff, 1);
+		} else {
+			_timeSlice[_inRecord[id].first][stationName].first += timeDiff;
+			++_timeSlice[_inRecord[id].first][stationName].second;
+		}
+	}
+
+	double getAverageTime(string startStation, string endStation) {
+		return _timeSlice[startStation][endStation].first / _timeSlice[startStation][endStation].second;
+	}
+    private :
+	unordered_map<int, pair<string, int>> _inRecord;
+	unordered_map<string, unordered_map<string, pair<double, double>>> _timeSlice;
+        virtual ~UndergroundSystem() {}
+        UndergroundSystem& operator=(const UndergroundSystem& source);
+        UndergroundSystem(const UndergroundSystem&);
+};
+
---
+++ 1396_Design_Underground_System/main.cpp
@@ -0,0 +1,11 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    UndergroundSystem* test = new UndergroundSystem();
+    return 0;
+}
+
