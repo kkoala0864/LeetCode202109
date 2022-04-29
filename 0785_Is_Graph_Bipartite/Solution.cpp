22b9d9be5338abae18e0abf38cb518fedf02ae5f
neilhuang <neilhuang@synology.com>
2022-04-29 17:16:14 +0800

    <LeetCode202109> 785. Is Graph Bipartite?

---
+++ 0785_Is_Graph_Bipartite/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+bool dfs(vector<vector<int>>& graph, vector<int>& group, int node, int idx) {
+	if (group[node] != 0) return group[node] == idx;
+	if (group[node] == 0) group[node] = idx;
+	for (auto& iter : graph[node]) {
+		if (!dfs(graph, group, iter, idx == 1 ? 2 : 1)) return false;
+	}
+	return true;
+}
+
+bool Solution::isBipartite(vector<vector<int>>& graph) {
+	vector<int> group(graph.size(), 0);
+	for (int i = 0 ; i < graph.size() ; ++i) {
+		if (graph[i].empty()) continue;
+		if (!dfs(graph, group, i, group[i] == 0 ? 1 : group[i])) return false;
+	}
+	return true;
+}
---
+++ 0785_Is_Graph_Bipartite/Solution.h
@@ -0,0 +1,15 @@
+#include <vector>
+
+using std::vector;
+
+
+class Solution {
+    public :
+        Solution() {}
+	bool isBipartite(vector<vector<int>>& graph);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
