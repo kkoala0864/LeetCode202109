e0fdb8c6d6827f0c14c1f8f5f2b703073501cb3c
neilhuang <neilhuang@synology.com>
2022-05-02 13:01:32 +0800

    <LeetCode202109> 905. Sort Array By Parity

---
+++ 0905_Sort_Array_By_Parity/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,1,2,4};
+	Solution* test = new Solution();
+	auto result = test->sortArrayByParity(input);
+	for (const auto& iter : result) {
+		cout << iter << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
