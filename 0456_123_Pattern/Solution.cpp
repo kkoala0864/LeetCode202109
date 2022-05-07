23586b2f7e004bbf6cb2de30e35b33f59223cd07
neilhuang <neilhuang@synology.com>
2022-05-07 20:21:35 +0800

    <LeetCode202109> 456. 132 Pattern

---
+++ 0456_123_Pattern/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::min;
+
+bool Solution::find132pattern(vector<int>& nums) {
+	if (nums.size() < 3) return false;
+	stack<int> stk;
+	vector<int> min_array(nums.size());
+	min_array[0] = nums[0];
+
+	for (size_t i = 1; i < nums.size(); i++) {
+		min_array[i] = min(min_array[i - 1], nums[i]);
+	}
+
+	for (size_t j = nums.size() - 1; j > 0; j--) {
+		if (nums[j] <= min_array[j]) {
+			continue;
+		}
+		while (!stk.empty() && stk.top() <= min_array[j]) {
+			stk.pop();
+		}
+		if (!stk.empty() && stk.top() < nums[j]) {
+			return true;
+		}
+		stk.push(nums[j]);
+	}
+	return false;
+}
