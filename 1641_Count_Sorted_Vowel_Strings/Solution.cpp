84ad1a64b39e59fc4fe0c46e52e1c6a5f43bfe49
neilhuang <neilhuang@synology.com>
2022-05-11 15:18:22 +0800

    <LeetCode202109> 1641. Count Sorted Vowel Strings

---
+++ 1641_Count_Sorted_Vowel_Strings/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int Solution::countVowelStrings(int n) {
+	vector<int> dp(5, 0);
+	for (int i = 0 ; i < n ; ++i) {
+		for (int j = 4 ; j >= 0 ; --j) {
+			if (i == 0) dp[j] = j+1;
+			else {
+				int val = 0;
+				for (int k = j - 1; k >= 0 ; --k) {
+					dp[j] += dp[k];
+				}
+			}
+		}
+	}
+	return dp[4];
+}
