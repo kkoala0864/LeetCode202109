40b44dc978443deb8949e519918d8339c0308ba9
neilhuang <neilhuang@synology.com>
2022-05-11 17:14:58 +0800

    <LeetCode202109> 489. Robot Room Cleaner

---
+++ 0489_Robot_Room_Cleaner/Solution.cpp
@@ -0,0 +1,39 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+
+struct hashFunc {
+	int operator() (const pair<int, int>& x) const {
+		return x.first ^ x.second;
+	}
+};
+
+vector<pair<int, int>> axis = {{-1,0}, {0, 1}, {1, 0}, {0, -1}};
+
+void goback(Robot& robot) {
+	robot.turnLeft();
+	robot.turnLeft();
+	robot.move();
+	robot.turnLeft();
+	robot.turnLeft();
+}
+
+void dfs(Robot& robot, int x, int y, int d, unordered_set<pair<int, int>, hashFunc>& coordinate) {
+	coordinate.insert({x, y});
+	robot.clean();
+	for (int k = 0 ; k < 4 ; ++k) {
+		int newd = (d + k) % 4;
+		int i = x + axis[newd].first;
+		int j = y + axis[newd].second;
+		if (coordinate.count({i, j}) == 0 && robot.move()) {
+			dfs(robot, i, j, newd, coordinate);
+			goback(robot);
+		}
+		robot.turnRight();
+	}
+}
+
+void Solution::cleanRoom(Robot& robot) {
+	unordered_set<pair<int, int>, hashFunc> coordinate;
+	dfs(robot, 0, 0, 0, coordinate);
+}
---
+++ 0489_Robot_Room_Cleaner/Solution.h
@@ -0,0 +1,29 @@
+/*
+ * // This is the robot's control interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Robot {
+ *   public:
+ *     // Returns true if the cell in front is open and robot moves into the cell.
+ *     // Returns false if the cell in front is blocked and robot stays in the current cell.
+ *     bool move();
+ *
+ *     // Robot will stay in the same cell after calling turnLeft/turnRight.
+ *     // Each turn will be 90 degrees.
+ *     void turnLeft();
+ *     void turnRight();
+ *
+ *     // Clean the current cell.
+ *     void clean();
+ * };
+ */
+
+class Solution {
+    public :
+        Solution() {}
+	void cleanRoom(Robot& robot);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
