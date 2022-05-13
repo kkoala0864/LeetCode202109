4100d612c44c8d9cd7ccb60f4cb83b3e0d331e89
neilhuang <neilhuang@synology.com>
2022-05-13 17:32:11 +0800

    <LeetCode202109> 221. Maximal Square

--- 0221_Maximal_Square/Solution.h
+++ 0221_Maximal_Square/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int maximalSquare(vector<vector<char>>& matrix);
+	int maximalSquare2(vector<vector<char>>& matrix);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0221_Maximal_Square/main.cpp
+++ 0221_Maximal_Square/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
 	vector<vector<char>> input = {{'1','0','1','0'},{'1','0','1','1'},{'1','0','1','1'},{'1','1','1','1'}};
 	Solution* test = new Solution();
 	cout << test->maximalSquare(input) << endl;
+	cout << test->maximalSquare2(input) << endl;
 	return 0;
 }
 
