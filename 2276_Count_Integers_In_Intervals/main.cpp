d3fad8f98e2f6966c4207016f1e38ac9c017cefa
neilhuang <neilhuang@synology.com>
2022-05-16 21:49:36 +0800

    <LeetCode202109> 2276. Count Integers in Intervals

---
+++ 2276_Count_Integers_In_Intervals/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    CountIntervals* test = new CountIntervals();
+    test->add(2,3);
+    cout << test->count() << endl;
+    return 0;
+}
+
