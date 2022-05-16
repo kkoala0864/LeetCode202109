058c0bf335ba64994f877508f7088c5a59720cdc
neilhuang <neilhuang@synology.com>
2022-05-16 19:36:09 +0800

    <LeetCode202109> 1091. Shortest Path in Binary Matrix

---
+++ 1091_Shortest_Path_In_Binary_Matrix/Solution.cpp
@@ -0,0 +1,37 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::queue;
+using std::pair;
+
+int Solution::shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+	int n = grid.size();
+	int result = 1;
+	vector<pair<int, int>> dir = {{0, -1}, {1,-1}, {1,0}, {1,1}, {0,1}, {-1,1}, {-1, 0}, {-1,-1}};
+
+	queue<pair<int, int>> que, next;
+	que.push({0,0});
+	while (!que.empty()) {
+		auto cur = que.front();
+		que.pop();
+
+		if (cur.first == n-1 && cur.second == n-1) return result;
+		if (grid[cur.first][cur.second] == 0) {
+			grid[cur.first][cur.second] = 1;
+			for (const auto& d : dir) {
+				int x = cur.first + d.first;
+				int y = cur.second + d.second;
+				if (x >= 0 && y >= 0 && x < n && y < n && grid[x][y] == 0) {
+					next.push({x, y});
+				}
+			}
+		}
+
+		if (que.empty()) {
+			++result;
+			que = move(next);
+		}
+	}
+	return -1;
+}
