b46f0132c5f9b0e5479fb1a5e4036d2e19715af0
neilhuang <neilhuang@synology.com>
2022-05-17 16:45:15 +0800

    <LeetCode202109> 694. Number of Distinct Islands

---
+++ 0694_Number_Of_Distinct_Islands/Solution.h
@@ -0,0 +1,23 @@
+#include <vector>
+
+using std::vector;
+
+struct Trie{
+	vector<Trie*> child;
+	bool End;
+	Trie() : End(false) {
+		child = vector<Trie*>(5, nullptr);
+	}
+};
+
+class Solution {
+    public :
+        Solution() {}
+	int numDistinctIslands(vector<vector<int>>& grid);
+	int numDistinctIslandsTrie(vector<vector<int>>& grid);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
