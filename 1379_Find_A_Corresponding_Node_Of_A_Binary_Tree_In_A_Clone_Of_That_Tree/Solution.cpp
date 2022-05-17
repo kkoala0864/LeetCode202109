fd7c4dfa4376b22782c13a491b46afe4374c2dce
neilhuang <neilhuang@synology.com>
2022-05-17 15:41:49 +0800

    <LeetCode202109> 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ 1379_Find_A_Corresponding_Node_Of_A_Binary_Tree_In_A_Clone_Of_That_Tree/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::queue;
+using std::pair;
+
+TreeNode* Solution::getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+	queue<pair<TreeNode*, TreeNode*>> que;
+
+	que.push({original, cloned});
+
+	while (!que.empty()) {
+		auto cur = que.front();
+		que.pop();
+		if (cur.first == target) return cur.second;
+
+		if (cur.first->left) que.push({cur.first->left, cur.second->left});
+		if (cur.first->right) que.push({cur.first->right, cur.second->right});
+	}
+	return nullptr;
+}
