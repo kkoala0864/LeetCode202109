d6362af8ca2db10e7a8cf7256994e49025f8eb33
neilhuang <neilhuang@synology.com>
2022-05-19 14:33:05 +0800

    <LeetCode202109> 1192. Critical Connections in a Network

---
+++ 1192_Critical_Connections_In_A_Network/Solution.h
@@ -0,0 +1,15 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections);
+	vector<vector<int>> Tarjan(int n, vector<vector<int>>& connections);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
