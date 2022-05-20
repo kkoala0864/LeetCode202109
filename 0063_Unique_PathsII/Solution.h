26e6af2018c21e97fa82bc15f4ea1788b3fe453c
neilhuang <neilhuang@synology.com>
2022-05-20 14:12:29 +0800

    <LeetCode202109> 63. Unique Paths II

--- 0063_Unique_PathsII/Solution.h
+++ 0063_Unique_PathsII/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid);
+	int uniquePathsWithObstacles2(vector<vector<int>>& obstacleGrid);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0063_Unique_PathsII/main.cpp
+++ 0063_Unique_PathsII/main.cpp
@@ -5,9 +5,10 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<vector<int>> input = {{0,1},{0,0}};
+	vector<vector<int>> input = {{0,0,0},{0,1,0},{0,0,0}};
 	Solution* test = new Solution();
 	cout << test->uniquePathsWithObstacles(input) << endl;
+	cout << test->uniquePathsWithObstacles2(input) << endl;
 	return 0;
 }
 
