71e2daf22d07a4402e37b041f02a18dd25360289
neilhuang <neilhuang@synology.com>
2022-05-26 22:44:40 +0800

    <LeetCode202109> 345. Reverse Vowels of a Strin

---
+++ 0345_Reverse_Vowels_Of_A_String/Solution.cpp
@@ -0,0 +1,46 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::swap;
+bool isVowels(char c) {
+	switch (c) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+			return true;
+		default:
+			return false;
+	}
+}
+
+string Solution::reverseVowels(string s) {
+	int l = 0, r = s.size() - 1;
+	while (l < r) {
+		if (isVowels(s[l])) break;
+		++l;
+	}
+	while (r > l) {
+		if (isVowels(s[r])) break;
+		--r;
+	}
+
+	while (l < r) {
+		swap(s[l++], s[r--]);
+		while (l < r) {
+			if (isVowels(s[l])) break;
+			++l;
+		}
+		while (r > l) {
+			if (isVowels(s[r])) break;
+			--r;
+		}
+	}
+	return s;
+}
---
+++ 0345_Reverse_Vowels_Of_A_String/Solution.h
@@ -0,0 +1,14 @@
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	string reverseVowels(string s);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
