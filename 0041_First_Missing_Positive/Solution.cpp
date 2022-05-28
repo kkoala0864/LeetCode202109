bfc3fdc227f6e9cedfe8c30a7f3e34a3fe281829
neilhuang <neilhuang@synology.com>
2022-05-28 15:01:33 +0800

    <LeetCode202109> 41. First Missing Positive

---
+++ 0041_First_Missing_Positive/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::firstMissingPositive(vector<int>& nums) {
+	int n = nums.size();
+	for (auto& val : nums) {
+		if (val == 0 || val < 0) val = n + 1;
+	}
+
+	for (int i = 0 ; i < n ; ++i) {
+		int idx = abs(nums[i]);
+		if (idx < (n + 1)) {
+			nums[idx-1] = nums[idx-1] > 0 ? -nums[idx-1] : nums[idx-1];
+		}
+	}
+
+	for (int i = 0 ; i < n ; ++i) {
+		if (nums[i] > 0) {
+			return i + 1;
+		}
+	}
+	return n + 1;
+}
---
+++ 0041_First_Missing_Positive/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {7,8,9,11,12};
+	Solution* test = new Solution();
+	cout << test->firstMissingPositive(input) << endl;
+	return 0;
+}
+
