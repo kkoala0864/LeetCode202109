52b18f45c28d7bccfec32c71b24c38a5f68337ee
neilhuang <neilhuang@synology.com>
2022-05-29 21:24:24 +0800

    <LeetCode202109> 2289. Steps to Make Array Non-decreasing

---
+++ 2289_Steps_To_Make_Array_Non-Decreasing/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::totalSteps(vector<int>& nums) {
+	vector<int> dp(nums.size(), 0), st;
+	int result = 0;
+
+	for (int i = nums.size() - 1 ; i >= 0 ; --i) {
+		while (!st.empty() && nums[st.back()] < nums[i]) {
+			dp[i] = max(dp[i] + 1, dp[st.back()]);
+			st.pop_back();
+		}
+		result = max(dp[i], result);
+		st.emplace_back(i);
+	}
+	return result;
+}
