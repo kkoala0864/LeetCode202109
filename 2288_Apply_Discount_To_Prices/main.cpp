a2decb16e0bf008404ac4001f8fd5b28e6396f1a
neilhuang <neilhuang@synology.com>
2022-05-29 20:16:38 +0800

    <LeetCode202109> 2288. Apply Discount to Prices

---
+++ 2288_Apply_Discount_To_Prices/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "1 2 $3 4 $5 $6 7 8$ $9 $10$";
+	Solution* test = new Solution();
+	cout << test->discountPrices(input, 100) << endl;
+	return 0;
+}
+
