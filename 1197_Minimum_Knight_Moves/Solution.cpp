895eab5da881c9fba664fcbaf40e50074a577add
neilhuang <neilhuang@synology.com>
2022-06-01 23:42:29 +0800

    <LeetCode202109> 1197. Minimum Knight Moves

---
+++ 1197_Minimum_Knight_Moves/Solution.cpp
@@ -0,0 +1,40 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using std::queue;
+using std::pair;
+using std::vector;
+
+int Solution::minKnightMoves(int x, int y) {
+	if (x == 0 && y == 0) return 0;
+	vector<pair<int, int>> dirs = {{1, 2}, {2, 1}, {-1, 2}, {2, -1}, {-2, 1}, {1, -2}, {-1, -2}, {-2, -1}};
+	vector<vector<bool>> visited(601, vector<bool>(601, false));
+	queue<pair<int, int>> que, next;
+
+	que.push({0, 0});
+	visited[300][300] = true;
+
+	int result = 1;
+	while (!que.empty()) {
+		pair<int, int> cur = que.front();
+		que.pop();
+
+		for (const auto& dir : dirs) {
+			int i = cur.first + dir.first;
+			int j = cur.second + dir.second;
+			if (visited[i + 300][j + 300]) continue;
+
+			if (i == x && j == y) return result;
+			next.push({i,j});
+			visited[i+300][j+300] = true;
+		}
+
+		if (que.empty()) {
+			++result;
+			que = move(next);
+		}
+	}
+	return -1;
+}
---
+++ 1197_Minimum_Knight_Moves/Solution.h
@@ -0,0 +1,10 @@
+class Solution {
+    public :
+        Solution() {}
+	int minKnightMoves(int x, int y);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
