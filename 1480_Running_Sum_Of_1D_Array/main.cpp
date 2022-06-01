99586bb1793b940c337d5d9c9e070e0cb6eca6e8
neilhuang <neilhuang@synology.com>
2022-06-01 14:43:36 +0800

    <LeetCode202109> 1480. Running Sum of 1d Array

---
+++ 1480_Running_Sum_Of_1D_Array/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,3,4};
+	Solution* test = new Solution();
+	auto result = test->runningSum(input);
+	for (const auto& val : result) {
+		cout << val << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
