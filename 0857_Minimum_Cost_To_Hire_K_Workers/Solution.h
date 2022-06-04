974ab3a4590e8330841fc41d2e12a538f049ca8c
neilhuang <neilhuang@synology.com>
2022-06-04 17:15:36 +0800

    <LeetCode202109> 857. Minimum Cost to Hire K Workers

---
+++ 0857_Minimum_Cost_To_Hire_K_Workers/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 0857_Minimum_Cost_To_Hire_K_Workers/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> quality = {25,68,35,62,52,57,35,83,40,51};
+	vector<int> wage = {147,97,251,129,438,443,120,366,362,343};
+	Solution* test = new Solution();
+	cout << test->mincostToHireWorkers(quality, wage, 6) << endl;
+	return 0;
+}
+
