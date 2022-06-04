fdc87c94e5ca71f2ae8568dffa1a6b2ce498674a
neilhuang <neilhuang@synology.com>
2022-06-04 14:18:26 +0800

    <LeetCode202109> 51. N-Queens

---
+++ 0051_N_Queens/Solution.cpp
@@ -0,0 +1,42 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+void outputResult(vector<int>& list, vector<vector<string>>& result) {
+	vector<string> local;
+	for (int row = 0 ; row < list.size() ; ++row) {
+		int colQ = list[row];
+		string tmp;
+		for (int col = 0 ; col < list.size() ; ++col) {
+			if (col == colQ) tmp.push_back('Q');
+			else tmp.push_back('.');
+		}
+		local.emplace_back(tmp);
+	}
+	result.emplace_back(local);
+}
+
+void dfs(int row, int n, vector<int>& list, vector<vector<string>>& result) {
+	for (int col = 0 ; col < n ; ++col) {
+		int lrow = 0;
+		for (; lrow < list.size() ; ++lrow) {
+			if (list[lrow] == col || abs(row - lrow) == abs(col - list[lrow])) {
+				break;
+			}
+		}
+		if (lrow != row) continue;
+		list.emplace_back(col);
+		if (row == n-1) outputResult(list, result);
+		else dfs(row + 1, n, list, result);
+		list.pop_back();
+	}
+}
+
+vector<vector<string>> Solution::solveNQueens(int n) {
+	vector<vector<string>> result;
+	vector<int> list;
+	dfs(0, n, list, result);
+	return result;
+}
---
+++ 0051_N_Queens/main.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	Solution* test = new Solution();
+	auto result = test->solveNQueens(4);
+	for (const auto& vec : result) {
+		for (const auto& val : vec) {
+			cout << val << endl;
+		}
+		cout << endl << endl;
+	}
+	return 0;
+}
+
