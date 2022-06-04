f7d9c3470cdbf4d4199e23338340f9a4cf66a3ac
neilhuang <neilhuang@synology.com>
2022-06-04 16:06:07 +0800

    <LeetCode202109> 358. Rearrange String k Distance Apart

---
+++ 0358_Rearrange_String_K_Distance_Apart/Solution.cpp
@@ -0,0 +1,34 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+using std::vector;
+
+string Solution::rearrangeString(string s, int k) {
+	vector<int> lastIdx(26, -400000), cnt(26, 0);
+
+	for (const auto& c : s) ++cnt[c-'a'];
+	string result;
+	while (result.size() < s.size()) {
+		bool find = false;
+		char c;
+		for (int i = 0 ; i < 26 ; ++i) {
+			if (cnt[i] == 0) continue;
+			int diff = result.size() - lastIdx[i];
+			if (find) {
+				if (diff >= k && (cnt[i] > cnt[c-'a'])) {
+					c = i + 'a';
+				}
+			} else {
+				if (diff >= k) {
+					c = i + 'a';
+					find = true;
+				}
+			}
+		}
+		if (!find) return "";
+		result.push_back(c);
+		lastIdx[c-'a'] = result.size() - 1;
+		--cnt[c-'a'];
+	}
+	return result;
+}
---
+++ 0358_Rearrange_String_K_Distance_Apart/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "aaadbbcc";
+	Solution* test = new Solution();
+	cout << test->rearrangeString(input, 2) << endl;
+	return 0;
+}
+
