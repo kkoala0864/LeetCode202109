156baf5060dbc949198b07d87fec15512dff08ae
neilhuang <neilhuang@synology.com>
2022-06-13 23:43:35 +0800

    <LeetCode202109> 37. Sudoku Solver

---
+++ 0037_Sudoku_Solver/Solution.cpp
@@ -0,0 +1,47 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::pair;
+
+bool dfs(vector<pair<int, int>>& e, vector<vector<char>>& board, int idx,
+		vector<vector<bool>>& x, vector<vector<bool>>& y, vector<vector<bool>>& b) {
+	if (idx == e.size()) return true;
+
+	auto cur = e[idx];
+	int bIdx = (cur.first / 3) * 3 + (cur.second / 3);
+	for (int i = 0 ; i < 9 ; ++i) {
+		if (x[cur.first][i] || y[cur.second][i] || b[bIdx][i]) continue;
+		x[cur.first][i] = true;
+		y[cur.second][i] = true;
+		b[bIdx][i] = true;
+		board[cur.first][cur.second] = '1' + i;
+		if (dfs(e, board, idx + 1, x, y, b)) return true;
+		x[cur.first][i] = false;
+		y[cur.second][i] = false;
+		b[bIdx][i] = false;
+	}
+	return false;
+}
+
+void Solution::solveSudoku(vector<vector<char>>& board) {
+	vector<vector<bool>> xMap(9, vector<bool>(9, false));
+	vector<vector<bool>> yMap(9, vector<bool>(9, false));
+	vector<vector<bool>> bMap(9, vector<bool>(9, false));
+	vector<pair<int, int>> e;
+
+	int xSize = board.size();
+	int ySize = board[0].size();
+	for (int i = 0 ; i < xSize ; ++i) {
+		for (int j = 0 ; j < ySize ; ++j) {
+			if (board[i][j] == '.') {
+				e.push_back({i, j});
+				continue;
+			}
+			xMap[i][board[i][j]-'1'] = true;
+			yMap[j][board[i][j]-'1'] = true;
+			bMap[(i / 3 * 3) + j / 3][board[i][j]-'1'] = true;
+		}
+	}
+
+	dfs(e, board, 0, xMap, yMap, bMap);
+}
---
+++ 0037_Sudoku_Solver/main.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<char>> input = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
+	Solution* test = new Solution();
+	test->solveSudoku(input);
+	for (const auto& vec : input) {
+		for (const auto& val : vec) {
+			cout << val << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
+
