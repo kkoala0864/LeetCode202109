f7972240527a49db1bf80c6146dda1a1bda77a14
neilhuang <neilhuang@synology.com>
2022-06-19 21:23:49 +0800

    <LeetCode202109> 2311. Longest Binary Subsequence Less Than or Equal to K

---
+++ 2311_Longest_Binary_Subsequence_Less_Than_Or_Equal_To_K/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using std::max;
+using std::vector;
+
+int Solution::longestSubsequence(string s, int k) {
+	int sum = 0, pow = 1, cnt = 0;
+	int i = s.size() - 1;
+	for (; i >= 0 && (sum + pow) <= k ; --i) {
+		if (s[i] == '1') {
+			sum += pow;
+		}
+		++cnt;
+		pow <<= 1;
+	}
+
+	for (; i >= 0 ; --i) if (s[i] == '0') ++cnt;
+	return cnt;
+}
