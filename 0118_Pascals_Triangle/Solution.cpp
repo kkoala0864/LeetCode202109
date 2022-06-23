e3be700533f8a90fbe65c142e6fd13a18c1f1102
neilhuang <neilhuang@synology.com>
2022-06-23 20:25:38 +0800

    <LeetCode202109> 118. Pascal's Triangle

---
+++ 0118_Pascals_Triangle/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<vector<int>> Solution::generate(int numRows) {
+	vector<vector<int>> result = {{1}};
+	vector<int> local;
+	for (int i = 1 ; i < numRows ; ++i) {
+		local.clear();
+		for (int j = 0 ; j < (i+1) ; ++j) {
+			if (j == 0 || j == i) {
+				local.emplace_back(1);
+			} else {
+				local.emplace_back(result[i-1][j-1] + result[i-1][j]);
+			}
+		}
+		result.emplace_back(local);
+	}
+	return result;
+}
---
+++ 0118_Pascals_Triangle/main.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    auto result = test->generate(5);
+    for (const auto& vec : result) {
+	for (const auto& val : vec) {
+		cout << val << " ";
+	}
+	cout << endl;
+    }
+    return 0;
+}
+
