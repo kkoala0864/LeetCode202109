ececc9b361b7849337586543efbd5fedd3caadba
neilhuang <neilhuang@synology.com>
2022-06-25 16:39:36 +0800

    <LeetCode202109> 665. Non-decreasing Array

---
+++ 0665_Non_Decreasing_Array/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+bool Solution::checkPossibility(vector<int>& nums) {
+	vector<int> v;
+
+	v.emplace_back(nums[0]);
+
+	int cnt = 0;
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		auto it = upper_bound(v.begin(), v.end(), nums[i]);
+		if (it == v.end()) v.emplace_back(nums[i]);
+		else {
+			*it = nums[i];
+			++cnt;
+			if (cnt > 1) return false;
+		}
+	}
+	return true;
+}
