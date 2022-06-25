960756df75addb2b577f30a2dbbe25e8fa438784
neilhuang <neilhuang@synology.com>
2022-06-25 15:40:36 +0800

    <LeetCode202109> 1354. Construct Target Array With Multiple Sums

---
+++ 1354_Construct_Target_Array_With_Multiple_Sums/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::priority_queue;
+
+bool Solution::isPossible(vector<int>& target) {
+	if (target.size() == 1) return target[0] == 1;
+	priority_queue<int> pq;
+
+	long long sum = 0;
+	for (const auto& val : target) {
+		pq.emplace(val);
+		sum += val;
+	}
+
+	while (sum > 1 && pq.top() > sum/2) {
+		int largest = pq.top(); pq.pop();
+		int rest = sum - largest;
+		if (rest <= 1) return rest;
+		int prevLarge = largest % rest;
+		pq.emplace(prevLarge);
+		sum = prevLarge + rest;
+	}
+
+	return sum == target.size();
+}
---
+++ 1354_Construct_Target_Array_With_Multiple_Sums/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {9,3,5};
+	Solution* test = new Solution();
+	cout << test->isPossible(input) << endl;
+	return 0;
+}
+
