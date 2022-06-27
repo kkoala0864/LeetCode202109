cb258e028c5201ce60b6d491da68d6b7670e1c7d
neilhuang <neilhuang@synology.com>
2022-06-27 14:19:03 +0800

    <LeetCode202109> 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers

---
+++ 1689_Partitioning_Into_Minimum_Number_Of_Deci-Binary_Numbers/Solution.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::minPartitions(string n) {
+	int result = 0;
+	for (int i = 0 ; i < n.size() ; ++i) {
+		result = max(result, n[i] - '0');
+	}
+	return result == 0 ? 1 : result;
+}
---
+++ 1689_Partitioning_Into_Minimum_Number_Of_Deci-Binary_Numbers/main.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    cout << test->minPartitions("32") << endl;
+    return 0;
+}
+
