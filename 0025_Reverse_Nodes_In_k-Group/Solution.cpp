46d0008be3212d0b74bfeba355ca77e1c0b2ea61
neilhuang <neilhuang@synology.com>
2022-07-07 20:08:13 +0800

    <LeetCode202109> 25. Reverse Nodes in k-Group

---
+++ 0025_Reverse_Nodes_In_k-Group/Solution.cpp
@@ -0,0 +1,31 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::reverseKGroup(ListNode* head, int k) {
+	ListNode *iter = head, *prev = nullptr, *next = nullptr, *gtail = head, *ghead = nullptr;
+
+	ListNode* dummy = new ListNode();
+	dummy->next = head;
+	ListNode* lastTail = dummy;
+	int cnt = k;
+
+	while (gtail) {
+		gtail = gtail->next;
+		--cnt;
+		if (cnt == 0) {
+			ghead = iter;
+			cnt = k;
+
+			while (iter != gtail) {
+				next = iter->next;
+				iter->next = prev;
+				prev = iter;
+				iter = next;
+			}
+			ghead->next = gtail;
+			lastTail->next = prev;
+			lastTail = ghead;
+		}
+	}
+	return dummy->next;
+}
