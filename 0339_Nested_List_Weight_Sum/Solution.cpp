b8c6a02d4845e83dd2d578a3408e78c6297942a1
neilhuang <neilhuang@synology.com>
2022-07-19 20:09:33 +0800

    <LeetCode202109> 339. Nested List Weight Sum

---
+++ 0339_Nested_List_Weight_Sum/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+void dfs(const vector<NestedInteger>& list, int depth, int& result) {
+	for (auto& n : list) {
+		if (n.isInteger()) {
+			result += (depth * n.getInteger());
+		} else {
+			dfs(n.getList(), depth + 1, result);
+		}
+	}
+}
+
+int Solution::depthSum(vector<NestedInteger>& nestedList) {
+	int depth = 1;
+	int result = 0;
+	dfs(nestedList, depth, result);
+	return result;
+}
---
+++ 0339_Nested_List_Weight_Sum/main.cpp
@@ -0,0 +1,11 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    return 0;
+}
+
