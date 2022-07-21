62f91312bfbcf1a0e48103b2c379dd171e63f5a5
neilhuang <neilhuang@synology.com>
2022-07-21 18:17:21 +0800

    <LeetCode202109> 202. Happy Number

---
+++ 0202_Happy_Number/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+
+using std::unordered_set;
+
+bool Solution::isHappy(int n) {
+	unordered_set<int> uSet;
+	while (n != 1) {
+		if (uSet.find(n) != uSet.end()) return false;
+		uSet.emplace(n);
+		int next = 0;
+		while (n > 0) {
+			next += ((n % 10) * (n % 10));
+			n /= 10;
+		}
+		n = next;
+	}
+	return true;
+}
