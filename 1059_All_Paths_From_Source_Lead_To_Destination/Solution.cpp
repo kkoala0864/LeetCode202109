fe464c0567ecbf6759fd603f81c0bae39e10640d
neilhuang <neilhuang@synology.com>
2022-07-22 16:24:11 +0800

    <LeetCode202109> 1059. All Paths from Source Lead to Destination

---
+++ 1059_All_Paths_From_Source_Lead_To_Destination/Solution.cpp
@@ -0,0 +1,31 @@
+#include <Solution.h>
+#include <iostream>
+
+bool dfs(vector<vector<int>>& odg, int cur, int destination, vector<bool>& visited, vector<int>& cache) {
+	if (cur == destination) return true;
+	if (cache[cur] != 0) return cache[cur] == 1;
+	visited[cur] = true;
+
+	if (odg[cur].empty()) return false;
+
+	int cnt = odg[cur].size();
+	for (const auto& node : odg[cur]) {
+		if (visited[node]) continue;
+		bool result = dfs(odg, node, destination, visited, cache);
+		if (result) --cnt;
+	}
+
+	visited[cur] = false;
+	cache[cur] = cnt == 0 ? 1 : 2;
+	return cnt == 0;
+}
+
+bool Solution::leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination) {
+	vector<vector<int>> odg(n, vector<int>());
+
+	for (auto& e : edges) odg[e[0]].emplace_back(e[1]);
+	if (!odg[destination].empty()) return false;
+	vector<bool> visited(n, false);
+	vector<int> cache(n, 0);
+	return dfs(odg, source, destination, visited, cache);
+}
---
+++ 1059_All_Paths_From_Source_Lead_To_Destination/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	bool leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
