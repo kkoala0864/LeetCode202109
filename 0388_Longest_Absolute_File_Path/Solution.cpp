769a85a98803282ea430d4348b04b0e1c6312a7a
neilhuang <neilhuang@synology.com>
2022-07-25 14:31:15 +0800

    <LeetCode202109> 388. Longest Absolute File Path

---
+++ 0388_Longest_Absolute_File_Path/Solution.cpp
@@ -0,0 +1,46 @@
+#include <Solution.h>
+#include <iostream>
+#include <stack>
+#include <algorithm>
+#include <vector>
+
+using std::vector;
+using std::max;
+using std::stack;
+using std::pair;
+
+int Solution::lengthLongestPath(string input) {
+	vector<string> lines;
+	string line;
+	for (int i = 0 ; i < input.size() ; ++i) {
+		if (input[i] == '\n') {
+			lines.emplace_back(line);
+			line.clear();
+		} else {
+			line.push_back(input[i]);
+		}
+	}
+	lines.emplace_back(line);
+
+	stack<pair<string, int>> st;
+	int result = 0;
+	int cur = 0;
+	for (const auto& l : lines) {
+		int tabCnt = 0;
+		int i = 0;
+		while (i < l.size() && l[i] == '\t') {
+			++i;
+			++tabCnt;
+		}
+		while (!st.empty() && tabCnt <= st.top().second) {
+			cur -= (st.top().first.size() + 1);
+			st.pop();
+			if (st.empty()) cur += 1;
+		}
+		string sub = l.substr(i);
+		cur += (sub.size() + (st.empty() ? 0 : 1));
+		st.emplace(pair<string, int>(sub, tabCnt));
+		if (sub.find(".") != std::string::npos) result = max(cur, result);
+	}
+	return result;
+}
---
+++ 0388_Longest_Absolute_File_Path/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext");
+    Solution* test = new Solution();
+    cout << test->lengthLongestPath(input) << endl;
+    return 0;
+}
+
