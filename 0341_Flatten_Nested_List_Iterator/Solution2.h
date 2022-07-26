0e9c97ab5051670ce5c0032a5b04856829b7c7f4
neilhuang <neilhuang@synology.com>
2022-07-26 14:46:02 +0800

    <LeetCode202109> 341. Flatten Nested List Iterator

---
+++ 0341_Flatten_Nested_List_Iterator/Solution2.h
@@ -0,0 +1,52 @@
+#include <vector>
+
+using std::vector;
+
+class NestedInteger {
+  public:
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const;
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const;
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger> &getList() const;
+};
+
+class NestedIterator {
+	public :
+		void flatten(const vector<NestedInteger>& nestedList) {
+			for (auto& v : nestedList) {
+				if (v.isInteger()) {
+					this->_list.emplace_back(v.getInteger());
+				} else {
+					flatten(v.getList());
+				}
+			}
+		}
+
+		NestedIterator(vector<NestedInteger> &nestedList) {
+			_list = vector<int>();
+			flatten(nestedList);
+			idx = 0;
+		}
+
+		int next() {
+			++idx;
+			return _list[idx-1];
+		}
+
+		bool hasNext() {
+			return idx < _list.size();
+		}
+	private :
+		vector<int> _list;
+		int idx;
+		virtual ~NestedIterator() {}
+		NestedIterator& operator=(const NestedIterator& source);
+		NestedIterator(const NestedIterator&);
+};
+
