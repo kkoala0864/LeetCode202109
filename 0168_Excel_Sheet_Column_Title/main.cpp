5cc4cae2a2cc0b86be7d4ff8d51839b94dd3f393
neilhuang <neilhuang@synology.com>
2022-07-29 16:12:31 +0800

    <LeetCode202109> 168. Excel Sheet Column Title

---
+++ 0168_Excel_Sheet_Column_Title/main.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    cout << test->convertToTitle(701) << endl;
+    return 0;
+}
+
