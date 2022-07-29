15f81809898b4f85f96b33d1b7b17fc4e76b9798
neilhuang <neilhuang@synology.com>
2022-07-29 11:33:17 +0800

    <LeetCode202109> 890. Find and Replace Pattern

---
+++ 0890_Find_And_Replace_Pattern/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+
+string convert(string& s) {
+	vector<char> map(26, ' ');
+
+	string result;
+	char mc = 'a' - 1;
+	for (const auto& c : s) {
+		if (map[c-'a'] == ' ') {
+			map[c-'a'] = ++mc;
+		}
+		result.push_back(map[c-'a']);
+	}
+	return result;
+}
+
+vector<string> Solution::findAndReplacePattern(vector<string>& words, string pattern) {
+	int n = pattern.size();
+	string golden = convert(pattern);
+
+	vector<string> result;
+	for (auto& w : words) {
+		if (golden == convert(w)) result.emplace_back(w);
+	}
+
+	return result;
+}
---
+++ 0890_Find_And_Replace_Pattern/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"abc","deq","mee","aqq","dkd","ccc"};
+	Solution* test = new Solution();
+	auto result = test->findAndReplacePattern(input, "abb");
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
