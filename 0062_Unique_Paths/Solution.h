e98145739a82ac38d084ee411d8ec596c161b23a
neilhuang <neilhuang@synology.com>
2022-08-01 14:12:15 +0800

    <LeetCode202109> 62. Unique Paths

--- 0062_Unique_Paths/Solution.h
+++ 0062_Unique_Paths/Solution.h
@@ -4,6 +4,7 @@ class Solution {
 	int uniquePaths(int m, int n);
 	int uniquePaths2(int m, int n);
 	int uniquePaths3(int m, int n);
+	int uniquePaths4(int m, int n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0062_Unique_Paths/main.cpp
+++ 0062_Unique_Paths/main.cpp
@@ -6,9 +6,8 @@ using std::endl;
 
 int main(void) {
     Solution* test = new Solution();
-    cout << test->uniquePaths(10, 10) << endl;
-    cout << test->uniquePaths2(10, 10) << endl;
-    cout << test->uniquePaths3(10, 10) << endl;
+    cout << test->uniquePaths(3, 7) << endl;
+    cout << test->uniquePaths4(3, 7) << endl;
     return 0;
 }
 
