9671450b5feb925d88ffcf2b334a91210649b220
neilhuang <neilhuang@synology.com>
2022-08-02 14:34:10 +0800

    <LeetCode202109> 378. Kth Smallest Element in a Sorted Matrix

---
+++ 0378_Kth_Smallest_Element_In_A_Sorted_Matrix/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::cout;
+using std::endl;
+using std::priority_queue;
+using std::pair;
+
+int Solution::kthSmallest(vector<vector<int>>& matrix, int k) {
+	int m = matrix.size();
+
+	vector<vector<int>::iterator> iv;
+	priority_queue<pair<int , int>, vector<pair<int, int>>, std::greater<pair<int, int>>> pq;
+	for (int i = 0 ; i < m ; ++i) {
+		iv.emplace_back(matrix[i].begin());
+		pq.emplace(*iv[i], i);
+	}
+
+	while (--k > 0) {
+		auto cur = pq.top();
+		pq.pop();
+		cout << cur.first << endl;
+		++iv[cur.second];
+		if (iv[cur.second] != matrix[cur.second].end()) pq.emplace(*iv[cur.second], cur.second);
+	}
+	return pq.top().first;
+}
---
+++ 0378_Kth_Smallest_Element_In_A_Sorted_Matrix/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+	Solution* test = new Solution();
+	cout << test->kthSmallest(input, 18) << endl;
+	return 0;
+}
+
