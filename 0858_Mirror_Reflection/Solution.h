2016643b8c90901593124391050a58627b7ab7fd
neilhuang <neilhuang@synology.com>
2022-08-04 18:22:58 +0800

    <LeetCode202109> 858. Mirror Reflection

---
+++ 0858_Mirror_Reflection/Solution.h
@@ -0,0 +1,10 @@
+class Solution {
+    public :
+        Solution() {}
+	int mirrorReflection(int p, int q);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 0858_Mirror_Reflection/main.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    cout << test->mirrorReflection(2, 1) << endl;
+    return 0;
+}
+
