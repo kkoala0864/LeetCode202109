7ba58de5d56aa6d1d047d9192a01c9702a5256ad
neilhuang <neilhuang@synology.com>
2022-08-05 16:01:31 +0800

    <LeetCode202109> 377. Combination Sum IV

---
+++ 0377_Combination_Sum_IV/Solution.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int Solution::combinationSum4(vector<int>& nums, int target) {
+	vector<unsigned long long> dp(target + 1, 0);
+	dp[0] = 1;
+
+	for (int i = 1 ; i <= target ; ++i) {
+		for (const auto& v : nums) {
+			if (v <= i) dp[i] += dp[i-v];
+		}
+	}
+	return dp[target];
+}
