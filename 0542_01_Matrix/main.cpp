5bd093c5b9aff040dba81da64a73c5a6382c3b6b
neilhuang <neilhuang@synology.com>
2022-08-08 16:01:18 +0800

    <LeetCode202109> 542. 01 Matrix

---
+++ 0542_01_Matrix/main.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,0,0},{0,1,0},{1,1,1}};
+	Solution* test = new Solution();
+	auto result = test->updateMatrix(input);
+	for (int i = 0 ; i < result.size() ; ++i) {
+		for (int j = 0 ; j < result[0].size() ; ++j) {
+			cout << result[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
