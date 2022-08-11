bd82b930ee93a92d25eb01d07b3e667b92253dcb
neilhuang <neilhuang@synology.com>
2022-08-11 14:30:56 +0800

    <LeetCode202109> 1319. Number of Operations to Make Network Connected

---
+++ 1319_Number_Of_Operations_To_Make_Network_Connected/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+int find(vector<int>& g, int val) {
+	if (g[val] == val) return val;
+	return g[val] = find(g, g[val]);
+}
+
+int Solution::makeConnected(int n, vector<vector<int>>& connections) {
+	vector<int> g;
+	for (int i = 0 ; i < n ; ++i) g.emplace_back(i);
+
+	int same = 0;
+	int groupCnt = n;
+	for (int i = 0 ; i < connections.size() ; ++i) {
+		int lr = find(g, connections[i][0]);
+		int rr = find(g, connections[i][1]);
+		if (lr == rr) {
+			++same;
+			continue;
+		}
+		--groupCnt;
+		g[lr] = g[rr] = min(lr, rr);
+	}
+
+	return groupCnt == 1 ? 0 : same >= (groupCnt - 1) ? groupCnt - 1 : -1;
+}
