aea5b5282bcf43cc28c28702af74ba800d7a9c7c
neilhuang <neilhuang@synology.com>
2022-08-15 15:36:38 +0800

    <LeetCode202109> 1570. Dot Product of Two Sparse Vectors

---
+++ 1570_Dot_Product_Of_Two_Sparse_Vectors/Solution.h
@@ -0,0 +1,29 @@
+#include <vector>
+#include <unordered_map>
+
+using std::vector;
+using std::unordered_map;
+
+class SparseVector {
+    public :
+	unordered_map<int, int> val;
+	SparseVector(vector<int> &nums) {
+		for (int i = 0 ; i < nums.size() ; ++i) {
+			val[i] = nums[i];
+		}
+	}
+
+	int dotProduct(SparseVector& vec) {
+		int result = 0;
+		for (const auto& e : val) {
+			if (vec.val.find(e.first) == vec.val.end()) continue;
+			result += (vec.val[e.first] * e.second);
+		}
+		return result;
+	}
+    private :
+        virtual ~SparseVector() {}
+        SparseVector& operator=(const SparseVector& source);
+        SparseVector(const SparseVector&);
+};
+
