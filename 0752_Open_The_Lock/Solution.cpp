3ad015a73dd5d1e73cece12216418aa8f9e0cc1f
neilhuang <neilhuang@synology.com>
2022-08-16 18:04:08 +0800

    <LeetCode202109> 127. Word Ladder

---
+++ 0752_Open_The_Lock/Solution.cpp
@@ -0,0 +1,46 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+
+using std::unordered_set;
+using std::queue;
+
+int Solution::openLock(vector<string>& deadends, string target) {
+	queue<string> que, next;
+	unordered_set<string> uSet;
+	int result = 0;
+
+	for (const auto& d : deadends) uSet.emplace(d);
+
+	if (uSet.find("0000") != uSet.end()) return -1;
+	que.emplace("0000");
+
+	while (!que.empty()) {
+		string cur = que.front();
+		que.pop();
+
+		if (cur == target) return result;
+		string tmp = cur;
+		for (int i = 0 ; i < 4 ; ++i) {
+			tmp = cur;
+			tmp[i] = (cur[i] - '0' + 1) % 10 + '0';
+			if (uSet.find(tmp) == uSet.end()) {
+				uSet.emplace(tmp);
+				next.emplace(tmp);
+			}
+
+			tmp[i] = (cur[i] - '0' + 9) % 10 + '0';
+			if (uSet.find(tmp) == uSet.end()) {
+				uSet.emplace(tmp);
+				next.emplace(tmp);
+			}
+		}
+
+		if (que.empty()) {
+			++result;
+			que = move(next);
+		}
+	}
+	return -1;
+}
---
+++ 0752_Open_The_Lock/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"0201","0101","0102","1212","2002"};
+	Solution* test = new Solution();
+	cout << test->openLock(input, "0202") << endl;
+	return 0;
+}
+
