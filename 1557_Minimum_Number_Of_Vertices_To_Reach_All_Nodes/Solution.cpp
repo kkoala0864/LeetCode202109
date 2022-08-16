a332aa97dbc5295548bf15ce045bf1c398559074
neilhuang <neilhuang@synology.com>
2022-08-16 18:30:20 +0800

    <LeetCode202109> 1557. Minimum Number of Vertices to Reach All Nodes

---
+++ 1557_Minimum_Number_Of_Vertices_To_Reach_All_Nodes/Solution.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<int> Solution::findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+	vector<int> id(n, 0);
+
+	vector<int> result;
+	for (const auto& e : edges) ++id[e[1]];
+
+	for (int i = 0 ; i < n ; ++i) {
+		if (id[i] == 0) result.emplace_back(i);
+	}
+	return result;
+}
---
+++ 1557_Minimum_Number_Of_Vertices_To_Reach_All_Nodes/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,1},{0,2},{2,5},{3,4},{4,2}};
+	Solution* test = new Solution();
+	auto result = test->findSmallestSetOfVertices(6, input);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
