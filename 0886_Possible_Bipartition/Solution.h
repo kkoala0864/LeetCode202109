f9a3dc25a4ca588a8378e402618ae222c5c03ceb
neilhuang <neilhuang@synology.com>
2022-08-18 11:41:16 +0800

    <LeetCode202109> 886. Possible Bipartition

---
+++ 0886_Possible_Bipartition/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	bool possibleBipartition(int n, vector<vector<int>>& dislikes);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
