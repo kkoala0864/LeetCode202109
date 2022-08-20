6fa5facf728de4a9498cf16a5b0381810b161213
neilhuang <neilhuang@synology.com>
2022-08-20 11:22:28 +0800

    <LeetCode202109> 1905. Count Sub Islands

---
+++ 1905_Count_Sub_Islands/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> g1 = {{1,1,1,0,0},{0,1,1,1,1},{0,0,0,0,0},{1,0,0,0,0},{1,1,0,1,1}};
+	vector<vector<int>> g2 = {{1,1,1,0,0},{0,0,1,1,1},{0,1,0,0,0},{1,0,1,1,0},{0,1,0,1,0}};
+	Solution* test = new Solution();
+	cout << test->countSubIslands(g1, g2) << endl;
+	return 0;
+}
+
