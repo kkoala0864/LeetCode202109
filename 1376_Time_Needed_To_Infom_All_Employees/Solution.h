ade7faea869d5626016332df67287294950865aa
neilhuang <neilhuang@synology.com>
2022-08-26 14:54:30 +0800

    <LeetCode202109> 1376. Time Needed to Inform All Employees

--- 1376_Time_Needed_To_Infom_All_Employees/Solution.h
+++ 1376_Time_Needed_To_Infom_All_Employees/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime);
+	int numOfMinutes2(int n, int headID, vector<int>& manager, vector<int>& informTime);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
---
+++ 1376_Time_Needed_To_Infom_All_Employees/Solution2.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using std::max;
+using std::queue;
+
+int Solution::numOfMinutes2(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+	vector<vector<int>> out(n, vector<int>());
+	vector<int> time(n, 0);
+
+	for (int i = 0 ; i < n ; ++i) {
+		if (manager[i] != -1) out[manager[i]].emplace_back(i);
+	}
+
+	queue<int> que;
+	que.emplace(headID);
+	int result = 0;
+
+	while (!que.empty()) {
+		int cur = que.front();
+		que.pop();
+
+		for (const auto& v : out[cur]) {
+			time[v] = time[cur] + informTime[cur];
+			result = max(result, time[v]);
+			que.emplace(v);
+		}
+	}
+	return result;
+}
