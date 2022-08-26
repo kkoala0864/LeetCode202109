e74ff4c06c5f6b1af3ee7fc50c0f809ee92c2913
neilhuang <neilhuang@synology.com>
2022-08-26 11:37:54 +0800

    <LeetCode202109> 173. Binary Search Tree Iterator

---
+++ 0173_Binary_Search_Tree_Iterator/Solution3.h
@@ -0,0 +1,45 @@
+#include <stack>
+
+using std::stack;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class BSTIterator {
+	public :
+		BSTIterator(TreeNode* root) {
+			while (root) {
+				st.emplace(root);
+				root = root->left;
+			}
+		}
+
+		int next() {
+			auto cur = st.top();
+			int result = cur->val;
+			st.pop();
+			cur = cur->right;
+			while (cur) {
+				st.emplace(cur);
+				cur = cur->left;
+			}
+		}
+
+		bool hasNext() {
+			return !st.empty();
+		}
+
+    private :
+		stack<TreeNode*> st;
+		virtual ~BSTIterator() {}
+		BSTIterator& operator=(const BSTIterator& source);
+		BSTIterator(const BSTIterator&);
+};
+
+
--- 0173_Binary_Search_Tree_Iterator/main.cpp
+++ 0173_Binary_Search_Tree_Iterator/main.cpp
@@ -1,11 +1,10 @@
-#include <Solution.h>
+#include <Solution3.h>
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
 int main(void) {
-    Solution* test = new Solution();
     return 0;
 }
 
