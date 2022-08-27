15d4b021cf4ec77830fe1f41e20a262f6e2dac55
neilhuang <neilhuang@synology.com>
2022-08-27 14:17:53 +0800

    <LeetCode202109> 994. Rotting Oranges

--- 0994_Rotting_Oranges/Solution.h
+++ 0994_Rotting_Oranges/Solution.h
@@ -7,6 +7,7 @@ class Solution {
         Solution() {}
 	int orangesRotting(vector<vector<int>>& grid);
 	int orangesRotting2(vector<vector<int>>& grid);
+	int orangesRotting3(vector<vector<int>>& grid);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0994_Rotting_Oranges/main.cpp
+++ 0994_Rotting_Oranges/main.cpp
@@ -7,8 +7,7 @@ using std::endl;
 int main(void) {
 	vector<vector<int>> input = {{2,1,1},{1,1,0},{0,1,1}};
 	Solution* test = new Solution();
-	cout << test->orangesRotting(input) << endl;
-	cout << test->orangesRotting2(input) << endl;
+	cout << test->orangesRotting3(input) << endl;
 	return 0;
 }
 
