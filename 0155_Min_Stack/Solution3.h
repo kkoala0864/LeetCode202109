d028d370e755c9109235f72c1cade90fb73f9dcc
neilhuang <neilhuang@synology.com>
2022-09-02 15:20:39 +0800

    <LeetCode202109> 155. Min Stack

---
+++ 0155_Min_Stack/Solution3.h
@@ -0,0 +1,33 @@
+#include <stack>
+#include <algorithm>
+using std::stack;
+using std::pair;
+using std::min;
+
+class MinStack {
+    public :
+	MinStack() {}
+
+	void push(int val) {
+		int minVal = _st.empty() ? val : min(_st.top().second, val);
+		_st.emplace(pair<int, int>(val, minVal));
+	}
+
+	void pop() {
+		_st.pop();
+	}
+
+	int top() {
+		return _st.top().first;
+	}
+
+	int getMin() {
+		return _st.top().second;
+	}
+    private :
+	stack<pair<int, int>> _st;
+        virtual ~MinStack() {}
+        MinStack& operator=(const MinStack& source);
+        MinStack(const MinStack&);
+};
+
