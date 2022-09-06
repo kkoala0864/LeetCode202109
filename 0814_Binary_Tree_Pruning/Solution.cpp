94382eafb60ccc63042f7e7354fcf093d036c2d4
neilhuang <neilhuang@synology.com>
2022-09-06 11:20:09 +0800

    <LeetCode202109> 814. Binary Tree Pruning

---
+++ 0814_Binary_Tree_Pruning/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+bool dfs(TreeNode* node) {
+	if (!node) return false;
+
+	bool l = dfs(node->left);
+	bool r = dfs(node->right);
+
+	if (!l) node->left = nullptr;
+	if (!r) node->right = nullptr;
+
+	return node->val == 1 ? true : l || r;
+}
+
+TreeNode* Solution::pruneTree(TreeNode* root) {
+	if (!root) return root;
+	if (!dfs(root)) return nullptr;
+	return root;
+}
