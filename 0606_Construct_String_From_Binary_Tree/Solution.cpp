90fadc7f9823df90cbfaebdb2dab90912ac2ad12
neilhuang <neilhuang@synology.com>
2022-09-07 11:24:14 +0800

    <LeetCode202109> 606. Construct String from Binary Tree

---
+++ 0606_Construct_String_From_Binary_Tree/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::to_string;
+
+void dfs(TreeNode* node, string& result) {
+	if (!node) return;
+
+	string val = to_string(node->val);
+	result += val;
+	if (!node->left && !node->right) return;
+	result.push_back('(');
+	dfs(node->left, result);
+	result.push_back(')');
+	if (node->right) {
+		result.push_back('(');
+		dfs(node->right, result);
+		result.push_back(')');
+	}
+}
+
+string Solution::tree2str(TreeNode* root) {
+	string result;
+	if (!root) return result;
+	dfs(root, result);
+	return result;
+}
