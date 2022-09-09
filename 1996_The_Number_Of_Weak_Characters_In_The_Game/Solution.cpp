4d50a5b705220fa7fa90cb95b5a70468c89f51b8
neilhuang <neilhuang@synology.com>
2022-09-09 15:22:51 +0800

    <LeetCode202109> 1996. The Number of Weak Characters in the Game

---
+++ 1996_The_Number_Of_Weak_Characters_In_The_Game/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+using std::sort;
+using std::stack;
+using std::max;
+
+int Solution::numberOfWeakCharacters(vector<vector<int>>& properties) {
+	auto cmp = [](const vector<int>& lhs, const vector<int>& rhs) {
+		return lhs[0] == rhs[0] ? lhs[1] > rhs[1] : lhs[0] < rhs[0];
+	};
+	sort(properties.begin(), properties.end(), cmp);
+
+	int result = 0;
+	int maxDef = 0;
+	for (int i = properties.size() - 1 ; i >= 0 ; --i) {
+		if (properties[i][1] < maxDef) {
+			++result;
+		}
+		maxDef = max(maxDef, properties[i][1]);
+	}
+	return result;
+}
---
+++ 1996_The_Number_Of_Weak_Characters_In_The_Game/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{2,4}, {2,5}, {3,6}, {3,7}, {3,8}};
+	//vector<vector<int>> input = {{1,5},{10,4},{4,3}};
+	Solution* test = new Solution();
+	cout << test->numberOfWeakCharacters(input) << endl;
+	return 0;
+}
+
