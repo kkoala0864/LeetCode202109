42a2eb0f0e4861a1c3b86a991f2c28ac3a71c393
neilhuang <neilhuang@synology.com>
2022-09-12 11:34:34 +0800

    <LeetCode202109> 948. Bag of Tokens

---
+++ 0948_Bag_Of_Tokens/Solution.cpp
@@ -0,0 +1,26 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+int Solution::bagOfTokensScore(vector<int>& tokens, int power) {
+	sort(tokens.begin(), tokens.end());
+
+	int curPower = power;
+	int curScore = 0;
+	int getScoreIdx = 0, getPowerIdx = tokens.size() - 1;
+	while (getScoreIdx <= getPowerIdx) {
+		while (getScoreIdx <= getPowerIdx && curPower >= tokens[getScoreIdx]) {
+			++curScore;
+			curPower -= tokens[getScoreIdx++];
+		}
+		int oriScore = curScore;
+		while (getScoreIdx <= getPowerIdx && curScore > 0 && curPower < tokens[getScoreIdx])  {
+			--curScore;
+			curPower += tokens[getPowerIdx--];
+		}
+		if (getScoreIdx > getPowerIdx || curPower < tokens[getScoreIdx]) return oriScore;
+	}
+	return curScore;
+}
---
+++ 0948_Bag_Of_Tokens/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {100};
+	Solution* test = new Solution();
+	cout << test->bagOfTokensScore(input, 50) << endl;
+	return 0;
+}
+
