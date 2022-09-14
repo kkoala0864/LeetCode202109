16e1eedaa76c7a93c6147d96929257b312b7646f
neilhuang <neilhuang@synology.com>
2022-09-14 16:32:06 +0800

    <LeetCode202109> 1457. Pseudo-Palindromic Paths in a Binary Tree

---
+++ 1457_Pseudo-palindromic_Path_In_A_Binary_Tree/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+
+void dfs(TreeNode* node, int bitmask, int& result) {
+	if (!node) return;
+	bitmask ^= 1 << node->val;
+
+	if (!node->left && !node->right) {
+		int cnt = 0;
+		while (bitmask) {
+			bitmask &= (bitmask - 1);
+			++cnt;
+		}
+		if (cnt < 2) ++result;
+		return;
+	}
+
+	if (node->left) dfs(node->left, bitmask, result);
+	if (node->right) dfs(node->right, bitmask, result);
+}
+
+int Solution::pseudoPalindromicPaths (TreeNode* root) {
+	int bitmask = 0;
+	int result = 0;
+	dfs(root, bitmask, result);
+	return result;
+}
