65f3983c54f616285a2379921ba35257a77ae043
neilhuang <neilhuang@synology.com>
2022-09-15 11:55:05 +0800

    <LeetCode202109> 2007. Find Original Array From Doubled Array

---
+++ 2007_Find_Original_Array_From_Doubled_Array/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<int> Solution::findOriginalArray(vector<int>& changed) {
+	int size = changed.size();
+	if (size == 0 || size & 1) return {};
+	vector<int> cnt(1e5 + 1, 0);
+
+	for (const auto& c : changed) ++cnt[c];
+	vector<int> result;
+
+	if (cnt[0] & 1) return {};
+	cnt[0] = cnt[0] >> 1;
+	for(int j = 0 ; j < cnt[0] ; ++j) result.emplace_back(0);
+
+	for (int i = 1 ; i <= 50000 ; ++i) {
+		if (cnt[i] == 0) continue;
+		if (cnt[i] > 0) {
+			if (cnt[i] <= cnt[i<<1]) {
+				for (int j = 0 ; j < cnt[i] ; ++j) {
+					result.emplace_back(i);
+					--cnt[i<<1];
+				}
+			} else {
+				return {};
+			}
+		}
+	}
+	return result;
+}
---
+++ 2007_Find_Original_Array_From_Doubled_Array/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,3,4,2,6,8};
+	Solution* test = new Solution();
+	vector<int> result = test->findOriginalArray(input);
+	for (const auto& v : result) {
+		cout << v << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
