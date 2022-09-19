23190e81ce1b7845ec62a012031a37d72549e8a3
neilhuang <neilhuang@synology.com>
2022-09-19 11:00:20 +0800

    <LeetCode202109> 609. Find Duplicate File in System

---
+++ 0609_Find_Duplicate_File_In_System/Solution.cpp
@@ -0,0 +1,43 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+
+vector<vector<string>> Solution::findDuplicate(vector<string>& paths) {
+	unordered_map<string, vector<string>> uMap;
+
+	for (auto& p : paths) {
+		vector<string> info;
+		string local;
+		for (const auto& c : p) {
+			if (c != ' ') local.push_back(c);
+			else {
+				if (local.size() > 0) {
+					info.emplace_back(local);
+					local = "";
+				}
+			}
+		}
+		if (local.size() > 0) {
+			info.emplace_back(local);
+			local = "";
+		}
+
+		for (int i = 1 ; i < info.size() ; ++i) {
+			int left = info[i].find("(");
+			string content = info[i].substr(left + 1, info[i].size() - left - 2);
+			info[i].resize(left);
+			info[i] = info[0] + "/" + info[i];
+			uMap[content].emplace_back(info[i]);
+		}
+	}
+
+	vector<vector<string>> result;
+	for (auto& e : uMap) {
+		if (e.second.size() > 1) {
+			result.emplace_back(e.second);
+		}
+	}
+	return result;
+}
---
+++ 0609_Find_Duplicate_File_In_System/main.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"root/a 1.txt(abcd) 2.txt(efgh)","root/c 3.txt(abcd)","root/c/d 4.txt(efgh)","root 4.txt(efgh)"};
+	Solution* test = new Solution();
+	auto result = test->findDuplicate(input);
+	for (const auto& r : result) {
+		for (const auto& f : r) {
+			cout << f << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
+
