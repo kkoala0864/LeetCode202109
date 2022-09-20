b84e24d678db70470198a04486d49c635d3cb6b3
neilhuang <neilhuang@synology.com>
2022-09-20 18:15:08 +0800

    <LeetCode202109> 785. Is Graph Bipartite?

--- 0785_Is_Graph_Bipartite/Solution.h
+++ 0785_Is_Graph_Bipartite/Solution.h
@@ -7,6 +7,7 @@ class Solution {
     public :
         Solution() {}
 	bool isBipartite(vector<vector<int>>& graph);
+	bool isBipartite2(vector<vector<int>>& graph);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0785_Is_Graph_Bipartite/main.cpp
+++ 0785_Is_Graph_Bipartite/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
 	vector<vector<int>> input = {{},{2,4,6},{1,4,8,9},{7,8},{1,2,8,9},{6,9},{1,5,7,8,9},{3,6,9},{2,3,4,6,9},{2,4,5,6,7,8}};
 	Solution* test = new Solution();
 	cout << test->isBipartite(input) << endl;
+	cout << test->isBipartite2(input) << endl;
 	return 0;
 }
 
