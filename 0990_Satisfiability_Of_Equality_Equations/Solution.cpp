8f0007ab880c1f9af2e5634c31ecf6af6426e64d
neilhuang <neilhuang@synology.com>
2022-09-26 11:48:17 +0800

    <LeetCode202109> 990. Satisfiability of Equality Equations

---
+++ 0990_Satisfiability_Of_Equality_Equations/Solution.cpp
@@ -0,0 +1,31 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::swap;
+using std::min;
+
+int find(vector<int>& map, int val) {
+	if (map[val] == val) return val;
+	return map[val] = find(map, map[val]);
+}
+
+bool Solution::equationsPossible(vector<string>& equations) {
+	vector<int> map;
+	for (int i = 0 ; i < 26 ; ++i) map.emplace_back(i);
+
+	for (const auto& e : equations) {
+		if (e[1] == '!') continue;
+		int lr = find(map, e[0]-'a');
+		int rr = find(map, e[3]-'a');
+		map[lr] = map[rr] = min(lr, rr);
+	}
+
+	for (const auto& e : equations) {
+		if (e[1] == '=') continue;
+		int lr = find(map, e[0]-'a');
+		int rr = find(map, e[3]-'a');
+		if (lr == rr) return false;
+	}
+	return true;
+}
