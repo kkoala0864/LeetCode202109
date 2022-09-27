e1d615fe6e8330b5f9366e35e62f86548ab0551b
neilhuang <neilhuang@synology.com>
2022-09-27 19:44:48 +0800

    <LeetCode202109> 1235. Maximum Profit in Job Scheduling

---
+++ 1235_Maximum_Profit_In_Job_Scheduling/Solution.cpp
@@ -0,0 +1,42 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int findMaxProfit(vector<int>& startTime, vector<vector<int>>& jobs, vector<int>& memo) {
+	int size = startTime.size();
+
+	for (int i = size - 1 ; i >= 0 ; --i) {
+		int curProfit = 0;
+		int nextIdx = lower_bound(startTime.begin(), startTime.end(), jobs[i][1]) - startTime.begin();
+
+		if (nextIdx != size) {
+			curProfit = jobs[i][2] + memo[nextIdx];
+		} else {
+			curProfit = jobs[i][2];
+		}
+
+		if (i == size - 1) {
+			memo[i] = curProfit;
+		} else {
+			memo[i] = max(curProfit, memo[i+1]);
+		}
+	}
+	return memo[0];
+}
+
+int Solution::jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+	vector<vector<int>> jobs;
+	vector<int> memo(50001, 0);
+	for (int i = 0 ; i < profit.size() ; ++i) {
+		jobs.push_back({startTime[i], endTime[i], profit[i]});
+	}
+
+	sort(jobs.begin(), jobs.end());
+
+	for (int i = 0 ; i < jobs.size() ; ++i) {
+		startTime[i] = jobs[i][0];
+	}
+	return findMaxProfit(startTime, jobs, memo);
+}
---
+++ 1235_Maximum_Profit_In_Job_Scheduling/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> s = {1,2,3,4,6};
+	vector<int> e = {3,5,10,6,9};
+	vector<int> p = {20,20,100,70,60};
+	Solution* test = new Solution();
+	cout << test->jobScheduling(s, e, p) << endl;
+	return 0;
+}
+
