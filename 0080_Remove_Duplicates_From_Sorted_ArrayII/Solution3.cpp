770d00b0a998511c9ebd4f37bb279e06c10ec98b
neilhuang <neilhuang@synology.com>
2022-10-05 11:31:58 +0800

    <LeetCode202109> 80. Remove Duplicates from Sorted Array II

--- 0080_Remove_Duplicates_From_Sorted_ArrayII/Solution3.cpp
+++ 0080_Remove_Duplicates_From_Sorted_ArrayII/Solution3.cpp
@@ -3,18 +3,15 @@
 
 int Solution::removeDuplicates3(vector<int>& nums) {
 	if (nums.empty()) return 0;
-	int idx = 1;
-	int cur = nums[0], cnt = 1;
-	for (int i = 1 ; i < nums.size() ; ++i) {
-		if (nums[i] == cur) ++cnt;
-		else {
-			cur = nums[i];
-			cnt = 1;
-		}
 
-		if (cnt > 2) continue;
-		nums[idx] = cur;
-		++idx;
+	int resultIdx = 1;
+	int cnt = 1;
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		if (nums[i] == nums[i-1] && cnt >= 2) continue;
+		if (nums[i] != nums[i-1]) cnt = 0;
+		nums[resultIdx] = nums[i];
+		++cnt;
+		++resultIdx;
 	}
-	return idx;
+	return resultIdx;
 }
