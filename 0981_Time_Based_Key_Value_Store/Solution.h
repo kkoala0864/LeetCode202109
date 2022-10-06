2401875727e59a57e3abfc2190421d47ca2b9ea0
neilhuang <neilhuang@synology.com>
2022-10-06 11:17:24 +0800

    <LeetCode202109> 981. Time Based Key-Value Store

---
+++ 0981_Time_Based_Key_Value_Store/Solution.h
@@ -0,0 +1,37 @@
+#include <string>
+#include <unordered_map>
+#include <map>
+
+using std::unordered_map;
+using std::map;
+
+using std::string;
+
+class TimeMap {
+    public :
+	TimeMap() {}
+
+	void set(string key, string value, int timestamp) {
+		_m[key][timestamp] = value;
+	}
+
+	string get(string key, int timestamp) {
+		if (_m.find(key) == _m.end()) return "";
+		auto iter = _m[key].lower_bound(timestamp);
+
+		if (iter->first == timestamp) return iter->second;
+		else {
+			if (iter == _m[key].begin()) return "";
+			else {
+				--iter;
+				return iter->second;
+			}
+		}
+	}
+    private :
+	unordered_map<string, map<int, string>> _m;
+        virtual ~TimeMap() {}
+        TimeMap& operator=(const TimeMap& source);
+        TimeMap(const TimeMap&);
+};
+
