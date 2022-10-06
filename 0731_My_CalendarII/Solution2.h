b50bbc10c21b9eb9d18c022c7190eacb9a974684
neilhuang <neilhuang@synology.com>
2022-10-07 00:56:26 +0800

    <LeetCode202109> 731. My Calendar II

--- 0731_My_CalendarII/Solution2.h
+++ 0731_My_CalendarII/Solution2.h
@@ -2,33 +2,30 @@
 #include <algorithm>
 
 using std::vector;
+using std::pair;
 using std::min;
 using std::max;
 
 class MyCalendarTwo {
     public :
-	    vector<vector<int>> once;
-	    vector<vector<int>> twice;
 	    MyCalendarTwo() {}
+
 	    bool book(int start, int end) {
-		    for (const auto& iter : twice) {
-			if (start >= iter[1] || end <= iter[0]) {
-				continue;
-			} else {
-				return false;
-			}
-		    }
-		    for (const auto& iter : once) {
-			if (start >= iter[1] || end <= iter[0]) {
-				continue;
-			} else {
-				twice.push_back({max(start, iter[0]), min(end, iter[1])});
-			}
-		    }
-		    once.push_back({start, end});
-		    return true;
+		for (const auto& t : twice) {
+			if (t.first >= end || t.second <= start) continue;
+			return false;
+		}
+
+		for (const auto& t : first) {
+			if (t.first >= end || t.second <= start) continue;
+			twice.push_back({max(t.first, start), min(t.second, end)});
+		}
+		first.push_back({start, end});
+		return true;
 	    }
     private :
+	    vector<pair<int, int>> first;
+	    vector<pair<int, int>> twice;
 	    virtual ~MyCalendarTwo() {}
 	    MyCalendarTwo& operator=(const MyCalendarTwo& source);
 	    MyCalendarTwo(const MyCalendarTwo&);
