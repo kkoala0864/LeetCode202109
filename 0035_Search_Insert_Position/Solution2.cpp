0a7f325e3c17cda54c361689d93d3794dec9f776
neilhuang <neilhuang@synology.com>
2022-10-07 14:12:16 +0800

    <LeetCode202109> 35. Search Insert Position

--- 0035_Search_Insert_Position/Solution2.cpp
+++ 0035_Search_Insert_Position/Solution2.cpp
@@ -1,25 +1,18 @@
 #include <Solution.h>
 #include <iostream>
-using std::cout;
-using std::endl;
-
-int bs(const vector<int>& nums, int target) {
-	int start = 0, end = nums.size() - 1;
-	int mid = 0;
-
-	while (start <= end) {
-		mid = (start + end) / 2;
-		if (nums[mid] == target) return mid;
-		else if (nums[mid] < target) {
-			start = mid + 1;
-		} else {
-			end = mid - 1;
-		}
-	}
-	return start;
-}
 
 int Solution::searchInsert2(vector<int>& nums, int target) {
-	int result = bs(nums, target);
-	return result;
+	int l = 0, r = nums.size();
+	int mid = 0;
+
+	while (l < r) {
+		mid = l + ((r - l) >> 1);
+
+		if (nums[mid] >= target) {
+			r = mid;
+		} else {
+			l = mid + 1;
+		}
+	}
+	return nums[l] < target ? l + 1 : l;
 }
