4b936b571877a5da60909cba4ec7b59687e956f9
neilhuang <neilhuang@synology.com>
2022-10-07 16:27:17 +0800

    <LeetCode202109> 374. Guess Number Higher or Lower

--- 0374_Guess_Number_Higher_Or_Lower/Solution2.cpp
+++ 0374_Guess_Number_Higher_Or_Lower/Solution2.cpp
@@ -2,15 +2,17 @@
 #include <iostream>
 
 int Solution::guessNumber2(int n) {
-	int start = 1, end = n;
+	int l = 1, r = n;
 	int mid = 0;
-	int ret = 0;
-	while (start <= end) {
-		mid = ((end - start) / 2) + start;
-		ret = guess(mid);
-		if (ret == 0) return mid;
-		else if (ret == 1) start = mid + 1;
-		else end = mid - 1;
+
+	while (l < r) {
+		mid = l + ((r - l) >> 1);
+		int result = guess(mid);
+		if (result <= 0) {
+			r = mid;
+		} else {
+			l = mid + 1;
+		}
 	}
-	return 0;
+	return l;
 }
