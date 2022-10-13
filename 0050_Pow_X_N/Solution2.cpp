eb62e9802b2d58bb3ef0821563cca9ae79dd3898
neilhuang <neilhuang@synology.com>
2022-10-13 11:38:02 +0800

    <LeetCode202109> 50. Pow(x, n)

--- 0050_Pow_X_N/Solution2.cpp
+++ 0050_Pow_X_N/Solution2.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 double Solution::myPow2(double x, int n) {
+	if (x == 1 || n == 0) return 1;
 	double result = 1;
 	while (n) {
 		if (n & 1) result = n > 0 ? result * x : result / x;
