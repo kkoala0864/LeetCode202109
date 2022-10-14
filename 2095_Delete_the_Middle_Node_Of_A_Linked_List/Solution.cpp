ad5a49d82eaeb6ecaf88e96aece25ad840409f3a
neilhuang <neilhuang@synology.com>
2022-10-14 11:18:07 +0800

    <LeetCode202109> 2095. Delete the Middle Node of a Linked List

---
+++ 2095_Delete_the_Middle_Node_Of_A_Linked_List/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+
+// 1 2 3 4
+//
+// 1 2 3
+// 1 2
+// 1 2 3 4 5
+ListNode* Solution::deleteMiddle(ListNode* head) {
+	if (!head || !head->next) return nullptr;
+
+	ListNode* slow = head;
+	ListNode* fast = head->next;
+
+	while (fast->next && fast->next->next) {
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	slow->next = slow->next->next;
+	return head;
+}
