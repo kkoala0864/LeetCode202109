ee0bf6a57daccc8eb052f451557eb47aff95fc57
neilhuang <neilhuang@synology.com>
2022-10-14 22:12:25 +0800

    <LeetCode202109> 215. Kth Largest Element in an Array

--- 0215_Kth_Largest_Element_In_An_Array/Solution3.cpp
+++ 0215_Kth_Largest_Element_In_An_Array/Solution3.cpp
@@ -1,12 +1,30 @@
 #include <Solution.h>
 #include <iostream>
-#include <queue>
+using std::swap;
 
-using std::priority_queue;
+// quick select
+
+int partition(vector<int>& nums, int left , int right) {
+	int pivot = nums[left], l = left + 1, r = right;
+
+	while (l <= r) {
+		if (nums[l] < pivot && nums[r] > pivot) swap(nums[l], nums[r]);
+		if (nums[l] >= pivot) ++l;
+		if (nums[r] <= pivot) --r;
+	}
+	swap(nums[r], nums[left]);
+	return r;
+}
+
 
 int Solution::findKthLargest3(vector<int>& nums, int k) {
-	priority_queue<int> heap;
-	for (const auto& val : nums) heap.emplace(val);
-	while (--k > 0) heap.pop();
-	return heap.top();
+	int left = 0, right = nums.size() - 1;
+	int idx = 0;
+	while (1) {
+		idx = partition(nums, left, right);
+		if (idx == k-1) break;
+		else if (idx < k - 1) left = idx + 1;
+		else right = idx - 1;
+	}
+	return nums[idx];
 }
