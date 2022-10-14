b9a0f5678397d7ae3978cab3d0621737bc0ae638
neilhuang <neilhuang@synology.com>
2022-10-14 17:06:42 +0800

    <LeetCode202109> 264. Ugly Number II

--- 0264_Ugly_NumberII/Solution2.cpp
+++ 0264_Ugly_NumberII/Solution2.cpp
@@ -2,24 +2,32 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <climits>
 
 using std::vector;
 using std::min;
 
 int Solution::nthUglyNumber2(int n) {
-	vector<int> dp(n+1, 0);
-	dp[1] = 1;
-	int i2 = 1, i3 = 1, i5 = 1;
+	if (n == 1) return 1;
+	vector<int> dp(2, 1);
+	vector<int> idx = {1, 1, 1};
+	vector<int> multiple = {2, 3, 5};
 
 	for (int i = 2 ; i <= n ; ++i) {
-		int v2 = 2 * dp[i2];
-		int v3 = 3 * dp[i3];
-		int v5 = 5 * dp[i5];
-		int v = min({v2, v3, v5});
-		dp[i] = v;
-		if (v == v2) ++i2;
-		if (v == v3) ++i3;
-		if (v == v5) ++i5;
+		int minVal = INT_MAX;
+		vector<int> increase;
+		for (int j = 0 ; j < 3 ; ++j) {
+			int val = dp[idx[j]] * multiple[j];
+			if (val < minVal) {
+				increase.clear();
+				increase.emplace_back(j);
+				minVal = val;
+			} else if (val == minVal) {
+				increase.emplace_back(j);
+			}
+		}
+		dp.emplace_back(minVal);
+		for (const auto& v : increase) ++idx[v];
 	}
 	return dp[n];
 }
