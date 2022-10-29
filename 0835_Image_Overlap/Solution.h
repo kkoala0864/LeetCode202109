c1679f83ec27e1f0e30338a9d154db0bfc05bb04
neilhuang <neilhuang@synology.com>
2022-10-29 14:34:31 +0800

    <LeetCode202109> 835. Image Overlap

---
+++ 0835_Image_Overlap/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
