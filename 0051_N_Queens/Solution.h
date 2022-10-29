b1c927cbd26f90dd7e0935fd6b7bc514f6583633
neilhuang <neilhuang@synology.com>
2022-10-29 21:16:43 +0800

    <LeetCode202109> 51. N-Queens

--- 0051_N_Queens/Solution.h
+++ 0051_N_Queens/Solution.h
@@ -8,6 +8,7 @@ class Solution {
     public :
         Solution() {}
 	vector<vector<string>> solveNQueens(int n);
+	vector<vector<string>> solveNQueens2(int n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0051_N_Queens/main.cpp
+++ 0051_N_Queens/main.cpp
@@ -6,7 +6,7 @@ using std::endl;
 
 int main(void) {
 	Solution* test = new Solution();
-	auto result = test->solveNQueens(4);
+	auto result = test->solveNQueens2(4);
 	for (const auto& vec : result) {
 		for (const auto& val : vec) {
 			cout << val << endl;
