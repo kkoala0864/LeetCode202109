ab02e6a1746abd60ab890780036f20951607bf98
neilhuang <neilhuang@synology.com>
2022-10-31 10:11:36 +0800

    <LeetCode202109> 766. Toeplitz Matrix

---
+++ 0766_Toeplitz_Matrix/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+	Solution* test = new Solution();
+	cout << test->isToeplitzMatrix(input) << endl;
+	return 0;
+}
+
