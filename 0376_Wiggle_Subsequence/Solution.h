19f3de06dbb177714f06d34c440cabae39544930
neilhuang <neilhuang@synology.com>
2022-10-31 17:55:20 +0800

    <LeetCode202109> 376. Wiggle Subsequence

--- 0376_Wiggle_Subsequence/Solution.h
+++ 0376_Wiggle_Subsequence/Solution.h
@@ -8,6 +8,7 @@ class Solution {
 	int wiggleMaxLength(vector<int>& nums);
 	int wiggleMaxLength2(vector<int>& nums);
 	int wiggleMaxLength3(vector<int>& nums);
+	int wiggleMaxLength4(vector<int>& nums);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
---
+++ 0376_Wiggle_Subsequence/Solution4.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::wiggleMaxLength4(vector<int>& nums) {
+	vector<vector<int>> dp(nums.size(), vector<int>(2, 0));
+
+	dp[0][0] = 1;
+	dp[0][1] = 1;
+
+	int result = 1;
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		for (int j = i - 1 ; j >= 0 && (dp[i][0] == 0 || dp[i][1] == 0) ; --j) {
+			if (nums[i] - nums[j] > 0) {
+				dp[i][0] = max(dp[j][1] + 1, dp[i][0]);
+				result = max(dp[i][0], result);
+			}
+			if (nums[i] - nums[j] < 0) {
+				dp[i][1] = max(dp[j][0] + 1, dp[i][1]);
+				result = max(dp[i][1], result);
+			}
+		}
+	}
+	return result;
+}
