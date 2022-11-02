3ad198c036a795a0182e3b7920c2e17426dc5646
neilhuang <neilhuang@synology.com>
2022-11-02 14:13:20 +0800

    <LeetCode202109> 1198. Find Smallest Common Element in All Rows

---
+++ 1198_Find_Smallest_Common_Element_In_All_Rows/Solution.cpp
@@ -0,0 +1,31 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using std::max;
+using std::min;
+
+int Solution::smallestCommonElement(vector<vector<int>>& mat) {
+	int start = 0, end = INT_MAX;
+
+	for (const auto& m : mat) {
+		start = max(start, m[0]);
+		end = min(end, m.back());
+	}
+
+	int curVal = start;
+	bool change = false;
+	while (curVal <= end) {
+		change = false;
+		for (const auto& m : mat) {
+			int idx = lower_bound(m.begin(), m.end(), curVal) - m.begin();
+			if (idx == m.size()) return -1;
+			if (m[idx] == curVal) continue;
+			curVal = m[idx];
+			change = true;
+		}
+		if (!change) return curVal;
+	}
+	return -1;
+}
---
+++ 1198_Find_Smallest_Common_Element_In_All_Rows/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,2,3},{2,3,4},{2,3,5}};
+	Solution* test = new Solution();
+	cout << test->smallestCommonElement(input) << endl;
+	return 0;
+}
+
