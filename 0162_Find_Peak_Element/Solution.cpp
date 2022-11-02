4c810d70c8f6c0ba7234e553679c779716fdc12f
neilhuang <neilhuang@synology.com>
2022-11-02 19:37:24 +0800

    <LeetCode202109> 162. Find Peak Element

---
+++ 0162_Find_Peak_Element/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+
+int Solution::findPeakElement(vector<int>& nums) {
+	int l = 0, r = nums.size() - 1;
+
+	int mid = 0;
+
+	while (l < r) {
+		mid = l + ((r - l) >> 1);
+
+		if (nums[mid] > nums[mid+1]) {
+			r = mid;
+		} else {
+			l = mid + 1;
+		}
+	}
+	return l;
+}
