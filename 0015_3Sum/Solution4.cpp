8566c9091abe0799e994c4fe25c039aebac913c1
neilhuang <neilhuang@synology.com>
2022-11-04 11:13:21 +0800

    <LeetCode202109> 15. 3Sum

--- 0015_3Sum/Solution4.cpp
+++ 0015_3Sum/Solution4.cpp
@@ -7,30 +7,30 @@ using std::sort;
 using std::unordered_map;
 
 vector<vector<int>> Solution::threeSum4(vector<int>& nums) {
-	int n = nums.size();
-	vector<vector<int>> result;
 	sort(nums.begin(), nums.end());
 
-	for (int i = 0 ; i < n ; ++i) {
+	vector<vector<int>> result;
+	for (int i = 0 ; i < nums.size() ; ++i) {
 		int target = -nums[i];
-		int left = i + 1, right = n - 1;
-		while (left < right) {
-			int val = nums[left] + nums[right];
-			if (val == target) {
-				result.push_back({nums[i], nums[left], nums[right]});
-				while (left < n - 1 && nums[left] == nums[left+1]) ++left;
-				++left;
-				while (right > 0 && nums[right] == nums[right-1]) --right;
-				--right;
-			} else if (val < target) {
-				while (left < n - 1 && nums[left] == nums[left+1]) ++left;
-				++left;
+		int l = i + 1, r = nums.size() - 1;
+		while (l < r) {
+			int sum = nums[l] + nums[r];
+
+			if (sum == target) {
+				result.push_back({nums[i], nums[l], nums[r]});
+				while (l < r - 1 && nums[l] == nums[l+1]) ++l;
+				++l;
+				while (r > l + 1 && nums[r] == nums[r-1]) --r;
+				--r;
+			} else if (sum < target) {
+				while (l < r - 1 && nums[l] == nums[l+1]) ++l;
+				++l;
 			} else {
-				while (right > 0 && nums[right] == nums[right-1]) --right;
-				--right;
+				while (r > l + 1 && nums[r] == nums[r-1]) --r;
+				--r;
 			}
 		}
-		while (i < n - 1 && nums[i] == nums[i+1]) ++i;
+		while (i < nums.size() - 1 && nums[i] == nums[i+1]) ++i;
 	}
 	return result;
 }
