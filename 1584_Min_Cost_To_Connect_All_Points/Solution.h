081c3bde1e7424c5fcc436a4783766f567c18a05
neilhuang <neilhuang@synology.com>
2022-11-04 19:46:14 +0800

    <LeetCode202109> 1584. Min Cost to Connect All Points

--- 1584_Min_Cost_To_Connect_All_Points/Solution.h
+++ 1584_Min_Cost_To_Connect_All_Points/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int minCostConnectPoints(vector<vector<int>>& points);
+	int minCostConnectPoints2(vector<vector<int>>& points);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 1584_Min_Cost_To_Connect_All_Points/main.cpp
+++ 1584_Min_Cost_To_Connect_All_Points/main.cpp
@@ -9,6 +9,7 @@ int main(void) {
 	vector<vector<int>> input = {{0,0},{2,2},{3,10},{5,2},{7,0}};
 	Solution* test = new Solution();
 	cout << test->minCostConnectPoints(input) << endl;
+	cout << test->minCostConnectPoints2(input) << endl;
 	return 0;
 }
 
