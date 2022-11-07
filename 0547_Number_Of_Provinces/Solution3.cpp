6f822e9f768b5ba0a9238123da5af106725e76ed
neilhuang <neilhuang@synology.com>
2022-11-07 11:34:28 +0800

    <LeetCode202109> 547. Number of Provinces

--- 0547_Number_Of_Provinces/Solution3.cpp
+++ 0547_Number_Of_Provinces/Solution3.cpp
@@ -4,25 +4,24 @@
 
 using std::min;
 
-int find3(vector<int>& root, int val) {
-	if (root[val] == val) return val;
-	return root[val] = find3(root, root[val]);
+int find3(vector<int>& root, int idx) {
+	if (root[idx] == idx) return idx;
+	return root[idx] = find3(root, root[idx]);
 }
 
 int Solution::findCircleNum3(vector<vector<int>>& isConnected) {
-	vector<int> root;
 	int result = isConnected.size();
+	vector<int> root;
 	for (int i = 0 ; i < isConnected.size() ; ++i) root.emplace_back(i);
 
 	for (int i = 0 ; i < isConnected.size() ; ++i) {
-		for (int j = i + 1 ; j < isConnected.size() ; ++j) {
-			if (isConnected[i][j] == 1) {
-				int ir = find3(root, i);
-				int jr = find3(root, j);
-				if (ir == jr) continue;
-				--result;
-				root[ir] = root[jr] = min(ir, jr);
-			}
+		for (int j = i + 1; j < isConnected[i].size() ; ++j) {
+			if (isConnected[i][j] == 0) continue;
+			int lr = find3(root, i);
+			int rr = find3(root, j);
+			if (lr == rr) continue;
+			--result;
+			root[lr] = root[rr] = min(lr, rr);
 		}
 	}
 	return result;
