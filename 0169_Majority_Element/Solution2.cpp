ec8dcea8c6600b5bda6b197e9ccbcc3def7f7a56
neilhuang <neilhuang@synology.com>
2022-11-07 16:19:29 +0800

    <LeetCode202109> 169. Majority Element

--- 0169_Majority_Element/Solution2.cpp
+++ 0169_Majority_Element/Solution2.cpp
@@ -1,25 +1,20 @@
 #include <Solution.h>
 #include <iostream>
-#include <unordered_map>
-
-using std::unordered_map;
 
 int Solution::majorityElement2(vector<int>& nums) {
-	int i = 0;
-	int iter = 0;
-	int cur = 0;
-	while (iter < nums.size()) {
-		if (i == 0) {
-			cur = nums[iter];
-			i = 1;
+	int val = 0;
+	int cnt = 0;
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		if (cnt == 0) {
+			val = nums[i];
+			cnt = 1;
 		} else {
-			if (cur == nums[iter]) {
-				++i;
+			if (val == nums[i]) {
+				++cnt;
 			} else {
-				--i;
+				--cnt;
 			}
 		}
-		++iter;
 	}
-	return cur;
+	return val;
 }
