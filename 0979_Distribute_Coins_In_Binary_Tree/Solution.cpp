f311e8e3f715122c407f7ed2f3b746c7f80f5889
neilhuang <neilhuang@synology.com>
2022-11-08 11:06:39 +0800

    <LeetCode202109> 979. Distribute Coins in Binary Tree

---
+++ 0979_Distribute_Coins_In_Binary_Tree/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+int dfs(TreeNode* node, int& result) {
+	if (!node) return 0;
+
+	int lr = dfs(node->left, result);
+	int rr = dfs(node->right, result);
+
+	result += (abs(lr) + abs(rr));
+
+	return node->val + lr + rr - 1;
+}
+
+int Solution::distributeCoins(TreeNode* root) {
+	int result = 0;
+	dfs(root, result);
+	return result;
+}
