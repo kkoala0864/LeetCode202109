62912fa0e4d14ff8cd8fbb48ea4f8e3152943971
neilhuang <neilhuang@synology.com>
2022-11-28 14:33:59 +0800

    <LeetCode202109> 2225. Find Players With Zero or One Losses

---
+++ 2225_Find_players_With_Zero_Or_One_Losses/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+#include <set>
+
+using std::unordered_map;
+using std::set;
+
+vector<vector<int>> Solution::findWinners(vector<vector<int>>& matches) {
+	unordered_map<int, int> lose;
+	set<int> man;
+	for (const auto& m : matches) {
+		man.emplace(m[0]);
+		man.emplace(m[1]);
+		++lose[m[1]];
+	}
+
+	vector<vector<int>> result(2);
+
+	for (const auto& m : man) {
+		if (lose.count(m) == 0) {
+			result[0].emplace_back(m);
+		} else if (lose[m] == 1) {
+			result[1].emplace_back(m);
+		}
+	}
+	return result;
+}
---
+++ 2225_Find_players_With_Zero_Or_One_Losses/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<vector<int>> findWinners(vector<vector<int>>& matches);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
