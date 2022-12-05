3d77e0849b76da243adacc22bea35efc5c85b603
neilhuang <neilhuang@synology.com>
2022-12-05 14:53:22 +0800

    <LeetCode202109> 876. Middle of the Linked List

---
+++ 0876_Middle_Of_Linked_List/Solution2.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::middleNode2(ListNode* head) {
+	ListNode* slow = head;
+	ListNode* fast = head;
+	while (fast && fast->next) {
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return slow;
+}
