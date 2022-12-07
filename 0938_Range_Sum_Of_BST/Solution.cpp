6c693dcdd46cb08cc346b471dbcc2adc0a2c11dd
neilhuang <neilhuang@synology.com>
2022-12-07 18:34:05 +0800

    <LeetCode202109> 938. Range Sum of BST

---
+++ 0938_Range_Sum_Of_BST/Solution.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+void dfs(TreeNode* node, int low, int high, int& result) {
+	if (!node) return;
+	if (low <= node->val && node->val <= high) {
+		result += node->val;
+	}
+	dfs(node->left, low, high, result);
+	dfs(node->right, low, high, result);
+}
+
+int Solution::rangeSumBST(TreeNode* root, int low, int high) {
+	int result = 0;
+	dfs(root, low, high, result);
+	return result;
+}
