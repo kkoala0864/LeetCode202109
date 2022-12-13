b677e2f80b42fc55ec6e5b08b785594a3a011350
neilhuang <neilhuang@synology.com>
2022-12-13 20:53:02 +0800

    <LeetCode202109> 931. Minimum Falling Path Sum

---
+++ 0931_Minimum_Falling_Path_Sum/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+
+int Solution::minFallingPathSum(vector<vector<int>>& matrix) {
+	vector<int> prev = matrix[0];
+	vector<int> dp(matrix[0].size(), 0);
+
+	for (int i = 1 ; i < matrix.size() ; ++i) {
+		for (int j = 0 ; j < matrix[0].size() ; ++j) {
+			int local = INT_MAX;
+			if (j > 0) {
+				local = min(prev[j-1] + matrix[i][j], local);
+			}
+			local = min(prev[j] + matrix[i][j], local);
+			if (j < matrix[0].size() - 1) {
+				local = min(prev[j+1] + matrix[i][j], local);
+			}
+			dp[j] = local;
+		}
+		swap(dp, prev);
+	}
+	int result = INT_MAX;
+	for (int i = 0 ; i < prev.size() ; ++i) {
+		result = min(result, prev[i]);
+	}
+	return result;
+}
---
+++ 0931_Minimum_Falling_Path_Sum/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{2,1,3},{6,5,4},{7,8,9}};
+	Solution* test = new Solution();
+	cout << test->minFallingPathSum(input) << endl;
+	return 0;
+}
+
