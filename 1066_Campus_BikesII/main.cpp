f54cafc5e34ed335f4c616e64f8f151494dd2789
neilhuang <neilhuang@synology.com>
2022-12-19 20:13:43 +0800

    <LeetCode202109> 1066. Campus Bikes II

---
+++ 1066_Campus_BikesII/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> workers = {{0,0},{1,0},{2,0},{3,0},{4,0}};
+	vector<vector<int>> bikes = {{0,999},{1,999},{2,999},{3,999},{4,999}};
+	Solution* test = new Solution();
+	cout << test->assignBikes(workers, bikes) << endl;
+	return 0;
+}
+
