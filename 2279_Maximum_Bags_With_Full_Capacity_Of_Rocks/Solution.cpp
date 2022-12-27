36de4dce67407c6f93f774bff4238102fe9fc081
neilhuang <neilhuang@synology.com>
2022-12-27 19:22:26 +0800

    <LeetCode202109> 2279. Maximum Bags With Full Capacity of Rocks

---
+++ 2279_Maximum_Bags_With_Full_Capacity_Of_Rocks/Solution.cpp
@@ -0,0 +1,24 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+int Solution::maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+	vector<int> diff;
+	int size = capacity.size();
+
+	for (int i = 0 ; i < size ; ++i) {
+		diff.emplace_back(capacity[i] - rocks[i]);
+	}
+
+	sort(diff.begin(), diff.end());
+	int result = 0;
+	for (int i = 0 ; i < size && additionalRocks > 0 ; ++i) {
+		if (additionalRocks >= diff[i]) {
+			additionalRocks -= diff[i];
+			++result;
+		}
+	}
+	return result;
+}
---
+++ 2279_Maximum_Bags_With_Full_Capacity_Of_Rocks/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {2,3,4,5};
+	vector<int> rocks = {1,2,4,4};
+	int additionalRocks = 2;
+	Solution* test = new Solution();
+	cout << test->maximumBags(input, rocks, additionalRocks) << endl;
+	return 0;
+}
+
