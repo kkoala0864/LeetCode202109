bff6848637fac6e84afdcd9cdb364fd03899cc30
neilhuang <neilhuang@synology.com>
2022-12-29 19:41:55 +0800

    <LeetCode202109> 1962. Remove Stones to Minimize the Total

---
+++ 1962_Remove_Stones_To_Minimize_The_Total/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::priority_queue;
+
+// TC: O((n+k)logn);
+int Solution::minStoneSum(vector<int>& piles, int k) {
+	priority_queue<int> pq;
+	for (const auto& p : piles) pq.emplace(p);
+
+	while (k-- > 0) {
+		double cur = pq.top();
+		pq.pop();
+
+		cur = ceil(cur/2);
+		pq.emplace(cur);
+	}
+
+	int result = 0;
+
+	while (!pq.empty()) {
+		result += pq.top();
+		pq.pop();
+	}
+	return result;
+}
