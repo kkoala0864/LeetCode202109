758a3a3e10da3c74cd0ee0dc54ede1088ac0bf51
neilhuang <neilhuang@synology.com>
2023-01-03 20:12:48 +0800

    <LeetCode202109> 1056. Confusing Number

---
+++ 1056_Confusing_Number/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using std::string;
+using std::to_string;
+using std::unordered_set;
+
+bool Solution::confusingNumber(int n) {
+	string val = to_string(n);
+	unordered_set<char> invalid = {'2', '3', '4', '5', '7'};
+
+	for (int i = 0 ; i < val.size() ; ++i) {
+		if (invalid.count(val[i])) return false;
+
+		if (val[i] == '6') {
+			val[i] = '9';
+		} else if (val[i] == '9') {
+			val[i] = '6';
+		}
+	}
+	while (!val.empty() && val.back() == '0') {
+		val.pop_back();
+	}
+
+	reverse(val.begin(), val.end());
+
+	return n != stoi(val);
+}
