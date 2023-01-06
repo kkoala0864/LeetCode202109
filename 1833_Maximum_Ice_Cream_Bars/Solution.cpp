67cd644ddb822f60e747c2430ec9082a575a3143
neilhuang <neilhuang@synology.com>
2023-01-06 19:06:01 +0800

    <LeetCode202109> 1833. Maximum Ice Cream Bars

---
+++ 1833_Maximum_Ice_Cream_Bars/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+// TC : O(nlogn);
+// SC : O(1);
+int Solution::maxIceCream(vector<int>& costs, int coins) {
+	sort(costs.begin(), costs.end());
+
+	int result = 0;
+	for (int i = 0 ; i < costs.size() ; ++i) {
+		if (coins >= costs[i]) {
+			++result;
+			coins -= costs[i];
+		} else {
+			break;
+		}
+	}
+	return result;
+}
---
+++ 1833_Maximum_Ice_Cream_Bars/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,3,2,4,1};
+	Solution* test = new Solution();
+	cout << test->maxIceCream(input, 7) << endl;
+	return 0;
+}
+
