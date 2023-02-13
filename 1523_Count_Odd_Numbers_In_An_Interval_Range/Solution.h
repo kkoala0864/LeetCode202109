b91067b9e8e38ad7e990692d3374121ace7e72f5
neilhuang <neilhuang@synology.com>
2023-02-13 18:23:27 +0800

    <LeetCode202109> 1523. Count Odd Numbers in an Interval Range

---
+++ 1523_Count_Odd_Numbers_In_An_Interval_Range/Solution.h
@@ -0,0 +1,10 @@
+class Solution {
+    public :
+        Solution() {}
+	int countOdds(int low, int high);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 1523_Count_Odd_Numbers_In_An_Interval_Range/main.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    cout << test->countOdds(8, 10) << endl;
+    return 0;
+}
+
