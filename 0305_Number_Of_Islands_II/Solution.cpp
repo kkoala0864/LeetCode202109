b1e8792d16b52cf53706c947eb2c0d49cfe286f3
neilhuang <neilhuang@synology.com>
2023-02-19 18:58:46 +0800

    <LeetCode202109> 305. Number of Islands II

--- 0305_Number_Of_Islands_II/Solution.cpp
+++ 0305_Number_Of_Islands_II/Solution.cpp
@@ -1,46 +1,42 @@
 #include <Solution.h>
 #include <iostream>
 #include <algorithm>
-#include <unordered_set>
 
 using std::min;
-using std::pair;
-using std::unordered_set;
-using std::cout;
-using std::endl;
 
-int find(vector<vector<int>>& m, int n, int x, int y) {
-	if (m[x][y] == (x * n + y)) return (x * n + y);
-	return m[x][y] = find(m, n, m[x][y] / n, m[x][y] % n);
+int find(vector<int>& root, int idx) {
+	if (idx == root[idx]) return idx;
+	return root[idx] = find(root, root[idx]);
 }
 
 vector<int> Solution::numIslands2(int m, int n, vector<vector<int>>& positions) {
+	vector<int> root(m * n, INT_MAX);
+
 	vector<int> result;
 	vector<int> dir = {1, 0, -1, 0, 1};
-	vector<vector<int>> map(m, vector<int>(n, -1));
+	int cnt = 0;
 
-	for (const auto& ps : positions) {
-		if (map[ps[0]][ps[1]] != -1) {
-			result.emplace_back(result.back());
-			continue;
-		}
-		unordered_set<int> combine;
-		int root = INT_MAX;
+	for (const auto& p : positions) {
+		int x = p[0];
+		int y = p[1];
+		int val = x * n + y;
+		if (root[val] != INT_MAX) continue;
+		root[val] = val;
+		++cnt;
+
 		for (int i = 0 ; i < 4 ; ++i) {
-			int nx = ps[0] + dir[i];
-			int ny = ps[1] + dir[i+1];
-			if (nx < 0 || ny < 0 || nx >= m || ny >= n || map[nx][ny] == -1) continue;
-			int lrt = find(map, n, nx, ny);
-			root = min(lrt, root);
-			combine.emplace(lrt);
+			int nx = x + dir[i];
+			int ny = y + dir[i+1];
+			int nVal = nx * n + ny;
+			if (nx < 0 || ny < 0 || nx >= m || ny >= n || root[nVal] == INT_MAX) continue;
+			int lr = find(root, val);
+			int rr = find(root, nVal);
+
+			if (lr == rr) continue;
+			root[lr] = root[rr] = min(lr, rr);
+			--cnt;
 		}
-		map[ps[0]][ps[1]] = root == INT_MAX ? (ps[0] * n + ps[1]) : root;
-		int curCnt = result.empty() ? 0 : result.back() - combine.size();
-		for (const auto& val : combine) {
-			if (val == root) continue;
-			map[val / n][val % n] = root;
-		}
-		result.emplace_back(curCnt + 1);
+		result.emplace_back(cnt);
 	}
 	return result;
 }
--- 0305_Number_Of_Islands_II/main.cpp
+++ 0305_Number_Of_Islands_II/main.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<vector<int>> input = {{0,1},{1,2},{2,1},{1,0},{0,2},{0,0},{1,1}};
+	vector<vector<int>> input = {{0,0},{0,1},{1,2},{1,2}};
 	Solution* test = new Solution();
 	auto result = test->numIslands2(3, 3, input);
 	for (const auto& val : result) {
