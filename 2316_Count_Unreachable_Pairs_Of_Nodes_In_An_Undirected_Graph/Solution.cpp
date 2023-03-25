3f0a9aa208ebedae7164634014bb336471f5c5f4
neilhuang <neilhuang@synology.com>
2023-03-25 11:53:40 +0800

    <LeetCode202109> 2316. Count Unreachable Pairs of Nodes in an Undirected Graph

---
+++ 2316_Count_Unreachable_Pairs_Of_Nodes_In_An_Undirected_Graph/Solution.cpp
@@ -0,0 +1,39 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using std::min;
+using std::unordered_map;
+
+int find(vector<int>& root, int val) {
+	if (root[val] == val) return val;
+	return root[val] = find(root, root[val]);
+}
+
+long long Solution::countPairs(int n, vector<vector<int>>& edges) {
+	vector<int> root;
+	for (int i = 0 ; i < n ; ++i) root.emplace_back(i);
+
+	for (const auto& e : edges) {
+		int lr = find(root, e[0]);
+		int rr = find(root, e[1]);
+
+		if (lr == rr) continue;
+		root[lr] = root[rr] = min(lr, rr);
+	}
+
+	unordered_map<int, long long> cnt;
+	for (int i = 0 ; i < n ; ++i) {
+		++cnt[find(root, i)];
+	}
+
+	long long result = 0;
+	long long remainCnt = n;
+	for (const auto& e : cnt) {
+		result += e.second * (remainCnt - e.second);
+		remainCnt -= e.second;
+	}
+
+	return result;
+}
---
+++ 2316_Count_Unreachable_Pairs_Of_Nodes_In_An_Undirected_Graph/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    vector<vector<int>> input = {{0,2},{0,5},{2,4},{1,6},{5,4}};
+    cout << test->countPairs(7, input) << endl;
+    return 0;
+}
+
