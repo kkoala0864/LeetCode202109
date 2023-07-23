6b6a705baf5fef526c4675c484ac34c1a724bf16
neilhuang <neilhuang@synology.com>
2023-07-23 15:28:03 +0800

    <LeetCode202109> 894. All Possible Full Binary Trees

---
+++ 0894_All_Possible_Full_Binary_Trees/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void dfs(int n, vector<vector<TreeNode*>>& dp) {
+	if (!dp[n].empty()) return;
+
+	for (int i = 1 ; i < n ; i += 2) {
+		dfs(i, dp);
+		dfs(n - i - 1, dp);
+
+		for (const auto& l : dp[i]) {
+			for (const auto& r : dp[n-i-1]) {
+				TreeNode* node = new TreeNode(0);
+				node->left = l;
+				node->right = r;
+				dp[n].emplace_back(node);
+			}
+		}
+	}
+}
+
+vector<TreeNode*> Solution::allPossibleFBT(int n) {
+	vector<vector<TreeNode*>> dp(n + 1, vector<TreeNode*>());
+	dp[1].emplace_back(new TreeNode(0));
+	dfs(n, dp);
+	return dp[n];
+}
---
+++ 0894_All_Possible_Full_Binary_Trees/main.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+    Solution* test = new Solution();
+    test->allPossibleFBT(3);
+    return 0;
+}
+
