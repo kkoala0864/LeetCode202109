4eef19d7ebbf00508dcb2ac53aee157f1ce76c0d
neilhuang <neilhuang@synology.com>
2023-08-23 20:16:32 +0800

    <LeetCode202109> 767. Reorganize String

---
+++ 0767_Reorganize_String/Solution.cpp
@@ -0,0 +1,40 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+#include <map>
+
+using std::vector;
+using std::map;
+
+string Solution::reorganizeString(string s) {
+	vector<int> cnt(26, 0);
+	int h = s.size() / 2;
+	for (const auto& c : s) ++cnt[c-'a'];
+
+	map<int, vector<char>, std::greater<int>> m;
+
+	for (int i = 0 ; i < 26 ; ++i) {
+		m[cnt[i]].emplace_back('a' + i);
+	}
+	if (s.size() - m.begin()->first < (m.begin()->first - 1)) {
+		return "";
+	}
+
+	string ss;
+	for (const auto& e : m) {
+		for (const auto& c : e.second) {
+			ss += string(e.first, c);
+		}
+	}
+
+	int l = 0, i = 0;
+	int r = h + (s.size() & 1);
+
+	while (i < s.size()) {
+		s[i++] = ss[l++];
+		if (i < s.size()) {
+			s[i++] = ss[r++];
+		}
+	}
+	return s;
+}
