705302ba75194ae43599a0d0b3b4356a907e8e83
neilhuang <neilhuang@synology.com>
2023-08-29 19:43:46 +0800

    <LeetCode202109> 2483. Minimum Penalty for a Shop

---
+++ 2483_Minimum_Penalty_For_A_Shop/Solution.h
@@ -0,0 +1,14 @@
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	int bestClosingTime(string customers);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
