ac060a7f3455dbb640043d7491253f3681eef1d8
neilhuang <neilhuang@synology.com>
2023-10-23 19:18:55 +0800

    <LeetCode202109> 1425. Constrained Subsequence Sum

---
+++ 1425_Constrained_Subsequence_Sum/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using std::map;
+using std::greater;
+using std::max;
+
+int Solution::constrainedSubsetSum(vector<int>& nums, int k) {
+	map<int, int, greater<int>> m;
+	vector<int> dp(nums.size(), INT_MIN);
+	int result = nums[0];
+	dp[0] = nums[0];
+	++m[dp[0]];
+
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		if (i - k - 1 >= 0) {
+			--m[dp[i-k-1]];
+			if (m[dp[i-k-1]] == 0) m.erase(dp[i-k-1]);
+		}
+
+		dp[i] = max(dp[i], m.begin()->first + nums[i]);
+		dp[i] = max(dp[i], nums[i]);
+		++m[dp[i]];
+		result = max(result, dp[i]);
+	}
+	return result;
+}
---
+++ 1425_Constrained_Subsequence_Sum/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {10,2,-10,5,20};
+	Solution* test = new Solution();
+	cout << test->constrainedSubsetSum(input, 2) << endl;
+	return 0;
+}
+
