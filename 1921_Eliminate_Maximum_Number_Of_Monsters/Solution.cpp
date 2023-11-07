9eda0888a0535ff8a10fe9e4c27b538e7ba244ad
neilhuang <neilhuang@synology.com>
2023-11-07 15:05:08 +0800

    <LeetCode202109> 1921. Eliminate Maximum Number of Monsters

---
+++ 1921_Eliminate_Maximum_Number_Of_Monsters/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+
+int Solution::eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+	int size = dist.size();
+	vector<int> t;
+	for (int i = 0 ; i < size ; ++i) {
+		int rest = dist[i] % speed[i] > 0 ? 1 : 0;
+		t.emplace_back((dist[i] / speed[i]) + rest);
+	}
+
+	sort(t.begin(), t.end());
+
+	int result = 0;
+	int baseT = 0;
+
+	for (int i = 0 ; i < size ; ++i) {
+		if (t[i] <= baseT) {
+			return i;
+		} else {
+			++result;
+		}
+		++baseT;
+	}
+	return size;
+}
---
+++ 1921_Eliminate_Maximum_Number_Of_Monsters/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> dist = {3,2,4};
+	vector<int> speed = {5,3,2};
+	Solution* test = new Solution();
+	cout << test->eliminateMaximum(dist, speed) << endl;
+	return 0;
+}
+
