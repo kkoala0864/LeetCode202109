aacba45adc77ae877af2caf9fce9a4f969d4730d
neilhuang <neilhuang@synology.com>
2023-11-09 17:52:19 +0800

    <LeetCode202109> 1759. Count Number of Homogenous Substrings

---
+++ 1759_Count_Number_Of_Homogenous_Substrings/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+using std::cout;
+using std::endl;
+
+int Solution::countHomogenous(string s) {
+	int mod = 1e9 + 7;
+	int result = 0;
+	int cnt = 0;
+	for (int i = 0 ; i < s.size() ; ++i) {
+		if (i == 0 || s[i] != s[i-1]) {
+			cnt = 0;
+		}
+		++cnt;
+		result += cnt;
+		result %= mod;
+	}
+
+	return result;
+}
---
+++ 1759_Count_Number_Of_Homogenous_Substrings/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "zzzzz";
+	Solution* test = new Solution();
+	cout << test->countHomogenous(input) << endl;
+	return 0;
+}
+
