fad6d9df60ca25f8c9a414502e6c61297c5ffb9b
neilhuang <neilhuang@synology.com>
2023-11-21 17:47:21 +0800

    <LeetCode202109> 2391. Minimum Amount of Time to Collect Garbage

---
+++ 2391_Minimum_Amount_Of_Time_To_Collect_Garbage/Solution.cpp
@@ -0,0 +1,33 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+using std::max;
+
+int Solution::garbageCollection(vector<string>& garbage, vector<int>& travel) {
+	vector<int> t(3, 0);
+	vector<int> preSum(1, 0);
+	int sum = 0;
+	int result = 0;
+
+	for (int i = 0 ; i < garbage.size() ; ++i) {
+		if (i < garbage.size() - 1) {
+			sum += travel[i];
+			preSum.emplace_back(sum);
+		}
+		for (const auto& c : garbage[i]) {
+			if (c == 'M') {
+				t[0] = max(t[0], i);
+			} else if (c == 'P') {
+				t[1] = max(t[1], i);
+			} else {
+				t[2] = max(t[2], i);
+			}
+		}
+		result += garbage[i].size();
+	}
+	for (int i = 0 ; i < 3 ; ++i) {
+		result += preSum[t[i]];
+	}
+
+	return result;
+}
---
+++ 2391_Minimum_Amount_Of_Time_To_Collect_Garbage/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"MMM","PGM","GP"};
+	vector<int> travel = {3, 10};
+	Solution* test = new Solution();
+	cout << test->garbageCollection(input, travel) << endl;
+	return 0;
+}
+
