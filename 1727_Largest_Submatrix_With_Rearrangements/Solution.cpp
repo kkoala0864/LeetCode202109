551996e467266e948710d3319f19dc52d438817a
neilhuang <neilhuang@synology.com>
2023-11-26 15:52:45 +0800

    <LeetCode202109> 1727. Largest Submatrix With Rearrangements

---
+++ 1727_Largest_Submatrix_With_Rearrangements/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::largestSubmatrix(vector<vector<int>>& matrix) {
+	int n = matrix[0].size();
+	int m = matrix.size();
+
+	for (int i = 0 ; i < m ; ++i) {
+		for (int j = 0 ; j < n ; ++j) {
+			if (i == 0 || matrix[i][j] == 0) continue;
+			matrix[i][j] = matrix[i-1][j] + 1;
+		}
+	}
+
+
+	vector<int> tmp;
+	int result = 0;
+	for (int i = 0 ; i < m ; ++i) {
+		tmp = matrix[i];
+		sort(tmp.begin(), tmp.end(), std::greater<int>());
+
+		for (int j = 0 ; j < n ; ++j) {
+			result = max(result, tmp[j] * (j + 1));
+		}
+	}
+	return result;
+}
