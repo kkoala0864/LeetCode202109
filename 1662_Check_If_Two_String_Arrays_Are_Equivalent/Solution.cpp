e1be0d87edac83857f03121413157f3142beb5b5
neilhuang <neilhuang@synology.com>
2023-12-01 11:45:28 +0800

    <Leetcode202109> 1662. Check If Two String Arrays are Equivalent

---
+++ 1662_Check_If_Two_String_Arrays_Are_Equivalent/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Solution::arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+	int x = 0, y = 0;
+	for (int i = 0 ; i < word1.size() ; ++i) {
+		for (const auto& c : word1[i]) {
+			if (x >= word2.size()) return false;
+			if (c != word2[x][y++]) {
+				return false;
+			}
+			if (y >= word2[x].size()) {
+				++x;
+				y = 0;
+			}
+		}
+	}
+	return x == word2.size() && y == 0;
+}
