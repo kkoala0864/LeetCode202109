825e85d72490d709469c837c47e8c5895e382636
neilhuang <neilhuang@synology.com>
2023-12-02 12:53:24 +0800

    <LeetCode202109> 487. Max Consecutive Ones II

--- 0487_Max_Consecutive_OnesII/main.cpp
+++ 0487_Max_Consecutive_OnesII/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
 	vector<int> input = {1,0,1,1,0};
 	Solution* test = new Solution();
 	cout << test->findMaxConsecutiveOnes(input) << endl;
+	cout << test->findMaxConsecutiveOnes2(input) << endl;
 	return 0;
 }
 
