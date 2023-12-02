e443ff62ea60b119f7cfbaeb588cf1df3cabf4f6
neilhuang <neilhuang@synology.com>
2023-12-02 12:23:10 +0800

    <LeetCode202109> 1099. Two Sum Less Than K

---
+++ 1099_Two_Sum_Less_Than_K/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {34,23,1,24,75,33,54,8};
+	Solution* test = new Solution();
+	cout << test->twoSumLessThanK(input, 60) << endl;
+	return 0;
+}
+
