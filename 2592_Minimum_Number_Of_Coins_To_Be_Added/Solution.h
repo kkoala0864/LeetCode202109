86724a606fe0b1234424876dc062fcd45a99ff67
neilhuang <neilhuang@synology.com>
2023-12-03 16:10:20 +0800

    <LeetCode202109> 2952. Minimum Number of Coins to be Added

---
+++ 2592_Minimum_Number_Of_Coins_To_Be_Added/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int minimumAddedCoins(vector<int>& coins, int target);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
