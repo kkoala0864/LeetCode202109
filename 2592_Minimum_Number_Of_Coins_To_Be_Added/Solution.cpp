ac4e57f248a3c3a54b27005004cbfbf98fcb2255
neilhuang <neilhuang@synology.com>
2023-12-03 16:10:20 +0800

    <LeetCode202109> 2952. Minimum Number of Coins to be Added

---
+++ 2592_Minimum_Number_Of_Coins_To_Be_Added/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::minimumAddedCoins(vector<int>& coins, int target) {
+	sort(coins.begin(), coins.end());
+	int result = 0;
+	long m = 0;
+	if (coins[0] != 1) {
+		++result;
+		m = 1;
+	}
+	for (const auto& c : coins) {
+		while (m < (c - 1)) {
+			m = max(m, m + m + 1);
+			++result;
+		}
+		m += c;
+	}
+	while (m < target) {
+		m = max(m, m + m + 1);
+		++result;
+	}
+
+	return result;
+}
---
+++ 2592_Minimum_Number_Of_Coins_To_Be_Added/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,4,10};
+	Solution* test = new Solution();
+	cout << test->minimumAddedCoins(input, 19) << endl;
+	return 0;
+}
+
