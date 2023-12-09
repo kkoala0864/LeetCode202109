bc553d031b7796dc89a5c25201dcc16a6f073813
neilhuang <neilhuang@synology.com>
2023-12-09 14:43:39 +0800

    <LeetCode202109> 2946. Matrix Similarity After Cyclic Shifts

---
+++ 2946_Matrix_Similarity_After_Cyclic_Shifts/Solution.h
@@ -0,0 +1,14 @@
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	bool areSimilar(vector<vector<int>>& mat, int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
