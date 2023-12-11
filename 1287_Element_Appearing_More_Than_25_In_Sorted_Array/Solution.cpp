0316f2bf0288570101f898597e918f6882a65087
neilhuang <neilhuang@synology.com>
2023-12-11 18:40:07 +0800

    <LeetCode202109> 1287. Element Appearing More Than 25% In Sorted Array

---
+++ 1287_Element_Appearing_More_Than_25_In_Sorted_Array/Solution.cpp
@@ -0,0 +1,10 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::findSpecialInteger(vector<int>& arr) {
+	int v = arr.size() / 4;
+	for (int i = 0 ; i < (arr.size() - v) ; ++i) {
+		if (arr[i] == arr[i+v]) return arr[i];
+	}
+	return -1;
+}
