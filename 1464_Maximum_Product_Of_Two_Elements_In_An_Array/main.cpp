114d90c4441cd142308f5296f80104439d4ca369
neilhuang <neilhuang@synology.com>
2023-12-12 18:22:18 +0800

    <LeetCode202109> 1464. Maximum Product of Two Elements in an Array

---
+++ 1464_Maximum_Product_Of_Two_Elements_In_An_Array/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,4,5,2};
+	Solution* test = new Solution();
+	cout << test->maxProduct(input) << endl;
+	return 0;
+}
+
