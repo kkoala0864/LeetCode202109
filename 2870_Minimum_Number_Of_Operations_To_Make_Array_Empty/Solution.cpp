35c0c79dc3fce0d689fd6cab589324bd80a9ffa6
neilhuang <neilhuang@synology.com>
2024-01-04 17:29:41 +0800

    <LeetCode202109> 2870. Minimum Number of Operations to Make Array Empty

---
+++ 2870_Minimum_Number_Of_Operations_To_Make_Array_Empty/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+
+int Solution::minOperations(vector<int>& nums) {
+	unordered_map<int, int> m;
+	for (const auto& v : nums) ++m[v];
+
+	int result = 0;
+	for (const auto& e : m) {
+		if (e.second == 1) return -1;
+		if (e.second == 2) ++result;
+		else {
+			result += (e.second / 3);
+			if (e.second % 3 != 0) ++result;
+		}
+	}
+	return result;
+}
