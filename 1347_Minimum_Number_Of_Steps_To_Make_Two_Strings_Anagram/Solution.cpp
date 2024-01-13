cddb4c92f2b24a50fa6e1bb581e2fd344cd3f788
neilhuang <neilhuang@synology.com>
2024-01-13 10:53:33 +0800

    <LeetCode202109> 1347. Minimum Number of Steps to Make Two Strings Anagram

---
+++ 1347_Minimum_Number_Of_Steps_To_Make_Two_Strings_Anagram/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+using std::vector;
+
+int Solution::minSteps(string s, string t) {
+	vector<int> cnt1(26, 0), cnt2(26, 0);
+
+	for (int i = 0 ; i < s.size() ; ++i) {
+		++cnt1[s[i]-'a'];
+		++cnt2[t[i]-'a'];
+	}
+
+	int cnt = 0;
+	for (int i = 0 ; i < 26 ; ++i) {
+		cnt += abs(cnt1[i] - cnt2[i]);
+	}
+	return cnt / 2;
+}
