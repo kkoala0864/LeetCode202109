85fe5c11915fe989f17d8963c722e05a300d55e2
neilhuang <neilhuang@synology.com>
2024-01-15 10:26:19 +0800

    <LeetCode202109> 1657. Determine if Two Strings Are Close

--- 1657_Determine_If_Two_Strings_Are_Close/Solution.h
+++ 1657_Determine_If_Two_Strings_Are_Close/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	bool closeStrings(string word1, string word2);
+	bool closeStrings2(string word1, string word2);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
