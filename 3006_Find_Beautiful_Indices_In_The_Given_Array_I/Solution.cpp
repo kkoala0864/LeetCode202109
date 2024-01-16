033821993aaca513cb741345120dd519a1201bd6
neilhuang <neilhuang@synology.com>
2024-01-16 16:49:34 +0800

    <LeetCode202109> 3006. Find Beautiful Indices in the Given Array I

--- 3006_Find_Beautiful_Indices_In_The_Given_Array_I/Solution.cpp
+++ 3006_Find_Beautiful_Indices_In_The_Given_Array_I/Solution.cpp
@@ -1,20 +1,51 @@
 #include <Solution.h>
 #include <iostream>
-using std::cout;
-using std::endl;
+#include <vector>
+using std::vector;
+void getNext(const string& a, vector<int>& v) {
+	int prev = 0;
+	int cur = 1;
+	v.emplace_back(0);
+
+	while (cur < a.size()) {
+		if (a[prev] == a[cur]) {
+			++prev;
+			++cur;
+			v.emplace_back(prev);
+		} else {
+			if (prev == 0) {
+				++cur;
+				v.emplace_back(0);
+			} else prev = v[prev - 1];
+		}
+	}
+}
+
+void getIdx(const string& s, const string& a, const vector<int>& next, vector<int>& idx) {
+	int sIdx = 0;
+	int aIdx = 0;
+	while (sIdx < s.size()) {
+		if (s[sIdx] == a[aIdx]) {
+			++aIdx;
+			++sIdx;
+			if (aIdx == a.size()) {
+				idx.emplace_back(sIdx - a.size());
+				aIdx = 0;
+			}
+		} else {
+			if (aIdx == 0) ++sIdx;
+			else aIdx = next[aIdx - 1];
+		}
+	}
+}
 
 vector<int> Solution::beautifulIndices(string s, string a, string b, int k) {
+	vector<int> next1, next2;
+	getNext(a, next1);
+	getNext(b, next2);
 	vector<int> aIdx, bIdx;
-
-	for (int i = 0 ; i <= (int)(s.size() - a.size()) ; ++i) {
-		if (s[i] != a[0]) continue;
-		if (s.substr(i, a.size()) == a) aIdx.emplace_back(i);
-	}
-
-	for (int i = 0 ; i <= (int)(s.size() - b.size()) ; ++i) {
-		if (s[i] != b[0]) continue;
-		if (s.substr(i, b.size()) == b) bIdx.emplace_back(i);
-	}
+	getIdx(s, a, next1, aIdx);
+	getIdx(s, b, next2, bIdx);
 	vector<int> result;
 	for (const auto& ai : aIdx) {
 		auto bi = lower_bound(bIdx.begin(), bIdx.end(), (ai - k));
