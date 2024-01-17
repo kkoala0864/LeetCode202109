83708d7b41d927cd735d4a085e3a998bc743ab81
neilhuang <neilhuang@synology.com>
2024-01-17 10:57:04 +0800

    <LeetCode202109> 1207. Unique Number of Occurrences

---
+++ 1207_Unique_Number_Of_Occurrences/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+using std::unordered_map;
+using std::unordered_set;
+
+bool Solution::uniqueOccurrences(vector<int>& arr) {
+	unordered_map<int, int> cnt;
+	for (const auto& v : arr) ++cnt[v];
+	unordered_set<int> us;
+
+	for(const auto& e : cnt) {
+		if (us.count(e.second)) return false;
+		us.emplace(e.second);
+	}
+	return true;
+}
---
+++ 1207_Unique_Number_Of_Occurrences/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {-3,0,1,-3,1,1,1,-3,10,0};
+	Solution* test = new Solution();
+	cout << test->uniqueOccurrences(input) << endl;
+	return 0;
+}
+
