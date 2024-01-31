c2d66bb39089366b0c257d7c0a6d0b43d6b63cfb
neilhuang <neilhuang@synology.com>
2024-01-31 16:28:08 +0800

    <LeetCode202109> 547. Number of Provinces

--- 0547_Number_Of_Provinces/Solution3.cpp
+++ 0547_Number_Of_Provinces/Solution3.cpp
@@ -1,7 +1,9 @@
 #include <Solution.h>
 #include <iostream>
 #include <algorithm>
+#include <unordered_set>
 
+using std::unordered_set;
 using std::min;
 
 int find3(vector<int>& root, int idx) {
@@ -10,19 +12,21 @@ int find3(vector<int>& root, int idx) {
 }
 
 int Solution::findCircleNum3(vector<vector<int>>& isConnected) {
-	int result = isConnected.size();
 	vector<int> root;
 	for (int i = 0 ; i < isConnected.size() ; ++i) root.emplace_back(i);
 
 	for (int i = 0 ; i < isConnected.size() ; ++i) {
-		for (int j = i + 1; j < isConnected[i].size() ; ++j) {
+		for (int j = i + 1 ; j < isConnected.size() ; ++j) {
 			if (isConnected[i][j] == 0) continue;
-			int lr = find3(root, i);
-			int rr = find3(root, j);
-			if (lr == rr) continue;
-			--result;
-			root[lr] = root[rr] = min(lr, rr);
+			int r1 = find3(root, i);
+			int r2 = find3(root, j);
+
+			if (r1 == r2) continue;
+			root[r1] = root[r2] = min(r1, r2);
 		}
 	}
-	return result;
+	unordered_set<int> us;
+	for (auto& v : root) us.emplace(find3(root, v));
+	return us.size();
 }
+
--- 0547_Number_Of_Provinces/main.cpp
+++ 0547_Number_Of_Provinces/main.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<vector<int>> input = {{1,1,0},{1,1,0},{0,0,1}};
+	vector<vector<int>> input = {{1,0,0,1},{0,1,1,0},{0,1,1,1},{1,0,1,1}};
 	Solution* test = new Solution();
 	cout << test->findCircleNum(input) << endl;
 	cout << test->findCircleNum3(input) << endl;
