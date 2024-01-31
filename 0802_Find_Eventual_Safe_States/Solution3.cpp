2517950ec2b03842d787e410010059fe4497ac5d
neilhuang <neilhuang@synology.com>
2024-01-31 16:55:44 +0800

    <LeetCode202109> 802. Find Eventual Safe States

--- 0802_Find_Eventual_Safe_States/Solution3.cpp
+++ 0802_Find_Eventual_Safe_States/Solution3.cpp
@@ -3,32 +3,32 @@
 #include <queue>
 #include <algorithm>
 
-using std::sort;
 using std::queue;
+using std::sort;
 
 vector<int> Solution::eventualSafeNodes3(vector<vector<int>>& graph) {
-	vector<int> result;
+	vector<int> oc;
 	queue<int> que;
-	vector<int> cnt(graph.size(), 0);
-	vector<vector<int>> indegree(graph.size(), vector<int>());
+
+	vector<vector<int>> from(graph.size(), vector<int>());
 	for (int i = 0 ; i < graph.size() ; ++i) {
-		if (graph[i].empty()) que.emplace(i);
-		else {
-			cnt[i] = graph[i].size();
-			for (const auto& e : graph[i]) {
-				indegree[e].emplace_back(i);
-			}
+		for (const auto& f : graph[i]) {
+			from[f].emplace_back(i);
+		}
+		oc.emplace_back(graph[i].size());
+		if (graph[i].empty()) {
+			que.emplace(i);
 		}
 	}
 
+	vector<int> result;
 	while (!que.empty()) {
 		int cur = que.front();
 		que.pop();
-
 		result.emplace_back(cur);
-		for (const auto& e : indegree[cur]) {
-			--cnt[e];
-			if (cnt[e] == 0) que.emplace(e);
+		for (const auto& f : from[cur]) {
+			--oc[f];
+			if (oc[f] == 0) que.emplace(f);
 		}
 	}
 	sort(result.begin(), result.end());
