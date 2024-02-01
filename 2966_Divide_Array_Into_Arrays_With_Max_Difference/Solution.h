9321eea8465d8d7ea452038e8fccade2ee8adcdd
neilhuang <neilhuang@synology.com>
2024-02-01 14:42:57 +0800

    <LeetCode202109> 2966. Divide Array Into Arrays With Max Difference

---
+++ 2966_Divide_Array_Into_Arrays_With_Max_Difference/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<vector<int>> divideArray(vector<int>& nums, int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
