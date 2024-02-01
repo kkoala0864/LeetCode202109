068423579e74264c93fb204e30628c413e3cd748
neilhuang <neilhuang@synology.com>
2024-02-01 20:28:50 +0800

    <LeetCode202109> 28. Find the Index of the First Occurrence in a String

--- 0028_Implement_strStr/main.cpp
+++ 0028_Implement_strStr/main.cpp
@@ -5,11 +5,12 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	string input = "aaa";
-	string needle = "aaaa";
+	string input = "leetcode";
+	string needle = "leeto";
 	Solution* test = new Solution();
 	cout << test->strStr(input, needle) << endl;
 	cout << test->strStr2(input, needle) << endl;
+	cout << test->strStr3(input, needle) << endl;
 	return 0;
 }
 
