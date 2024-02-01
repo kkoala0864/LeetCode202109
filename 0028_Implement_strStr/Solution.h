e1542c8b86e3329573657e4a039acafbd2b06ea3
neilhuang <neilhuang@synology.com>
2024-02-01 20:28:50 +0800

    <LeetCode202109> 28. Find the Index of the First Occurrence in a String

--- 0028_Implement_strStr/Solution.h
+++ 0028_Implement_strStr/Solution.h
@@ -7,6 +7,7 @@ class Solution {
         Solution() {}
 	int strStr(string haystack, string needle);
 	int strStr2(string haystack, string needle);
+	int strStr3(string haystack, string needle);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
