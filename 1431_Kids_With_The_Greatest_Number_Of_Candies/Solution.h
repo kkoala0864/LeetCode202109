b0bff39a0282705adb3ad3f47ea0de0d4d847ba1
neilhuang <neilhuang@synology.com>
2024-02-01 16:24:59 +0800

    <LeetCode202109> 1431. Kids With the Greatest Number of Candies

---
+++ 1431_Kids_With_The_Greatest_Number_Of_Candies/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
