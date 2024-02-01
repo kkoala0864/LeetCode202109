355ab3c152ac69058194a920cb5c9e0ddf35646e
neilhuang <neilhuang@synology.com>
2024-02-01 15:25:14 +0800

    <LeetCode202109> 1466. Reorder Routes to Make All Paths Lead to the City Zero

--- 1466_Reorder_Routes_To_Make_All_Paths_Lead_To_The_City_Zero/Solution.h
+++ 1466_Reorder_Routes_To_Make_All_Paths_Lead_To_The_City_Zero/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int minReorder(int n, vector<vector<int>>& connections);
+	int minReorder2(int n, vector<vector<int>>& connections);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 1466_Reorder_Routes_To_Make_All_Paths_Lead_To_The_City_Zero/main.cpp
+++ 1466_Reorder_Routes_To_Make_All_Paths_Lead_To_The_City_Zero/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
 	vector<vector<int>> input = {{1,0},{1,2},{3,2},{3,4}};
 	Solution* test = new Solution();
 	cout << test->minReorder(5, input) << endl;
+	cout << test->minReorder2(5, input) << endl;
 	return 0;
 }
 
