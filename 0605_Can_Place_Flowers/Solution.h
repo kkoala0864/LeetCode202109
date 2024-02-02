5d0070e7864d7c6139d3521c7f69d51217517b6c
neilhuang <neilhuang@synology.com>
2024-02-02 15:50:53 +0800

    <LeetCode202109> 605. Can Place Flowers

--- 0605_Can_Place_Flowers/Solution.h
+++ 0605_Can_Place_Flowers/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	bool canPlaceFlowers(vector<int>& flowerbed, int n);
+	bool canPlaceFlowers2(vector<int>& flowerbed, int n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
---
+++ 0605_Can_Place_Flowers/Solution2.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Solution::canPlaceFlowers2(vector<int>& flowerbed, int n) {
+	for (int i = 0 ; i < flowerbed.size() ; ++i) {
+		if (flowerbed[i] == 1) continue;
+		if (i > 0) {
+			if (flowerbed[i-1] == 1) continue;
+		}
+		if (i < flowerbed.size() - 1) {
+			if (flowerbed[i+1] == 1) continue;
+		}
+		flowerbed[i] = 1;
+		--n;
+	}
+	return n <= 0;
+}
