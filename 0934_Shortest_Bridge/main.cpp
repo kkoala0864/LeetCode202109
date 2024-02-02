26e634db79e49cea55b3daba0a1f70a07865f96c
neilhuang <neilhuang@synology.com>
2024-02-02 14:54:09 +0800

    <LeetCode202109> 934. Shortest Bridge

--- 0934_Shortest_Bridge/main.cpp
+++ 0934_Shortest_Bridge/main.cpp
@@ -7,7 +7,8 @@ using std::endl;
 int main(void) {
 	vector<vector<int>> input = {{1,1,1,1,1},{1,0,0,0,1},{1,0,1,0,1},{1,0,0,0,1},{1,1,1,1,1}};
 	Solution* test = new Solution();
-	cout << test->shortestBridge(input) << endl;
+	//cout << test->shortestBridge(input) << endl;
+	cout << test->shortestBridge2(input) << endl;
 	return 0;
 }
 
