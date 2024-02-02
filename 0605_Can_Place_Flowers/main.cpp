cc5f5c8db75d577029ea13b5bf35475edb40e446
neilhuang <neilhuang@synology.com>
2024-02-02 15:50:53 +0800

    <LeetCode202109> 605. Can Place Flowers

--- 0605_Can_Place_Flowers/main.cpp
+++ 0605_Can_Place_Flowers/main.cpp
@@ -7,7 +7,8 @@ using std::endl;
 int main(void) {
 	vector<int> input = {1,0,0,0,1};
 	Solution* test = new Solution();
-	cout << test->canPlaceFlowers(input, 1) << endl;
+	//cout << test->canPlaceFlowers(input, 1) << endl;
+	cout << test->canPlaceFlowers2(input, 1) << endl;
 	return 0;
 }
 
