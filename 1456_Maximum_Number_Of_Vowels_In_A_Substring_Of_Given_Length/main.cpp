38815f0f29c5056d3506dfee0f47e1483f668b5c
neilhuang <neilhuang@synology.com>
2024-02-06 16:13:04 +0800

    <LeetCode202109> 1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ 1456_Maximum_Number_Of_Vowels_In_A_Substring_Of_Given_Length/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "abciiidef";
+	Solution* test = new Solution();
+	cout << test->maxVowels(input, 3) << endl;
+	return 0;
+}
+
