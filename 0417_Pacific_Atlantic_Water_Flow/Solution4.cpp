3c9fe5266e67970ed63158317f1cbf3177e60ffd
neilhuang <neilhuang@synology.com>
2024-02-07 11:51:05 +0800

    <LeetCode202109> 417. Pacific Atlantic Water Flow

--- 0417_Pacific_Atlantic_Water_Flow/Solution4.cpp
+++ 0417_Pacific_Atlantic_Water_Flow/Solution4.cpp
@@ -1,19 +1,16 @@
 #include <Solution.h>
 #include <iostream>
 
-vector<int> axis4 = {1, 0, -1, 0, 1};
+vector<int> dir = {1, 0, -1, 0, 1};
 
-void dfs4(vector<vector<int>>& heights, vector<vector<int>>& flow, int x, int y, int mark) {
-	if (flow[x][y] == mark || flow[x][y] == 3) return;
-
-	flow[x][y] += mark;
+void render(vector<vector<int>>& h, int x, int y, int v, vector<vector<int>>& m) {
+	m[x][y] |= (1 << v);
 
 	for (int i = 0 ; i < 4 ; ++i) {
-		int nx = x + axis4[i];
-		int ny = y + axis4[i+1];
-		if (nx < 0 || ny < 0 || nx >= flow.size() || ny >= flow[0].size() ||
-				heights[x][y] > heights[nx][ny] || flow[nx][ny] == mark || flow[nx][ny] == 3) continue;
-		dfs4(heights, flow, nx, ny, mark);
+		int nx = x + dir[i];
+		int ny = y + dir[i+1];
+		if (nx < 0 || ny < 0 || nx >= h.size() || ny >= h[0].size() || h[nx][ny] < h[x][y] || m[nx][ny] & (1 << v)) continue;
+		render(h, nx, ny, v, m);
 	}
 }
 
@@ -21,19 +18,20 @@ vector<vector<int>> Solution::pacificAtlantic4(vector<vector<int>>& heights) {
 	int m = heights.size();
 	int n = heights[0].size();
 
-	vector<vector<int>> flow(m, vector<int>(n, 0));
-	for (int i = 0 ; i < m ; ++i) {
-		dfs4(heights, flow, i, 0, 1);
-		dfs4(heights, flow, i, n-1, 2);
-	}
-	for (int j = 0 ; j < n ; ++j) {
-		dfs4(heights, flow, 0, j, 1);
-		dfs4(heights, flow, m-1, j, 2);
-	}
+	vector<vector<int>> mark(m, vector<int>(n, 0));
+
+	for (int i = 0 ; i < n ; ++i) if (mark[0][i] == 0) render(heights, 0, i, 0, mark);
+	for (int i = 0 ; i < m ; ++i) if (mark[i][0] == 0) render(heights, i, 0, 0, mark);
+
+	for (int i = 0 ; i < n ; ++i) if (mark[m-1][i] < 2) render(heights, m-1, i, 1, mark);
+	for (int i = 0 ; i < m ; ++i) if (mark[i][n-1] < 2) render(heights, i, n-1, 1, mark);
 	vector<vector<int>> result;
+
 	for (int i = 0 ; i < m ; ++i) {
 		for (int j = 0 ; j < n ; ++j) {
-			if (flow[i][j] == 3) result.push_back({i, j});
+			if (mark[i][j] == 3) {
+				result.emplace_back(vector<int>({i, j}));
+			}
 		}
 	}
 	return result;
