c55990c9c00346236d66b8789e948efa8af08a09
neilhuang <neilhuang@synology.com>
2024-02-07 15:11:40 +0800

    <LeetCode202109> 864. Shortest Path to Get All Keys

---
+++ 0864_Shortest_Path_To_Get_All_Keys/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"@...a",".###A","b.BCc"};
+	Solution* test = new Solution();
+	cout << test->shortestPathAllKeys(input) << endl;
+	return 0;
+}
+
