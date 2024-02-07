cac458c5aa5c2aac1ffdeeee27dccdc5164048f8
neilhuang <neilhuang@synology.com>
2024-02-07 14:25:10 +0800

    <LeetCode202109> 1493. Longest Subarray of 1's After Deleting One Element

---
+++ 1493_Longest_Subarray_Of_1s_After_Deleting_One_Element/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {0,1,1,1,0,1,1,0,1};
+	Solution* test = new Solution();
+	cout << test->longestSubarray(input) << endl;
+	return 0;
+}
+
