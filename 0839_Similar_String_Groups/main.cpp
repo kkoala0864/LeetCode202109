508749f937765633fc7f17c10c97dab594e1d828
neilhuang <neilhuang@synology.com>
2024-02-10 12:28:35 +0800

    <LeetCode202109> 839. Similar String Groups

---
+++ 0839_Similar_String_Groups/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"tars","rats","arts","star"};
+	Solution* test = new Solution();
+	cout << test->numSimilarGroups(input) << endl;
+	return 0;
+}
+
