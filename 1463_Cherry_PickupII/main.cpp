4bf14a445b1389c02424d8e2b4c2501368c6a519
neilhuang <neilhuang@synology.com>
2024-02-11 17:03:16 +0800

    <LeetCode202109> 1463. Cherry Pickup II

--- 1463_Cherry_PickupII/main.cpp
+++ 1463_Cherry_PickupII/main.cpp
@@ -7,7 +7,8 @@ using std::endl;
 int main(void) {
 	vector<vector<int>> input = {{3,1,1},{2,5,1},{1,5,5},{2,1,1}};
 	Solution* test = new Solution();
-	cout << test->cherryPickup(input) << endl;
+	//cout << test->cherryPickup(input) << endl;
+	cout << test->cherryPickup2(input) << endl;
 	return 0;
 }
 
