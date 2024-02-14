7eb54c0043e2738a9b3d3bf91ac85bcb5a553f65
neilhuang <neilhuang@synology.com>
2024-02-14 15:59:10 +0800

    <LeetCode202109> 1168. Optimize Water Distribution in a Village

---
+++ 1168_Optimize_Water_Distribution_In_A_Village/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> wells = {1,2,2};
+	vector<vector<int>> pipes = {{1,2,1},{2,3,1}};
+	Solution* test = new Solution();
+	cout << test->minCostToSupplyWater(3, wells, pipes) << endl;
+	return 0;
+}
+
