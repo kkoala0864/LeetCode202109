2a0a13eb083c430b03c586ea6d745df3646ab6dd
neilhuang <neilhuang@synology.com>
2024-02-15 16:32:15 +0800

    <LeetCode202109> 1857. Largest Color Value in a Directed Graph

---
+++ 1857_Largest_Color_Value_In_A_Directed_Graph/Solution.h
@@ -0,0 +1,17 @@
+#include <data_type.h>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int largestPathValue(string colors, vector<vector<int>>& edges);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 1857_Largest_Color_Value_In_A_Directed_Graph/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> graph = {{4,2},{3,4},{0,3},{1,0},{2,1}};
+	string color = "rrrde";
+	Solution* test = new Solution();
+	cout << test->largestPathValue(color, graph) << endl;
+	return 0;
+}
+
