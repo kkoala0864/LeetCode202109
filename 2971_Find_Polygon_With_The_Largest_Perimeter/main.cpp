8fb55d936dcee8e3d81f9c8b1d85875c77ef8c61
neilhuang <neilhuang@synology.com>
2024-02-15 14:11:07 +0800

    <LeetCode202109> 2971. Find Polygon With the Largest Perimeter

---
+++ 2971_Find_Polygon_With_The_Largest_Perimeter/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,12,1,2,5,50,3};
+	Solution* test = new Solution();
+	cout << test->largestPerimeter(input) << endl;
+	return 0;
+}
+
