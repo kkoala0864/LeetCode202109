a010f8f70742ff24f02fab271d7773a509bcd382
neilhuang <neilhuang@synology.com>
2024-02-16 20:46:56 +0800

    <LeetCode202109> 1192. Critical Connections in a Network

--- 0269_Alien_Dictionary/Solution2.cpp
+++ 0269_Alien_Dictionary/Solution2.cpp
@@ -1,63 +1,53 @@
 #include <Solution.h>
 #include <iostream>
 #include <unordered_set>
-#include <unordered_map>
 #include <queue>
 #include <algorithm>
 
 using std::unordered_set;
-using std::unordered_map;
 using std::queue;
 using std::min;
 
-string topologicalSortBFS2(unordered_map<char, unordered_set<char>>& uMap) {
-	unordered_map<char, int> indegree;
-	queue<char> que;
-	string result;
-
-	for (const auto& iter : uMap) {
-		char src = iter.first;
-		for (const auto& iter : uMap[src]) ++indegree[iter];
-	}
-
-	for (const auto& iter : uMap) {
-		char ch = iter.first;
-		if (indegree[ch] == 0) que.emplace(ch);
-	}
-
-	while (!que.empty()) {
-		char ch = que.front();
-		que.pop();
-		result.push_back(ch);
-
-		for (const auto& iter : uMap[ch]) {
-			--indegree[iter];
-			if (indegree[iter] == 0) que.emplace(iter);
-		}
-	}
-	return result.size() < uMap.size() ? "" : result;
-}
-
 string Solution::alienOrder2(vector<string>& words) {
-	unordered_map<char, unordered_set<char>> uMap;
-	for (const auto& word : words) {
-		for (const auto& ch : word) {
-			uMap[ch] = unordered_set<char>();
-		}
-	}
+	vector<unordered_set<int>> od(26, unordered_set<int>());
 
-	for (int i = 0 ; i < words.size () ; ++i) {
-		string lhs = words[i];
+	vector<int> icnt(26, 0);
+	unordered_set<int> us;
+
+	for (int i = 0 ; i < words.size() ; ++i) {
+		for (const auto& c : words[i]) us.emplace(c-'a');
 		for (int j = i + 1 ; j < words.size() ; ++j) {
-			string rhs = words[j];
-			int size = min(lhs.size(), rhs.size());
-			if (lhs.size() > rhs.size() && lhs.substr(0, size) == rhs) return "";
+			int size = min(words[i].size(), words[j].size());
+			if (words[i].size() > words[j].size() && words[i].substr(0, size) == words[j]) return "";
 			for (int k = 0 ; k < size ; ++k) {
-				if (lhs[k] == rhs[k]) continue;
-				uMap[lhs[k]].emplace(rhs[k]);
+				if (words[i][k] == words[j][k]) continue;
+				if (od[words[i][k]-'a'].count(words[j][k]-'a')) break;
+				od[words[i][k]-'a'].emplace(words[j][k]-'a');
+				++icnt[words[j][k]-'a'];
 				break;
 			}
 		}
 	}
-	return topologicalSortBFS2(uMap);
+
+	queue<int> que;
+	string result;
+	for (int i = 0 ; i < 26 ; ++i) {
+		if (us.count(i) == 0) continue;
+		if (icnt[i] == 0) que.emplace(i);
+	}
+
+	while (!que.empty()) {
+		int cur = que.front();
+		que.pop();
+
+		result.push_back(cur + 'a');
+		us.erase(cur);
+
+		for (const auto& c : od[cur]) {
+			--icnt[c];
+			if (icnt[c] == 0) que.emplace(c);
+		}
+	}
+	return us.empty() ? result : "";
 }
+
