b46555be9106ded23e65bd92b05bcc66d8693dc3
neilhuang <neilhuang@synology.com>
2024-02-16 18:14:16 +0800

    <LeetCode202109> 1372. Longest ZigZag Path in a Binary Tree

--- 1372_Longest_ZigZag_Path_In_A_Binary_Tree/Solution.cpp
+++ 1372_Longest_ZigZag_Path_In_A_Binary_Tree/Solution.cpp
@@ -1,31 +1,18 @@
 #include <Solution.h>
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
-using std::vector;
-using std::pair;
 using std::max;
 
-void dfs(vector<pair<int, int>>& m, TreeNode* node, int& result) {
-	if (!node) return;
-	node->val = m.size();
-	m.emplace_back(pair<int, int>(1, 1));
-	if (node->left) {
-		dfs(m, node->left, result);
-		m[node->val].first = max(m[node->val].first, m[node->left->val].second + 1);
-	}
-	if (node->right) {
-		dfs(m, node->right, result);
-		m[node->val].second = max(m[node->val].second, m[node->right->val].first + 1);
-	}
-	result = max({result, m[node->val].first, m[node->val].second});
+int dfs(TreeNode* node, int side, int& result) {
+	if (!node) return 0;
+	int left = dfs(node->left, 0, result) + 1;
+	int right = dfs(node->right, 1, result) + 1;
+	result = max({result, left, right});
+	return side == 0 ? right : left;
 }
 
 int Solution::longestZigZag(TreeNode* root) {
-	vector<pair<int, int>> m;
 	int result = 0;
-
-	dfs(m, root, result);
-	return result - 1;
+	return max({dfs(root, 0, result), dfs(root, 1, result), result}) - 1;
 }
