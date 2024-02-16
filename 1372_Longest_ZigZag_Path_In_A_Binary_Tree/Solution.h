dfcba75e20fedcf6ff20743a13f6b5d629465b16
neilhuang <neilhuang@synology.com>
2024-02-16 17:47:24 +0800

    <LeetCode202109> 1372. Longest ZigZag Path in a Binary Tree

---
+++ 1372_Longest_ZigZag_Path_In_A_Binary_Tree/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int longestZigZag(TreeNode* root);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
