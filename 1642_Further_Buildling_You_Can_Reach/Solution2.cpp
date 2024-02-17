d75c60afaef8f926f02f360b5fbc56a07031e79a
neilhuang <neilhuang@synology.com>
2024-02-17 11:42:58 +0800

    <LeetCode202109> 1642. Furthest Building You Can Reach

---
+++ 1642_Further_Buildling_You_Can_Reach/Solution2.cpp
@@ -0,0 +1,33 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using std::priority_queue;
+using std::max;
+
+int Solution::furthestBuilding2(vector<int>& heights, int bricks, int ladders) {
+	priority_queue<int, vector<int>, std::greater<int>> pq;
+
+	int i = 0;
+	while (i < heights.size() - 1) {
+		int diff = max(0, heights[i+1] - heights[i]);
+		if (pq.size() < ladders) {
+			pq.emplace(diff);
+		} else {
+			if (!pq.empty() && pq.top() < diff) {
+				bricks -= pq.top();
+				pq.pop();
+				pq.emplace(diff);
+			} else {
+				bricks -= diff;
+			}
+		}
+		if (bricks < 0) {
+			break;
+		}
+		++i;
+	}
+	return i;
+}
+
