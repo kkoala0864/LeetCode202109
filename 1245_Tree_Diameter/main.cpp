7eb3ac7059e15ad8d206bc03cb0092070abdfe91
neilhuang <neilhuang@synology.com>
2024-02-24 13:42:28 +0800

    <LeetCode202109> 1245. Tree Diameter

---
+++ 1245_Tree_Diameter/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> edges = {{0,1},{1,2},{2,3},{1,4},{4,5}};
+	Solution* test = new Solution();
+	cout << test->treeDiameter(edges) << endl;
+	return 0;
+}
+
