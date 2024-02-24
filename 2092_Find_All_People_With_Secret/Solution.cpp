034c9a44f45e7e661e765ee3cd9bcf1bdf8b747d
neilhuang <neilhuang@synology.com>
2024-02-24 14:51:41 +0800

    <LeetCode202109> 2092. Find All People With Secret

---
+++ 2092_Find_All_People_With_Secret/Solution.cpp
@@ -0,0 +1,53 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <map>
+
+using std::unordered_map;
+using std::map;
+using std::pair;
+using std::min;
+
+int find(unordered_map<int, int>& root, int idx) {
+	if (root.count(idx) == 0 || root[idx] == idx) {
+		root[idx] = idx;
+		return idx;
+	}
+	return root[idx] = find(root, root[idx]);
+}
+
+vector<int> Solution::findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+	vector<bool> secret(n, false);
+	secret[0] = true;
+	secret[firstPerson] = true;
+
+	map<int, vector<pair<int, int>>> timeline;
+	for (const auto& m : meetings) {
+		timeline[m[2]].emplace_back(pair<int, int>(m[0], m[1]));
+	}
+
+	for (const auto& t : timeline) {
+		unordered_map<int, int> root;
+		for (const auto& p : t.second) {
+			int lr = find(root, p.first);
+			int rr = find(root, p.second);
+			if (lr == rr) continue;
+			int croot = min(lr, rr);
+			root[lr] = root[rr] = croot;
+			secret[croot] = secret[croot] | (secret[lr] | secret[rr]);
+		}
+		for (const auto& e : root) {
+			secret[e.first] = secret[e.first] | secret[find(root, e.first)];
+		}
+	}
+
+	vector<int> result;
+	for (int i = 0 ; i < n ; ++i) {
+		if (secret[i]) {
+			result.emplace_back(i);
+		}
+	}
+	return result;
+
+}
---
+++ 2092_Find_All_People_With_Secret/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
