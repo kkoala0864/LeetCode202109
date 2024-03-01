5c038fd6090d70684ff21ff094f1031262b058d8
neilhuang <neilhuang@synology.com>
2024-03-01 14:59:35 +0800

    <LeetCode202109> 714. Best Time to Buy and Sell Stock with Transaction Fee

--- 0714_Best_Time_To_Buy_And_Sell_Stock_With_Transaction_Fee/main.cpp
+++ 0714_Best_Time_To_Buy_And_Sell_Stock_With_Transaction_Fee/main.cpp
@@ -5,10 +5,11 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<int> prices = {1,3,7,5,10,3};
-	int fee = 3;
+	vector<int> prices = {1,3,2,8,4,9};
+	int fee = 2;
 	Solution* test = new Solution();
 	cout << test->maxProfit(prices, fee) << endl;
+	cout << test->maxProfit2(prices, fee) << endl;
 	return 0;
 }
 
