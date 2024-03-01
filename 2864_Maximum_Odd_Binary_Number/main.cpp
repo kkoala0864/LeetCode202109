2bdd339b09d60f188d8df42a7408e59ad934ebcf
neilhuang <neilhuang@synology.com>
2024-03-01 14:23:24 +0800

    <LeetCode202109> 2864. Maximum Odd Binary Number

---
+++ 2864_Maximum_Odd_Binary_Number/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "0101";
+	Solution* test = new Solution();
+	cout << test->maximumOddBinaryNumber(input) << endl;
+	return 0;
+}
+
