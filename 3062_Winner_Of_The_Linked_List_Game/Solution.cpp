7d5802b83610841551945076a4bdf9c121c5acb5
neilhuang <neilhuang@synology.com>
2024-03-02 12:11:24 +0800

    <LeetCode202109> 3062. Winner of the Linked List Game

---
+++ 3062_Winner_Of_The_Linked_List_Game/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+string Solution::gameResult(ListNode* head) {
+	ListNode* eIter = head;
+	ListNode* oIter = head->next;
+
+	int eVal = 0, oVal = 0;
+	while (eIter) {
+		eIter->val > oIter->val ? ++eVal : ++oVal;
+		eIter = oIter->next;
+		if (eIter) oIter = eIter->next;
+	}
+	return eVal == oVal ? "Tie" : eVal > oVal ? "Even" : "Odd";
+}
