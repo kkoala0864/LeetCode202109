f79ce94015b065050c698aa0490a448740532bd5
neilhuang <neilhuang@synology.com>
2024-03-04 18:28:21 +0800

    <LeetCode202109> 712. Minimum ASCII Delete Sum for Two Strings

---
+++ 0712_Delete_Minimum_ASCII_Delete_Sum_For_Two_Strings/Solution.cpp
@@ -0,0 +1,33 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::vector;
+using std::min;
+
+int Solution::minimumDeleteSum(string s1, string s2) {
+	int sz1 = s1.size();
+	int sz2 = s2.size();
+
+	vector<vector<int>> dp(sz1 + 1, vector<int>(sz2 + 1, INT_MAX));
+
+	dp[0][0] = 0;
+	for (int i = 0 ; i <= sz1 ; ++i) {
+		for (int j = 0 ; j <= sz2 ; ++j) {
+			if (i == 0 && j == 0) continue;
+			if (i == 0) {
+				dp[i][j] = dp[i][j-1] + s2[j-1];
+			} else if (j == 0) {
+				dp[i][j] = dp[i-1][j] + s1[i-1];
+			} else {
+				if (s1[i-1] == s2[j-1]) {
+					dp[i][j] = dp[i-1][j-1];
+				} else {
+					dp[i][j] = min(dp[i-1][j] + s1[i-1], dp[i][j-1] + s2[j-1]);
+				}
+			}
+		}
+	}
+	return dp[sz1][sz2];
+}
---
+++ 0712_Delete_Minimum_ASCII_Delete_Sum_For_Two_Strings/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string s1 = "delete", s2 = "leet";
+	Solution* test = new Solution();
+	cout << test->minimumDeleteSum(s1, s2) << endl;
+	return 0;
+}
+
