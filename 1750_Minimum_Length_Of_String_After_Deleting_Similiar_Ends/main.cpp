7f8d593bb2eb3283a392956a78a18562ba540f78
neilhuang <neilhuang@synology.com>
2024-03-05 10:46:46 +0800

    <LeetCode202109> 1750. Minimum Length of String After Deleting Similar Ends

---
+++ 1750_Minimum_Length_Of_String_After_Deleting_Similiar_Ends/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "aabccabba";
+	Solution* test = new Solution();
+	cout << test->minimumLength(input) << endl;
+	return 0;
+}
+
