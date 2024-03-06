37a1ea52bd65f12469f33fd61410dc89e5e0f995
neilhuang <neilhuang@synology.com>
2024-03-06 16:43:54 +0800

    <LeetCode202109> 2140. Solving Questions With Brainpower

---
+++ 2140_Solving_Question_With_Brainpower/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,1},{2,2},{3,3},{4,4},{5,5}};
+	Solution* test = new Solution();
+	cout << test->mostPoints(input) << endl;
+	return 0;
+}
+
