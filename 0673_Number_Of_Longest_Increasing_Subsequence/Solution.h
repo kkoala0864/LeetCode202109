65fba5eda50d73dd0b992720611b04f8373b3ee8
neilhuang <neilhuang@synology.com>
2024-03-11 15:16:26 +0800

    <LeetCode202109> 673. Number of Longest Increasing Subsequence

--- 0673_Number_Of_Longest_Increasing_Subsequence/Solution.h
+++ 0673_Number_Of_Longest_Increasing_Subsequence/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int findNumberOfLIS(vector<int>& nums);
+	int findNumberOfLIS2(vector<int>& nums);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
