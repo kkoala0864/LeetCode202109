538b68fb375c9374b9f1eabb5bf2d48543aa6b57
neilhuang <neilhuang@synology.com>
2024-03-11 10:27:57 +0800

    <LeetCode202109> 791. Custom Sort String

---
+++ 0791_Custom_Sort_String/Solution.cpp
@@ -0,0 +1,26 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+string Solution::customSortString(string order, string s) {
+	vector<int> sIdx(26, -1);
+	vector<int> sCnt;
+
+	for (int i = 0 ; i < order.size() ; ++i) {
+		sIdx[order[i]-'a'] = i;
+		sCnt.emplace_back(0);
+	}
+
+	string result;
+	for (const auto& c : s) {
+		int idx = sIdx[c-'a'];
+		if (idx == -1) result.push_back(c);
+		else ++sCnt[idx];
+	}
+	for (int i = 0 ; i < order.size() ; ++i) {
+		result += string(sCnt[i], order[i]);
+	}
+	return result;
+}
---
+++ 0791_Custom_Sort_String/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input1 = "cba";
+	string input2 = "abcd";
+	Solution* test = new Solution();
+	cout << test->customSortString(input1, input2) << endl;
+	return 0;
+}
+
