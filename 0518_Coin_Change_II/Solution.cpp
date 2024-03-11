08437378b501210349fe0d53f0264ea355c4b31c
neilhuang <neilhuang@synology.com>
2024-03-11 11:43:01 +0800

    <LeetCode202109> 518. Coin Change II

---
+++ 0518_Coin_Change_II/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int Solution::change(int amount, vector<int>& coins) {
+	vector<int> dp(amount + 1, 0);
+	dp[0] = 1;
+
+	for (const auto& c : coins) {
+		for (int i = 1 ; i <= amount ; ++i) {
+			if (i >= c) {
+				dp[i] += dp[i-c];
+			}
+		}
+	}
+	return dp[amount];
+}
---
+++ 0518_Coin_Change_II/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> coins = {3};
+	Solution* test = new Solution();
+	cout << test->change(2, coins) << endl;
+	return 0;
+}
+
