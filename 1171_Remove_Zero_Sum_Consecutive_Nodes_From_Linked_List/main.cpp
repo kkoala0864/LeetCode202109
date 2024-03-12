e2062b0706861e95f77c4a89f5651160775cce65
neilhuang <neilhuang@synology.com>
2024-03-12 11:51:26 +0800

    <LeetCode202109> 1171. Remove Zero Sum Consecutive Nodes from Linked List

---
+++ 1171_Remove_Zero_Sum_Consecutive_Nodes_From_Linked_List/main.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	ListNode* five = new ListNode(-2, nullptr);
+	ListNode* four = new ListNode(-3, five);
+	ListNode* three = new ListNode(3, four);
+	ListNode* two = new ListNode(2, three);
+	ListNode* one = new ListNode(1, two);
+	Solution* test = new Solution();
+	auto result = test->removeZeroSumSublists(one);
+	auto iter = result;
+	while (iter) {
+		cout << iter->val << " ";
+		iter = iter->next;
+	}
+	cout << endl;
+	return 0;
+}
+
