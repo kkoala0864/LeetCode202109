8009c6b28f6fdb3cd73dbb3903183494abefe6e8
neilhuang <neilhuang@synology.com>
2024-03-12 15:19:52 +0800

    <LeetCode202109> 1218. Longest Arithmetic Subsequence of Given Difference

---
+++ 1218_Longest_Arithmetic_Subsequence_Of_Given_Difference/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+using std::max;
+
+int Solution::longestSubsequence(vector<int>& arr, int difference) {
+	unordered_map<int, int> m;
+
+	vector<int> dp(arr.size(), 1);
+
+	int result = 0;
+	for (int i = 0 ; i < arr.size() ; ++i) {
+		if (m.count(arr[i] - difference)) {
+			dp[i] = m[arr[i] - difference] + 1;
+		}
+		m[arr[i]] = dp[i];
+		result = max(result, dp[i]);
+	}
+	return result;
+}
