627914b30c126653dd51b298738d3316bc4b1d8d
neilhuang <neilhuang@synology.com>
2024-03-13 17:24:08 +0800

    <LeetCode202109> 2485. Find the Pivot Integer

---
+++ 2485_Find_The_Pivot_Integer/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int pivotInteger(int n);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
