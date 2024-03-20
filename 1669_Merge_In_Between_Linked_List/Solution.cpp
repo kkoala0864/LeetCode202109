c980f52c56a388a8fe756b2364ba2564505c1f8e
neilhuang <neilhuang@synology.com>
2024-03-20 17:30:57 +0800

    <LeetCode202109> 1669. Merge In Between Linked Lists

---
+++ 1669_Merge_In_Between_Linked_List/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
+	ListNode* dummy = new ListNode(0, list1);
+	ListNode* iter = dummy;
+
+	while (a--) {
+		iter = iter->next;
+		--b;
+	}
+	ListNode* tmp = iter->next;
+	iter->next = list2;
+	iter = tmp;
+	while (b--) {
+		iter = iter->next;
+	}
+	tmp = list2;
+	while (tmp->next) {
+		tmp = tmp->next;
+	}
+	tmp->next = iter->next;
+	iter->next = nullptr;
+	return dummy->next;
+}
---
+++ 1669_Merge_In_Between_Linked_List/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
