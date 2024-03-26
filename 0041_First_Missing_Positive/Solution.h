5e5ae4dec57d77dd79e4d6b3f542340db1783128
neilhuang <neilhuang@synology.com>
2024-03-26 11:13:47 +0800

    <LeetCode202109> 41. First Missing Positive

--- 0041_First_Missing_Positive/Solution.h
+++ 0041_First_Missing_Positive/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int firstMissingPositive(vector<int>& nums);
+	int firstMissingPositive2(vector<int>& nums);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0041_First_Missing_Positive/main.cpp
+++ 0041_First_Missing_Positive/main.cpp
@@ -8,6 +8,8 @@ int main(void) {
 	vector<int> input = {7,8,9,11,12};
 	Solution* test = new Solution();
 	cout << test->firstMissingPositive(input) << endl;
+	input = {7,8,9,11,12};
+	cout << test->firstMissingPositive2(input) << endl;
 	return 0;
 }
 
