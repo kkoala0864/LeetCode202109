1800376ae741e1b1d77a98cd938ac646561bc9f1
neilhuang <neilhuang@synology.com>
2024-03-26 11:49:11 +0800

    <LeetCode202109> 91. Decode Ways

--- 0091_Decode_Ways/main.cpp
+++ 0091_Decode_Ways/main.cpp
@@ -5,12 +5,9 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-
 	string input("12");
 	Solution* test = new Solution();
 	cout << test->numDecodings(input) << endl;
-	cout << test->numDecodings2(input) << endl;
-	cout << test->numDecodings3(input) << endl;
 	cout << test->numDecodings4(input) << endl;
 	return 0;
 }
