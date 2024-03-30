27091e29d41ea849156ee8fc01816f46dafe39e3
neilhuang <neilhuang@synology.com>
2024-03-30 15:33:10 +0800

    <LeetCode202109> 2962. Count Subarrays Where Max Element Appears at Least K Times

--- 2962_Count_Subarrays_Where_Max_Element_Appears_At_least_K_Times/Solution.cpp
+++ 2962_Count_Subarrays_Where_Max_Element_Appears_At_least_K_Times/Solution.cpp
@@ -6,20 +6,20 @@ using std::max;
 
 long long Solution::countSubarrays(vector<int>& nums, int k) {
 	long long result = 0;
+	int maxVal = 0;
+	for (const auto& v : nums) maxVal = max(maxVal, v);
+	int l = 0;
 	int cnt = 0;
 
-	int mx = INT_MIN;
 	for (int i = 0 ; i < nums.size() ; ++i) {
-		mx = max(mx, nums[i]);
-	}
-	int l = 0;
-	for (int i = 0 ; i < nums.size() ; ++i) {
-		if (nums[i] == mx) ++cnt;
-		while (cnt >= k) {
-			result += (nums.size() - i);
-			if (nums[l] == mx) --cnt;
+		if (nums[i] == maxVal) ++cnt;
+
+		while (l <= i && cnt == k) {
+			if (nums[l] == maxVal) --cnt;
 			++l;
 		}
+
+		result += l;
 	}
 	return result;
 }
--- 2962_Count_Subarrays_Where_Max_Element_Appears_At_least_K_Times/main.cpp
+++ 2962_Count_Subarrays_Where_Max_Element_Appears_At_least_K_Times/main.cpp
@@ -5,9 +5,10 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<int> input = {61,23,38,23,56,40,82,56,82,82,82,70,8,69,8,7,19,14,58,42,82,10,82,78,15,82};
+	vector<int> input = {1,3,2,3,3};
+	int k = 2;
 	Solution* test = new Solution();
-	cout << test->countSubarrays(input, 2) << endl;
+	cout << test->countSubarrays(input, k) << endl;
 	return 0;
 }
 
