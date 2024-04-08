9048eae91e7c4091bfd0bc21ac24df5016482106
neilhuang <neilhuang@synology.com>
2024-04-08 16:09:36 +0800

    <LeetCode202109> 1700. Number of Students Unable to Eat Lunch

---
+++ 1700_Number_Of_Students_Unable_To_Eat_Lunch/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int countStudents(vector<int>& students, vector<int>& sandwiches);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
