78a312f8bbf8ea13f1e6f7fb4eca615e532a8152
neilhuang <neilhuang@synology.com>
2024-04-23 17:02:25 +0800

    <LeetCode202109> 305. Number of Islands II

--- 0305_Number_Of_Islands_II/Solution.h
+++ 0305_Number_Of_Islands_II/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	vector<int> numIslands2(int m, int n, vector<vector<int>>& positions);
+	vector<int> numIslands22(int m, int n, vector<vector<int>>& positions);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
