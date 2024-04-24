2ce1c750e636a7704a2a82bc9fa456eeebf5fbef
neilhuang <neilhuang@synology.com>
2024-04-24 11:37:10 +0800

    <LeetCode202109> 3121. Count the Number of Special Characters I

---
+++ 3120_Count_The_Number_Of_Special_Characters_I/Solution.cpp
@@ -0,0 +1,26 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::numberOfSpecialChars(string word) {
+	int bm = 0;
+	int sm = 0;
+	int result = 0;
+
+	for (const auto& c : word) {
+		if (c >= 'A' && c <= 'Z') {
+			int offset = 1 << (c - 'A');
+			if (offset & sm) {
+				if ((offset & bm) == 0) ++result;
+			}
+			bm = bm | offset;
+		} else {
+			int offset = 1 << (c - 'a');
+			if (offset & bm) {
+				if ((offset & sm) == 0) ++result;
+			}
+			sm = sm | offset;
+		}
+	}
+	return result;
+
+}
