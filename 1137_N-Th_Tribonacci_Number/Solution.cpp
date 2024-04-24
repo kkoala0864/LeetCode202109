23ec42ca44c9151bede36e341db6032c4c404cd9
neilhuang <neilhuang@synology.com>
2024-04-24 11:04:38 +0800

    <LeetCode202109> 1137. N-th Tribonacci Number

--- 1137_N-Th_Tribonacci_Number/Solution.cpp
+++ 1137_N-Th_Tribonacci_Number/Solution.cpp
@@ -1,18 +1,16 @@
 #include <Solution.h>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using std::vector;
-using std::max;
 
 int Solution::tribonacci(int n) {
-	vector<int> dp(max(n+1,3) , 0);
-	dp[0] = 0;
-	dp[1] = 1;
-	dp[2] = 1;
+	vector<int> dp;
+	dp.emplace_back(0);
+	dp.emplace_back(1);
+	dp.emplace_back(1);
 	for (int i = 3 ; i <= n ; ++i) {
-		dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		dp.emplace_back(dp[i-1] + dp[i-2] + dp[i-3]);
 	}
 	return dp[n];
 }
