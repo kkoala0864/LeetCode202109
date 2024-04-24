641d50f8114ea86a1418bc52803aa4f49fa327fc
neilhuang <neilhuang@synology.com>
2024-04-24 15:02:06 +0800

    <LeetCode202109> 3122. Minimum Number of Operations to Satisfy Conditions

---
+++ 3122_Minimum_Number_Of_Operations_To_Satisfy_Conditions/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,1,1},{0,0,0}};
+	Solution* test = new Solution();
+	cout << test->minimumOperations(input) << endl;
+	return 0;
+}
+
