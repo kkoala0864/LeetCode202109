1692e4af7a30ad99d480dfb2a5c8185ba9128123
neilhuang <neilhuang@synology.com>
2024-04-26 15:47:51 +0800

    <LeetCode202109> 1289. Minimum Falling Path Sum II

---
+++ 1289_Minimum_Falling_Path_Sum_II/Solution.cpp
@@ -0,0 +1,35 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using std::min;
+using std::pair;
+using std::cout;
+using std::endl;
+
+int Solution::minFallingPathSum(vector<vector<int>>& grid) {
+	int n = grid.size();
+	int result = INT_MAX;
+	vector<vector<int>> dp = grid;
+
+	for (int i = 1 ; i < n ; ++i) {
+		int m1 = INT_MAX, m2 = INT_MAX;
+		for (const auto v : dp[i-1]) {
+			if (v <= m1) {
+				m2 = m1;
+				m1 = v;
+			} else if (v <= m2) {
+				m2 = v;
+			}
+		}
+		for (int j = 0 ; j < n ; ++j) {
+			dp[i][j] = dp[i][j] + (dp[i-1][j] > m1 ? m1 : m2);
+			if (i == n-1) {
+				result = min(result, dp[i][j]);
+			}
+		}
+	}
+	return n == 1 ? grid[0][0] : result;
+
+}
