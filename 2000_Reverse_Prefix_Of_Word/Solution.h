7b3e920650d52cee5cea106386b2eb0c6dc24981
neilhuang <neilhuang@synology.com>
2024-05-01 12:44:23 +0800

    <LeetCode202109> 1915. Number of Wonderful Substrings

---
+++ 2000_Reverse_Prefix_Of_Word/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	string reversePrefix(string word, char ch);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
