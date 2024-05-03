88365a8d3b83893e7fdedc5df9b2009849534f0c
neilhuang <neilhuang@synology.com>
2024-05-03 15:46:51 +0800

    <LeetCode202109> 165. Compare Version Numbers

--- 0165_Compare_Version_Numbers/Solution.h
+++ 0165_Compare_Version_Numbers/Solution.h
@@ -6,6 +6,7 @@ class Solution {
     public :
         Solution() {}
 	int compareVersion(string version1, string version2);
+	int compareVersion2(string version1, string version2);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
