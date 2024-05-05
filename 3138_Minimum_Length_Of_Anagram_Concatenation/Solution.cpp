f6cb01de81a58f9d7aca1e950b6b6cb38c17914d
neilhuang <neilhuang@synology.com>
2024-05-05 17:13:01 +0800

    <LeetCode202109> 3138. Minimum Length of Anagram Concatenation

---
+++ 3138_Minimum_Length_Of_Anagram_Concatenation/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using std::vector;
+using std::gcd;
+
+int Solution::minAnagramLength(string s) {
+	vector<int> cnt(26, 0);
+	for (const auto& c : s) ++cnt[c-'a'];
+
+	int times = cnt[0];
+
+	for (int i = 1 ; i < 26 ; ++i) {
+		times = gcd(times, cnt[i]);
+	}
+
+	int result = 0;
+	for (int i = 0 ; i < 26 ; ++i) {
+		result += cnt[i] / times;
+	}
+
+	return result;
+}
