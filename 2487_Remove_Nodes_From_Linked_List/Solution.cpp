73b41f6e2d6a8cb051988b0e26abb357630cfd5f
neilhuang <neilhuang@synology.com>
2024-05-06 11:19:51 +0800

    <LeetCode202109> 2487. Remove Nodes From Linked List

---
+++ 2487_Remove_Nodes_From_Linked_List/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+ListNode* Solution::removeNodes(ListNode* head) {
+	vector<ListNode*> l;
+
+	ListNode* iter = head;
+	while (iter) {
+		while (!l.empty() && l.back()->val < iter->val) {
+			l.pop_back();
+		}
+		l.emplace_back(iter);
+		iter = iter->next;
+	}
+	for (int i = 0 ; i < l.size() - 1 ; ++i) {
+		l[i]->next = l[i+1];
+	}
+	l.back()->next = nullptr;
+	return l[0];
+}
