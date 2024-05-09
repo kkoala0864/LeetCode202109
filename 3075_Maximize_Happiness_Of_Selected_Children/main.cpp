3f05a43ce3bd9dc6d30ee4eb18ae1a0997093bf8
neilhuang <neilhuang@synology.com>
2024-05-09 10:55:14 +0800

    <LeetCode202109> 3075. Maximize Happiness of Selected Children

---
+++ 3075_Maximize_Happiness_Of_Selected_Children/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {2,3,4,5};
+	int k = 1;
+	Solution* test = new Solution();
+	cout << test->maximumHappinessSum(input, k) << endl;
+	return 0;
+}
+
