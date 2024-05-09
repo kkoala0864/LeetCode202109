c598509e14a21c93674af4e826d99718175f0476
neilhuang <neilhuang@synology.com>
2024-05-09 10:55:14 +0800

    <LeetCode202109> 3075. Maximize Happiness of Selected Children

---
+++ 3075_Maximize_Happiness_Of_Selected_Children/Solution.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::priority_queue;
+
+long long Solution::maximumHappinessSum(vector<int>& happiness, int k) {
+	priority_queue<long long> pq;
+
+	for (const auto& h : happiness) pq.emplace(h);
+
+	long long result = 0;
+	for (long long i = 0 ; i < k ; ++i) {
+		if ((pq.top() - i) <= 0) break;
+		result += (pq.top() - i);
+		pq.pop();
+	}
+	return result;
+}
