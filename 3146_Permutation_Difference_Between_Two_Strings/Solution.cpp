7cb8260bf9d2931442206c9a166cb14d895cf9e1
neilhuang <neilhuang@synology.com>
2024-05-13 11:41:28 +0800

    <LeetCode202109> 3146. Permutation Difference between Two Strings

---
+++ 3146_Permutation_Difference_Between_Two_Strings/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+int Solution::findPermutationDifference(string s, string t) {
+	vector<int> i1(26, -1);
+	vector<int> i2(26, -1);
+
+	for (int i = 0 ; i < s.size() ; ++i) {
+		i1[s[i]-'a'] = i;
+		i2[t[i]-'a'] = i;
+	}
+	int result = 0;
+
+	for (int i = 0 ; i < 26 ; ++i) {
+		if (i1[i] == -1) continue;
+		result += abs(i1[i] - i2[i]);
+	}
+	return result;
+
+}
---
+++ 3146_Permutation_Difference_Between_Two_Strings/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string s1 = "abc";
+	string s2 = "bac";
+	Solution* test = new Solution();
+	cout << test->findPermutationDifference(s1, s2) << endl;
+	return 0;
+}
+
