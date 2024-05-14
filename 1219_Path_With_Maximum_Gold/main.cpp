7e60d66b060e0cf16299bcd0813f0977055ae373
neilhuang <neilhuang@synology.com>
2024-05-14 11:17:57 +0800

    <LeetCode202109> 1219. Path with Maximum Gold

---
+++ 1219_Path_With_Maximum_Gold/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,6,0},{5,8,7},{0,9,0}};
+	Solution* test = new Solution();
+	cout << test->getMaximumGold(input) << endl;
+	return 0;
+}
+
