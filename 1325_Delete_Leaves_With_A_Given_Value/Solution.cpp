988c58c3e69f7c69b09ddd84162047fcc5bd671a
neilhuang <neilhuang@synology.com>
2024-05-17 11:09:32 +0800

    <LeetCode202109> 1325. Delete Leaves With a Given Value

---
+++ 1325_Delete_Leaves_With_A_Given_Value/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+
+bool dfs(TreeNode* node, int target) {
+	if (!node) return false;
+
+	if (node->left) {
+		if (dfs(node->left, target)) node->left = nullptr;
+	}
+	if (node->right) {
+		if (dfs(node->right, target)) node->right = nullptr;
+	}
+	return !node->left && !node->right && node->val == target;
+}
+
+TreeNode* Solution::removeLeafNodes(TreeNode* root, int target) {
+	TreeNode* dummy = new TreeNode(-1);
+	dummy->left = root;
+
+	dfs(dummy, target);
+	return dummy->left;
+}
---
+++ 1325_Delete_Leaves_With_A_Given_Value/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	TreeNode* removeLeafNodes(TreeNode* root, int target);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
