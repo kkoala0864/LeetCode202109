ba207073dfa7793801fa347309632df07640452d
neilhuang <neilhuang@synology.com>
2024-05-19 15:35:54 +0800

    <LeetCode202109> 3153. Sum of Digit Differences of All Pairs

---
+++ 3153_Sum_Of_Digit_Differences_Of_All_Pairs/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {13,23,12};
+	Solution* test = new Solution();
+	cout << test->sumDigitDifferences(input) << endl;
+	return 0;
+}
+
