209c117540acb6d549bb1220cfb975106b285f03
neilhuang <neilhuang@synology.com>
2024-05-19 14:51:02 +0800

    <LeetCode202109> 3152. Special Array II

---
+++ 3152_Special_Array_II/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,4,1,2,6};
+	vector<vector<int>> queries = {{0,4}};
+	Solution* test = new Solution();
+	auto result = test->isArraySpecial(input, queries);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
