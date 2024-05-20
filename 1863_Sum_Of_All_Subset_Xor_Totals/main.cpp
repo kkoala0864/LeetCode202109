f92861d1a899c726d9f5ce77c4f2fe2d4a560ac3
neilhuang <neilhuang@synology.com>
2024-05-20 11:12:02 +0800

    <LeetCode202109> 1863. Sum of All Subset XOR Totals

---
+++ 1863_Sum_Of_All_Subset_Xor_Totals/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {5,1,6};
+	Solution* test = new Solution();
+	cout << test->subsetXORSum(input) << endl;
+	return 0;
+}
+
