135bbce6976c66dfa7ff29b13e247602b4e2e2b3
neilhuang <neilhuang@synology.com>
2024-05-28 16:50:51 +0800

    <LeetCode202109> 3164. Find the Number of Good Pairs II

---
+++ 3164_Find_The_Number_Of_Good_Pairs_II/Solution.cpp
@@ -0,0 +1,35 @@
+#include <Solution.h>
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <climits>
+
+using std::map;
+using std::max;
+
+long long Solution::numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+	map<int, long long> m;
+
+	int maxValue = INT_MIN;
+	for (const auto& v : nums1) {
+		++m[v];
+		maxValue = max(maxValue, v);
+	}
+
+	long long result = 0;
+	for (const auto& v : nums2) {
+		int n2 = v * k;
+
+		int value = m.lower_bound(n2)->first;
+
+		int idx = value / n2;
+		while ((n2 * idx) <= maxValue) {
+			int tmp = n2 * idx;
+			if (m.count(tmp)) {
+				result += m[tmp];
+			}
+			++idx;
+		}
+	}
+	return result;
+}
---
+++ 3164_Find_The_Number_Of_Good_Pairs_II/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> v1 = {1,3,4};
+	vector<int> v2 = {1,3,4};
+	int k = 1;
+	Solution* test = new Solution();
+	cout << test->numberOfPairs(v1, v2, k) << endl;
+	return 0;
+}
+
