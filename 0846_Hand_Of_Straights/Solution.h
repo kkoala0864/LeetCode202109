0967e3097a33a099d1c21d331afa8bdc9920eef8
neilhuang <neilhuang@synology.com>
2024-06-06 11:03:43 +0800

    <LeetCode202109> 846. Hand of Straights

---
+++ 0846_Hand_Of_Straights/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	bool isNStraightHand(vector<int>& hand, int groupSize);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 0846_Hand_Of_Straights/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,3,6,2,3,4,7,8};
+	int groupSize = 3;
+	Solution* test = new Solution();
+	cout << test->isNStraightHand(input, groupSize) << endl;
+	return 0;
+}
+
