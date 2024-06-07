ab3a7030d12d59982d1236e1ebfece251f370704
neilhuang <neilhuang@synology.com>
2024-06-07 14:36:55 +0800

    <LeetCode202109> 648. Replace Words

---
+++ 0648_Replace_Words/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+
+using std::unordered_set;
+using std::cout;
+using std::endl;
+
+string Solution::replaceWords(vector<string>& dictionary, string sentence) {
+	unordered_set<string> m;
+
+	for (const auto& d : dictionary) {
+		m.emplace(d);
+	}
+
+	string result;
+	string cur;
+	for (int i = 0 ; i < sentence.size() ; ++i) {
+		result.push_back(sentence[i]);
+		if (sentence[i] == ' ') cur = "";
+		else cur.push_back(sentence[i]);
+		if (m.count(cur) == 0) continue;
+		while ((i + 1) < sentence.size() && sentence[i+1] != ' ') {
+			++i;
+		}
+		cur = "";
+	}
+	return result;
+}
---
+++ 0648_Replace_Words/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"cat","bat","rat"};
+	string sentence = "the cattle was rattled by the battery";
+	Solution* test = new Solution();
+	cout << test->replaceWords(input, sentence) << endl;
+	return 0;
+}
+
