fdaf014a45ebafa511f13716c7c758e39f000801
neilhuang <neilhuang@synology.com>
2024-06-11 16:40:31 +0800

    <LeetCode202109> 1122. Relative Sort Array

---
+++ 1122_Relative_Sort_Array/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
