c77abfc24828cea9e26daf332bb7bd618cdf6713
neilhuang <neilhuang@synology.com>
2024-06-15 11:40:11 +0800

    <LeetCode202109> 945. Minimum Increment to Make Array Unique

---
+++ 0945_Minimum_Increment_To_Make_Array_Unique/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::minIncrementForUnique(vector<int>& nums) {
+
+	sort(nums.begin(), nums.end());
+
+	int result = 0;
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		if (nums[i-1] < nums[i]) continue;
+		result += (nums[i-1] - nums[i] + 1);
+		nums[i] = nums[i-1] + 1;
+	}
+	return result;
+}
