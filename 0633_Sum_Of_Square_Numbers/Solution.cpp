476b8e414db5f46196ab8531db5ffafcbc2fccf3
neilhuang <neilhuang@synology.com>
2024-06-17 15:51:54 +0800

    <LeetCode202109> 633. Sum of Square Numbers

---
+++ 0633_Sum_Of_Square_Numbers/Solution.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+#include <cmath>
+
+using std::unordered_set;
+
+bool Solution::judgeSquareSum(int c) {
+	unordered_set<int> us;
+	int limit = sqrt(c);
+	for (int i = 0 ; i <= limit ; ++i) {
+		us.emplace(i*i);
+		if (us.count(c - i*i)) return true;
+	}
+	return false;
+}
+
