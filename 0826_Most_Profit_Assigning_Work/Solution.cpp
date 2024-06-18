70fa313fb6833499a0dace01a5ab5dab3447fc2e
neilhuang <neilhuang@synology.com>
2024-06-18 11:24:16 +0800

    <LeetCode202109> 826. Most Profit Assigning Work

---
+++ 0826_Most_Profit_Assigning_Work/Solution.cpp
@@ -0,0 +1,34 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <map>
+
+using std::max;
+using std::map;
+
+int Solution::maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+	map<int, int> m;
+	for (int i = 0 ; i < difficulty.size() ; ++i) {
+		m[difficulty[i]] = m[difficulty[i]] > 0 ? max(m[difficulty[i]], profit[i]) : profit[i];
+	}
+
+	int cur = INT_MIN;
+	for (auto& e : m) {
+		cur = max(cur, e.second);
+		e.second = cur;
+	}
+
+	int result = 0;
+	for (const auto& w : worker) {
+		auto iter = m.lower_bound(w);
+		if (iter == m.end()) {
+			--iter;
+		} else if (iter->first > w) {
+			if (iter == m.begin()) continue;
+			--iter;
+		}
+		result += iter->second;
+	}
+	return result;
+}
---
+++ 0826_Most_Profit_Assigning_Work/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
