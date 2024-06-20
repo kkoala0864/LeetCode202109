3ca03a66f7f63d6fae2e6648ed2aca08d39f1923
neilhuang <neilhuang@synology.com>
2024-06-20 11:53:05 +0800

    <LeetCode202109> 1482. Minimum Number of Days to Make m Bouquets

---
+++ 1482_Minimum_Number_Of_Days_To_Make_M_Bouquets/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {7,7,7,7,12,7,7};
+	int m = 2;
+	int k = 3;
+	Solution* test = new Solution();
+	cout << test->minDays(input, m, k) << endl;
+	return 0;
+}
+
