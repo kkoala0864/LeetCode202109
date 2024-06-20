54d1cba4009eee3c8310b15fa66e12ee1a6da8f9
neilhuang <neilhuang@synology.com>
2024-06-20 13:01:45 +0800

    <LeetCode202109> 1552. Magnetic Force Between Two Balls

---
+++ 1552_Magnetic_Force_Between_Two_Balls/Solution.cpp
@@ -0,0 +1,38 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+bool getResult(vector<int>& p, int m, int k) {
+	int last = p[0];
+	int cnt = 1;
+
+	for (int i = 1 ; i < p.size() && cnt < m ; ++i) {
+		if (p[i] - last >= k) {
+			++cnt;
+			last = p[i];
+		}
+	}
+	return cnt == m;
+}
+
+int Solution::maxDistance(vector<int>& position, int m) {
+	sort(position.begin(), position.end());
+	int diff = position.back() - position[0] + 1;
+
+	int l = 1, r = diff, mid = 0;
+	int result = 1;
+
+	while (l < r) {
+		mid = l + ((r - l) >> 1);
+
+		if (getResult(position, m, mid)) {
+			result = mid;
+			l = mid + 1;
+		} else {
+			r = mid;
+		}
+	}
+	return result;
+}
---
+++ 1552_Magnetic_Force_Between_Two_Balls/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,3,4,7};
+	int m = 3;
+	Solution* test = new Solution();
+	cout << test->maxDistance(input, m) << endl;
+	return 0;
+}
+
