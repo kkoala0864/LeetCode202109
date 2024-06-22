f7a8515ca7171392c326d48e2c5be8781ab8b13d
neilhuang <neilhuang@synology.com>
2024-06-22 20:13:32 +0800

    <LeetCode202109> 2743. Count Substrings Without Repeating Character

---
+++ 2743_Count_Substrings_Without_Repeating_Character/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int Solution::numberOfSpecialSubstrings(string s) {
+	int l = 0;
+	int result = 0;
+	vector<int> cnt(26, 0);
+
+	for (int i = 0 ; i < s.size() ; ++i) {
+		++cnt[s[i] - 'a'];
+
+		while (cnt[s[i]-'a'] > 1) {
+			--cnt[s[l]-'a'];
+			++l;
+		}
+		result += (i - l + 1);
+	}
+
+	return result;
+}
