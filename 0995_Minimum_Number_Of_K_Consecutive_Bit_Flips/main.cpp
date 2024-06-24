e5007913746054a61c1d49b5e23695af098827be
neilhuang <neilhuang@synology.com>
2024-06-24 09:25:01 +0800

    <LeetCode202109> 995. Minimum Number of K Consecutive Bit Flips

---
+++ 0995_Minimum_Number_Of_K_Consecutive_Bit_Flips/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {0,0,0,1,0,1,1,0};
+	int k = 3;
+	Solution* test = new Solution();
+	cout << test->minKBitFlips(input, k) << endl;
+	return 0;
+}
+
