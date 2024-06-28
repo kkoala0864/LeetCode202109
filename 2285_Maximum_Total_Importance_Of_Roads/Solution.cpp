92dd02c33998c079285ab3f3edf5e922ce0a28bb
neilhuang <neilhuang@synology.com>
2024-06-28 11:48:08 +0800

    <LeetCode202109> 2285. Maximum Total Importance of Roads

---
+++ 2285_Maximum_Total_Importance_Of_Roads/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+long long Solution::maximumImportance(int n, vector<vector<int>>& roads) {
+	vector<int> cnt(n, 0);
+
+	for (const auto& r : roads) {
+		++cnt[r[0]];
+		++cnt[r[1]];
+	}
+
+	sort(cnt.begin(), cnt.end(), std::greater<int>());
+
+	long long result = 0;
+	for (int i = 0 ; i < n ; ++i) {
+		result += (long long)cnt[i] * (long long)(n - i);
+	}
+	return result;
+}
---
+++ 2285_Maximum_Total_Importance_Of_Roads/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,3},{2,4},{1,3}};
+	int n = 5;
+	Solution* test = new Solution();
+	cout << test->maximumImportance(n, input) << endl;
+	return 0;
+}
+
