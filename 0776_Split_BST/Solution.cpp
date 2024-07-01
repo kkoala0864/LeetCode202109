3f12835e16f5788bc47510724bc3dea70d3c1f90
neilhuang <neilhuang@synology.com>
2024-07-01 19:41:07 +0800

    <LeetCode202109> 776. Split BST

---
+++ 0776_Split_BST/Solution.cpp
@@ -0,0 +1,42 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::queue;
+
+void insert(TreeNode*& node, TreeNode*& cur) {
+	if (!node) {
+		node = cur;
+	} else if (cur->val > node->val) {
+		insert(node->right, cur);
+	} else {
+		insert(node->left, cur);
+	}
+}
+
+vector<TreeNode*> Solution::splitBST(TreeNode* root, int target) {
+	vector<TreeNode*> result(2, nullptr);
+
+	queue<TreeNode*> que;
+	que.emplace(root);
+
+	while (!que.empty()) {
+		TreeNode* cur = que.front();
+		que.pop();
+
+		if (cur->left) {
+			que.emplace(cur->left);
+		}
+		if (cur->right) {
+			que.emplace(cur->right);
+		}
+		cur->left = nullptr;
+		cur->right = nullptr;
+		if (cur->val <= target) {
+			insert(result[0], cur);
+		} else {
+			insert(result[1], cur);
+		}
+	}
+	return result;
+}
