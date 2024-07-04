62a20df075f93f1c4053a973d78aaa9aa1fcc95d
neilhuang <neilhuang@synology.com>
2024-07-04 11:56:29 +0800

    <LeetCode202109> 2181. Merge Nodes in Between Zeros

---
+++ 2181_Merge_Nodes_In_Between_Zeros/Solution.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+ListNode* Solution::mergeNodes(ListNode* head) {
+	ListNode* iter = head->next;
+	ListNode* curZero = head;
+	while (iter) {
+		if (iter->val == 0) {
+			curZero->next = iter->next ? iter : nullptr;
+			curZero = curZero->next;
+		} else {
+			curZero->val += iter->val;
+		}
+		iter = iter->next;
+	}
+	return head;
+}
