5bc8b83a740317861c96b4ef84794dd39a35fcb9
neilhuang <neilhuang@synology.com>
2024-07-06 12:58:15 +0800

    <LeetCode202109> 2582. Pass the Pillow

---
+++ 2582_Pass_The_Pillow/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int passThePillow(int n, int time);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
