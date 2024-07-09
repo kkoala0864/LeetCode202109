430400dfcf60cc1ce88771c4564c4203073817fe
neilhuang <neilhuang@synology.com>
2024-07-09 20:12:28 +0800

    <LeetCode202109> 1836. Remove Duplicates From an Unsorted Linked List

---
+++ 1836_Remove_Duplicates_From_An_Unsorted_Linked_List/Solution.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+
+using std::unordered_set;
+using std::unordered_map;
+
+ListNode* Solution::deleteDuplicatesUnsorted(ListNode* head) {
+	unordered_map<int, int> m;
+	unordered_set<ListNode*> remove_set;
+	ListNode* dummy = new ListNode(-1, head);
+
+	ListNode* iter = head;
+	while (iter) {
+		++m[iter->val];
+		iter = iter->next;
+	}
+	iter = head;
+	ListNode* prev = dummy;
+
+	while (iter) {
+		if (m[iter->val] > 1) {
+			prev->next = iter->next;
+		} else {
+			prev = iter;
+		}
+		iter = iter->next;
+	}
+	return dummy->next;
+}
+
---
+++ 1836_Remove_Duplicates_From_An_Unsorted_Linked_List/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	ListNode* deleteDuplicatesUnsorted(ListNode* head);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
