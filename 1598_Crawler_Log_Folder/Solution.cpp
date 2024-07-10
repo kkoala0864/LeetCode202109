c3d96c7bfd80fc54d04a98fa429df961ff64288b
neilhuang <neilhuang@synology.com>
2024-07-10 11:18:16 +0800

    <LeetCode202109> 1598. Crawler Log Folder

---
+++ 1598_Crawler_Log_Folder/Solution.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::minOperations(vector<string>& logs) {
+	int cnt = 0;
+	for (const auto& l : logs) {
+		if (l == "./") continue;
+		else if (l == "../") {
+			if (cnt > 0) --cnt;
+		} else {
+			++cnt;
+		}
+	}
+	return cnt;
+}
