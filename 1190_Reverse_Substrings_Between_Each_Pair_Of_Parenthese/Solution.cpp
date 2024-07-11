fe30e284f53a8ef93902e92301c22aaa34c907f7
neilhuang <neilhuang@synology.com>
2024-07-11 13:53:21 +0800

    <LeetCode202109> 1190. Reverse Substrings Between Each Pair of Parentheses

---
+++ 1190_Reverse_Substrings_Between_Each_Pair_Of_Parenthese/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+
+string recur(string& s, int& idx) {
+	if (idx == s.size()) return "";
+
+	string result;
+	for (; idx < s.size() ; ++idx) {
+		if (s[idx] == ')') {
+			reverse(result.begin(), result.end());
+			return result;
+		} else if (s[idx] == '(') {
+			++idx;
+			result += recur(s, idx);
+		} else {
+			result.push_back(s[idx]);
+		}
+	}
+	return result;
+}
+
+string Solution::reverseParentheses(string s) {
+	int idx = 0;
+	return recur(s, idx);
+}
