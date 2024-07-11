576e268ff95a0e122e12fa79352ae61fed857f78
neilhuang <neilhuang@synology.com>
2024-07-11 13:53:21 +0800

    <LeetCode202109> 1190. Reverse Substrings Between Each Pair of Parentheses

---
+++ 1190_Reverse_Substrings_Between_Each_Pair_Of_Parenthese/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "(ed(et(oc))el)";
+	Solution* test = new Solution();
+	cout << test->reverseParentheses(input) << endl;
+	return 0;
+}
+
