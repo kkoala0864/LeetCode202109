56e64fee23ea8997ff5f3ee34579dae1dcc1c5af
neilhuang <neilhuang@synology.com>
2024-07-13 13:42:58 +0800

    <LeetCode202109> 1717. Maximum Score From Removing Substrings

---
+++ 1717_Maximum_Score_From_Removing_Substrings/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	int maximumGain(string s, int x, int y);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 1717_Maximum_Score_From_Removing_Substrings/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "cdbcbbaaabab";
+	Solution* test = new Solution();
+	cout << test->maximumGain(input, 4, 5) << endl;
+	return 0;
+}
+
