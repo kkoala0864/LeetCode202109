3ceb7538b62bb453552dd236a2e1e99715a5cdaf
neilhuang <neilhuang@synology.com>
2024-07-13 14:28:25 +0800

    <LeetCode202109> 2751. Robot Collisions

---
+++ 2751_Robot_Collisions/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> positions = {5,4,3,2,1};
+	vector<int> healths = {2,17,9,15,10};
+	string directions = "RRRRR";
+	Solution* test = new Solution();
+	auto result = test->survivedRobotsHealths(positions, healths, directions);
+	for (const auto& c : result) cout << c << " ";
+	cout << endl;
+	return 0;
+}
+
