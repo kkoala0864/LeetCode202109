7c32ad2e44401aa4fb22936518a03f508e3dbd2c
neilhuang <neilhuang@synology.com>
2024-07-18 12:05:03 +0800

    <LeetCode202109> 1530. Number of Good Leaf Nodes Pairs

---
+++ 1530_Number_Of_Good_Leaf_Nodes_Pairs/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int countPairs(TreeNode* root, int distance);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
