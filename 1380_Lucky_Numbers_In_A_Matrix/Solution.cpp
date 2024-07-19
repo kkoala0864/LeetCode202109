d87152eb32e86ff10ea2d777ef4e61c74970b119
neilhuang <neilhuang@synology.com>
2024-07-19 10:44:35 +0800

    <LeetCode202109> 1380. Lucky Numbers in a Matrix

---
+++ 1380_Lucky_Numbers_In_A_Matrix/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using std::min;
+using std::max;
+
+vector<int> Solution::luckyNumbers(vector<vector<int>>& matrix) {
+	vector<int> row(matrix.size(), INT_MAX);
+	vector<int> colums(matrix[0].size(), INT_MIN);
+
+	for (int i = 0 ; i < matrix.size() ; ++i) {
+		for (int j = 0 ; j < matrix[0].size() ; ++j) {
+			row[i] = min(row[i], matrix[i][j]);
+			colums[j] = max(colums[j], matrix[i][j]);
+		}
+	}
+	vector<int> result;
+	for (int i = 0 ; i < matrix.size() ; ++i) {
+		for (int j = 0 ; j < matrix[0].size() ; ++j) {
+			if (row[i] == colums[j]) result.emplace_back(row[i]);
+		}
+	}
+	return result;
+
+}
---
+++ 1380_Lucky_Numbers_In_A_Matrix/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,10,4,2},{9,3,8,7},{15,16,17,12}};
+	Solution* test = new Solution();
+	auto result = test->luckyNumbers(input);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
