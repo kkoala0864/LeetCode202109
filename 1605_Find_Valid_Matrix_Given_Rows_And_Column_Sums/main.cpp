cc88c66e3b6a542421fbe4152645a3ad39393096
neilhuang <neilhuang@synology.com>
2024-07-20 15:11:20 +0800

    <LeetCode202109> 1605. Find Valid Matrix Given Row and Column Sums

---
+++ 1605_Find_Valid_Matrix_Given_Rows_And_Column_Sums/main.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> rs = {3,8};
+	vector<int> cs = {4,7};
+	Solution* test = new Solution();
+	auto result = test->restoreMatrix(rs, cs);
+	for (const auto& v : result) {
+		for (const auto& c : v) cout << c << " ";
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
+
