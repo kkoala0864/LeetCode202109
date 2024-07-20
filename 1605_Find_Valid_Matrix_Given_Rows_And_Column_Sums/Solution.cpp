e03dfd6b852a4f0289d8e668b74cf58c3f8465eb
neilhuang <neilhuang@synology.com>
2024-07-20 15:11:20 +0800

    <LeetCode202109> 1605. Find Valid Matrix Given Row and Column Sums

---
+++ 1605_Find_Valid_Matrix_Given_Rows_And_Column_Sums/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+
+
+vector<vector<int>> Solution::restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+	int m = rowSum.size(), n = colSum.size();
+
+	vector<vector<int>> result = vector<vector<int>>(m, vector<int>(n, 0));
+
+	for (int i = 0 ; i < m ; ++i) {
+		for (int j = 0 ; j < n ; ++j) {
+			int v = min(rowSum[i], colSum[j]);
+			result[i][j] += v;
+			rowSum[i] -= v;
+			colSum[j] -= v;
+		}
+	}
+	return result;
+}
