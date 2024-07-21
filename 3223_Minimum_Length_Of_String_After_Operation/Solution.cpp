3192409da9f1d9464501ebfc4c04a12ec292236f
neilhuang <neilhuang@synology.com>
2024-07-21 12:37:05 +0800

    <LeetCode202109> 3223. Minimum Length of String After Operations

---
+++ 3223_Minimum_Length_Of_String_After_Operation/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int Solution::minimumLength(string s) {
+
+	vector<int> cnt(26, 0);
+	for (const auto& c : s) ++cnt[c-'a'];
+	int result = 0;
+	for (const auto& v : cnt) {
+		if (v < 3) {
+			result += v;
+		} else {
+			int mod = v % 2;
+			result += (mod == 0 ? 2 : 1);
+		}
+	}
+	return result;
+}
