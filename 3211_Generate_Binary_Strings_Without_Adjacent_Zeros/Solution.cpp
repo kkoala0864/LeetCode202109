4d653c1aaea3dfb79ec74eb1c00ab2c4846b1ad0
neilhuang <neilhuang@synology.com>
2024-07-23 16:54:22 +0800

    <LeetCode202109> 3211. Generate Binary Strings Without Adjacent Zeros

---
+++ 3211_Generate_Binary_Strings_Without_Adjacent_Zeros/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<string> Solution::validStrings(int n) {
+	vector<string> result;
+	result.emplace_back("0");
+	result.emplace_back("1");
+
+	for (int i = 1 ; i < n ; ++i) {
+		vector<string> tmp;
+		for (const auto& v : result) {
+			if (v.back() == '0') {
+				string s = v;
+				s.push_back('1');
+				tmp.emplace_back(s);
+			} else {
+				string s = v;
+				s.push_back('1');
+				tmp.emplace_back(s);
+				s = v;
+				s.push_back('0');
+				tmp.emplace_back(s);
+			}
+		}
+		result = move(tmp);
+	}
+	return result;
+}
