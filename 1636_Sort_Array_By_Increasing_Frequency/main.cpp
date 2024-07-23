f93ae8c068f6b0c5430ae7f162eebc536520fc93
neilhuang <neilhuang@synology.com>
2024-07-23 09:59:43 +0800

    <LeetCode202109> 1636. Sort Array by Increasing Frequency

---
+++ 1636_Sort_Array_By_Increasing_Frequency/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,1,2,2,2,3};
+	Solution* test = new Solution();
+	auto result = test->frequencySort(input);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
