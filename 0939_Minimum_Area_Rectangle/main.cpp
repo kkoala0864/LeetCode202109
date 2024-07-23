bc01cac437417d2a9f7c38e4bbc4fdc7b30db3c8
neilhuang <neilhuang@synology.com>
2024-07-23 12:10:31 +0800

    <LeetCode202109> 939. Minimum Area Rectangle

---
+++ 0939_Minimum_Area_Rectangle/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,1},{1,3},{3,1},{3,3},{2,2}};
+	Solution* test = new Solution();
+	cout << test->minAreaRect(input) << endl;
+	return 0;
+}
+
