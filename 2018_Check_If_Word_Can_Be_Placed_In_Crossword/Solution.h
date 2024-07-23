a39ed44a2dd4a9b4aa0b28e8b5b03ef32e9c7522
neilhuang <neilhuang@synology.com>
2024-07-23 16:27:40 +0800

    <LeetCode202109> 2018. Check if Word Can Be Placed In Crossword

---
+++ 2018_Check_If_Word_Can_Be_Placed_In_Crossword/Solution.h
@@ -0,0 +1,17 @@
+#include <data_type.h>
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	bool placeWordInCrossword(vector<vector<char>>& board, string word);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 2018_Check_If_Word_Can_Be_Placed_In_Crossword/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<char>> input = {{' '},{'#'},{'o'},{' '},{'t'},{'m'},{'o'},{' '},{'#'},{' '}};
+	string word = "octmor";
+	Solution* test = new Solution();
+	cout << test->placeWordInCrossword(input, word) << endl;
+	return 0;
+}
+
