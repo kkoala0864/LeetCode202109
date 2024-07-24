b2565c38e5d6f6e4821fe4ce25e72a7d6a608226
neilhuang <neilhuang@synology.com>
2024-07-24 11:54:12 +0800

    <LeetCode202109> 2191. Sort the Jumbled Numbers

---
+++ 2191_Sort_The_Jumbled_Numbers/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> mapping = {8,9,4,0,2,1,3,5,7,6};
+	vector<int> numbers = {991,338,38};
+	Solution* test = new Solution();
+	auto result = test->sortJumbled(mapping, numbers);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
