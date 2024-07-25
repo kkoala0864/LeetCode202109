ad5a6655c49b4d43834df529f6d510b4570ddf82
neilhuang <neilhuang@synology.com>
2024-07-25 16:40:49 +0800

    <LeetCode202109> add quick sort template

--- code_template/main.cpp
+++ code_template/main.cpp
@@ -5,7 +5,11 @@ using std::cout;
 using std::endl;
 
 int main(void) {
+	vector<int> nums = {5,1,1,2,0,0};
 	Solution* test = new Solution();
+	auto result = test->Quick_Sort(nums);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
 	return 0;
 }
 
