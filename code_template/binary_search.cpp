17c826d67b762bc44686c60e728c6cefb740265b
neilhuang <neilhuang@synology.com>
2024-07-26 18:56:49 +0800

    <LeetCode202109> add binary search code template

---
+++ code_template/binary_search.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::binary_search(vector<int>& nums, int target) {
+	int l = 0, r = nums.size() - 1;
+	int mid = -1;
+
+	while (l < r) {
+		mid = l + ((r - l) / 2);
+		if (nums[mid] < target) {
+			l = mid + 1;
+		} else {
+			r = mid;
+		}
+	}
+	return nums[l] == target ? l : -1;
+}
