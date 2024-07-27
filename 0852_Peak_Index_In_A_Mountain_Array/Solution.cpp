d181f540c60b6e3f2ded89e5c4f7ce4c7f635ba4
neilhuang <neilhuang@synology.com>
2024-07-27 12:34:49 +0800

    <LeetCode202109> 852. Peak Index in a Mountain Array

---
+++ 0852_Peak_Index_In_A_Mountain_Array/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int Solution::peakIndexInMountainArray(vector<int>& arr) {
+	int l = 0, r = arr.size() - 1;
+	int mid = -1;
+
+	while (l < r) {
+		mid = l + ((r - l) >> 1);
+
+		if (arr[mid] < arr[mid+1]) {
+			l = mid + 1;
+		} else {
+			r = mid;
+		}
+	}
+	return l;
+}
