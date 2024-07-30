3ca24986edfda2c938da960382cb8916c3dd71eb
neilhuang <neilhuang@synology.com>
2024-07-30 16:27:21 +0800

    <LeetCode202109> 853. Car Fleet

---
+++ 0853_Car_Fleet/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int target = 12;
+	vector<int> position = {10,8,0,5,3};
+	vector<int> speed = {2,4,1,1,3};
+	Solution* test = new Solution();
+	cout << test->carFleet(target, position, speed) << endl;
+	return 0;
+}
+
