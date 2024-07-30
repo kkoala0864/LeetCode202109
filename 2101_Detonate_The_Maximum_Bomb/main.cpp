c28941653acc664df565356cb36fa27e62fe4717
neilhuang <neilhuang@synology.com>
2024-07-30 17:30:05 +0800

    <LeetCode202109> 2101. Detonate the Maximum Bombs

---
+++ 2101_Detonate_The_Maximum_Bomb/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> bombs = {{1,2,3},{2,3,1},{3,4,2},{4,5,3},{5,6,4}};
+	Solution* test = new Solution();
+	cout << test->maximumDetonation(bombs) << endl;
+	return 0;
+}
+
