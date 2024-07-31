755bebd36b75886d87fd7d04b8538adee8b4ac3e
neilhuang <neilhuang@synology.com>
2024-07-31 21:05:26 +0800

    <LeetCode202109> 281. Zigzag Iterator

---
+++ 0281_Zigzag_Iterator/Solution.h
@@ -0,0 +1,57 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class ZigzagIterator {
+    public :
+	    ZigzagIterator() {}
+	    ZigzagIterator(vector<int>& v1, vector<int>& v2) {
+		    v.emplace_back(v1);
+		    v.emplace_back(v2);
+		    x = 0;
+		    y = 0;
+		    cnt = 0;
+		    total = x.size() + y.size();
+		    if (cnt < total) {
+			    while (x == 2 || y >= v[x].size()) {
+				    if (x == 2) {
+					++y;
+					x = 0;
+				    } else {
+					++x;
+				    }
+			    }
+		    }
+	    }
+
+	    int next() {
+		    int ret = v[x][y];
+		    ++cnt;
+		    ++x;
+		    if (cnt < total) {
+			    while (x == 2 || y >= v[x].size()) {
+				    if (x == 2) {
+					++y;
+					x = 0;
+				    } else {
+					++x;
+				    }
+			    }
+		    }
+		    return ret;
+	    }
+
+	    bool hasNext() {
+		    return cnt < total;
+	    }
+    private :
+	int x, y;
+	vector<vector<int>> v;
+	int total;
+	int cnt = 0;
+        virtual ~ZigzagIterator() {}
+        ZigzagIterator& operator=(const ZigzagIterator& source);
+        ZigzagIterator(const ZigzagIterator&);
+};
+
