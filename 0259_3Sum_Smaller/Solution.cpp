1e8c6bfacca4fbd07707c4691b6f7b6cd3303a32
neilhuang <neilhuang@synology.com>
2024-07-31 18:06:37 +0800

    <LeetCode202109> 259. 3Sum Smaller

---
+++ 0259_3Sum_Smaller/Solution.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+int twoSumSmaller(vector<int>& nums, int start, int target) {
+	int l = start, r = nums.size() - 1;
+	int result = 0;
+
+	while (l < r) {
+		int v = nums[l] + nums[r];
+		if (v < target) {
+			result += (r - l);
+			++l;
+		} else {
+			--r;
+		}
+	}
+	return result;
+}
+
+int Solution::threeSumSmaller(vector<int>& nums, int target) {
+	sort(nums.begin(), nums.end());
+	int result = 0;
+	int size = nums.size();
+
+	for (int i = 0 ; i < size - 2 ; ++i) {
+		result += twoSumSmaller(nums, i + 1, target - nums[i]);
+	}
+	return result;
+}
