4045bd8a6d4d572dd70e32c38b1f35b65ea67624
neilhuang <neilhuang@synology.com>
2024-08-01 19:21:43 +0800

    <LeetCode202109> 313. Super Ugly Number

---
+++ 0313_Super_Ugly_Number/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int nthSuperUglyNumber(int n, vector<int>& primes);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
