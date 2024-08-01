370f5e9bc8c7b9600c4ae94dac07ddb6e3d07f20
neilhuang <neilhuang@synology.com>
2024-08-01 12:44:00 +0800

    <LeetCode202109> 1062. Longest Repeating Substring

---
+++ 1062_Longest_Repeating_Substring/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	int longestRepeatingSubstring(string s);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
