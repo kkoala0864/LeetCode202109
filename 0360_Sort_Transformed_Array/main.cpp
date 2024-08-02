a688b01cd6a9c7a32dd37c29ea291a77be8dbace
neilhuang <neilhuang@synology.com>
2024-08-02 15:01:00 +0800

    <LeetCode202109> 360. Sort Transformed Array

---
+++ 0360_Sort_Transformed_Array/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {-4,-2,2,4};
+	Solution* test = new Solution();
+	int a = -1, b = 3, c = 5;
+	auto result = test->sortTransformedArray(input, a, b, c);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
