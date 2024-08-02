e66a82754fc745693a936edc54f05e684c9455be
neilhuang <neilhuang@synology.com>
2024-08-02 10:20:00 +0800

    <LeetCode202109> 2134. Minimum Swaps to Group All 1's Together II

--- 2134_Minimum_Swaps_To_Group_All_1s_TogetherII/main.cpp
+++ 2134_Minimum_Swaps_To_Group_All_1s_TogetherII/main.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<int> input = {0,1,0,1,1,0,0};
+	vector<int> input = {1,1,1,0,0,1,0,1,1,0};
 	Solution* test = new Solution();
 	cout << test->minSwaps(input) << endl;
 	return 0;
