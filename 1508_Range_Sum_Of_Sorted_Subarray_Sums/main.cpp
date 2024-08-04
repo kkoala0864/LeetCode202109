0f51f5d35fa266f441faf00af2caba607572336c
neilhuang <neilhuang@synology.com>
2024-08-04 12:46:17 +0800

    <LeetCode202109> 1508. Range Sum of Sorted Subarray Sums

---
+++ 1508_Range_Sum_Of_Sorted_Subarray_Sums/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,3,4};
+	Solution* test = new Solution();
+	cout << test->rangeSum(input, 4, 1, 5) << endl;
+	return 0;
+}
+
