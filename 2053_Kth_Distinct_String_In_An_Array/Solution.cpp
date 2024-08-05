66034a96c8b56e6c6661bbdb2a232fe67c0d023e
neilhuang <neilhuang@synology.com>
2024-08-05 09:51:23 +0800

    <LeetCode202109> 2053. Kth Distinct String in an Array

---
+++ 2053_Kth_Distinct_String_In_An_Array/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+using std::cout;
+using std::endl;
+
+string Solution::kthDistinct(vector<string>& arr, int k) {
+	vector<int> strCnt;
+	vector<string> disCnt;
+	unordered_map<string, int> vToIdx;
+
+	for (int i = 0 ; i < arr.size() ; ++i) {
+		if (vToIdx.count(arr[i]) == 0) {
+			strCnt.emplace_back(1);
+			disCnt.emplace_back(arr[i]);
+			vToIdx[arr[i]] = disCnt.size() - 1;
+		} else {
+			++strCnt[vToIdx[arr[i]]];
+		}
+	}
+	int cnt = 0;
+	for (int i = 0 ; i < strCnt.size() ; ++i) {
+		if (strCnt[i] == 1) ++cnt;
+		if (cnt == k) return disCnt[i];
+	}
+	return "";
+}
