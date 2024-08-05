39daf3bd7a89ad8cdc123dcb3f45205fd4d3b9e6
neilhuang <neilhuang@synology.com>
2024-08-05 10:10:06 +0800

    <LeetCode202109> 1277. Count Square Submatrices with All Ones

---
+++ 1277_Count_Square_Submatrices_With_All_Ones/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int countSquares(vector<vector<int>>& matrix);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
