35c03aaae4a5874e50b0b8c3014b6f9430eaae53
neilhuang <neilhuang@synology.com>
2024-08-06 19:55:47 +0800

    <LeetCode202109> 2424. Longest Uploaded Prefix

---
+++ 2424_Longest_Uploaded_Prefix/Solution.h
@@ -0,0 +1,36 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class LUPrefix {
+    public :
+        LUPrefix() {}
+	LUPrefix(int n) {
+		size = n;
+		cur = 0;
+		v = vector<bool>(n + 1, false);
+	}
+
+	void upload(int video) {
+		if (video > size) return;
+
+		v[video] = true;
+		while (cur < size) {
+			if (v[cur+1]) ++cur;
+			else break;
+		}
+	}
+
+	int longest() {
+		return cur;
+	}
+    private :
+	int size;
+	vector<bool> v;
+	int cur;
+        virtual ~LUPrefix() {}
+        LUPrefix& operator=(const LUPrefix& source);
+        LUPrefix(const LUPrefix&);
+};
+
