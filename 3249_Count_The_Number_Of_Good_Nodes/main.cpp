5fc35132a0515af0d0bb36032fad464fdda25c48
neilhuang <neilhuang@synology.com>
2024-08-11 14:21:02 +0800

    <LeetCode202109> 3249. Count the Number of Good Nodes

---
+++ 3249_Count_The_Number_Of_Good_Nodes/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,1},{1,2},{2,3},{3,4},{0,5},{1,6},{2,7},{3,8}};
+	Solution* test = new Solution();
+	cout << test->countGoodNodes(input) << endl;
+	return 0;
+}
+
