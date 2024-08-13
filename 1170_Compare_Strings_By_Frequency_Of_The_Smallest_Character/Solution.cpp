5e1d1f1d7db7d532b33874a24c2e5e6fb435a410
neilhuang <neilhuang@synology.com>
2024-08-13 17:44:30 +0800

    <LeetCode202109> 1170. Compare Strings by Frequency of the Smallest Character

---
+++ 1170_Compare_Strings_By_Frequency_Of_The_Smallest_Character/Solution.cpp
@@ -0,0 +1,35 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+
+int getF(const string& s) {
+	vector<int> cnt(26, 0);
+	for (const auto& c : s) {
+		++cnt[c-'a'];
+	}
+	for (int i = 0 ; i < 26 ; ++i) {
+		if (cnt[i] != 0) return cnt[i];
+	}
+	return 0;
+}
+
+vector<int> Solution::numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+	vector<int> qF, wF;
+	for (const auto& q : queries) {
+		qF.emplace_back(getF(q));
+	}
+	for (const auto& w : words) {
+		wF.emplace_back(getF(w));
+	}
+	sort(wF.begin(), wF.end());
+	vector<int> result;
+	for (const auto& q : qF) {
+		int idx = upper_bound(wF.begin(), wF.end(), q) - wF.begin();
+
+		result.emplace_back(wF.size() - idx);
+	}
+	return result;
+
+}
---
+++ 1170_Compare_Strings_By_Frequency_Of_The_Smallest_Character/main.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> queries = {"bbb","cc"};
+	vector<string> words = {"a","aa","aaa","aaaa"};
+	Solution* test = new Solution();
+	auto result = test->numSmallerByFrequency(queries, words);
+	for (const auto& v : result) {
+		cout << v << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
