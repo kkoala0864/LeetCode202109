170dd1d95a2c83df2b195dd6dbf1a19566a42bcb
neilhuang <neilhuang@synology.com>
2024-08-14 17:00:26 +0800

    <LeetCode202109> 1296. Divide Array in Sets of K Consecutive Numbers

---
+++ 1296_Divide_Array_In_Sets_Of_K_Consecutive_Numbers/Solution.cpp
@@ -0,0 +1,26 @@
+#include <Solution.h>
+#include <iostream>
+#include <map>
+
+using std::map;
+
+bool Solution::isPossibleDivide(vector<int>& nums, int k) {
+	if (nums.size() % k != 0) return false;
+
+	map<int, int> m;
+	for (const auto& v : nums) ++m[v];
+
+	while (m.size() > 0) {
+		int start = m.begin()->first;
+		int cnt = m.begin()->second;
+		for (int i = 0 ; i < k ; ++i) {
+			if (m.count(start + i) && m[start + i] >= cnt) {
+				m[start+i] -= cnt;
+				if (m[start+i] == 0) m.erase(start + i);
+			} else {
+				return false;
+			}
+		}
+	}
+	return true;
+}
