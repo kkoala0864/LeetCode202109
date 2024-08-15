6c6febfaaba1e5ee087b8f91982bd2db9ca0db73
neilhuang <neilhuang@synology.com>
2024-08-15 14:31:47 +0800

    <LeetCode202109> 3189. Minimum Moves to Get a Peaceful Board

--- 3189_Minimum_Moves_To_Get_A_Peaceful_Board/Solution.cpp
+++ 3189_Minimum_Moves_To_Get_A_Peaceful_Board/Solution.cpp
@@ -1,27 +1,24 @@
 #include <Solution.h>
 #include <iostream>
-#include <queue>
+#include <algorithm>
 
-using std::priority_queue;
-using std::cout;
-using std::endl;
+using std::sort;
 
 int Solution::minMoves(vector<vector<int>>& rooks) {
 	int n = rooks.size();
-	priority_queue<int, vector<int>, std::greater<int>> xpq, ypq;
+	vector<int> x, y;
 
 	for (int i = 0 ; i < n ; ++i) {
-		xpq.emplace(rooks[i][0]);
-		ypq.emplace(rooks[i][1]);
+		x.emplace_back(rooks[i][0]);
+		y.emplace_back(rooks[i][1]);
 	}
 
 	int result = 0;
+	sort(x.begin(), x.end());
+	sort(y.begin(), y.end());
 	for (int i = 0 ; i < n ; ++i) {
-		result += abs(i-xpq.top());
-		result += abs(i-ypq.top());
-
-		xpq.pop();
-		ypq.pop();
+		result += abs(i-x[i]);
+		result += abs(i-y[i]);
 	}
 	return result;
 }
