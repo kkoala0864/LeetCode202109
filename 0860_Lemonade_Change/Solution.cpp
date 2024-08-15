70aa1c17416c9a14f7243d153e815eb7c3db7eee
neilhuang <neilhuang@synology.com>
2024-08-15 11:30:30 +0800

    <LeetCode202109> 860. Lemonade Change

---
+++ 0860_Lemonade_Change/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+
+bool Solution::lemonadeChange(vector<int>& bills) {
+	int sum = 0;
+	vector<int> price = {20, 10, 5};
+	vector<int> cnt(3, 0);
+
+	for (auto& b : bills) {
+		int diff = b - 5;
+		if (diff > sum) return false;
+
+		for (int i = 0 ; i < 3 ; ++i) {
+			int v = min(cnt[i], diff / price[i]);
+			diff -= (v * price[i]);
+			cnt[i] -= v;
+		}
+		if (diff != 0) return false;
+		for(int i = 0 ; i < 3 ; ++i) {
+			int v = b / price[i];
+			cnt[i] += v;
+			b -= (price[i] * v);
+		}
+		sum += 5;
+	}
+	return true;
+}
