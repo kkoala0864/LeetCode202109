aba0d9edaeb8eea0228b48deb8f70a9ff0a2527a
neilhuang <neilhuang@synology.com>
2024-08-18 18:29:27 +0800

    <LeetCode202109> 264. Ugly Number II

--- 0264_Ugly_NumberII/main.cpp
+++ 0264_Ugly_NumberII/main.cpp
@@ -6,8 +6,8 @@ using std::endl;
 
 int main(void) {
     Solution* test = new Solution();
-    cout << test->nthUglyNumber(469) << endl;
-    cout << test->nthUglyNumber2(469) << endl;
+    cout << test->nthUglyNumber(10) << endl;
+    cout << test->nthUglyNumber2(10) << endl;
     return 0;
 }
 
