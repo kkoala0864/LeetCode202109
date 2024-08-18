b450894469c9b50aadcce0884cc969f58c13bd1c
neilhuang <neilhuang@synology.com>
2024-08-18 18:29:27 +0800

    <LeetCode202109> 264. Ugly Number II

--- 0264_Ugly_NumberII/Solution2.cpp
+++ 0264_Ugly_NumberII/Solution2.cpp
@@ -1,33 +1,32 @@
 #include <Solution.h>
 #include <iostream>
-#include <algorithm>
+#include <queue>
 #include <vector>
-#include <climits>
+#include <algorithm>
 
+using std::priority_queue;
 using std::vector;
-using std::min;
+using std::pair;
+using std::greater;
 
 int Solution::nthUglyNumber2(int n) {
-	if (n == 1) return 1;
-	vector<int> dp(2, 1);
-	vector<int> idx = {1, 1, 1};
-	vector<int> multiple = {2, 3, 5};
+	vector<int> dpIdx(3, 0);
+	vector<int> v = {2, 3, 5};
+	vector<int> dp = {1};
 
-	for (int i = 2 ; i <= n ; ++i) {
-		int minVal = INT_MAX;
-		vector<int> increase;
-		for (int j = 0 ; j < 3 ; ++j) {
-			int val = dp[idx[j]] * multiple[j];
-			if (val < minVal) {
-				increase.clear();
-				increase.emplace_back(j);
-				minVal = val;
-			} else if (val == minVal) {
-				increase.emplace_back(j);
-			}
-		}
-		dp.emplace_back(minVal);
-		for (const auto& v : increase) ++idx[v];
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	for (int i = 0 ; i < 3 ; ++i) {
+		pq.emplace(pair<int, int>(v[i] * dp[dpIdx[i]], i));
 	}
-	return dp[n];
+
+	while (dp.size() < n) {
+		int curV = pq.top().first;
+		int idx = pq.top().second;
+		pq.pop();
+
+		if (dp.back() != curV) dp.emplace_back(curV);
+		++dpIdx[idx];
+		pq.emplace(pair<int, int>(v[idx] * dp[dpIdx[idx]], idx));
+	}
+	return dp.back();
 }
