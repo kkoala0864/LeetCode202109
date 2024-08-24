613762cd65f084b4eba454775dba814bd0578c23
neilhuang <neilhuang@synology.com>
2024-08-24 15:49:16 +0800

    <LeetCode202109> 592. Fraction Addition and Subtraction

---
+++ 0592_Fraction_Addition_And_Subtraction/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string expression = "1/3-1/2";
+	Solution* test = new Solution();
+	cout << test->fractionAddition(expression) << endl;
+	return 0;
+}
+
