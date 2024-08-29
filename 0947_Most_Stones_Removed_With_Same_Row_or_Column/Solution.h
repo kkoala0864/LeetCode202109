1ff86005a179ee08353c53ba84a116b621abb08b
neilhuang <neilhuang@synology.com>
2024-08-29 16:25:04 +0800

    <LeetCode202109> 947. Most Stones Removed with Same Row or Column

--- 0947_Most_Stones_Removed_With_Same_Row_or_Column/Solution.h
+++ 0947_Most_Stones_Removed_With_Same_Row_or_Column/Solution.h
@@ -8,6 +8,7 @@ class Solution {
 	int removeStones(vector<vector<int>>& stones);
 	int removeStones2(vector<vector<int>>& stones);
 	int removeStones3(vector<vector<int>>& stones);
+	int removeStones4(vector<vector<int>>& stones);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0947_Most_Stones_Removed_With_Same_Row_or_Column/main.cpp
+++ 0947_Most_Stones_Removed_With_Same_Row_or_Column/main.cpp
@@ -7,9 +7,9 @@ using std::endl;
 int main(void) {
 	vector<vector<int>> input = {{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}};
 	Solution* test = new Solution();
-	cout << test->removeStones(input) << endl;
 	cout << test->removeStones2(input) << endl;
 	cout << test->removeStones3(input) << endl;
+	cout << test->removeStones4(input) << endl;
 	return 0;
 }
 
