c6ccb7bffd153ce054f4925cd1d10b691b9dae84
neilhuang <neilhuang@synology.com>
2024-08-31 13:10:24 +0800

    <LeetCode202109> 1514. Path with Maximum Probability

--- 1514_Path_With_Maximum_Probability/Solution.cpp
+++ 1514_Path_With_Maximum_Probability/Solution.cpp
@@ -7,36 +7,30 @@ using std::priority_queue;
 using std::unordered_map;
 using std::pair;
 
+// Bellman-Ford Algorithm
 double Solution::maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-	unordered_map<int, unordered_map<int, double>> prob;
-	vector<vector<int>> e(n, vector<int>());
 	vector<double> maxProb(n, 0);
 
-	for (int i = 0 ; i < edges.size() ; ++i) {
-		e[edges[i][0]].emplace_back(edges[i][1]);
-		e[edges[i][1]].emplace_back(edges[i][0]);
-		prob[edges[i][0]][edges[i][1]] = succProb[i];
-		prob[edges[i][1]][edges[i][0]] = succProb[i];
-	}
+	maxProb[start_node] = 1.0;
 
-	priority_queue<pair<double, int>> que;
-	maxProb[start_node] = 1;
-	que.emplace(pair<double, int>(1, start_node));
+	for (int i = 0 ; i < n - 1 ; ++i) {
+		bool update = false;
+		for (int ei = 0 ; ei < edges.size() ; ++ei) {
+			int n1 = edges[ei][0];
+			int n2 = edges[ei][1];
 
-	while (!que.empty()) {
-		double curProb = que.top().first;
-		int curNode = que.top().second;
-		que.pop();
+			double prob = succProb[ei];
 
-		if (curProb < maxProb[curNode]) continue;
-		if (curNode == end_node) return curProb;
-
-		for (const auto& n : e[curNode]) {
-			double p = maxProb[curNode] * prob[curNode][n];
-			if (p <= maxProb[n]) continue;
-			maxProb[n] = p;
-			que.emplace(pair<double, int>(p, n));
+			if (maxProb[n1] * prob > maxProb[n2]) {
+				maxProb[n2] = maxProb[n1] * prob;
+				update = true;
+			}
+			if (maxProb[n2] * prob > maxProb[n1]) {
+				maxProb[n1] = maxProb[n2] * prob;
+				update = true;
+			}
 		}
+		if (!update) break;
 	}
-	return 0;
+	return maxProb[end_node];
 }
