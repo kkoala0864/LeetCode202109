01ac74d40cd8c9c5b62074c4a91b3e30e897fb60
neilhuang <neilhuang@synology.com>
2024-09-01 16:34:39 +0800

    <LeetCode202109> 1634. Add Two Polynomials Represented as Linked Lists

---
+++ 1634_Add_Two_Polynomials_Represented_As_Linked_List/Solution.cpp
@@ -0,0 +1,34 @@
+#include <Solution.h>
+#include <iostream>
+
+PolyNode* Solution::addPoly(PolyNode* poly1, PolyNode* poly2) {
+	auto i1 = poly1;
+	auto i2 = poly2;
+
+	PolyNode* iter = new PolyNode();
+	PolyNode* result = iter;
+
+	while (i1 && i2) {
+		if (i1->power == i2->power) {
+			if (i1->coefficient + i2->coefficient != 0) {
+				iter->next = new PolyNode();
+				iter = iter->next;
+				iter->power = i1->power;
+				iter->coefficient = i1->coefficient + i2->coefficient;
+			}
+			i1 = i1->next;
+			i2 = i2->next;
+		} else {
+			if (i1->power > i2->power) {
+				iter->next = i1;
+				i1 = i1->next;
+			} else {
+				iter->next = i2;
+				i2 = i2->next;
+			}
+			iter = iter->next;
+		}
+	}
+	iter->next = i1 ? i1 : i2;
+	return result->next;
+}
---
+++ 1634_Add_Two_Polynomials_Represented_As_Linked_List/Solution.h
@@ -0,0 +1,20 @@
+#include <data_type.h>
+
+struct PolyNode {
+	int coefficient, power;
+	PolyNode *next;
+	PolyNode(): coefficient(0), power(0), next(nullptr) {};
+	PolyNode(int x, int y): coefficient(x), power(y), next(nullptr) {};
+	PolyNode(int x, int y, PolyNode* next): coefficient(x), power(y), next(next) {};
+};
+
+class Solution {
+    public :
+        Solution() {}
+	PolyNode* addPoly(PolyNode* poly1, PolyNode* poly2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
