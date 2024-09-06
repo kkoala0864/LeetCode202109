18f12953f2c262c0b5ce1d5325cad3f574f775d5
neilhuang <neilhuang@synology.com>
2024-09-06 12:07:35 +0800

    <LeetCode202109> 874. Walking Robot Simulation

---
+++ 0874_Walking_Robot_Simulation/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> commands = {4,-1,3};
+	vector<vector<int>> obstacles = {};
+	Solution* test = new Solution();
+	cout << test->robotSim(commands, obstacles) << endl;
+	return 0;
+}
+
