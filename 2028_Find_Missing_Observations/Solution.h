68c060e01a153fc40f0b96dc0d4294f63babdd17
neilhuang <neilhuang@synology.com>
2024-09-06 17:45:02 +0800

    <LeetCode202109> 2028. Find Missing Observations

---
+++ 2028_Find_Missing_Observations/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> missingRolls(vector<int>& rolls, int mean, int n);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
