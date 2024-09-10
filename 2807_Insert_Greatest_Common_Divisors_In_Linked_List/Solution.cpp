4fee5d66e6ca95b12fe6b87e978df1784f0732a3
neilhuang <neilhuang@synology.com>
2024-09-10 14:07:13 +0800

    <LeetCode202109> 2807. Insert Greatest Common Divisors in Linked List

---
+++ 2807_Insert_Greatest_Common_Divisors_In_Linked_List/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::swap;
+
+int getGCD(int lhs, int rhs) {
+	if (lhs < rhs) swap(lhs, rhs);
+	if (rhs == 0) return lhs;
+
+	lhs %= rhs;
+	return getGCD(rhs, lhs);
+}
+
+ListNode* Solution::insertGreatestCommonDivisors(ListNode* head) {
+	ListNode* iter = head;
+	while (iter->next) {
+		int gcd = getGCD(iter->val, iter->next->val);
+		ListNode* g = new ListNode(gcd, iter->next);
+		iter->next = g;
+		iter = g->next;
+	}
+	return head;
+}
