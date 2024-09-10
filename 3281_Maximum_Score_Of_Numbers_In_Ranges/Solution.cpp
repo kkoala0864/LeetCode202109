0b203f3222fec3058ffde01b18f1407675899d1c
neilhuang <neilhuang@synology.com>
2024-09-10 20:28:47 +0800

    <LeetCode202109> 3281. Maximize Score of Numbers in Ranges

---
+++ 3281_Maximum_Score_Of_Numbers_In_Ranges/Solution.cpp
@@ -0,0 +1,34 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::sort;
+using std::min;
+using std::max;
+
+bool check(vector<int>& s, long long diff, long long d) {
+	long long l = s[0];
+	for (int i = 1 ; i < s.size() ; ++i) {
+		l += diff;
+		if (l > (s[i] + d)) return false;
+		l = max(l, (long long)s[i]);
+	}
+	return true;
+}
+
+int Solution::maxPossibleScore(vector<int>& start, int d) {
+	sort(start.begin(), start.end());
+	long long l = 1, r = start.back() + d - start[0];
+	long long mid = 0;
+
+	while (l < r) {
+		mid = l + ((r-l) >> 1);
+
+		if (check(start, mid, d)) {
+			l = mid + 1;
+		} else {
+			r = mid;
+		}
+	}
+	return check(start, l, d) ? l : l - 1;
+}
