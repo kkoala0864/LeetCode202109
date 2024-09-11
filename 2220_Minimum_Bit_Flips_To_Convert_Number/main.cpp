c2e03c0dc04910a148d236ccb110b21601e59a25
neilhuang <neilhuang@synology.com>
2024-09-11 11:10:41 +0800

    <LeetCode202109> 2220. Minimum Bit Flips to Convert Number

---
+++ 2220_Minimum_Bit_Flips_To_Convert_Number/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int start = 10;
+	int goal = 7;
+	Solution* test = new Solution();
+	cout << test->minBitFlips(10, 7) << endl;
+	return 0;
+}
+
