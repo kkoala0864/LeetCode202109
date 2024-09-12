cbcd1f92a6a18eccc960df3c3d2366501347e36b
neilhuang <neilhuang@synology.com>
2024-09-12 17:56:08 +0800

    <LeetCode202109> 1684. Count the Number of Consistent Strings

---
+++ 1684_Count_The_Number_Of_Consistent_Strings/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::countConsistentStrings(string allowed, vector<string>& words) {
+	vector<bool> allow(26, false);
+
+	for (const auto& c : allowed) {
+		allow[c-'a'] = true;
+	}
+	int result = 0;
+	for (const auto& w : words) {
+		bool check = true;
+		for (const auto& c : w) {
+			if (!allow[c-'a']) {
+				check = false;
+				break;
+			}
+		}
+		if (check) ++result;
+	}
+	return result;
+}
---
+++ 1684_Count_The_Number_Of_Consistent_Strings/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string allow = "ab";
+	vector<string> words = {"ad","bd","aaab","baa","badab"};
+	Solution* test = new Solution();
+	cout << test->countConsistentStrings(allow, words) << endl;
+	return 0;
+}
+
