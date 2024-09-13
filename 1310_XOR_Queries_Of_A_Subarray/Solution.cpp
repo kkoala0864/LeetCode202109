2bafc44bb46d3136a62dee7888335a7d3d20059f
neilhuang <neilhuang@synology.com>
2024-09-13 11:09:40 +0800

    <LeetCode202109> 1310. XOR Queries of a Subarray

---
+++ 1310_XOR_Queries_Of_A_Subarray/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+vector<int> Solution::xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+	vector<int> preOR;
+
+	int cur = 0;
+	for (const auto& v : arr) {
+		cur = cur ^ v;
+		preOR.emplace_back(cur);
+	}
+
+	vector<int> result;
+	for (const auto& q : queries) {
+		int v = preOR[q[1]];
+		if (q[0] > 0) v = v ^ preOR[q[0]-1];
+		result.emplace_back(v);
+	}
+	return result;
+}
