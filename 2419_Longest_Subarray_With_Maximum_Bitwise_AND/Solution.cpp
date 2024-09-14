cf689195210b9b96ec1b1d90f7291bd61bc40b0f
neilhuang <neilhuang@synology.com>
2024-09-14 14:05:33 +0800

    <LeetCode202109> 2419. Longest Subarray With Maximum Bitwise AND

---
+++ 2419_Longest_Subarray_With_Maximum_Bitwise_AND/Solution.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+
+//kadane's algorithm
+int Solution::longestSubarray(vector<int>& nums) {
+	int curMax = 0;
+	int maxLen = 0;
+
+	int i = 0;
+	while (i < nums.size()) {
+		int cur = nums[i];
+		int cnt = 0;
+		int j = i;
+
+		for (; j < nums.size() && cur == nums[j] ; ++j) {
+			++cnt;
+		}
+		i = j;
+		if (cur == curMax) {
+			maxLen = max(cnt, maxLen);
+		} else if (cur > curMax) {
+			maxLen = cnt;
+			curMax = cur;
+		}
+	}
+	return maxLen;
+
+}
