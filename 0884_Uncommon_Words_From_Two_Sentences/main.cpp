812a59dad8cdf10309d2f37cc5da31fe0aff1d14
neilhuang <neilhuang@synology.com>
2024-09-17 11:05:39 +0800

    <LeetCode202109> 884. Uncommon Words from Two Sentences

---
+++ 0884_Uncommon_Words_From_Two_Sentences/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string s1 = "this apple is sweet";
+	string s2 = "this apple is sour";
+	Solution* test = new Solution();
+	auto result = test->uncommonFromSentences(s1, s2);
+	for (const auto& i : result) cout << i << " ";
+	cout << endl;
+	return 0;
+}
+
