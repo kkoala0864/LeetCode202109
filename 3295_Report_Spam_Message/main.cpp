55001ea8b26c8661cc842a513b57fab940264359
neilhuang <neilhuang@synology.com>
2024-09-22 14:35:12 +0800

    <LeetCode202109> 3295. Report Spam Message

---
+++ 3295_Report_Spam_Message/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"hello","world","leetcode"};
+	vector<string> bannedWords = {"world", "hello"};
+	Solution* test = new Solution();
+	cout << test->reportSpam(input, bannedWords) << endl;
+	return 0;
+}
+
