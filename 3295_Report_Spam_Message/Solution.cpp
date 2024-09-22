db45f2aca419ba0985f049b7195a93ae98f1dcb2
neilhuang <neilhuang@synology.com>
2024-09-22 14:35:12 +0800

    <LeetCode202109> 3295. Report Spam Message

---
+++ 3295_Report_Spam_Message/Solution.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+
+using std::unordered_set;
+
+bool Solution::reportSpam(vector<string>& message, vector<string>& bannedWords) {
+	unordered_set<string> ban;
+	for (const auto& b : bannedWords) ban.emplace(b);
+	int cnt = 0;
+
+	for (const auto& m : message) {
+		if (ban.count(m)) return true;
+	}
+	return false;
+}
