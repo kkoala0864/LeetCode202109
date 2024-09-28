9b5fc8497b00387a07e7b8b435e84518ee449800
neilhuang <neilhuang@synology.com>
2024-09-28 12:04:23 +0800

    <LeetCode202109> 641. Design Circular Deque

---
+++ 0641_Design_Circular_Deque/Solution.h
@@ -0,0 +1,114 @@
+#include <data_type.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct Node{
+	Node* next;
+	Node* prev;
+	int val;
+	Node(int v) {
+		val = v;
+	}
+	Node(int v, Node* n, Node* p) {
+		val = v;
+		next = n;
+		prev = p;
+	}
+};
+
+class MyCircularDeque {
+    public :
+	MyCircularDeque(int k) {
+		cap = k;
+		cnt = 0;
+		front = nullptr;
+		tail = nullptr;
+	}
+
+	bool insertFront(int value) {
+		if (cnt == cap) return false;
+		if (!front) {
+			Node *v = new Node(value, v, v);
+			front = v;
+			tail = v;
+		} else {
+			Node* v = new Node(value, front, tail);
+			front->prev = v;
+			tail->next = v;
+			front = v;
+		}
+		++cnt;
+		return true;
+	}
+
+	bool insertLast(int value) {
+		if (cnt == cap) return false;
+		if (!front) {
+			Node *v = new Node(value, v, v);
+			front = v;
+			tail = v;
+		} else {
+			Node* v = new Node(value, front, tail);
+			front->prev = v;
+			tail->next = v;
+			tail = v;
+		}
+		++cnt;
+		return true;
+	}
+
+	bool deleteFront() {
+		if (cnt == 0) return false;
+		if (cnt == 1) {
+			front = nullptr;
+			tail = nullptr;
+		} else {
+			front->next->prev = tail;
+			tail->next = front->next;
+			front = front->next;
+		}
+		--cnt;
+		return true;
+	}
+
+	bool deleteLast() {
+		if (cnt == 0) return false;
+		if (cnt == 1) {
+			front = nullptr;
+			tail = nullptr;
+		} else {
+			front->prev = tail->prev;
+			tail->prev->next = front;
+			tail = tail->prev;
+		}
+		--cnt;
+		return true;
+	}
+
+	int getFront() {
+		return front ? front->val : -1;
+	}
+
+	int getRear() {
+		return tail ? tail->val : -1;
+	}
+
+	bool isEmpty() {
+		return cnt == 0;
+	}
+
+	bool isFull() {
+		return cnt == cap;
+	}
+    private :
+	Node* front;
+	Node* tail;
+	int cap;
+	int cnt;
+        virtual ~MyCircularDeque() {}
+        MyCircularDeque& operator=(const MyCircularDeque& source);
+        MyCircularDeque(const MyCircularDeque&);
+};
+
---
+++ 0641_Design_Circular_Deque/main.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+int main(void) {
+	MyCircularDeque* obj = new MyCircularDeque(8);
+	return 0;
+}
+
