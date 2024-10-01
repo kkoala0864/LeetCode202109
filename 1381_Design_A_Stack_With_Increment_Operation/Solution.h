f986d7f13010bfc30afbc7a60f782d4491573150
neilhuang <neilhuang@synology.com>
2024-10-01 12:00:14 +0800

    <LeetCode202109> 1381. Design a Stack With Increment Operation

---
+++ 1381_Design_A_Stack_With_Increment_Operation/Solution.h
@@ -0,0 +1,48 @@
+#include <data_type.h>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using std::stack;
+using std::vector;
+using std::pair;
+using std::min;
+
+class CustomStack {
+    public :
+	CustomStack(int maxSize) {
+		_ms = maxSize;
+	}
+
+	void push(int x) {
+		if (st.size() == _ms) return;
+		st.push(x);
+	}
+
+	int pop() {
+		if (st.empty()) return -1;
+		int size = st.size();
+		int ret = st.top();
+		st.pop();
+		for (auto& i : increList) {
+			if (i.first >= size) {
+				--i.first;
+				ret += i.second;
+			}
+		}
+		return ret;
+
+	}
+
+	void increment(int k, int val) {
+		increList.emplace_back(pair<int, int>(min(k, (int)st.size()), val));
+	}
+    private :
+	int _ms;
+	vector<pair<int, int>> increList;
+	stack<int> st;
+        virtual ~CustomStack() {}
+        CustomStack& operator=(const CustomStack& source);
+        CustomStack(const CustomStack&);
+};
+
