55a67d4eae2397cc932e72a9e51272a460ea38fd
neilhuang <neilhuang@synology.com>
2024-10-12 14:09:11 +0800

    <LeetCode202109> 2406. Divide Intervals Into Minimum Number of Groups

--- 2406_Divide_Intervals_Into_Minimum_Number_Of_Groups/Solution.cpp
+++ 2406_Divide_Intervals_Into_Minimum_Number_Of_Groups/Solution.cpp
@@ -1,27 +1,23 @@
 #include <Solution.h>
 #include <iostream>
 #include <algorithm>
-#include <map>
 
-using std::sort;
-using std::map;
-using std::cout;
-using std::endl;
+using std::max;
 
 int Solution::minGroups(vector<vector<int>>& intervals) {
-	sort(intervals.begin(), intervals.end());
-	map<int, int> groups;
-	int result = 0;
-	for (auto& i : intervals) {
-		auto it = groups.lower_bound(i[0]);
-		if (!groups.empty() && it != groups.begin()) {
-			--it;
-			--it->second;
-			--result;
-			if (it->second == 0) groups.erase(it->first);
-		}
-		++groups[i[1]];
-		++result;
+	vector<int> room(1000002, 0);
+	int maxIdx = 0;
+
+	for (const auto& i : intervals) {
+		++room[i[0]];
+		--room[i[1]+1];
+		maxIdx = max(maxIdx, i[1] + 1);
+	}
+	int result = 1;
+	int cur = 0;
+	for (int i = 0 ; i <= maxIdx ; ++i) {
+		cur += room[i];
+		result = max(cur, result);
 	}
 	return result;
 }
