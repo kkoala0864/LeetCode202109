fe9353480e6f2f2c5b344272e4af0af27ab7f7a9
neilhuang <neilhuang@synology.com>
2024-10-14 20:18:10 +0800

    <LeetCode202109> 2530. Maximal Score After Applying K Operations

---
+++ 2530_Maximal_Score_After_Applying_K_Operations/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::priority_queue;
+
+long long Solution::maxKelements(vector<int>& nums, int k) {
+	long long result = 0;
+	priority_queue<long long> pq;
+
+	for (const auto& v : nums) pq.emplace(v);
+
+	while (k-- > 0) {
+		int v = pq.top();
+		result += v;
+		v /= 3;
+		if ((pq.top() % 3) > 0) v += 1;
+		pq.pop();
+		pq.emplace(v);
+	}
+	return result;
+
+}
