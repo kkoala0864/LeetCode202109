cab466551e1e0af0a752b60e0e4f6e4e05c116e0
neilhuang <neilhuang@synology.com>
2024-10-15 17:34:12 +0800

    <LeetCode202109> 2938. Separate Black and White Balls

---
+++ 2938_Separate_Black_And_White_Balls/Solution.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::swap;
+long long Solution::minimumSteps(string s) {
+	long long result = 0;
+	int l = 0, r = s.size() - 1;
+	while (l < r) {
+		while (l < r && s[l] == '0') ++l;
+		while (l < r && s[r] == '1') --r;
+		if (l < r) {
+			result += (r - l);
+			++l;
+			--r;
+		}
+	}
+	return result;
+}
