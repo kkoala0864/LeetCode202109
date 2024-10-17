9234c6f487347e350a61a781a32b0b4399c1816f
neilhuang <neilhuang@synology.com>
2024-10-17 10:20:14 +0800

    <LeetCode202109> 670. Maximum Swap

---
+++ 0670_Maximum_Swap/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int maximumSwap(int num);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
