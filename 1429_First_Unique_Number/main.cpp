b6eafcbafc4ff3102582aefb65dd2748b8db09d0
neilhuang <neilhuang@synology.com>
2024-10-18 16:00:11 +0800

    <LeetCode202109> 1429. First Unique Number

---
+++ 1429_First_Unique_Number/main.cpp
@@ -0,0 +1,19 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+int main(void) {
+	vector<int> input = {2,3,5};
+	FirstUnique* test = new FirstUnique(input);
+	cout << test->showFirstUnique() << endl;
+	test->add(5);
+	cout << test->showFirstUnique() << endl;
+	test->add(2);
+	cout << test->showFirstUnique() << endl;
+	test->add(3);
+	cout << test->showFirstUnique() << endl;
+
+	return 0;
+}
+
