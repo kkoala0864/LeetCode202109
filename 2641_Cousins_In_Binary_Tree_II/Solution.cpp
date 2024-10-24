2d4b59180b10a023dd2be0af840bdf36b2dd2aec
neilhuang <neilhuang@synology.com>
2024-10-24 14:11:23 +0800

    <LeetCode202109> 2641. Cousins in Binary Tree II

---
+++ 2641_Cousins_In_Binary_Tree_II/Solution.cpp
@@ -0,0 +1,58 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using std::vector;
+using std::queue;
+
+TreeNode* Solution::replaceValueInTree(TreeNode* root) {
+	vector<int> levelSum;
+	queue<TreeNode*> que, next;
+	vector<vector<TreeNode*>> level;
+	que.emplace(root);
+	int sum = 0;
+	level.emplace_back(vector<TreeNode*>());
+
+	while (!que.empty()) {
+		auto cur = que.front();
+		level.back().push_back(cur);
+		que.pop();
+		sum += cur->val;
+
+		if (cur->left) {
+			next.emplace(cur->left);
+		}
+		if (cur->right) {
+			next.emplace(cur->right);
+		}
+		if (que.empty()) {
+			levelSum.emplace_back(sum);
+			sum = 0;
+			que = move(next);
+			if (!que.empty()) {
+				level.push_back(vector<TreeNode*>());
+			}
+		}
+	}
+
+	root->val = 0;
+	for (int i = 0 ; i < level.size() - 1 ; ++i) {
+		for (int j = 0 ; j < level[i].size() ; ++j) {
+			int sum = levelSum[i+1];
+			if (level[i][j]->left) {
+				sum -= level[i][j]->left->val;
+			}
+			if (level[i][j]->right) {
+				sum -= level[i][j]->right->val;
+			}
+			if (level[i][j]->left) {
+				level[i][j]->left->val = sum;
+			}
+			if (level[i][j]->right) {
+				level[i][j]->right->val = sum;
+			}
+		}
+	}
+	return root;
+}
