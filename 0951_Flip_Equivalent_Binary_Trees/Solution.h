1a97ba8b5818982c32bb3bc713a2b24328a51cc9
neilhuang <neilhuang@synology.com>
2024-10-24 13:31:15 +0800

    <LeetCode202109> 951. Flip Equivalent Binary Trees

---
+++ 0951_Flip_Equivalent_Binary_Trees/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	bool flipEquiv(TreeNode* root1, TreeNode* root2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
