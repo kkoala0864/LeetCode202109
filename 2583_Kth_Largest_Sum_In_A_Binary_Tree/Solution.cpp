ff5a1b24953d95064f9085142361fdb4507afad7
neilhuang <neilhuang@synology.com>
2024-10-24 14:24:59 +0800

    <LeetCode202109> 2583. Kth Largest Sum in a Binary Tree

---
+++ 2583_Kth_Largest_Sum_In_A_Binary_Tree/Solution.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::queue;
+using std::priority_queue;
+
+long long Solution::kthLargestLevelSum(TreeNode* root, int k) {
+	priority_queue<long long> pq;
+
+	queue<TreeNode*> que, next;
+	long long sum = 0;
+
+	que.emplace(root);
+	while (!que.empty()) {
+		auto cur = que.front();
+		que.pop();
+
+		sum += (long long)cur->val;
+		if (cur->left) next.emplace(cur->left);
+		if (cur->right) next.emplace(cur->right);
+
+		if (que.empty()) {
+			pq.emplace(sum);
+			sum = 0;
+			que = move(next);
+		}
+	}
+	if (pq.size() < k) return -1;
+	while (k-- > 1) pq.pop();
+	return pq.top();
+}
---
+++ 2583_Kth_Largest_Sum_In_A_Binary_Tree/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	long long kthLargestLevelSum(TreeNode* root, int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
