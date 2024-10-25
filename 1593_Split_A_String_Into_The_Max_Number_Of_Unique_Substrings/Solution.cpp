c7ca7d5af212b4f6049b76237bf53df0145b97c6
neilhuang <neilhuang@synology.com>
2024-10-25 15:13:04 +0800

    <LeetCode202109> 1593. Split a String Into the Max Number of Unique Substrings

---
+++ 1593_Split_A_String_Into_The_Max_Number_Of_Unique_Substrings/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+
+using std::max;
+using std::unordered_set;
+
+void dfs(int idx, const string& s, int cnt, int& result, unordered_set<string>& ss) {
+	if (idx == s.size()) {
+		result = max(result, cnt);
+		return;
+	}
+
+	string curStr;
+	for (int i = idx ; i < s.size() ; ++i) {
+		curStr.push_back(s[i]);
+		if (ss.count(curStr)) continue;
+		ss.emplace(curStr);
+		dfs(i + 1, s, cnt + 1, result, ss);
+		ss.erase(curStr);
+	}
+}
+
+int Solution::maxUniqueSplit(string s) {
+	unordered_set<string> ss;
+	int result = 0;
+	dfs(0, s, 0, result, ss);
+	return result;
+}
