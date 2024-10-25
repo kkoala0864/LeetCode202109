7908a5c27036c3c70fc6d2383c9f515d602f51ee
neilhuang <neilhuang@synology.com>
2024-10-25 15:13:04 +0800

    <LeetCode202109> 1593. Split a String Into the Max Number of Unique Substrings

---
+++ 1593_Split_A_String_Into_The_Max_Number_Of_Unique_Substrings/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "wwwzfvedwfvhsww";
+	Solution* test = new Solution();
+	cout << test->maxUniqueSplit(input) << endl;
+	return 0;
+}
+
