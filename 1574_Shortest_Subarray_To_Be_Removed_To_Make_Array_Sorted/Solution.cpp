100e99bb1b0cc8a9e85b32eeefcccb2c59e6dc78
neilhuang <neilhuang@synology.com>
2024-11-15 14:39:22 +0800

    <LeetCode202109> 1574. Shortest Subarray to be Removed to Make Array Sorted

---
+++ 1574_Shortest_Subarray_To_Be_Removed_To_Make_Array_Sorted/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+using std::cout;
+using std::endl;
+
+int Solution::findLengthOfShortestSubarray(vector<int>& arr) {
+	vector<int> pre, post;
+	for (int i = 0 ; i < arr.size() ; ++i) {
+		if (!pre.empty() && arr[i] < pre.back()) break;
+		pre.emplace_back(arr[i]);
+	}
+	for (int i = arr.size() - 1 ; i >= 0 ; --i) {
+		if (!post.empty() && arr[i] > post.back()) break;
+		post.emplace_back(arr[i]);
+	}
+	reverse(post.begin(), post.end());
+	if (pre.size() + post.size() > arr.size()) return 0;
+	int maxLen = max(pre.size(), post.size());
+	while (!pre.empty()) {
+		int idx = lower_bound(post.begin(), post.end(), pre.back()) - post.begin();
+		if (idx != post.size()) {
+			maxLen = max(maxLen, (int)pre.size() + (int)post.size() - idx);
+		}
+		pre.pop_back();
+	}
+	return arr.size() - maxLen;
+}
