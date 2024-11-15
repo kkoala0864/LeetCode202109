7f2fb71de104e6bb5b1b6e3aa65278742bfa4d6f
neilhuang <neilhuang@synology.com>
2024-11-15 14:39:22 +0800

    <LeetCode202109> 1574. Shortest Subarray to be Removed to Make Array Sorted

---
+++ 1574_Shortest_Subarray_To_Be_Removed_To_Make_Array_Sorted/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,2,2,2,2,3,1,7,5,1,2,2,2,2,2,2,5,6};
+	Solution* test = new Solution();
+	cout << test->findLengthOfShortestSubarray(input) << endl;
+	return 0;
+}
+
