a56e9f923051b2ff2ccf01ce343dad1db1b3a3d4
neilhuang <neilhuang@synology.com>
2024-11-16 11:49:32 +0800

    <LeetCode202109> 2107. Number of Unique Flavors After Sharing K Candies

---
+++ 2107_Number_Of_Unique_Flavors_After_Sharing_K_Candies/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,2,3,4,3};
+	int k = 3;
+	Solution* test = new Solution();
+	cout << test->shareCandies(input, k) << endl;
+	return 0;
+}
+
