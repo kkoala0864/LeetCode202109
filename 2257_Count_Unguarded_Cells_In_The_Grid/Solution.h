7880d054a793744dc44f815de54bc4413e2cecc5
neilhuang <neilhuang@synology.com>
2024-11-21 14:19:49 +0800

    <LeetCode202109> 2257. Count Unguarded Cells in the Grid

---
+++ 2257_Count_Unguarded_Cells_In_The_Grid/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
