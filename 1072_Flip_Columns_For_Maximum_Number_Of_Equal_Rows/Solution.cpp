4190cfe2a91dad6b1f13b00a990749d6f67206ad
neilhuang <neilhuang@synology.com>
2024-11-22 11:25:49 +0800

    <LeetCode202109> 1072. Flip Columns For Maximum Number of Equal Rows

---
+++ 1072_Flip_Columns_For_Maximum_Number_Of_Equal_Rows/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+
+using std::unordered_map;
+using std::max;
+using std::string;
+
+int Solution::maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+	unordered_map<string, int> um;
+	int result = 0;
+	for (const auto& m : matrix) {
+		bool flip = m[0] == 1;
+		string s;
+		for (const auto& i : m) {
+			if (flip) {
+				s.push_back((i == 0 ? 1 : 0) + '0');
+			}
+			else s.push_back(i + '0');
+		}
+		++um[s];
+		result = max(result, um[s]);
+	}
+	return result;
+}
