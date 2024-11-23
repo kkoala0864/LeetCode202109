dc31f3c3925d1bd5cd00349fe18a2fde7680fed0
neilhuang <neilhuang@synology.com>
2024-11-23 14:00:19 +0800

    <LeetCode202109> 1861. Rotating the Box

---
+++ 1861_Rotating_The_Box/main.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<char>> input = {{'*','#','*','.','.','.','#','.','*','.'}};
+	Solution* test = new Solution();
+	auto result = test->rotateTheBox(input);
+	for (const auto& v : result) {
+		for (const auto& c : v) {
+			cout << c << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	return 0;
+}
+
