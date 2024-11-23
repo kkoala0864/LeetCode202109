21583ba29a052354ed719cf82e8686e860ef12a3
neilhuang <neilhuang@synology.com>
2024-11-23 14:00:19 +0800

    <LeetCode202109> 1861. Rotating the Box

---
+++ 1861_Rotating_The_Box/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::swap;
+
+vector<vector<char>> Solution::rotateTheBox(vector<vector<char>>& box) {
+	int m = box.size();
+	int n = box[0].size();
+
+	vector<vector<char>> result(n, vector<char>(m, '.'));
+
+	for (int i = 0 ; i < m ; ++i) {
+		int ei = n - 1;
+		for (int j = n - 1 ; j >= 0 ; --j) {
+			if (box[i][j] == '*') {
+				ei = j - 1;
+			} else if (box[i][j] == '#') {
+				swap(box[i][j], box[i][ei--]);
+			}
+		}
+	}
+
+	for (int i = 0 ; i < n ; ++i) {
+		for (int j = 0 ; j < m ; ++j) {
+			result[i][j] = box[m - 1 - j][i];
+		}
+	}
+	return result;
+}
---
+++ 1861_Rotating_The_Box/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<vector<char>> rotateTheBox(vector<vector<char>>& box);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
