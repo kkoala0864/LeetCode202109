e170c3da9e16ee7d9ba70839f2cf3e07030c14a1
neilhuang <neilhuang@synology.com>
2024-11-24 14:57:27 +0800

    <LeetCode202109> 3355. Zero Array Transformation I

--- 3355_Zero_Array_Transformation_I/Solution.cpp
+++ 3355_Zero_Array_Transformation_I/Solution.cpp
@@ -1,34 +1,20 @@
 #include <Solution.h>
 #include <iostream>
-#include <map>
-
-using std::map;
 
 bool Solution::isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-	map<int, int> m;
+	vector<int> m(nums.size() + 1, 0);
 
 	for (const auto& q : queries) {
 		++m[q[0]];
 		--m[q[1]+1];
 	}
 
-	vector<int> idx;
-	vector<int> v;
-	idx.emplace_back(-1);
-	v.emplace_back(0);
-	int cnt = 0;
-	for (const auto& e : m) {
-		cnt += e.second;
-		idx.emplace_back(e.first);
-		v.emplace_back(cnt);
-	}
+	int cur = 0;
 
 	for (int i = 0 ; i < nums.size() ; ++i) {
-		if (nums[i] == 0) continue;
-		int iv = lower_bound(idx.begin(), idx.end(), i) - idx.begin();
-		if (iv == idx.size()) return false;
-		if (idx[iv] > i) --iv;
-		if (v[iv] < nums[i]) return false;
+		cur += m[i];
+		if (cur < nums[i]) return false;
+
 	}
 	return true;
 }
