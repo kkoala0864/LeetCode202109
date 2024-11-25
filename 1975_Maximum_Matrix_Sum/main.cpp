621cddd8792fe22717ac9fd92758e0860d550c95
neilhuang <neilhuang@synology.com>
2024-11-25 10:04:34 +0800

    <LeetCode202109> 1975. Maximum Matrix Sum

---
+++ 1975_Maximum_Matrix_Sum/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,2,3},{-1,-2,-3},{1,2,3}};
+	Solution* test = new Solution();
+	cout << test->maxMatrixSum(input) << endl;
+	return 0;
+}
+
