40ef704d5028ac3c8e52b8b10e69a372acbf194a
neilhuang <neilhuang@synology.com>
2024-11-26 14:30:04 +0800

    <LeetCode202109> 2924. Find Champion II

--- 2924_Find_ChampionII/main.cpp
+++ 2924_Find_ChampionII/main.cpp
@@ -5,9 +5,9 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<vector<int>> input = {{0,1},{1,2}};
+	vector<vector<int>> input = {{0,2},{1,3},{1,2}};
 	Solution* test = new Solution();
-	cout << test->findChampion(3, input) << endl;
+	cout << test->findChampion(4, input) << endl;
 	return 0;
 }
 
