e9cf8a473e7192f41177134e6d70048d1532676a
neilhuang <neilhuang@synology.com>
2024-11-26 14:30:04 +0800

    <LeetCode202109> 2924. Find Champion II

--- 2924_Find_ChampionII/Solution.cpp
+++ 2924_Find_ChampionII/Solution.cpp
@@ -1,27 +1,20 @@
 #include <Solution.h>
 #include <iostream>
-#include <unordered_set>
-
-using std::unordered_set;
-using std::cout;
-using std::endl;
 
 int Solution::findChampion(int n, vector<vector<int>>& edges) {
-	vector<int> id(n, 0);
+	vector<int> weakDegree(n, 0);
 
 	for (const auto& e : edges) {
-		++id[e[1]];
+		++weakDegree[e[1]];
 	}
-
-	int result = 0;
+	int result = -1;
 	int cnt = 0;
 	for (int i = 0 ; i < n ; ++i) {
-		if (id[i] == 0) {
+		if (weakDegree[i] == 0) {
 			++cnt;
+			if (cnt > 1) return -1;
 			result = i;
 		}
 	}
-
-	return cnt > 1 ? -1 : result;
-
+	return result;
 }
