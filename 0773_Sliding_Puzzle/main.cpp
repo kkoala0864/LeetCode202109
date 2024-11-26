fec225171db69905c289b96288d1bf4ef8745ca4
neilhuang <neilhuang@synology.com>
2024-11-26 11:30:14 +0800

    <LeetCode202109> 773. Sliding Puzzle

---
+++ 0773_Sliding_Puzzle/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{3,2,4},{1,5,0}};
+	Solution* test = new Solution();
+	cout << test->slidingPuzzle(input) << endl;
+	return 0;
+}
+
