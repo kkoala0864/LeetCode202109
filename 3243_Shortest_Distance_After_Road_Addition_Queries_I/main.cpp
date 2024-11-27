c48ce02c35329648a325c3a22dfe0f9541ce82f5
neilhuang <neilhuang@synology.com>
2024-11-27 11:44:46 +0800

    <LeetCode202109> 3243. Shortest Distance After Road Addition Queries I

---
+++ 3243_Shortest_Distance_After_Road_Addition_Queries_I/main.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> query = {{0,6},{4,12}};
+	int n = 14;
+	Solution* test = new Solution();
+	auto result = test->shortestDistanceAfterQueries(n, query);
+	for (const auto& v : result) {
+		cout << v << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
