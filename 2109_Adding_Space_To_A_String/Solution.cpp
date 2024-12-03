4693b4b740fa15a6339eca4a79430e35670e1961
neilhuang <neilhuang@synology.com>
2024-12-03 19:48:15 +0800

    <LeetCode202109> 2109. Adding Spaces to a String

---
+++ 2109_Adding_Space_To_A_String/Solution.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+string Solution::addSpaces(string s, vector<int>& spaces) {
+	string result;
+	int si = 0;
+	for (int i = 0 ; i < s.size() ; ++i) {
+		if (si < spaces.size() && i == spaces[si]) {
+			result.push_back(' ');
+			++si;
+		}
+		result.push_back(s[i]);
+	}
+	return result;
+
+}
---
+++ 2109_Adding_Space_To_A_String/Solution.h
@@ -0,0 +1,17 @@
+#include <data_type.h>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	string addSpaces(string s, vector<int>& spaces);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
