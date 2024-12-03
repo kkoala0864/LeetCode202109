e6343bb365799ad9bf9e67aa3f93ea1b6bc2f164
neilhuang <neilhuang@synology.com>
2024-12-03 20:23:10 +0800

    <LeetCode202109> 2204. Distance to a Cycle in Undirected Graph

---
+++ 2204_Distance_To_A_Cycle_In_Undirect_Graph/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> graph = {{1,2},{2,4},{4,3},{3,1},{0,1},{5,2},{6,5}};
+	int n = 7;
+	Solution* test = new Solution();
+	auto result = test->distanceToCycle(n, graph);
+	for (const auto& iter : result) cout << iter << " ";
+	cout << endl;
+	return 0;
+}
+
