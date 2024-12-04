132729366d8c7e58ac7c8dae40ac86afef9816f7
neilhuang <neilhuang@synology.com>
2024-12-04 10:48:09 +0800

    <LeetCode202109> 2825. Make String a Subsequence Using Cyclic Increments

---
+++ 2825_Make_String_A_Subsequence_Using_Cyclic_Increments/Solution.cpp
@@ -0,0 +1,12 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Solution::canMakeSubsequence(string str1, string str2) {
+	int i2 = 0;
+
+	for (int i = 0 ; i < str1.size() && i2 < str2.size() ; ++i) {
+		char nextChar = ((str1[i] - 'a' + 1) % 26) + 'a';
+		if (str1[i] == str2[i2] || nextChar == str2[i2]) ++i2;
+	}
+	return i2 == str2.size();
+}
---
+++ 2825_Make_String_A_Subsequence_Using_Cyclic_Increments/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	bool canMakeSubsequence(string str1, string str2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
