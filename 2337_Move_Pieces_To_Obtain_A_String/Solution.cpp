2845068491b799e4407e52b1fde46110af3e0f56
neilhuang <neilhuang@synology.com>
2024-12-05 11:41:20 +0800

    <LeetCode202109> 2337. Move Pieces to Obtain a String

---
+++ 2337_Move_Pieces_To_Obtain_A_String/Solution.cpp
@@ -0,0 +1,42 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::pair;
+using std::cout;
+using std::endl;
+
+bool Solution::canChange(string start, string target) {
+	vector<pair<char, int>> order;
+	int tbCnt = 0;
+	for (const auto& v : start) {
+		if (v != '_') continue;
+		++tbCnt;
+	}
+	int cnt = 0;
+	for (int i = 0 ; i < start.size() ; ++i) {
+		if (start[i] == '_') {
+			++cnt;
+		} else {
+			if (start[i] == 'L') {
+				order.emplace_back(pair<char, int>(start[i], cnt));
+			} else {
+				order.emplace_back(pair<char, int>(start[i], tbCnt - cnt));
+			}
+		}
+	}
+
+	int si = 0;
+	cnt = 0;
+	for (int i = 0 ; i < target.size() ; ++i) {
+		if (target[i] == '_') ++cnt;
+		else {
+			if (si >= order.size() || order[si].first != target[i]) return false;
+			if (target[i] == 'L' && order[si].second < cnt) return false;
+			else if (target[i] == 'R' && order[si].second < (tbCnt - cnt)) return false;
+			++si;
+		}
+	}
+	return si == order.size();
+}
---
+++ 2337_Move_Pieces_To_Obtain_A_String/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	bool canChange(string start, string target);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
