a931d972a0d4467ec073b246525168ab540800ae
neilhuang <neilhuang@synology.com>
2024-12-08 18:41:39 +0800

    <LeetCode202109> 2054. Two Best Non-Overlapping Events

---
+++ 2054_Two_Best_Non_Overlapping_Events/Solution.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using std::map;
+using std::max;
+using std::sort;
+using std::cout;
+using std::endl;
+
+int Solution::maxTwoEvents(vector<vector<int>>& events) {
+	map<int, int> dp;
+	int result = 0;
+
+	auto cmp = [](const vector<int>& lhs, const vector<int>& rhs) {
+		return lhs[1] == rhs[1] ? lhs[0] < rhs[0] : lhs[1] < rhs[1];
+	};
+	sort(events.begin(), events.end(), cmp);
+
+	dp[0] = 0;
+	for (const auto& e : events) {
+		result = max(result, e[2]);
+		dp[e[1]] = result;
+	}
+	for (const auto& e : events) {
+		auto iter = dp.lower_bound(e[0]);
+		--iter;
+		result = max(result, iter->second + e[2]);
+	}
+	return result;
+}
