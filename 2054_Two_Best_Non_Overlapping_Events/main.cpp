3cc72c18f812813dffecba7040fc52b817490aef
neilhuang <neilhuang@synology.com>
2024-12-08 18:41:39 +0800

    <LeetCode202109> 2054. Two Best Non-Overlapping Events

---
+++ 2054_Two_Best_Non_Overlapping_Events/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{10,83,53},{63,87,45},{97,100,32},{51,61,16}};
+	Solution* test = new Solution();
+	cout << test->maxTwoEvents(input) << endl;
+	return 0;
+}
+
