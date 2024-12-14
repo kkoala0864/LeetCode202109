faa92d00630accec40a03f62cde472a1bf6e6208
neilhuang <neilhuang@synology.com>
2024-12-14 14:06:44 +0800

    <LeetCode202109> 2762. Continuous Subarrays

---
+++ 2762_Continuous_Subarrays/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {5,4,2,4};
+	Solution* test = new Solution();
+	cout << test->continuousSubarrays(input) << endl;
+	return 0;
+}
+
