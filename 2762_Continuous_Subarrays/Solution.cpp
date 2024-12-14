efe81875cc4e5c546910f6ff7d8fcefdf1509585
neilhuang <neilhuang@synology.com>
2024-12-14 14:06:44 +0800

    <LeetCoded202109> 2762. Continuous Subarrays

---
+++ 2762_Continuous_Subarrays/Solution.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+#include <map>
+
+using std::map;
+using std::cout;
+using std::endl;
+
+long long Solution::continuousSubarrays(vector<int>& nums) {
+	long long result = 0;
+
+	int l = 0;
+	map<int, int> m;
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		++m[nums[i]];
+
+		while (abs(m.begin()->first - m.rbegin()->first) > 2) {
+			--m[nums[l]];
+			if (m[nums[l]] == 0) m.erase(nums[l]);
+			++l;
+		}
+		result += (i - l + 1);
+	}
+	return result;
+}
