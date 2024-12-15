e5ef1f109b6dacc98f5b03fed41976b7e75e2e47
neilhuang <neilhuang@synology.com>
2024-12-15 15:14:36 +0800

    <LeetCode202109> 1792. Maximum Average Pass Ratio

---
+++ 1792_Maximum_Average_Pass_Ratio/Solution.cpp
@@ -0,0 +1,38 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::priority_queue;
+using std::pair;
+using std::cout;
+using std::endl;
+
+double Solution::maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+	priority_queue<pair<double,int>> pq;
+	double size = classes.size();
+
+	for (int i = 0 ; i < classes.size() ; ++i) {
+		if (classes[i][0] == classes[i][1]) continue;
+		double c = (double)classes[i][0] / (double) classes[i][1];
+		double n = (double)(classes[i][0] + 1) / (double)(classes[i][1] + 1);
+		pq.emplace(pair<double, int>(n - c, i));
+	}
+	if (pq.empty()) return 1;
+
+	while (extraStudents-- > 0) {
+		int i = pq.top().second;
+		int r = pq.top().first;
+		pq.pop();
+
+		++classes[i][0];
+		++classes[i][1];
+		double c = (double)classes[i][0] / (double) classes[i][1];
+		double n = (double)(classes[i][0] + 1) / (double)(classes[i][1] + 1);
+		pq.emplace(pair<double, int>(n - c, i));
+	}
+	double rSum = 0;
+	for (const auto& v : classes) {
+		rSum += ((double) v[0] / (double) v[1]);
+	}
+	return rSum / size;
+}
---
+++ 1792_Maximum_Average_Pass_Ratio/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	double maxAverageRatio(vector<vector<int>>& classes, int extraStudents);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
