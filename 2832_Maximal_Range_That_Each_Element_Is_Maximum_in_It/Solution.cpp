b40fbbe1a7861da88a5c6e5938ad4b14e556112a
neilhuang <neilhuang@synology.com>
2024-12-16 11:25:49 +0800

    <LeetCode202109> 2832. Maximal Range That Each Element Is Maximum in It

---
+++ 2832_Maximal_Range_That_Each_Element_Is_Maximum_in_It/Solution.cpp
@@ -0,0 +1,31 @@
+#include <Solution.h>
+#include <iostream>
+#include <stack>
+
+using std::stack;
+using std::cout;
+using std::endl;
+
+vector<int> Solution::maximumLengthOfRanges(vector<int>& nums) {
+	stack<int> st;
+
+	vector<int> result;
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		while (!st.empty() && nums[st.top()] < nums[i]) {
+			st.pop();
+		}
+		if (st.empty()) result.emplace_back(i+1);
+		else result.emplace_back(i - st.top());
+		st.emplace(i);
+	}
+	st = stack<int>();
+	for (int i = nums.size() - 1 ; i >= 0 ; --i) {
+		while (!st.empty() && nums[st.top()] < nums[i]) {
+			st.pop();
+		}
+		if (st.empty()) result[i] += (nums.size()-i-1);
+		else result[i] += (st.top() - i - 1);
+		st.emplace(i);
+	}
+	return result;
+}
---
+++ 2832_Maximal_Range_That_Each_Element_Is_Maximum_in_It/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,5,4,3,6};
+	Solution* test = new Solution();
+	auto result = test->maximumLengthOfRanges(input);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
