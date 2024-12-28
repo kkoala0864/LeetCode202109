d6acd255a7fafaf4170ce4b3b1b43acde4435f29
neilhuang <neilhuang@synology.com>
2024-12-28 14:04:17 +0800

    <LeetCode202109> 1014. Best Sightseeing Pair

---
+++ 1014_Best_Sightseeing_Pair/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::maxScoreSightseeingPair(vector<int>& values) {
+	vector<int> dp;
+
+	int v = INT_MIN;
+	for (int i = 0 ; i < values.size() ; ++i) {
+		v = max(v, i + values[i]);
+		dp.emplace_back(v);
+	}
+	int result = INT_MIN;
+	for (int i = 1 ; i < values.size() ; ++i) {
+		result = max(result, values[i] - i + dp[i-1]);
+	}
+	return result;
+}
