fdd24c94de230c1ff3d65e92d22b6aae9253ba44
neilhuang <neilhuang@synology.com>
2024-12-29 14:50:29 +0800

    <LeetCode202109> 1746. Maximum Subarray Sum After One Operation

---
+++ 1746_Maximum_Subarray_Sum_After_One_Operation/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::maxSumAfterOperation(vector<int>& nums) {
+	int change = 0;
+	int notchange = 0;
+
+	int cc = 0;
+	int cn = 0;
+	int result = INT_MIN;
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		cc = max(change + nums[i], notchange + (nums[i] * nums[i]));
+		cn = notchange + nums[i];
+		result = max({result, cc, cn});
+		// if prefix is negative, we don't take
+		change = max(cc, 0);
+		notchange = max(cn, 0);
+	}
+	return result;
+}
