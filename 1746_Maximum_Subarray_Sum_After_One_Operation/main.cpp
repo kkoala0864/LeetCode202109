49051087a8d3815b3770ed5df249e58b32bb0d3e
neilhuang <neilhuang@synology.com>
2024-12-29 14:50:29 +0800

    <LeetCode202109> 1746. Maximum Subarray Sum After One Operation

---
+++ 1746_Maximum_Subarray_Sum_After_One_Operation/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {-9, 1, 3, -11, 2, -3, 1};
+	Solution* test = new Solution();
+	cout << test->maxSumAfterOperation(input) << endl;
+	return 0;
+}
+
