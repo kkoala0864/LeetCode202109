288bb2d41a6cbebf1033a2847433d1076e424193
neilhuang <neilhuang@synology.com>
2025-01-01 15:20:59 +0800

    <LeetCode202109> 983. Minimum Cost For Tickets

---
+++ 0983_Minimum_Cost_For_Tickets/Solution.cpp
@@ -0,0 +1,28 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+using std::cout;
+using std::endl;
+
+int Solution::mincostTickets(vector<int>& days, vector<int>& costs) {
+	int size = days.size();
+	vector<int> offset({1, 7, 30});
+	vector<int> dp;
+
+	for (int i = 0 ; i < size ; ++i) {
+		int curMin = INT_MAX;
+		for (int oi = 0 ; oi < 3 ; ++oi) {
+			int mc = INT_MAX;
+			int j = i - 1;
+			for (; j >= 0 && (days[i] - days[j]) < offset[oi] ; --j) {}
+			int v = j >= 0 ? dp[j] : 0;
+			v += costs[oi];
+			curMin = min(curMin, v);
+		}
+		dp.emplace_back(curMin);
+	}
+	return dp.back();
+
+}
---
+++ 0983_Minimum_Cost_For_Tickets/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,8,9,10,12,13,20};
+	vector<int> costs = {3,12,54};
+	Solution* test = new Solution();
+	cout << test->mincostTickets(input, costs) << endl;
+	return 0;
+}
+
