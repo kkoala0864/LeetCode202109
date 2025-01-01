0bb641ea664ae2816cd27b97e1b555675d246ff4
neilhuang <neilhuang@synology.com>
2025-01-01 13:44:09 +0800

    <LeetCode202109> 1422. Maximum Score After Splitting a String

---
+++ 1422_Maximum_Score_After_Splitting_a_String/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "011101";
+	Solution* test = new Solution();
+	cout << test->maxScore(input) << endl;
+	return 0;
+}
+
