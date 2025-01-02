f1a8f6aa4affabbf77bb8437f711f97f5538d661
neilhuang <neilhuang@synology.com>
2025-01-02 21:26:41 +0800

    <LeetCode202109> 3403. Find the Lexicographically Largest String From the Box I

---
+++ 3403_Find_the_Lexicographically_Largest_String_From_the_Box_I/Solution.cpp
@@ -0,0 +1,34 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+
+using std::vector;
+using std::priority_queue;
+using std::max;
+
+string Solution::answerString(string word, int numFriends) {
+	if (numFriends == 1) return word;
+	vector<vector<int>> idx(26, vector<int>());
+	int size = word.size();
+
+	int maxIdx = -1;
+	for (int i = 0 ; i < size ; ++i) {
+		idx[word[i]-'a'].emplace_back(i);
+		maxIdx = max(maxIdx, word[i] - 'a');
+	}
+
+	priority_queue<string> pq;
+	for (const auto& i : idx[maxIdx]) {
+		string w;
+		if (i >= numFriends - 1) {
+			w = word.substr(i);
+		} else {
+			int sz = size - (numFriends - i - 1) - i;
+			w = word.substr(i, sz);
+		}
+		pq.push(w);
+	}
+	return pq.top();
+}
---
+++ 3403_Find_the_Lexicographically_Largest_String_From_the_Box_I/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "dbca";
+	int num = 2;
+	Solution* test = new Solution();
+	cout << test->answerString(input, num) << endl;
+	return 0;
+}
+
