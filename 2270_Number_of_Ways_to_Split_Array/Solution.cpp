e10616181eaa6f7a86eb620dd23888d729343840
neilhuang <neilhuang@synology.com>
2025-01-03 10:43:59 +0800

    <LeetCode202109> 2270. Number of Ways to Split Array

---
+++ 2270_Number_of_Ways_to_Split_Array/Solution.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::waysToSplitArray(vector<int>& nums) {
+	long long sum = 0;
+	for (const auto& v : nums) sum += (long long)v;
+	int result = 0;
+	long long cur = 0;
+	for (int i = 0 ; i < nums.size() - 1 ; ++i) {
+		cur += (long long)nums[i];
+		if (cur >= (sum - cur)) ++result;
+	}
+	return result;
+}
