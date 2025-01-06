689ac5365862fd0de04ffce40f96648f1416f28e
neilhuang <neilhuang@synology.com>
2025-01-06 11:05:54 +0800

    <LeetCode202109> 1769. Minimum Number of Operations to Move All Balls to Each Box

---
+++ 1769_Minimum_Number_of_Operations_to_Move_All_Balls_to_Each_Box/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "110";
+	Solution* test = new Solution();
+	auto result = test->minOperations(input);
+	for (const auto& v: result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
