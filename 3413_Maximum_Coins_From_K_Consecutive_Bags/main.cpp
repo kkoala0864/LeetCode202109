ecfc9ea553473c04a6990f51db99463976cab083
neilhuang <neilhuang@synology.com>
2025-01-06 21:07:18 +0800

    <LeetCode202109> 3413. Maximum Coins From K Consecutive Bags

---
+++ 3413_Maximum_Coins_From_K_Consecutive_Bags/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> coins = {{8,10,1},{1,3,2},{5,6,4}};
+	int k = 4;
+	Solution* test = new Solution();
+	cout << test->maximumCoins(coins, k) << endl;
+	return 0;
+}
+
