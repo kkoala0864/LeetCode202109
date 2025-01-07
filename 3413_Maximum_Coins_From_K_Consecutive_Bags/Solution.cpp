44c7bbb1d92283259015caa50dbbb899e1abf8fd
neilhuang <neilhuang@synology.com>
2025-01-07 10:57:49 +0800

    <LeetCode202109> 3413. Maximum Coins From K Consecutive Bags

--- 3413_Maximum_Coins_From_K_Consecutive_Bags/Solution.cpp
+++ 3413_Maximum_Coins_From_K_Consecutive_Bags/Solution.cpp
@@ -4,13 +4,9 @@
 
 using std::sort;
 using std::max;
-using std::cout;
-using std::endl;
+using std::swap;
 
-long long Solution::maximumCoins(vector<vector<int>>& coins, int k) {
-	coins.push_back({0,0,0});
-	sort(coins.begin(), coins.end());
-
+long long helper(vector<vector<int>>& coins, int k) {
 	long long result = 0;
 	long long sum = 0;
 	int l = 0;
@@ -29,21 +25,20 @@ long long Solution::maximumCoins(vector<vector<int>>& coins, int k) {
 		}
 		result = max(sum - rest, result);
 	}
-	sum = 0;
-	l = coins.size() - 1;
-	for (int i = coins.size() - 1 ; i >= 0 ; --i) {
-		sum += ((long long)(coins[i][1] - coins[i][0] + 1) * (long long)coins[i][2]);
-		int rightSide = coins[i][0] + k - 1;
+	return result;
+}
 
-		while (i < l && rightSide < coins[l][0]) {
-			sum -= ((long long)(coins[l][1] - coins[l][0] + 1) * (long long) coins[l][2]);
-			--l;
-		}
-		long long rest = 0;
-		if (rightSide <= coins[l][1]) {
-			rest = ((long long)(coins[l][1] - rightSide) * (long long)coins[l][2]);
-		}
-		result = max(sum - rest, result);
+long long Solution::maximumCoins(vector<vector<int>>& coins, int k) {
+	sort(coins.begin(), coins.end());
+
+	long long result = 0;
+	result = max(helper(coins, k), result);
+	for (auto& c : coins) {
+		swap(c[0], c[1]);
+		c[0] *= -1;
+		c[1] *= -1;
 	}
+	reverse(coins.begin(), coins.end());
+	result = max(helper(coins, k), result);
 	return result;
 }
