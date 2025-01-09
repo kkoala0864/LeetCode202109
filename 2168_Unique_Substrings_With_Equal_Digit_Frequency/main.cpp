2bb6df38fdae2f3e0b07e8c1d6885ee19c5883f3
neilhuang <neilhuang@synology.com>
2025-01-09 11:27:43 +0800

    <LeetCode202109> 2168. Unique Substrings With Equal Digit Frequency

---
+++ 2168_Unique_Substrings_With_Equal_Digit_Frequency/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "1212";
+	Solution* test = new Solution();
+	cout << test->equalDigitFrequency(input) << endl;
+	return 0;
+}
+
