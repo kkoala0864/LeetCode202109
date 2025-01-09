f9221475b3ed784197a4820f5721d5d81e8aed2e
neilhuang <neilhuang@synology.com>
2025-01-09 11:18:56 +0800

    <LeetCode202109> 2185. Counting Words With a Given Prefix

---
+++ 2185_Counting_Words_With_a_Given_Prefix/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"pay","attention","practice","attend"};
+	string pref = "at";
+	Solution* test = new Solution();
+	cout << test->prefixCount(input, pref) << endl;
+	return 0;
+}
+
