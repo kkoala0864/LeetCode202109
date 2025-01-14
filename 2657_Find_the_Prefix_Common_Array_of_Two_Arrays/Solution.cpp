9c804f9e7b04769ee0b443dabc679449063d3095
neilhuang <neilhuang@synology.com>
2025-01-14 17:33:30 +0800

    <LeetCode202109> 2657. Find the Prefix Common Array of Two Arrays

---
+++ 2657_Find_the_Prefix_Common_Array_of_Two_Arrays/Solution.cpp
@@ -0,0 +1,21 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+vector<int> Solution::findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+
+	int size = A.size();
+	vector<int> freq(size + 1, 0);
+	vector<int> result;
+	int cnt = 0;
+	for (int i = 0 ; i < size ; ++i) {
+		++freq[A[i]];
+		if (freq[A[i]] == 2) ++cnt;
+		++freq[B[i]];
+		if (freq[B[i]] == 2) ++cnt;
+		result.emplace_back(cnt);
+	}
+	return result;
+}
