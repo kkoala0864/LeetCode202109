413b276fc6e5e4f94d4c0aa8a990d926cb7bbc0e
neilhuang <neilhuang@synology.com>
2025-01-16 20:32:44 +0800

    <LeetCode202109> 2425. Bitwise XOR of All Pairings

---
+++ 2425_Bitwise_XOR_of_All_Pairings/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input1 = {2,1,3};
+	vector<int> input2 = {10,2,5,0};
+	Solution* test = new Solution();
+	cout << test->xorAllNums(input1, input2) << endl;
+	return 0;
+}
+
