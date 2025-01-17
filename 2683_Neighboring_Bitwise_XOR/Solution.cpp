6ba51ed44364b6efbac2358b0f167784a80e59e6
neilhuang <neilhuang@synology.com>
2025-01-17 19:57:24 +0800

    <LeetCode202109> 2683. Neighboring Bitwise XOR

---
+++ 2683_Neighboring_Bitwise_XOR/Solution.cpp
@@ -0,0 +1,10 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Solution::doesValidArrayExist(vector<int>& derived) {
+	int v = 0;
+	for (const auto& d : derived) {
+		v = v ^ d;
+	}
+	return v == 0;
+}
