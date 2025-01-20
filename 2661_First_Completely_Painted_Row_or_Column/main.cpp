e117ec829d3c5d7f7e52292ee6596c3d67a8f2bc
neilhuang <neilhuang@synology.com>
2025-01-20 11:02:12 +0800

    <LeetCode202109> 2661. First Completely Painted Row or Column

---
+++ 2661_First_Completely_Painted_Row_or_Column/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> arr = {1,3,4,2};
+	vector<vector<int>> mat = {{1,4},{2,3}};
+	Solution* test = new Solution();
+	cout << test->firstCompleteIndex(arr, mat) << endl;
+	return 0;
+}
+
