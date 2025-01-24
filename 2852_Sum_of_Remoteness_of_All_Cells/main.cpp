085db697faa6b2a848729a056a9b54f1458453a9
neilhuang <neilhuang@synology.com>
2025-01-24 10:49:53 +0800

    <LeetCode202109> 2852. Sum of Remoteness of All Cells

---
+++ 2852_Sum_of_Remoteness_of_All_Cells/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{-1,3,4},{-1,-1,-1},{3,-1,-1}};
+	Solution* test = new Solution();
+	cout << test->sumRemoteness(input) << endl;
+	return 0;
+}
+
