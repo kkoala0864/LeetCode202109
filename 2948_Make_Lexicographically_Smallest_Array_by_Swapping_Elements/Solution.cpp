866ad2730d5902f41d4eb10e2fe27cd0a7f8ba42
neilhuang <neilhuang@synology.com>
2025-01-25 13:32:40 +0800

    <LeetCode202109> 2948. Make Lexicographically Smallest Array by Swapping Elements

---
+++ 2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements/Solution.cpp
@@ -0,0 +1,47 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using std::sort;
+using std::pair;
+using std::min;
+using std::priority_queue;
+
+int find(vector<int>& root, int idx) {
+	if (root[idx] == idx) return idx;
+	return root[idx] = find(root, root[idx]);
+}
+
+vector<int> Solution::lexicographicallySmallestArray(vector<int>& nums, int limit) {
+	vector<pair<int, int>> list;
+	int size = nums.size();
+
+	vector<int> root;
+	for (int i = 0 ; i < size ; ++i) {
+		root.emplace_back(i);
+		list.emplace_back(pair<int, int>(nums[i], i));
+	}
+	sort(list.begin(), list.end());
+
+	for (int i = 1 ; i < size ; ++i) {
+		if ((list[i].first - list[i-1].first) > limit) continue;
+		int lr = find(root, list[i].second);
+		int rr = find(root, list[i-1].second);
+		if (lr == rr) continue;
+		root[lr] = root[rr] = min(lr, rr);
+	}
+	vector<priority_queue<int, vector<int>, std::greater<>>> pqs(size);
+	for (int i = 0 ; i < size ; ++i) {
+		int r = find(root, list[i].second);
+		pqs[r].emplace(list[i].first);
+	}
+
+	for (int i = 0 ; i < size ; ++i) {
+		int r = find(root, i);
+		nums[i] = pqs[r].top();
+		pqs[r].pop();
+	}
+	return nums;
+
+}
---
+++ 2948_Make_Lexicographically_Smallest_Array_by_Swapping_Elements/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,5,3,9,8};
+	int limit = 2;
+	Solution* test = new Solution();
+	auto result = test->lexicographicallySmallestArray(input, limit);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
