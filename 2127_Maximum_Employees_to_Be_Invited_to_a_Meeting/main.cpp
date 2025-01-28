6076013e0d1672ca0d1003fc1d79aef9b027aac5
neilhuang <neilhuang@synology.com>
2025-01-28 13:44:06 +0800

    <LeetCode202109> 2127. Maximum Employees to Be Invited to a Meeting

---
+++ 2127_Maximum_Employees_to_Be_Invited_to_a_Meeting/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,0,0,2,1,4};
+	Solution* test = new Solution();
+	cout << test->maximumInvitations(input) << endl;
+	return 0;
+}
+
