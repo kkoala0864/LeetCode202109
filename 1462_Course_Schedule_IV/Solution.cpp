c4b06926cbdccce3bae72b5548ebd31935f05d36
neilhuang <neilhuang@synology.com>
2025-01-28 12:42:10 +0800

    <LeetCode202109> 1462. Course Schedule IV

---
+++ 1462_Course_Schedule_IV/Solution.cpp
@@ -0,0 +1,41 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+
+using std::queue;
+using std::unordered_set;
+
+vector<bool> Solution::checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+	vector<vector<int>> od(numCourses, vector<int>());
+	vector<int> idc(numCourses, 0);
+
+	for (const auto& p : prerequisites) {
+		od[p[0]].emplace_back(p[1]);
+		++idc[p[1]];
+	}
+	vector<unordered_set<int>> preNode(numCourses, unordered_set<int>());
+	queue<int> que;
+	for (int i = 0 ; i < numCourses ; ++i) {
+		if (idc[i] == 0) que.emplace(i);
+	}
+
+	while (!que.empty()) {
+		int cur = que.front();
+		que.pop();
+
+		for (const auto& n : od[cur]) {
+			for (const auto& pn : preNode[cur]) {
+				preNode[n].emplace(pn);
+			}
+			preNode[n].emplace(cur);
+			--idc[n];
+			if (idc[n] == 0) que.emplace(n);
+		}
+	}
+	vector<bool> result;
+	for (const auto& q : queries) {
+		result.push_back(preNode[q[1]].count(q[0]));
+	}
+	return result;
+}
---
+++ 1462_Course_Schedule_IV/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
