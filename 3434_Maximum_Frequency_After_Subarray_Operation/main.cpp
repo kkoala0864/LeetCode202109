0f1bafdfd8dbbc51169356aef1d6d8e4360bf6f3
neilhuang <neilhuang@synology.com>
2025-01-29 13:44:29 +0800

    <LeetCode202109> 3434. Maximum Frequency After Subarray Operation

---
+++ 3434_Maximum_Frequency_After_Subarray_Operation/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {9,8,4,3,2,8,10,2};
+	int k = 8;
+	Solution* test = new Solution();
+	cout << test->maxFrequency(input, k) << endl;
+	return 0;
+}
+
