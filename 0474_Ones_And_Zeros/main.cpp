743e41e4e92c01fe40f240ca30d5be0b888f1823
neilhuang <neilhuang@synology.com>
2025-01-29 14:43:40 +0800

    <LeetCode202109> 474. Ones and Zeroes

--- 0474_Ones_And_Zeros/main.cpp
+++ 0474_Ones_And_Zeros/main.cpp
@@ -7,7 +7,7 @@ using std::endl;
 int main(void) {
 	vector<string> input = {"10","0001","111001","1","0"};
 	Solution* test = new Solution();
-	cout << test->findMaxForm(input, 3, 4) << endl;
+	cout << test->findMaxForm(input, 5, 3) << endl;
 	return 0;
 }
 
