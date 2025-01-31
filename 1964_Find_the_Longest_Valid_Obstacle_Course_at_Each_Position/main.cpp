c211db895c53ce3291606c6c55561f73170f828d
neilhuang <neilhuang@synology.com>
2025-01-31 14:16:32 +0800

    <LeetCode202109> 1964. Find the Longest Valid Obstacle Course at Each Position

---
+++ 1964_Find_the_Longest_Valid_Obstacle_Course_at_Each_Position/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,1,5,6,4,2};
+	Solution* test = new Solution();
+	auto result = test->longestObstacleCourseAtEachPosition(input);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
