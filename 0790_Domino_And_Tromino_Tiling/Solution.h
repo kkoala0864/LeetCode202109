230666df78667551c8d681b3cb3212a0295452cb
neilhuang <neilhuang@synology.com>
2025-01-31 13:28:18 +0800

    <LeetCode202109> 790. Domino and Tromino Tiling

--- 0790_Domino_And_Tromino_Tiling/Solution.h
+++ 0790_Domino_And_Tromino_Tiling/Solution.h
@@ -2,7 +2,6 @@ class Solution {
     public :
         Solution() {}
 	int numTilings(int n);
-	int numTilings2(int n);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
