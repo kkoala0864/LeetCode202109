8cd26b5ec9934bffa38dac22ff64b5f8135aabd1
neilhuang <neilhuang@synology.com>
2025-02-01 13:17:05 +0800

    <LeetCode202109> 1852. Distinct Numbers in Each Subarray

---
+++ 1852_Distinct_Numbers_in_Each_Subarray/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> distinctNumbers(vector<int>& nums, int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
