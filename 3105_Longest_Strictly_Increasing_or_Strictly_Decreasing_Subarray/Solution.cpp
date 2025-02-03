b96281b0e44147b72c065686f3be08a4409955ae
neilhuang <neilhuang@synology.com>
2025-02-03 19:46:11 +0800

    <LeetCode202109> 3105. Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ 3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+
+int Solution::longestMonotonicSubarray(vector<int>& nums) {
+	int il = 1;
+	int dl = 1;
+	int result = 1;
+	for (int i = 1 ; i < nums.size() ; ++i) {
+		if (nums[i] > nums[i-1]) {
+			++il;
+			dl = 1;
+			result = max(result, il);
+		} else if (nums[i] == nums[i-1]) {
+			il = 1;
+			dl = 1;
+		} else {
+			++dl;
+			il = 1;
+			result = max(result, dl);
+		}
+	}
+
+	return result;
+}
---
+++ 3105_Longest_Strictly_Increasing_or_Strictly_Decreasing_Subarray/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,4,3,3,2};
+	Solution* test = new Solution();
+	cout << test->longestMonotonicSubarray(input) << endl;
+	return 0;
+}
+
