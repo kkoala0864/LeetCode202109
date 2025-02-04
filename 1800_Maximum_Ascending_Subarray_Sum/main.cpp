940f069c43c059982656826bfac1f9cfd832adc7
neilhuang <neilhuang@synology.com>
2025-02-04 19:56:06 +0800

    <LeetCode202109> 1800. Maximum Ascending Subarray Sum

---
+++ 1800_Maximum_Ascending_Subarray_Sum/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {10,20,30,5,10,50};
+	Solution* test = new Solution();
+	cout << test->maxAscendingSum(input) << endl;
+	return 0;
+}
+
