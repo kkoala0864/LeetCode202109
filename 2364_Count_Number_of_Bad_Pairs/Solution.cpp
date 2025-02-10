523ade0382efbf9aaef5970750c810744ca069df
neilhuang <neilhuang@synology.com>
2025-02-10 10:36:59 +0900

    <LeetCode202109> 2364. Count Number of Bad Pairs

---
+++ 2364_Count_Number_of_Bad_Pairs/Solution.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+
+long long Solution::countBadPairs(vector<int>& nums) {
+	unordered_map<int, long long> m;
+	long long result = 0;
+
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		result += i;
+		int offset = i - nums[i];
+		result -= m[offset];
+		++m[offset];
+	}
+	return result;
+}
