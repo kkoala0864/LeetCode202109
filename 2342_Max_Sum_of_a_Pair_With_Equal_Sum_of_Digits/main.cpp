07bc3d87c0f7ccc8f3126a8435f7b2befae61b72
neilhuang <neilhuang@synology.com>
2025-02-12 15:27:16 +0800

    <LeetCode202109> 2342. Max Sum of a Pair With Equal Sum of Digits

---
+++ 2342_Max_Sum_of_a_Pair_With_Equal_Sum_of_Digits/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {229,398,269,317,420,464,491,218,439,153,482,169,411,93,147,50,347,210,251,366,401};
+	int result = 973;
+	Solution* test = new Solution();
+	cout << result << endl;
+	cout << test->maximumSum(input) << endl;
+	return 0;
+}
+
