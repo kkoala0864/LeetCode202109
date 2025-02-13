235afc1d09535674c2a9f9d7454b654f07c8a8f1
neilhuang <neilhuang@synology.com>
2025-02-13 15:40:25 +0800

    <LeetCode202109> 2599. Make the Prefix Sum Non-negative

---
+++ 2599_Make_the_Prefix_Sum_Non-negative/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {6,-6,-3,3,1,5,-4,-3,-2,-3,4,-1,4,4,-2,6,0};
+	Solution* test = new Solution();
+	cout << test->makePrefSumNonNegative(input) << endl;
+	return 0;
+}
+
