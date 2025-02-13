9c37ad1c52c1a96ddc784b0ba8f75241e35da964
neilhuang <neilhuang@synology.com>
2025-02-13 11:25:09 +0800

    <LeetCode202109> 3066. Minimum Operations to Exceed Threshold Value II

---
+++ 3066_Minimum_Operations_to_Exceed_Threshold_Value_II/Solution.cpp
@@ -0,0 +1,30 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+
+using std::priority_queue;
+using std::cout;
+using std::endl;
+
+int Solution::minOperations(vector<int>& nums, int k) {
+	priority_queue<long, vector<long>, std::greater<>> pq;
+
+	for (const auto& v : nums) {
+		if (v >= k) continue;
+		pq.emplace(v);
+	}
+
+	int result = 0;
+	while (pq.size() > 1) {
+		long v1 = pq.top();
+		pq.pop();
+		long v2 = pq.top();
+		pq.pop();
+		long v = v1 * 2 + v2;
+		if (v < k) {
+			pq.emplace(v);
+		}
+		++result;
+	}
+	return result + (pq.empty() ? 0 : 1);
+}
---
+++ 3066_Minimum_Operations_to_Exceed_Threshold_Value_II/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {2,11,10,1,3};
+	int k = 10;
+	Solution* test = new Solution();
+	cout << test->minOperations(input, k) << endl;
+	return 0;
+}
+
