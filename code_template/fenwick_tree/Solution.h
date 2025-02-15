86083b6d9dec62a4f2103128b196a467ac3635d6
neilhuang <neilhuang@synology.com>
2025-02-15 15:27:06 +0800

    <LeetCode202109> add fenwick tree algorithm

---
+++ code_template/fenwick_tree/Solution.h
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+class fenwickTree {
+	public:
+		fenwickTree(int size) : tree(size + 1, 0) {}
+		int sum(int idx) {
+			int result = 0;
+			while (idx > 0) {
+				result += tree[idx];
+				idx &= idx - 1;
+			}
+			return result;
+		}
+
+		void update(int idx, int v) {
+			++idx;
+			while (idx < tree.size()) {
+				tree[idx] += v;
+				idx += (idx & -idx);
+			}
+		}
+	private:
+		vector<int> tree;
+};
+
---
+++ code_template/fenwick_tree/main.cpp
@@ -0,0 +1,18 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	fenwickTree tree(10);
+
+	for (int i = 0 ; i < 10 ; ++i) {
+		tree.update(i, i + 1);
+	}
+	for (int i = 0 ; i < 10 ; ++i) {
+		cout << tree.sum(i) << endl;
+	}
+	return 0;
+}
+
