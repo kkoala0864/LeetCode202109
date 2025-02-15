2d20c2bd2625c4d086b4d55d9320583a79718935
neilhuang <neilhuang@synology.com>
2025-02-15 14:01:49 +0800

    <LeetCode202109> 2698. Find the Punishment Number of an Integer

---
+++ 2698_Find_the_Punishment_Number_of_an_Integer/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int n = 37;
+	Solution* test = new Solution();
+	cout << test->punishmentNumber(n) << endl;
+	return 0;
+}
+
