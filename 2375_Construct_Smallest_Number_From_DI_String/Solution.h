479e5c81551e6165bd2d711b6cff8e90aec86e0c
neilhuang <neilhuang@synology.com>
2025-02-18 16:23:47 +0800

    <LeetCode202109> 2375. Construct Smallest Number From DI String

---
+++ 2375_Construct_Smallest_Number_From_DI_String/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	string smallestNumber(string pattern);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
