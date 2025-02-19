a91726f3baae7c22f87f058cbffe93413b2daf09
neilhuang <neilhuang@synology.com>
2025-02-19 20:38:29 +0800

    <LeetCode202109> 777. Swap Adjacent in LR String

---
+++ 0777_Swap_Adjacent_in_LR_String/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string start = "RXXLRXRXL";
+	string result = "XRLXXRRLX";
+	Solution* test = new Solution();
+	cout << test->canTransform(start, result) << endl;
+	return 0;
+}
+
