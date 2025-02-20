68535d9a2875dc8749a7a94e3ad45708dc4618ec
neilhuang <neilhuang@synology.com>
2025-02-20 10:22:26 +0800

    <LeetCode202109> 3439. Reschedule Meetings for Maximum Free Time I

---
+++ 3439_Reschedule_Meetings_for_Maximum_Free_Time_I/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int eventTime = 10;
+	int k = 1;
+	vector<int> startTime = {0,2,9};
+	vector<int> endTime = {1,4,10};
+	Solution* test = new Solution();
+	cout << test->maxFreeTime(eventTime, k, startTime, endTime) << endl;
+	return 0;
+}
+
