a837d8c2aaaee988d007d366e677af8c9462b181
neilhuang <neilhuang@synology.com>
2025-02-20 18:19:27 +0800

    <LeetCode202109> 430. Flatten a Multilevel Doubly Linked List

---
+++ 0430_Flatten_a_Multilevel_Doubly_Linked_List/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	Node* flatten(Node* head);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
