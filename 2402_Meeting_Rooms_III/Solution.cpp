6d10c3f84d8a410b3b0ced6e663865fcc29f7bd6
neilhuang <neilhuang@synology.com>
2025-02-24 15:30:23 +0800

    <LeetCode202109> 2402. Meeting Rooms III

---
+++ 2402_Meeting_Rooms_III/Solution.cpp
@@ -0,0 +1,62 @@
+#include <Solution.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <climits>
+
+using std::priority_queue;
+using std::sort;
+using std::pair;
+using std::max;
+using std::cout;
+using std::endl;
+
+int Solution::mostBooked(int n, vector<vector<int>>& meetings) {
+	priority_queue<pair<long, long>, vector<pair<long, long>>, std::greater<>> pq, m;
+	priority_queue<int, vector<int>, std::greater<>> free;
+
+	vector<int> cnt(n, 0);
+	for (int i = 0 ; i < meetings.size() ; ++i) {
+		m.emplace(pair<int, int>(meetings[i][0], meetings[i][1]));
+	}
+
+	for (int i = 0 ; i < n ; ++i) free.emplace(i);
+
+	while (!m.empty()) {
+		long start = m.top().first;
+		long end = m.top().second;
+		long offset = end - start;
+		m.pop();
+
+		while (!pq.empty() && pq.top().first <= start) {
+			free.emplace(pq.top().second);
+			pq.pop();
+		}
+
+		long curT = 0;
+		long r = 0;
+		if (free.empty()) {
+			curT = pq.top().first;
+			r = pq.top().second;
+			pq.pop();
+		} else {
+			curT = -1;
+			r = free.top();
+			free.pop();
+		}
+
+		curT = max(curT, start);
+		++cnt[r];
+		pq.emplace(pair<long, long>(curT + offset, r));
+	}
+
+	int result = -1;
+	int maxCnt = 0;
+	for (int i = 0 ; i < n ; ++i) {
+		if (maxCnt < cnt[i]) {
+			maxCnt = cnt[i];
+			result = i;
+		}
+	}
+	return result;
+}
