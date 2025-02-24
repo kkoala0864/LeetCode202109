2f4322c66db162fde1c1b93e2b54c798b49c65e6
neilhuang <neilhuang@synology.com>
2025-02-24 20:51:18 +0800

    <LeetCode202109> 4. Median of Two Sorted Arrays

---
+++ 0004_Median_Of_Two_Sorted_Array/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> im = {1, 2};
+	vector<int> in = {3, 4};
+	Solution* test = new Solution();
+	cout << test->findMedianSortedArrays(im, in) << endl;
+	return 0;
+}
+
