757f10e5cf4149518edb4568db2561cd9cd6f352
neilhuang <neilhuang@synology.com>
2025-02-25 19:50:02 +0800

    <LeetCode202109> 2534. Time Taken to Cross the Door

---
+++ 2534_Time_Taken_to_Cross_the_Door/Solution.cpp
@@ -0,0 +1,42 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using std::pair;
+using std::queue;
+using std::cout;
+using std::endl;
+
+vector<int> Solution::timeTaken(vector<int>& arrival, vector<int>& state) {
+	vector<queue<int>> delayList(2, queue<int>());
+	vector<int> result(arrival.size(), 0);
+	int cs = 1;
+	int curT = -1;
+
+	for (int i = 0 ; i < arrival.size() ; ++i) {
+		int os = (cs + 1) & 1;
+		while (curT < arrival[i]) {
+			if (delayList[0].empty() && delayList[1].empty()) {
+				curT = arrival[i];
+				cs = 1;
+			} else {
+				int state = delayList[cs].empty() ? os : cs;
+				result[delayList[state].front()] = curT++;
+				delayList[state].pop();
+				cs = state;
+			}
+		}
+		delayList[state[i]].emplace(i);
+	}
+	int os = (cs + 1) & 1;
+	while (!delayList[cs].empty()) {
+		result[delayList[cs].front()] = curT++;
+		delayList[cs].pop();
+	}
+	while (!delayList[os].empty()) {
+		result[delayList[os].front()] = curT++;
+		delayList[os].pop();
+	}
+	return result;
+}
---
+++ 2534_Time_Taken_to_Cross_the_Door/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> arrival = {0,1,1,2,4};
+	vector<int> state = {0,1,0,0,1};
+	Solution* test = new Solution();
+	auto result = test->timeTaken(arrival, state);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
