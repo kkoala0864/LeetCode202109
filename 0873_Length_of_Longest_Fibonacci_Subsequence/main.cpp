cfec56313d8f5e71a147f9405588fc98892bfc10
neilhuang <neilhuang@synology.com>
2025-02-27 11:48:53 +0800

    <LeetCode202109> 873. Length of Longest Fibonacci Subsequence

---
+++ 0873_Length_of_Longest_Fibonacci_Subsequence/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,5,6,11,13,15,17,28};
+	Solution* test = new Solution();
+	cout << test->lenLongestFibSubseq(input) << endl;
+	return 0;
+}
+
