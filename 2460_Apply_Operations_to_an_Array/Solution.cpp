ca4c801e6b3fc2970d6b28493186c29420932ff1
neilhuang <neilhuang@synology.com>
2025-03-01 13:55:59 +0800

    <LeetCode202109> 2460. Apply Operations to an Array

---
+++ 2460_Apply_Operations_to_an_Array/Solution.cpp
@@ -0,0 +1,20 @@
+#include <Solution.h>
+#include <iostream>
+
+vector<int> Solution::applyOperations(vector<int>& nums) {
+	vector<int> result(nums.size(), 0);
+
+	int ri = 0;
+	for (int i = 0 ; i < nums.size() - 1 ; ++i) {
+		if (nums[i] == 0) continue;
+		if (nums[i] == nums[i+1]) {
+			result[ri++] = (nums[i] << 1);
+			nums[i+1] = 0;
+		} else {
+			result[ri++] = nums[i];
+		}
+	}
+	if (nums.back() != 0) result[ri] = nums.back();
+	return result;
+}
+
---
+++ 2460_Apply_Operations_to_an_Array/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,2,1,1,0};
+	Solution* test = new Solution();
+	auto result = test->applyOperations(input);
+	for (const auto& e : result) {
+		cout << e << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
