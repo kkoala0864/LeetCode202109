ab4142b6ac2bf56af7a0c33245a75177ac2e4b7a
neilhuang <neilhuang@synology.com>
2025-03-03 14:11:07 +0800

    <LeetCode202109> 2570. Merge Two 2D Arrays by Summing Values

---
+++ 2570_Merge_Two_2D_Arrays_by_Summing_Values/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
