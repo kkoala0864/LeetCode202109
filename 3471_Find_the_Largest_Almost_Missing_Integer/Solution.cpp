b54d78560c00577edf164f4f4a10300ff7f5fd8e
neilhuang <neilhuang@synology.com>
2025-03-03 15:16:49 +0800

    <LeetCode202109> 3471. Find the Largest Almost Missing Integer

---
+++ 3471_Find_the_Largest_Almost_Missing_Integer/Solution.cpp
@@ -0,0 +1,29 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+#include <climits>
+#include <algorithm>
+
+using std::max;
+using std::unordered_map;
+
+int Solution::largestInteger(vector<int>& nums, int k) {
+	if (nums.size() == k) {
+		int maxVal = INT_MIN;
+		for (const auto& v : nums) maxVal = max(maxVal, v);
+		return maxVal;
+	} else {
+		unordered_map<int, int> cnt;
+
+		for (int i = 0 ; i < k - 1 ; ++i) cnt[nums[i]] += (i + 1);
+		for (int i = k - 1 ; i < (nums.size() - k + 1) ; ++i) {
+			cnt[nums[i]] += k;
+		}
+		for (int i = nums.size() - k + 1 ; i < nums.size() ; ++i) {
+			cnt[nums[i]] += (nums.size() - i);
+		}
+		int result = INT_MIN;
+		for (const auto& e : cnt) if (e.second == 1) result = max(result, e.first);
+		return result == INT_MIN ? -1 : result;
+	}
+}
---
+++ 3471_Find_the_Largest_Almost_Missing_Integer/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,1,7,10,0};
+	int k = 1;
+	Solution* test = new Solution();
+	cout << test->largestInteger(input, k) << endl;
+	return 0;
+}
+
