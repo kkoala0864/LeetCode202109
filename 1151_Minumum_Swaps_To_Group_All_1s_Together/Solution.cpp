ae8a70dc27613e1e5bf72466de2abdb34bc86197
neilhuang <neilhuang@synology.com>
2025-03-03 14:23:18 +0800

    <LeetCode202109> 1151. Minimum Swaps to Group All 1's Together

--- 1151_Minumum_Swaps_To_Group_All_1s_Together/Solution.cpp
+++ 1151_Minumum_Swaps_To_Group_All_1s_Together/Solution.cpp
@@ -6,17 +6,20 @@ using std::min;
 
 int Solution::minSwaps(vector<int>& data) {
 	int cnt = 0;
-	for (const auto& val : data) if (val == 1) ++cnt;
-	int result = data.size();
+	for (const auto& v : data) cnt += v;
+
+	int minZeroCnt = cnt;
 	int cur = 0;
 	for (int i = 0 ; i < data.size() ; ++i) {
-		if (data[i] == 1) ++cur;
+		if (data[i] == 0) ++cur;
+		if ((i - cnt) >= 0) {
+			if (data[i-cnt] == 0) --cur;
+		}
 
-		if (i >= (cnt - 1)) {
-			result = min(result, cnt - cur);
-			if (data[i - (cnt - 1)] == 1) --cur;
+		if (i >= cnt-1) {
+			minZeroCnt = min(minZeroCnt, cur);
 		}
 	}
-	return result;
+	return minZeroCnt;
 }
 
