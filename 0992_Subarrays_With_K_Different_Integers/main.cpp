c785e0a0fc29d5f314cf0c25dd3d94ba90321306
neilhuang <neilhuang@synology.com>
2025-03-04 20:14:52 +0800

    <LeetCode202109> 992. Subarrays with K Different Integers

---
+++ 0992_Subarrays_With_K_Different_Integers/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,2,1,2,3};
+	int k = 2;
+	Solution* test = new Solution();
+	cout << test->subarraysWithKDistinct(input, k) << endl;
+	return 0;
+}
+
