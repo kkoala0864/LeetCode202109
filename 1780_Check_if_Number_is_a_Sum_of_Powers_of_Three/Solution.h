078743e361d162882c869cff82e5bc7f4e2c5424
neilhuang <neilhuang@synology.com>
2025-03-04 10:12:00 +0800

    <LeetCode202109> 1780. Check if Number is a Sum of Powers of Three

---
+++ 1780_Check_if_Number_is_a_Sum_of_Powers_of_Three/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	bool checkPowersOfThree(int n);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
