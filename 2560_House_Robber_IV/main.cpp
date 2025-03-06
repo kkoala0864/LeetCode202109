42f4ea28884e650aedad906c98199dfc63c70615
neilhuang <neilhuang@synology.com>
2025-03-06 20:55:38 +0800

    <LeetCode202109> 2560. House Robber IV

---
+++ 2560_House_Robber_IV/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {2,3,5,9};
+	int k = 2;
+	Solution* test = new Solution();
+	cout << test->minCapability(input, k) << endl;
+	return 0;
+}
+
