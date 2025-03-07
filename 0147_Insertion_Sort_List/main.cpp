a3276789ed06219b2ee7e7a3a2ed837b9e4ef8b2
neilhuang <neilhuang@synology.com>
2025-03-07 17:30:29 +0800

    <LeetCode202109> 147. Insertion Sort List

---
+++ 0147_Insertion_Sort_List/main.cpp
@@ -0,0 +1,25 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	ListNode* head = new ListNode(4);
+	ListNode* node1 = new ListNode(2);
+	ListNode* node2 = new ListNode(1);
+	ListNode* node3 = new ListNode(3);
+	head->next = node1;
+	node1->next = node2;
+	node2->next = node3;
+
+	Solution* test = new Solution();
+	auto result = test->insertionSortList(head);
+	while (result) {
+		cout << result->val << " ";
+		result = result->next;
+	}
+	cout << endl;
+	return 0;
+}
+
