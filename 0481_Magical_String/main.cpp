9080c9589859139e548d0ea54b695d74604d83d2
neilhuang <neilhuang@synology.com>
2025-03-08 14:06:33 +0800

    <LeetCode202109> 481. Magical String

---
+++ 0481_Magical_String/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int n = 1777;
+	Solution* test = new Solution();
+	cout << test->magicalString(n) << endl;
+	return 0;
+}
+
