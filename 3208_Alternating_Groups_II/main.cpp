1aa6d1bd2958c585e3a508ad936d86bf733a8449
neilhuang <neilhuang@synology.com>
2025-03-09 15:41:23 +0800

    <LeetCode202109> 3208. Alternating Groups II

---
+++ 3208_Alternating_Groups_II/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {0,1,0,1};
+	int k = 3;
+	Solution* test = new Solution();
+	cout << test->numberOfAlternatingGroups(input, k) << endl;
+	return 0;
+}
+
