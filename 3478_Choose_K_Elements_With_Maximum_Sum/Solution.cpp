3422dc6ec5019a853f1190c77d35568037718dac
neilhuang <neilhuang@synology.com>
2025-03-09 16:21:17 +0800

    <LeetCode202109> 3478. Choose K Elements With Maximum Sum

---
+++ 3478_Choose_K_Elements_With_Maximum_Sum/Solution.cpp
@@ -0,0 +1,41 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using std::sort;
+using std::pair;
+using std::priority_queue;
+
+vector<long long> Solution::findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+	int size = nums1.size();
+	vector<long long> result(size, 0);
+
+	vector<pair<int, int>> vToIdx;
+
+	for (int i = 0 ; i < size ; ++i) {
+		vToIdx.emplace_back(pair<int, int>(nums1[i], i));
+	}
+	sort(vToIdx.begin(), vToIdx.end());
+
+	priority_queue<int, vector<int>, std::greater<>> minHeap;
+	long long sum = 0;
+	for (int i = 0 ; i < size ; ++i) {
+		int idx = vToIdx[i].second;
+		if (i > 0) {
+			if (vToIdx[i].first == vToIdx[i-1].first) {
+				result[idx] = result[vToIdx[i-1].second];
+			} else {
+				result[idx] = sum;
+			}
+		}
+		minHeap.emplace(nums2[idx]);
+		sum += (long long)nums2[idx];
+		while (minHeap.size() > k) {
+			sum -= (long long)minHeap.top();
+			minHeap.pop();
+		}
+	}
+	return result;
+}
+
