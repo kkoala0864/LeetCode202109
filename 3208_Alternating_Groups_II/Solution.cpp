eeb808455c85a85145e4b474f3d2732d60629310
neilhuang <neilhuang@synology.com>
2025-03-09 15:41:23 +0800

    <LeetCode202109> 3208. Alternating Groups II

---
+++ 3208_Alternating_Groups_II/Solution.cpp
@@ -0,0 +1,23 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int Solution::numberOfAlternatingGroups(vector<int>& colors, int k) {
+	int size = colors.size();
+	int result = 0;
+	int l = 0;
+	for (int i = 1 ; i < (size + k - 1); ++i) {
+		if (colors[i%size] == colors[(i-1+size) % size]) {
+			l = i;
+			continue;
+		}
+		int cur = i;
+		if (cur < l) cur += size;
+		if ((cur - l + 1) >= k) {
+			++result;
+		}
+	}
+	return result;
+}
---
+++ 3208_Alternating_Groups_II/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	int numberOfAlternatingGroups(vector<int>& colors, int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
