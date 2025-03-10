ed0f5210a0d2323d86890412d26a5b26c4d623b4
neilhuang <neilhuang@synology.com>
2025-03-10 12:02:16 +0800

    <LeetCode202109> 3479. Fruits Into Baskets III

---
+++ 3479_Fruits_Into_Baskets_III/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <climits>
+
+using std::cout;
+using std::endl;
+
+int Solution::numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+	STree tree(baskets);
+	int result = 0;
+	int size = fruits.size();
+	for (int i = 0 ; i < size ; ++i) {
+		int idx = tree.query(fruits[i]);
+		cout << idx << endl;
+		if (idx == -1) {
+			++result;
+		} else {
+			tree.set(INT_MIN, idx, 1, 0, size-1);
+		}
+	}
+	return result;
+}
