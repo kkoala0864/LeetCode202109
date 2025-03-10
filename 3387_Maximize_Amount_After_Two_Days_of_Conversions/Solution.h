7b2ee5504ffb43d46729345b7d89a73659607a39
neilhuang <neilhuang@synology.com>
2025-03-10 21:10:18 +0800

    <LeetCode202109> 3387. Maximize Amount After Two Days of Conversions

---
+++ 3387_Maximize_Amount_After_Two_Days_of_Conversions/Solution.h
@@ -0,0 +1,17 @@
+#include <data_type.h>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	double maxAmount(string initialCurrency, vector<vector<string>>& pairs1, vector<double>& rates1, vector<vector<string>>& pairs2, vector<double>& rates2);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
