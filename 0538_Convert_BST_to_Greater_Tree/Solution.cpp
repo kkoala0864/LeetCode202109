a328474e085a088b25abf3bdfab1c7c1f3de9318
neilhuang <neilhuang@synology.com>
2025-03-10 18:03:13 +0800

    <LeetCode202109> 538. Convert BST to Greater Tree

---
+++ 0538_Convert_BST_to_Greater_Tree/Solution.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+void convert(TreeNode* node, int& sum) {
+	if (!node) return;
+	convert(node->right, sum);
+	sum += node->val;
+	node->val = sum;
+	convert(node->left, sum);
+}
+
+TreeNode* Solution::convertBST(TreeNode* root) {
+	if (!root) return root;
+	int sum = 0;
+	convert(root, sum);
+	return root;
+}
