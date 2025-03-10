981eba268ce60437ee7036e5e8c247584869e3b4
neilhuang <neilhuang@synology.com>
2025-03-10 20:12:18 +0800

    <LeetCode202109> 3306. Count of Substrings Containing Every Vowel and K Consonants II

---
+++ 3306_Count_of_Substrings_Containing_Every_Vowel_and_K_Consonants_II/Solution.cpp
@@ -0,0 +1,49 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+
+using std::vector;
+using std::unordered_set;
+using std::unordered_map;
+
+long long Solution::countOfSubstrings(string word, int k) {
+	long long result = 0;
+	unordered_set<char> us = {'a', 'e', 'i', 'o', 'u'};
+	unordered_map<char, int> um;
+	int conCnt = 0;
+	int j = 0;
+	vector<int> cc(word.size(), 0);
+	int cnt = 0;
+	for (int i = word.size() - 1 ; i >= 0 ; --i) {
+		if (us.count(word[i])) ++cnt;
+		else cnt = 0;
+		cc[i] = cnt;
+	}
+
+	for (int i = 0 ; i < word.size() ; ++i) {
+		while (j < word.size() && (um.size() < 5 || conCnt < k)) {
+			if (us.count(word[j])) {
+				++um[word[j]];
+			} else {
+				++conCnt;
+			}
+			++j;
+		}
+
+		if (um.size() == 5 && conCnt == k) {
+			result += (1 + (j == word.size() ? 0 : cc[j]));
+		}
+
+		if (us.count(word[i])) {
+			--um[word[i]];
+			if (um[word[i]] == 0) {
+				um.erase(word[i]);
+			}
+		} else {
+			--conCnt;
+		}
+	}
+	return result;
+}
---
+++ 3306_Count_of_Substrings_Containing_Every_Vowel_and_K_Consonants_II/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	string input = "ieaouqqieaouqq";
+	int k = 1;
+	Solution* test = new Solution();
+	cout << test->countOfSubstrings(input, k) << endl;
+	return 0;
+}
+
