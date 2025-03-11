8b35a15e0a5b06f94b8b927aba7e8515f5436314
neilhuang <neilhuang@synology.com>
2025-03-11 20:28:55 +0800

    <LeetCode202109> 3346. Maximum Frequency of an Element After Performing Operations I

---
+++ 3346_Maximum_Frequency_of_an_Element_After_Performing_Operations_I/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {5,11,20,20};
+	int k = 5;
+	int operations = 1;
+	Solution* test = new Solution();
+	cout << test->maxFrequency(input, k, operations) << endl;
+	return 0;
+}
+
