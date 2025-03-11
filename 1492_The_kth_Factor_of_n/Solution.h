124f79022d7cee77baf4b5574b1c53e4f58f8284
neilhuang <neilhuang@synology.com>
2025-03-11 20:43:22 +0800

    <LeetCode202109> 1492. The kth Factor of n

---
+++ 1492_The_kth_Factor_of_n/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int kthFactor(int n, int k);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
