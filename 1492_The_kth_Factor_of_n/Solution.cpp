2e89154874f763ecc0da41e02d68cbe4bfd6e60d
neilhuang <neilhuang@synology.com>
2025-03-11 20:43:22 +0800

    <LeetCode202109> 1492. The kth Factor of n

---
+++ 1492_The_kth_Factor_of_n/Solution.cpp
@@ -0,0 +1,26 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+int Solution::kthFactor(int n, int k) {
+	vector<int> pre, post;
+
+	for (int i = 1 ; (i * i) <= n ; ++i) {
+		if (n % i) continue;
+		pre.emplace_back(i);
+		if (i != (n / i)) {
+			post.emplace_back(n / i);
+		}
+	}
+	if (k > (pre.size() + post.size())) {
+		return -1;
+	}
+	if (k <= pre.size()) {
+		return pre[k-1];
+	} else {
+		return post[post.size() - (k - pre.size())];
+	}
+
+}
