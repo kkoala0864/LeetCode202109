040114f9db351e4d4f7d74214bf465f213427f9f
neilhuang <neilhuang@synology.com>
2025-03-14 10:53:57 +0800

    <LeetCode202109> 2226. Maximum Candies Allocated to K Children

--- 2226_Maximum_Candies_Allocated_To_K_Children/main.cpp
+++ 2226_Maximum_Candies_Allocated_To_K_Children/main.cpp
@@ -5,9 +5,9 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<int> input = {5,8,6};
+	vector<int> input = {5,6,4,10,10,1,1,2,2,2};
 	Solution* test = new Solution();
-	cout << test->maximumCandies(input, 3) << endl;
+	cout << test->maximumCandies(input, 9) << endl;
 	return 0;
 }
 
