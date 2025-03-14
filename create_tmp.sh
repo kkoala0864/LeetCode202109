78e0179b8ce9b9f733d073232ea54633c1c6e840
neilhuang <neilhuang@synology.com>
2025-03-14 20:24:45 +0800

    <LeetCode202109> update create template script

--- create_tmp.sh
+++ create_tmp.sh
@@ -1,19 +1,29 @@
-#! /bin/bash
 
-if [ -z "$1" ]; then
-	  echo "請提供一個輸入字串"
-	    exit 1
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "請提供一個輸入字串"
+	exit 1
 fi
 
-input_str="$1"
+input_str="$*"
+
 number=$(echo "$input_str" | cut -d '.' -f 1)
+
+if ! [[ "$number" =~ ^[0-9]+$ ]]; then
+	echo "錯誤：輸入的第一部分應該是數字"
+	exit 1
+fi
+
 formatted_number=$(printf "%04d" "$number")
 
-string_part=$(echo "$input_str" | cut -d '.' -f 2-)
+string_part=$(echo "$input_str" | cut -d '.' -f 2- | sed 's/^ *//g')
 formatted_string=$(echo "$string_part" | tr ' ' '_')
 
-new_filename="${formatted_number}${formatted_string}"
-echo $new_filename
+new_filename="${formatted_number}_${formatted_string}"
+echo "$new_filename"
 
 cp -r "0000_Template" "$new_filename"
 
+cd "$new_filename" || exit
+pwd
