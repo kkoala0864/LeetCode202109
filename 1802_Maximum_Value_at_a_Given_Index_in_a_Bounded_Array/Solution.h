f7e8bff709bab4c821a7ec2f4f9068fee42c61be
neilhuang <neilhuang@synology.com>
2025-03-14 20:02:05 +0800

    <LeetCode202109> 1802. Maximum Value at a Given Index in a Bounded Array

---
+++ 1802_Maximum_Value_at_a_Given_Index_in_a_Bounded_Array/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int maxValue(int n, int index, int maxSum);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
---
+++ 1802_Maximum_Value_at_a_Given_Index_in_a_Bounded_Array/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int n = 4;
+	int index = 0;
+	int maxSum = 4;
+	Solution* test = new Solution();
+	cout << test->maxValue(n, index, maxSum) << endl;
+	return 0;
+}
+
