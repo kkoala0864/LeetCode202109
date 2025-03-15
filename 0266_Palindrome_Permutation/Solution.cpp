0eb50b702d1d574d8e62b9fe286d68255f2adf30
neilhuang <neilhuang@synology.com>
2025-03-15 12:44:05 +0800

    <LeetCode202109> 266. Palindrome Permutation

---
+++ 0266_Palindrome_Permutation/Solution.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+#include <vector>
+
+using std::vector;
+
+bool Solution::canPermutePalindrome(string s) {
+	vector<int> cnt(26, 0);
+
+	for (const auto& c : s) ++cnt[c-'a'];
+
+	int odd_cnt = 0;
+	for (const auto& v : cnt) {
+		if (v & 1) ++odd_cnt;
+	}
+	return odd_cnt < 2;
+}
