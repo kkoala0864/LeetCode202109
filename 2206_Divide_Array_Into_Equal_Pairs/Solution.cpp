3216b92647a2ff241e481ce553b9bef21bc47415
neilhuang <neilhuang@synology.com>
2025-03-17 09:40:11 +0800

    <LeetCode202109> 2206. Divide Array Into Equal Pairs

---
+++ 2206_Divide_Array_Into_Equal_Pairs/Solution.cpp
@@ -0,0 +1,9 @@
+#include <Solution.h>
+#include <iostream>
+
+bool Solution::divideArray(vector<int>& nums) {
+	vector<int> cnt(501, 0);
+	for (const auto& v : nums) ++cnt[v];
+	for (const auto& v : cnt) if (v & 1) return false;
+	return true;
+}
