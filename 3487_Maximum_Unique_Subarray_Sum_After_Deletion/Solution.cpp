4c5863b3a63a43b27cfc8ac4d1d2e40e11c4b8f0
neilhuang <neilhuang@synology.com>
2025-03-17 10:27:15 +0800

    <LeetCode202109> 3487. Maximum Unique Subarray Sum After Deletion

---
+++ 3487_Maximum_Unique_Subarray_Sum_After_Deletion/Solution.cpp
@@ -0,0 +1,22 @@
+#include <Solution.h>
+#include <iostream>
+#include <set>
+#include <climits>
+#include <algorithm>
+
+using std::max;
+using std::set;
+
+int Solution::maxSum(vector<int>& nums) {
+	set<int> st;
+	for (const auto& v : nums) st.emplace(v);
+
+	int sum = 0;
+	int result = INT_MIN;
+	for (const auto& v : st) {
+		sum += v;
+		result = max(result, sum);
+		if (sum < 0) sum = 0;
+	}
+	return result;
+}
