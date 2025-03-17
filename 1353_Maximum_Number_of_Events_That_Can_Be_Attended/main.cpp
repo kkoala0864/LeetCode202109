1fbec10a02fc6dc376491870eec1950b0ad8057f
neilhuang <neilhuang@synology.com>
2025-03-17 21:01:20 +0800

    <LeetCode202109> 1353. Maximum Number of Events That Can Be Attended

---
+++ 1353_Maximum_Number_of_Events_That_Can_Be_Attended/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{1,2},{2,3},{3,4},{1,2}};
+	Solution* test = new Solution();
+	cout << test->maxEvents(input) << endl;
+	return 0;
+}
+
