a4b1b91530e7d215c7c2797ffabefcd636884be3
neilhuang <neilhuang@synology.com>
2025-03-17 20:36:37 +0800

    <LeetCode202109> 3489. Zero Array Transformation IV

---
+++ 3489_Zero_Array_Transformation_IV/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> nums = {1,2,3,2,6};
+	vector<vector<int>> queries = {{0,1,1},{0,2,1},{1,4,2},{4,4,4},{3,4,1},{4,4,5}};
+	Solution* test = new Solution();
+	cout << test->minZeroArray(nums, queries) << endl;
+	return 0;
+}
+
