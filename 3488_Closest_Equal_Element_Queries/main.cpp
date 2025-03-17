e0ca51c82f474be776a7058bf90487e47506fa43
neilhuang <neilhuang@synology.com>
2025-03-17 11:09:38 +0800

    <LeetCode202109> 3488. Closest Equal Element Queries

---
+++ 3488_Closest_Equal_Element_Queries/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> nums = {1,3,1,4,1,3,2};
+	vector<int> queries = {0,3,5};
+	Solution* test = new Solution();
+	auto result = test->solveQueries(nums, queries);
+	for (const auto& v : result) cout << v << " ";
+	cout << endl;
+	return 0;
+}
+
