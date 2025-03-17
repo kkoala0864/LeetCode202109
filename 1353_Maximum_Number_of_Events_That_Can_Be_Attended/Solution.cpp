efd6ad265e5ae98cce7c3e6f42c43e945bea1af2
neilhuang <neilhuang@synology.com>
2025-03-17 21:01:20 +0800

    <LeetCode202109> 1353. Maximum Number of Events That Can Be Attended

---
+++ 1353_Maximum_Number_of_Events_That_Can_Be_Attended/Solution.cpp
@@ -0,0 +1,34 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using std::max;
+using std::priority_queue;
+
+int Solution::maxEvents(vector<vector<int>>& events) {
+	sort(events.begin(), events.end());
+	priority_queue<int, vector<int>, std::greater<int>> pq;
+
+
+	int result = 0;
+	int ei = 0;
+	for (int d = 1 ; d <= 1e5 ; ++d) {
+		while (ei < events.size() && events[ei][0] <= d) {
+			pq.emplace(events[ei][1]);
+			++ei;
+		}
+
+		while (!pq.empty() && pq.top() < d) {
+			pq.pop();
+		}
+
+		if (!pq.empty()) {
+			pq.pop();
+			++result;
+		}
+	}
+
+	return result;
+
+}
