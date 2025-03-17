d9431905bd83f5a504d15cbb91e47e30a434a22c
neilhuang <neilhuang@synology.com>
2025-03-17 17:59:56 +0800

    <LeetCode202109> 690. Employee Importance

---
+++ 0690_Employee_Importance/Solution.h
@@ -0,0 +1,22 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Employee {
+	public:
+		int id;
+		int importance;
+		vector<int> subordinates;
+};
+
+class Solution {
+    public :
+        Solution() {}
+	int getImportance(vector<Employee*> employees, int id);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
