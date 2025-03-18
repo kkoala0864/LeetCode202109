18276e85341e8ee43d3403c40c10cd18e391c77a
neilhuang <neilhuang@synology.com>
2025-03-18 20:19:49 +0800

    <LeetCode202109> 10. Regular Expression Matching

---
+++ 0010_Regular_Expression_Matching/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <string>
+
+using std::string;
+
+class Solution {
+    public :
+        Solution() {}
+	bool isMatch(string s, string p);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
