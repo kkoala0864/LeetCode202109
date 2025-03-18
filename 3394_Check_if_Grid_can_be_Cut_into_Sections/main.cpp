57136f5097b15197cc7dd764bf266a1c0a419031
neilhuang <neilhuang@synology.com>
2025-03-18 11:02:48 +0800

    <LeetCode202109> 3394. Check if Grid can be Cut into Sections

---
+++ 3394_Check_if_Grid_can_be_Cut_into_Sections/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int n = 4;
+	vector<vector<int>> input = {{0,2,2,4},{1,0,3,2},{2,2,3,4},{3,0,4,2},{3,2,4,4}};
+	Solution* test = new Solution();
+	cout << test->checkValidCuts(n, input) << endl;
+	return 0;
+}
+
