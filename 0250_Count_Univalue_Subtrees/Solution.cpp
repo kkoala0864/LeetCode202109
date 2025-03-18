31dc4e85c8109676ca493d979099f4d7a15a6ace
neilhuang <neilhuang@synology.com>
2025-03-18 20:43:52 +0800

    <LeetCode202109> 250. Count Univalue Subtrees

---
+++ 0250_Count_Univalue_Subtrees/Solution.cpp
@@ -0,0 +1,24 @@
+#include <Solution.h>
+#include <iostream>
+
+bool dfs(TreeNode* node, int& result) {
+	if (!node) return true;
+
+	int lv = node->left ? node->left->val : node->val;
+	int rv = node->right ? node->right->val : node->val;
+
+	bool lr = dfs(node->left, result);
+	bool rr = dfs(node->right, result);
+
+	if ((lr && rr) && (lv == node->val && rv == node->val)) {
+		++result;
+		return true;
+	}
+	return false;
+}
+
+int Solution::countUnivalSubtrees(TreeNode* root) {
+	int result = 0;
+	dfs(root, result);
+	return result;
+}
