f7a6f64d0b598e45f5b56ef4aafe7d2d6f9ef0ed
neilhuang <neilhuang@synology.com>
2025-03-19 11:01:56 +0800

    <LeetCode202109> 3192. Minimum Operations to Make Binary Array Elements Equal to One II

---
+++ 3192_Minimum_Operations_to_Make_Binary_Array_Elements_Equal_to_One_II/Solution.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+int Solution::minOperations(vector<int>& nums) {
+	int cnt = 0;
+	int result = 0;
+
+	for (const auto& v : nums) {
+		if ((v + cnt) & 1) continue;
+		++cnt;
+		++result;
+	}
+	return result;
+}
