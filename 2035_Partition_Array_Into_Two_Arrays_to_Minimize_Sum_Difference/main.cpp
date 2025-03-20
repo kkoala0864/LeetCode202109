86aa374e58fab10dea26fc0ae00c104383f91372
neilhuang <neilhuang@synology.com>
2025-03-20 11:46:22 +0800

    <LeetCode202109> 2035. Partition Array Into Two Arrays to Minimize Sum Difference

---
+++ 2035_Partition_Array_Into_Two_Arrays_to_Minimize_Sum_Difference/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,9,7,3};
+	Solution* test = new Solution();
+	cout << test->minimumDifference(input) << endl;
+	return 0;
+}
+
