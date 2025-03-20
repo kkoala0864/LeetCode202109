91f45442417def980922cb9d9f480102bf40f4b3
neilhuang <neilhuang@synology.com>
2025-03-20 10:35:46 +0800

    <LeetCode202109> 3108. Minimum Cost Walk in Weighted Graph

---
+++ 3108_Minimum_Cost_Walk_in_Weighted_Graph/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
