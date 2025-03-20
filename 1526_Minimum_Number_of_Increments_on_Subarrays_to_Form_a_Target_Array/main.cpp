abb9c785ed308a9308d2de9ae6431eb72f26835a
neilhuang <neilhuang@synology.com>
2025-03-20 14:47:27 +0800

    <LeetCode202109> 1526. Minimum Number of Increments on Subarrays to Form a Target Array

---
+++ 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {3,1,5,4,2};
+	Solution* test = new Solution();
+	cout << test->minNumberOperations(input) << endl;
+	return 0;
+}
+
