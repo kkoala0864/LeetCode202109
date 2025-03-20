5b0671e9c0293d99949d2fe59dc42cd3a7ec08ca
neilhuang <neilhuang@synology.com>
2025-03-20 16:44:01 +0800

    <LeetCode202109> 1406. Stone Game III

---
+++ 1406_Stone_Game_III/main.cpp
@@ -0,0 +1,13 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {-1, -2, -3};
+	Solution* test = new Solution();
+	cout << test->stoneGameIII(input) << endl;
+	return 0;
+}
+
