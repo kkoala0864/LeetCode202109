0b501a85fcfb1e417bfdf0ed74f39546a75d3e20
neilhuang <neilhuang@synology.com>
2025-03-21 20:37:26 +0800

    <LeetCode202109> 2334. Subarray With Elements Greater Than Varying Threshold

---
+++ 2334_Subarray_With_Elements_Greater_Than_Varying_Threshold/Solution.cpp
@@ -0,0 +1,32 @@
+#include <Solution.h>
+#include <iostream>
+#include <stack>
+
+using std::stack;
+using std::cout;
+using std::endl;
+
+int Solution::validSubarraySize(vector<int>& nums, int threshold) {
+	stack<int> st;
+	int result = 0;
+
+	int size = nums.size();
+	vector<int> preSmaller(size, -1);
+	vector<int> postSmaller(size, size);
+
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		while (!st.empty() && nums[st.top()] > nums[i]) {
+			postSmaller[st.top()] = i;
+			st.pop();
+		}
+		if (!st.empty()) {
+			preSmaller[i] = st.top();
+		}
+		st.emplace(i);
+	}
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		int cnt = postSmaller[i] - preSmaller[i] - 1;
+		if (nums[i] > (threshold / cnt)) return cnt;
+	}
+	return -1;
+}
