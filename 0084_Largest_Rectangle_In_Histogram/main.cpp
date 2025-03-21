198d871e8ece043ee1973d1aad7ca8e44ff9aa1d
neilhuang <neilhuang@synology.com>
2025-03-21 19:51:40 +0800

    <LeetCode202109> 84. Largest Rectangle in Histogram

--- 0084_Largest_Rectangle_In_Histogram/main.cpp
+++ 0084_Largest_Rectangle_In_Histogram/main.cpp
@@ -8,7 +8,6 @@ int main(void) {
 	vector<int> input = {2,1,5,6,2,3};
 	Solution* test = new Solution();
 	cout << test->largestRectangleArea(input) << endl;
-	cout << test->largestRectangleArea2(input) << endl;
 	return 0;
 }
 
