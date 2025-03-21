cfe180e2031f61bf386f85020d94b14af14a158a
neilhuang <neilhuang@synology.com>
2025-03-21 20:37:26 +0800

    <LeetCode202109> 2334. Subarray With Elements Greater Than Varying Threshold

---
+++ 2334_Subarray_With_Elements_Greater_Than_Varying_Threshold/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<int> input = {1,3,4,3,1};
+	int threshold = 6;
+	Solution* test = new Solution();
+	cout << test->validSubarraySize(input, threshold) << endl;
+	return 0;
+}
+
