b7b831883b7ea603a608c0dce3ee01df35cf7da9
neilhuang <neilhuang@synology.com>
2025-03-22 11:32:38 +0800

    <LeetCode202109> 2737. Find the Closest Marked Node

---
+++ 2737_Find_the_Closest_Marked_Node/main.cpp
@@ -0,0 +1,16 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int n = 5;
+	vector<vector<int>> input = {{0,1,2},{0,2,4},{1,3,1},{2,3,3},{3,4,2}};
+	int s = 1;
+	vector<int> marked = {0,4};
+	Solution* test = new Solution();
+	cout << test->minimumDistance(n, input, s, marked) << endl;
+	return 0;
+}
+
