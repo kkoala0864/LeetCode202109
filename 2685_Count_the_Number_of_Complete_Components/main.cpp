f5e0739bf6e518f2866f6d4f51504898f992cbea
neilhuang <neilhuang@synology.com>
2025-03-22 11:10:55 +0800

    <LeetCode202109> 2685. Count the Number of Complete Components

---
+++ 2685_Count_the_Number_of_Complete_Components/main.cpp
@@ -0,0 +1,14 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<vector<int>> input = {{0,1},{0,2},{1,2},{3,4}};
+	int n = 6;
+	Solution* test = new Solution();
+	cout << test->countCompleteComponents(n, input) << endl;
+	return 0;
+}
+
