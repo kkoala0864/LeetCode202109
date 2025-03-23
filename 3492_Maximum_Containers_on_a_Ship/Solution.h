006bb7c268230e0fc4e10f642a3ab42177b49850
neilhuang <neilhuang@synology.com>
2025-03-23 14:45:54 +0800

    <LeetCode202109> 3492. Maximum Containers on a Ship

---
+++ 3492_Maximum_Containers_on_a_Ship/Solution.h
@@ -0,0 +1,12 @@
+#include <data_type.h>
+
+class Solution {
+    public :
+        Solution() {}
+	int maxContainers(int n, int w, int maxWeight);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
