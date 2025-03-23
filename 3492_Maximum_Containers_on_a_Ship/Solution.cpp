383a7afd7047ca6c53d9f4afb5dec3c8660cb65a
neilhuang <neilhuang@synology.com>
2025-03-23 14:45:54 +0800

    <LeetCode202109> 3492. Maximum Containers on a Ship

---
+++ 3492_Maximum_Containers_on_a_Ship/Solution.cpp
@@ -0,0 +1,10 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+
+int Solution::maxContainers(int n, int w, int maxWeight) {
+	int total = n * n;
+	return min(maxWeight/w, total);
+}
---
+++ 3492_Maximum_Containers_on_a_Ship/main.cpp
@@ -0,0 +1,15 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int n = 2;
+	int w = 3;
+	int maxWeight = 15;
+	Solution* test = new Solution();
+	cout << test->maxContainers(n, w, maxWeight) << endl;
+	return 0;
+}
+
