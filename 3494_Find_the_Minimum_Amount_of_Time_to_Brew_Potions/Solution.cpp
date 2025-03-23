ae4ca51a6cc702c4b1e3bb7b526f59571457271e
neilhuang <neilhuang@synology.com>
2025-03-23 15:54:29 +0800

    <LeetCode202109> 3494. Find the Minimum Amount of Time to Brew Potions

---
+++ 3494_Find_the_Minimum_Amount_of_Time_to_Brew_Potions/Solution.cpp
@@ -0,0 +1,27 @@
+#include <Solution.h>
+#include <iostream>
+#include <algorithm>
+
+using std::max;
+using std::cout;
+using std::endl;
+
+long long dfs(vector<int>& skill, int si, vector<int>& mana, int mi, vector<long long>& endTime, long long lastSkillEnd) {
+
+	if (si == skill.size()) {
+		return lastSkillEnd;
+	}
+	long long v = max(lastSkillEnd, endTime[si]);
+	endTime[si] = dfs(skill, si + 1, mana, mi, endTime, v + ((long long)skill[si] * (long long)mana[mi]));
+	return endTime[si] - ((long long)skill[si] * (long long)mana[mi]);
+}
+
+long long Solution::minTime(vector<int>& skill, vector<int>& mana) {
+	int size = skill.size();
+	vector<long long> endTime(size, 0);
+
+	for (int i = 0 ; i < mana.size() ; ++i) {
+		dfs(skill, 0, mana, i, endTime, 0);
+	}
+	return endTime.back();
+}
