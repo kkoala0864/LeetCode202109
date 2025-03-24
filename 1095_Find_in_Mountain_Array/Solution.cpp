8aac30c9bd7864b5c3d46200e350d73ad3a762c0
neilhuang <neilhuang@synology.com>
2025-03-24 20:26:55 +0800

    <LeetCode202109> 1095. Find in Mountain Array

---
+++ 1095_Find_in_Mountain_Array/Solution.cpp
@@ -0,0 +1,50 @@
+#include <Solution.h>
+#include <iostream>
+
+int findPeak(MountainArray& mountainArr) {
+	int l = 0, r = mountainArr.length() - 1;
+	int mid = 0;
+
+	while (l < r) {
+		mid = l + ((r-l) >> 1);
+		if (mountainArr.get(mid) < mountainArr.get(mid+1)) {
+			l = mid + 1;
+		} else {
+			r = mid;
+		}
+	}
+	return l;
+}
+
+int find(MountainArray& mountainArr, int target, int start, int end, bool isAscending) {
+	int l = start, r = end;
+	int mid = 0;
+
+	while (l < r) {
+		mid = l + ((r-l) >> 1);
+		if (isAscending) {
+			if (mountainArr.get(mid) < target) {
+				l = mid + 1;
+			} else {
+				r = mid;
+			}
+		} else {
+			if (mountainArr.get(mid) > target) {
+				l = mid + 1;
+			} else {
+				r = mid;
+			}
+		}
+	}
+	return mountainArr.get(l) == target ? l : -1;
+}
+
+int Solution::findInMountainArray(int target, MountainArray& mountainArr) {
+
+	int pi = findPeak(mountainArr);
+
+	int lhs = find(mountainArr, target, 0, pi, true);
+	int rhs = find(mountainArr, target, pi + 1, mountainArr.length() - 1, false);
+
+	return lhs == -1 ? rhs : lhs;
+}
---
+++ 1095_Find_in_Mountain_Array/Solution.h
@@ -0,0 +1,28 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class MountainArray {
+	public:
+		MountainArray(vector<int> &v) : arr(v) {}
+		int get(int index) {
+			return arr[index];
+		}
+		int length() {
+			return arr.size();
+		}
+	private:
+		vector<int> arr;
+};
+
+class Solution {
+    public :
+        Solution() {}
+	int findInMountainArray(int target, MountainArray& mountainArr);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
