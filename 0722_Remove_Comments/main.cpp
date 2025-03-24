82a4a3d222a0d132f34fff81357e413dbf1bef99
neilhuang <neilhuang@synology.com>
2025-03-24 19:39:29 +0800

    <LeetCode202109> 722. Remove Comments

---
+++ 0722_Remove_Comments/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	vector<string> input = {"main() {", "/* here is commments", "  // still comments */", "   double s = 33;", "   cout << s;", "}"};
+	Solution* test = new Solution();
+	auto result = test->removeComments(input);
+	for (const auto& iter : result) {
+		cout << iter << endl;
+	}
+	cout << endl;
+	return 0;
+}
+
