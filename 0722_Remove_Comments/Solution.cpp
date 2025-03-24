bc8348de772d64bd68c4cbbd1db7afd93c511558
neilhuang <neilhuang@synology.com>
2025-03-24 19:39:29 +0800

    <LeetCode202109> 722. Remove Comments

---
+++ 0722_Remove_Comments/Solution.cpp
@@ -0,0 +1,46 @@
+#include <Solution.h>
+#include <iostream>
+
+bool checkEmpty(const string& s) {
+	for (const auto& c : s) {
+		if (c == ' ') continue;
+		return false;
+	}
+	return true;
+}
+
+vector<string> Solution::removeComments(vector<string>& source) {
+	vector<string> result;
+	bool bc = false;
+
+	string curline;
+	for (const auto& s : source) {
+		for (int i = 0 ; i < s.size() ; ++i) {
+			if (!bc && s[i] == '/' && i < s.size() - 1) {
+				if (s[i+1] == '/') {
+					i = s.size() - 1;
+					continue;
+				} else if (s[i+1] == '*') {
+					bc = true;
+					++i;
+				} else {
+					if (!bc) {
+						curline.push_back(s[i]);
+					}
+				}
+			} else if (bc && s[i] == '*' && i < s.size() - 1 && s[i+1] == '/') {
+				bc = false;
+				++i;
+			} else {
+				if (!bc) {
+					curline.push_back(s[i]);
+				}
+			}
+		}
+		if (!bc && !curline.empty()) {
+			result.emplace_back(curline);
+			curline.clear();
+		}
+	}
+	return result;
+}
---
+++ 0722_Remove_Comments/Solution.h
@@ -0,0 +1,17 @@
+#include <data_type.h>
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<string> removeComments(vector<string>& source);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
