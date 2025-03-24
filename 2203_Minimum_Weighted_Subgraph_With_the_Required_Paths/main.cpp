4f70500546883af9a2325bd499b2678d6c7df723
neilhuang <neilhuang@synology.com>
2025-03-24 20:10:42 +0800

    <LeetCode202109> 2203. Minimum Weighted Subgraph With the Required Paths

---
+++ 2203_Minimum_Weighted_Subgraph_With_the_Required_Paths/main.cpp
@@ -0,0 +1,17 @@
+#include <Solution.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+	int n = 6;
+	vector<vector<int>> edges = {{0,2,2},{0,5,6},{1,0,3},{1,4,5},{2,1,1},{2,3,3},{2,3,4},{3,4,2},{4,5,1}};
+	int src1 = 0;
+	int src2 = 1;
+	int dest = 5;
+	Solution* test = new Solution();
+	cout << test->minimumWeight(n, edges, src1, src2, dest) << endl;
+	return 0;
+}
+
