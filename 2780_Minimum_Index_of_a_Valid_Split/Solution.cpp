5c98e76da309aa5a1242ccf4c9e064c6e75242aa
neilhuang <neilhuang@synology.com>
2025-03-27 14:11:51 +0800

    <LeetCode202109> 2780. Minimum Index of a Valid Split

---
+++ 2780_Minimum_Index_of_a_Valid_Split/Solution.cpp
@@ -0,0 +1,33 @@
+#include <Solution.h>
+#include <iostream>
+#include <unordered_map>
+
+using std::unordered_map;
+using std::cout;
+using std::endl;
+
+int Solution::minimumIndex(vector<int>& nums) {
+	int size = nums.size();
+	unordered_map<int, int> count;
+	int dominate = 0;
+	for (const auto& v : nums) {
+		++count[v];
+		if (count[v] > (nums.size() >> 1)) {
+			dominate = v;
+		}
+	}
+
+	int result = size;
+	int curCnt = 0;
+	for (int i = 0 ; i < size ; ++i) {
+		if (nums[i] == dominate) ++curCnt;
+		int postSize = size - i - 1;
+		int restCnt = count[dominate] - curCnt;
+		if (((curCnt << 1) > (i + 1)) && ((restCnt << 1)> postSize)) {
+			result = i;
+			break;
+		}
+	}
+
+	return result == size ? -1 : result;
+}
