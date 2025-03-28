93632556d27037954123546fae191833fa5b8862
neilhuang <neilhuang@synology.com>
2025-03-28 11:32:55 +0800

    <LeetCode202109> 2503. Maximum Number of Points From Grid Queries

---
+++ 2503_Maximum_Number_of_Points_From_Grid_Queries/Solution.h
@@ -0,0 +1,15 @@
+#include <data_type.h>
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    public :
+        Solution() {}
+	vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries);
+    private :
+        virtual ~Solution() {}
+        Solution& operator=(const Solution& source);
+        Solution(const Solution&);
+};
+
