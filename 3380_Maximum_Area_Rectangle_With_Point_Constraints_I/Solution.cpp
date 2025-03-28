03cb784a327dcc305fd149ff49a32a3d27098d41
neilhuang <neilhuang@synology.com>
2025-03-28 16:14:48 +0800

    <LeetCode202109> 3380. Maximum Area Rectangle With Point Constraints I

--- 3380_Maximum_Area_Rectangle_With_Point_Constraints_I/Solution.cpp
+++ 3380_Maximum_Area_Rectangle_With_Point_Constraints_I/Solution.cpp
@@ -5,44 +5,50 @@
 #include <algorithm>
 #include <climits>
 
-using std::max;
-using std::unordered_map;
-using std::unordered_set;
-using std::pair;
 using std::cout;
 using std::endl;
+using std::max;
+using std::pair;
+using std::unordered_map;
+using std::unordered_set;
 
-int Solution::maxRectangleArea(vector<vector<int>>& points) {
+int Solution::maxRectangleArea(vector<vector<int>> &points) {
 	unordered_map<int, unordered_set<int>> xa;
 
-	for (const auto& p : points) {
+	for (const auto &p : points) {
 		xa[p[0]].emplace(p[1]);
 	}
 
 	sort(points.begin(), points.end());
 	vector<vector<int>> candidate;
 
-	for (int i = 0 ; i < points.size() ; ++i) {
-		for (int j = i + 1 ; j < points.size() ; ++j) {
-			if (points[j][0] <= points[i][0] || points[j][1] <= points[i][1]) continue;
-			if (xa[points[j][0]].count(points[i][1]) == 0 || xa[points[i][0]].count(points[j][1]) == 0) continue;
+	for (int i = 0; i < points.size(); ++i) {
+		for (int j = i + 1; j < points.size(); ++j) {
+			if (points[j][0] <= points[i][0] || points[j][1] <= points[i][1])
+				continue;
+			if (xa[points[j][0]].count(points[i][1]) == 0 || xa[points[i][0]].count(points[j][1]) == 0)
+				continue;
 			int area = (points[j][0] - points[i][0]) * (points[j][1] - points[i][1]);
 			candidate.emplace_back(vector<int>({area, i, j}));
 		}
 	}
 	int result = -1;
-	for (const auto& c : candidate) {
+	for (const auto &c : candidate) {
 		int x1 = points[c[1]][0];
 		int y1 = points[c[1]][1];
 		int x2 = points[c[2]][0];
 		int y2 = points[c[2]][1];
 		int i = 0;
-		for (; i < points.size() ; ++i) {
-			if (i == c[1] || i == c[2]) continue;
-			if ((points[i][0] == x1 && points[i][1] == y2) || (points[i][0] == x2 && points[i][1] == y1)) continue;
-			if ((x1 <= points[i][0] && points[i][0] <= x2) && (y1 <= points[i][1] && points[i][1] <= y2)) break;
+		for (; i < points.size(); ++i) {
+			if (i == c[1] || i == c[2])
+				continue;
+			if ((points[i][0] == x1 && points[i][1] == y2) || (points[i][0] == x2 && points[i][1] == y1))
+				continue;
+			if ((x1 <= points[i][0] && points[i][0] <= x2) && (y1 <= points[i][1] && points[i][1] <= y2))
+				break;
 		}
-		if (i < points.size()) continue;
+		if (i < points.size())
+			continue;
 		result = max(result, c[0]);
 	}
 	return result;
--- 3380_Maximum_Area_Rectangle_With_Point_Constraints_I/main.cpp
+++ 3380_Maximum_Area_Rectangle_With_Point_Constraints_I/main.cpp
@@ -5,9 +5,8 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<vector<int>> input = {{96,44},{23,72},{96,72},{23,44},{65,44}};
-	Solution* test = new Solution();
+	vector<vector<int>> input = {{96, 44}, {23, 72}, {96, 72}, {23, 44}, {65, 44}};
+	Solution *test = new Solution();
 	cout << test->maxRectangleArea(input) << endl;
 	return 0;
 }
-
