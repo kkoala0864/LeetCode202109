19d4b94cde2683fb1ebee52e067b34dd5599bd8e
neilhuang <neilhuang@synology.com>
2025-03-31 21:11:40 +0800

    <LeetCode202109> update z algorithm

--- code_template/zAlgo.cpp
+++ code_template/zAlgo.cpp
@@ -1,16 +1,16 @@
 #include <Solution.h>
 #include <iostream>
 
-vector<int> getZArray(const string& s) {
-	string str = s;
+vector<int> getZArray(const string &s, const string &pattern) {
+	string str = pattern + "#" + s;
 	vector<int> z(str.size(), 0);
 
 	int l = 0, r = 0;
-	for (int i = 1 ; i < str.size() ; ++i) {
+	for (int i = 1; i < str.size(); ++i) {
 		if (i <= r) {
-			z[i] = min(r - i + 1, z[i-l]);
+			z[i] = min(r - i + 1, z[i - l]);
 		}
-		while ((i + z[i]) < str.size() && s[z[i]] == s[i+z[i]]) {
+		while ((i + z[i]) < str.size() && str[z[i]] == str[i + z[i]]) {
 			++z[i];
 		}
 		if (i + z[i] - 1 > r) {
@@ -24,8 +24,8 @@ vector<int> getZArray(const string& s) {
 vector<int> Solution::zAlgorithm(const string &text, const string &pattern) {
 	int m = text.size(), n = pattern.size();
 
-	auto result = getZArray(text);
-	for (int i =  0 ; i < result.size() ; ++i) cout << result[i] << " ";
+	auto result = getZArray(text, pattern);
+	for (const auto& v : result) cout << v << " ";
 	cout << endl;
 	return result;
 }
