c7b5ded4b9d97e71d121c7dc57526aa369239e23
neilhuang <neilhuang@synology.com>
2025-04-01 11:04:51 +0800

    <LeetCode202109> 2140. Solving Questions With Brainpower

--- 2140_Solving_Question_With_Brainpower/Solution.cpp
+++ 2140_Solving_Question_With_Brainpower/Solution.cpp
@@ -1,26 +1,15 @@
 #include <Solution.h>
 #include <iostream>
 #include <algorithm>
-#include <queue>
 
 using std::max;
-using std::pair;
-using std::priority_queue;
 
 long long Solution::mostPoints(vector<vector<int>> &questions) {
-	vector<long long> dp(questions.size(), 0);
-	priority_queue<pair<int, long long>, vector<pair<int, long long>>, std::greater<pair<int, long long>>> pq;
+	vector<long long> dp(questions.size() + 1, 0);
 
-	long long result = 0;
-	long long curMax = 0;
-	for (int i = 0; i < questions.size(); ++i) {
-		while (!pq.empty() && pq.top().first < i) {
-			curMax = max(curMax, pq.top().second);
-			pq.pop();
-		}
-		dp[i] = curMax + questions[i][0];
-		pq.emplace(pair<int, long long>(i + questions[i][1], dp[i]));
-		result = max(result, dp[i]);
+	for (int i = questions.size() - 1; i >= 0; --i) {
+		int nextIdx = i + questions[i][1] + 1;
+		dp[i] = max(questions[i][0] + (nextIdx >= questions.size() ? 0 : dp[nextIdx]), dp[i + 1]);
 	}
-	return result;
+	return dp[0];
 }
--- 2140_Solving_Question_With_Brainpower/main.cpp
+++ 2140_Solving_Question_With_Brainpower/main.cpp
@@ -5,7 +5,7 @@ using std::cout;
 using std::endl;
 
 int main(void) {
-	vector<vector<int>> input = {{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}};
+	vector<vector<int>> input = {{21,5},{92,3},{74,2},{39,4},{58,2},{5,5},{49,4},{65,3}};
 	Solution *test = new Solution();
 	cout << test->mostPoints(input) << endl;
 	return 0;
