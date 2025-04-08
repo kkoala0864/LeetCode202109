74e815c8554267d3a2a407bdb2dc5afdd54731e7
neilhuang <neilhuang@synology.com>
2025-04-08 11:03:58 +0800

    <LeetCode202109> 1781. Sum of Beauty of All Substrings

---
+++ 1781_Sum_of_Beauty_of_All_Substrings/Solution.h
@@ -0,0 +1,17 @@
+#include <data_type.h>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+	Solution() {
+	}
+	int beautySum(string s);
+
+private:
+	virtual ~Solution() {
+	}
+	Solution &operator=(const Solution &source);
+	Solution(const Solution &);
+};
