a3dea0c102287a84d1b7ce358b31b42f88726f94
neilhuang <neilhuang@synology.com>
2025-04-08 16:46:22 +0800

    <LeetCode202109> 778. Swim in Rising Water

---
+++ 0778_Swim_in_Rising_Water/Solution.h
@@ -0,0 +1,16 @@
+#include <data_type.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	Solution() {
+	}
+	int swimInWater(vector<vector<int>>& grid);
+private:
+	virtual ~Solution() {
+	}
+	Solution &operator=(const Solution &source);
+	Solution(const Solution &);
+};
