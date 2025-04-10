1a0a9167a8dce00626e1819ab6d65c742a2ae6cc
neilhuang <neilhuang@synology.com>
2025-04-10 15:44:02 +0800

    <LeetCode202109> 1109. Corporate Flight Bookings

--- 1109_Corporate_Flight_Bookings/Solution.cpp
+++ 1109_Corporate_Flight_Bookings/Solution.cpp
@@ -6,21 +6,17 @@
 using std::map;
 
 vector<int> Solution::corpFlightBookings(vector<vector<int>>& bookings, int n) {
-	map<int, int> m;
+	vector<int> m(n + 1, 0);
+
 	for (const auto& b : bookings) {
-		m[b[0]] += b[2];
-		m[b[1] + 1] -= b[2];
+		m[b[0]-1] += b[2];
+		m[b[1]] -= b[2];
 	}
 
 	vector<int> result(n, 0);
-
 	int cur = 0;
-	auto iter = m.begin();
 	for (int i = 0 ; i < n ; ++i) {
-		while (iter != m.end() && iter->first <= (i+1)) {
-			cur += iter->second;
-			++iter;
-		}
+		cur += m[i];
 		result[i] = cur;
 	}
 	return result;
