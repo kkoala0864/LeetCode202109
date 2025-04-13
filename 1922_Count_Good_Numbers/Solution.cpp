a5016dfb293da08b058dd6e2254fe48c0304813b
neilhuang <neilhuang@synology.com>
2025-04-13 14:13:59 +0800

    <LeetCode202109> 1922. Count Good Numbers

--- 1922_Count_Good_Numbers/Solution.cpp
+++ 1922_Count_Good_Numbers/Solution.cpp
@@ -1,25 +1,23 @@
 #include <Solution.h>
 #include <iostream>
 
-// 9 : 0 ~ 8 : 1 3 5 7 : 0 2 4 6 8
-// 8 : 0 ~ 7 : 1 3 5 7 : 0 2 4 6
-// 7 : 0 ~ 6 : 1 3 5 : 0 2 4 6
-// 4 : 0 ~ 3 : 1 3 : 0 2
+// 0 1 2 3 4 => even: 3 odd :2
+// 0 1 2 3 => even : 2 odd : 2
 
 long long quickPow(long long x, long long n, long long mod) {
-	if (n == 0)
-		return 1;
+	if (n == 0) return 1;
 	long long v = quickPow(x, n / 2, mod);
-	return n & 1 ? v * v % mod * x % mod : v * v % mod;
+	long long vv = v * v % mod;
+	return n & 1 ? vv * x % mod : vv;
 }
 
 int Solution::countGoodNumbers(long long n) {
-	if (n == 1)
-		return 5;
+	long long evenCnt = (n >> 1) + (n & 1);
+	long long oddCnt = (n >> 1);
 
 	long long mod = 1e9 + 7;
-	long long cnt4 = n / 2;
-	long long cnt5 = (n - 1) / 2 + 1;
-
-	return quickPow(4, cnt4, mod) * quickPow(5, cnt5, mod) % mod;
+	long long result = quickPow(5, evenCnt, mod);
+	result *= quickPow(4, oddCnt, mod);
+	result %= mod;
+	return result;
 }
