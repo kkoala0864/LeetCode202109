83ef272791441d7600f942400b8ddbcd2f8cec11
neilhuang <neilhuang@synology.com>
2025-04-18 10:18:57 +0800

    <LeetCode202109> 38. Count and Say

--- 0038_Count_And_Say/Solution.cpp
+++ 0038_Count_And_Say/Solution.cpp
@@ -1,28 +1,22 @@
 #include <Solution.h>
 #include <iostream>
 
-using std::to_string;
-
 string Solution::countAndSay(int n) {
-	string result("1");
-	for (int i = 2; i <= n; ++i) {
-		string local("");
-		char c = result[0];
+	if (n == 1) return "1";
+	string result(1, '1');
+	for (int i = 2 ; i <= n ; ++i) {
+		string tmp;
 		int cnt = 1;
-		for (int j = 1; j < result.size(); ++j) {
-			if (c != result[j]) {
-				local += to_string(cnt);
-				local.push_back(c);
-				c = result[j];
-				cnt = 1;
-			} else {
+		for (int j = 1 ; j < result.size() ; ++j) {
+			if (result[j] == result[j-1]) {
 				++cnt;
+			} else {
+				tmp += to_string(cnt) + string(1,result[j-1]);
+				cnt = 1;
 			}
 		}
-
-		local += to_string(cnt);
-		local.push_back(c);
-		result = local;
+		tmp += to_string(cnt) + string(1, result.back());
+		result = move(tmp);
 	}
 	return result;
 }
--- 0038_Count_And_Say/main.cpp
+++ 0038_Count_And_Say/main.cpp
@@ -6,9 +6,7 @@ using std::endl;
 
 int main(void) {
 	Solution *test = new Solution();
-	int input = 30;
+	int input = 4;
 	cout << test->countAndSay(input) << endl;
-	cout << endl;
-	cout << test->countAndSay2(input) << endl;
 	return 0;
 }
