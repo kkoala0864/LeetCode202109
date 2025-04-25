6c5e3fe288617f2d2059d36b894918fa8d344d28
neilhuang <neilhuang@synology.com>
2025-04-25 10:44:25 +0800

    <LeetCode202109> update create_tmp.sh

--- create_tmp.sh
+++ create_tmp.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	echo "請提供一個輸入字串"
+	echo "please enter a string"
 	exit 1
 fi
 
@@ -11,7 +11,7 @@ input_str="$*"
 number=$(echo "$input_str" | cut -d '.' -f 1)
 
 if ! [[ "$number" =~ ^[0-9]+$ ]]; then
-	echo "錯誤：輸入的第一部分應該是數字"
+	echo "error : first part of the string must be a number"
 	exit 1
 fi
 
@@ -23,7 +23,13 @@ formatted_string=$(echo "$string_part" | tr ' ' '_')
 new_filename="${formatted_number}_${formatted_string}"
 echo "$new_filename"
 
-cp -r "0000_Template" "$new_filename"
+
+if [ -d "$new_filename" ]; then
+	echo "Directory $new_filename already exists"
+else
+	echo "Creating directory $new_filename"
+	cp -r "0000_Template" "$new_filename"
+fi
 
 cd "$new_filename" || exit
 pwd
