01956c5a966640c86447ab118859ddda204597b8
neilhuang <neilhuang@synology.com>
2025-05-02 11:18:11 +0800

    <Leetcode202109> 353. Design Snake Game

--- 0353_Design_Snake_Game/Solution.h
+++ 0353_Design_Snake_Game/Solution.h
@@ -1,45 +1,50 @@
 #include <vector>
 #include <string>
+#include <iostream>
 #include <deque>
+#include <unordered_map>
+#include <unordered_set>
 
-using std::deque;
-using std::string;
-using std::vector;
+using namespace std;
 
 class SnakeGame {
 public:
 	SnakeGame(int width, int height, vector<vector<int>> &food)
 	    : _width(width),
-	      _height(height), _food(food), _bodySize(1), _score(0), _foodIdx(0) {
-		vector<int> init = {0, 0};
-		_body.emplace_back(init);
+	      _height(height), _food(food), _bodySize(1), _score(0), _foodIdx(0), _bodySet(), _body(1, {0, 0}) {
 	}
 	int move(string direction) {
-		vector<int> nextAxis = _body.back();
+		pair<int, int> nextAxis = _body.back();
 		if (direction == "U")
-			nextAxis[0] -= 1;
+			nextAxis.first -= 1;
 		else if (direction == "D")
-			nextAxis[0] += 1;
+			nextAxis.first += 1;
 		else if (direction == "L")
-			nextAxis[1] -= 1;
+			nextAxis.second -= 1;
 		else if (direction == "R")
-			nextAxis[1] += 1;
+			nextAxis.second += 1;
 
-		if (nextAxis[0] < 0 || nextAxis[0] >= _height || nextAxis[1] < 0 || nextAxis[1] >= _width)
+		if (nextAxis.first < 0 || nextAxis.first >= _height || nextAxis.second < 0 || nextAxis.second >= _width) {
 			return -1;
-		for (int i = 1; i < _body.size(); ++i)
-			if ((nextAxis[0] == _body[i][0]) && (nextAxis[1] == _body[i][1]))
-				return -1;
+		}
 
-		_body.emplace_back(nextAxis);
-		if (_foodIdx < _food.size() && (_food[_foodIdx][0] == nextAxis[0]) && (_food[_foodIdx][1] == nextAxis[1])) {
+		if (_foodIdx < _food.size() && (_food[_foodIdx][0] == nextAxis.first) && (_food[_foodIdx][1] == nextAxis.second)) {
 			++_foodIdx;
 			++_bodySize;
 			++_score;
 		}
 
-		if (_body.size() > _bodySize)
+		_body.emplace_back(nextAxis);
+		while (_body.size() > _bodySize) {
+			_bodySet[_body.front().first].erase(_body.front().second);
 			_body.pop_front();
+		}
+
+		if (_bodySet.count(nextAxis.first) && _bodySet[nextAxis.first].count(nextAxis.second)) {
+			return -1;
+		}
+
+		_bodySet[nextAxis.first].emplace(nextAxis.second);
 
 		return _score;
 	}
@@ -47,11 +52,12 @@ public:
 private:
 	int _width;
 	int _height;
-	deque<vector<int>> _body;
+	deque<pair<int, int>> _body;
 	int _bodySize;
 	int _score;
 	vector<vector<int>> _food;
 	int _foodIdx;
+	unordered_map<int, unordered_set<int>> _bodySet;
 	virtual ~SnakeGame() {
 	}
 	SnakeGame &operator=(const SnakeGame &source);
--- 0353_Design_Snake_Game/main.cpp
+++ 0353_Design_Snake_Game/main.cpp
@@ -1,9 +1,25 @@
-#include <Solution2.h>
+#include <Solution.h>
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
 int main(void) {
+	// ["SnakeGame","move","move","move","move","move","move","move","move","move","move","move","move"]
+	// [[3,3,[[2,0],[0,0],[0,2],[2,2]]],["D"],["D"],["R"],["U"],["U"],["L"],["D"],["R"],["R"],["U"],["L"],["D"]]
+	vector<vector<int>> food = {{2, 0}, {0, 0}, {0, 2}, {2, 2}};
+	SnakeGame * test = new SnakeGame(3, 3, food);
+	cout << test->move("D") << endl;
+	cout << test->move("D") << endl;
+	cout << test->move("R") << endl;
+	cout << test->move("U") << endl;
+	cout << test->move("U") << endl;
+	cout << test->move("L") << endl;
+	cout << test->move("D") << endl;
+	cout << test->move("R") << endl;
+	cout << test->move("R") << endl;
+	cout << test->move("U") << endl;
+	cout << test->move("L") << endl;
+	cout << test->move("D") << endl;
 	return 0;
 }
