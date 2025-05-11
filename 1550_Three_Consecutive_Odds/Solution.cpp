ec5505842a06d4c1fb97efd37d952de8d8dcb754
neilhuang <neilhuang@synology.com>
2025-05-11 15:44:19 +0800

    <LeetCode202109> 1550. Three Consecutive Odds

--- 1550_Three_Consecutive_Odds/Solution.cpp
+++ 1550_Three_Consecutive_Odds/Solution.cpp
@@ -1,16 +1,15 @@
 #include <Solution.h>
-#include <iostream>
 
-bool Solution::threeConsecutiveOdds(vector<int> &arr) {
+bool Solution::threeConsecutiveOdds(vector<int>& arr) {
 	int cnt = 0;
 
-	for (const auto &v : arr) {
-		if (v & 1)
-			++cnt;
-		else
+	for (const auto& v : arr) {
+		if ((v & 1) == 0) {
 			cnt = 0;
-		if (cnt == 3)
-			return true;
+		} else {
+			++cnt;
+		}
+		if (cnt == 3) return true;
 	}
 	return false;
 }
