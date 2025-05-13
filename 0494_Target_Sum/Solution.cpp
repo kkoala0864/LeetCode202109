ab5cf963142b5438e4f79082c1f6fb9c5e214d40
neilhuang <neilhuang@synology.com>
2025-05-13 19:48:23 +0800

    <LeetCode202109> 494. Target Sum

--- 0494_Target_Sum/Solution.cpp
+++ 0494_Target_Sum/Solution.cpp
@@ -1,29 +1,16 @@
 #include <Solution.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 int Solution::findTargetSumWays(vector<int> &nums, int target) {
-	int sum = 0;
-	for (const auto &v : nums)
-		sum += v;
-	vector<int> last(2 * sum + 1, 0), cur(2 * sum + 1, 0);
+	unordered_map<int, int> dp, next;
 
-	int offset = sum;
+	dp[0] = 1;
 
-	last[sum] = 1;
-
-	for (const auto &v : nums) {
-		for (int i = 0; i < last.size(); ++i) {
-			if (last[i] > 0) {
-				cur[i + v] += last[i];
-				cur[i - v] += last[i];
-			}
+	for (const auto& v : nums) {
+		for (const auto& e : dp) {
+			next[e.first + v] += e.second;
+			next[e.first - v] += e.second;
 		}
-		last = cur;
-		fill(cur.begin(), cur.end(), 0);
+		dp = move(next);
 	}
-
-	return (target + offset) >= last.size() ? 0 : last[target + offset];
+	return dp[target];
 }
