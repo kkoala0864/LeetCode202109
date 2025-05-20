820995e2d8064c0e5f70f1b8312b84808cf2cddf
neilhuang <neilhuang@synology.com>
2025-05-20 09:51:10 +0800

    <LeetCode202109> 3355. Zero Array Transformation I

--- 3355_Zero_Array_Transformation_I/Solution.cpp
+++ 3355_Zero_Array_Transformation_I/Solution.cpp
@@ -1,20 +1,17 @@
 #include <Solution.h>
-#include <iostream>
 
 bool Solution::isZeroArray(vector<int> &nums, vector<vector<int>> &queries) {
 	vector<int> m(nums.size() + 1, 0);
-
-	for (const auto &q : queries) {
+	for (const auto& q : queries) {
 		++m[q[0]];
-		--m[q[1] + 1];
+		--m[q[1]+1];
 	}
 
-	int cur = 0;
+	int offset = 0;
 
-	for (int i = 0; i < nums.size(); ++i) {
-		cur += m[i];
-		if (cur < nums[i])
-			return false;
+	for (int i = 0 ; i < nums.size() ; ++i) {
+		offset += m[i];
+		if (offset < nums[i]) return false;
 	}
 	return true;
 }
