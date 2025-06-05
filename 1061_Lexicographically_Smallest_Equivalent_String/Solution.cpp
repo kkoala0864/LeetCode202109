5d799fb23ceb3f4e5496fdd8346256839370cec0
neilhuang <neilhuang@synology.com>
2025-06-05 10:14:32 +0800

    <LeetCode202109> 1061. Lexicographically Smallest Equivalent String

--- 1061_Lexicographically_Smallest_Equivalent_String/Solution.cpp
+++ 1061_Lexicographically_Smallest_Equivalent_String/Solution.cpp
@@ -1,31 +1,26 @@
 #include <Solution.h>
-#include <iostream>
-#include <vector>
-#include <algorithm>
 
-using std::min;
-using std::vector;
-int find(vector<int> &root, int idx) {
-	if (root[idx] == idx)
-		return idx;
+int find(vector<int>& root, int idx) {
+	if (root[idx] == idx) return idx;
 	return root[idx] = find(root, root[idx]);
 }
 
 string Solution::smallestEquivalentString(string s1, string s2, string baseStr) {
 	vector<int> root;
-	for (int i = 0; i < 26; ++i)
+	for (int i = 0 ; i < 26 ; ++i) {
 		root.emplace_back(i);
+	}
 
-	for (int i = 0; i < s1.size(); ++i) {
+	for (int i = 0 ; i < s1.size() ; ++i) {
 		int lr = find(root, s1[i] - 'a');
 		int rr = find(root, s2[i] - 'a');
-		if (lr == rr)
-			continue;
+		if (lr == rr) continue;
 		root[lr] = root[rr] = min(lr, rr);
 	}
+
 	string result;
-	for (const auto &c : baseStr) {
-		result.push_back(find(root, c - 'a') + 'a');
+	for (int i = 0 ; i < baseStr.size() ; ++i) {
+		result.push_back(find(root, baseStr[i] - 'a') + 'a');
 	}
 	return result;
 }
