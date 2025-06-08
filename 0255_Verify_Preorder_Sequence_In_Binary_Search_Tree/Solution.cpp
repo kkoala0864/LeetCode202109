64a8b3775fe9f8a11293b4be1ec7d70cd6afecc6
neilhuang <neilhuang@synology.com>
2025-06-08 10:13:50 +0800

    <LeetCode202109> 255. Verify Preorder Sequence in Binary Search Tree

--- 0255_Verify_Preorder_Sequence_In_Binary_Search_Tree/Solution.cpp
+++ 0255_Verify_Preorder_Sequence_In_Binary_Search_Tree/Solution.cpp
@@ -1,24 +1,17 @@
 #include <Solution.h>
 
-bool dfs(vector<int>& preorder, int rv, int ps, int pe, int lower, int upper) {
-	if (ps > pe) return true;
-	if (ps == pe) return preorder[ps] > lower && preorder[ps] < upper;
+bool Solution::verifyPreorder(vector<int> &preorder) {
+	stack<int> st;
 
-	int rs = ps;
-	for (; rs <= pe ; ++rs) {
-		if (preorder[rs] < lower || preorder[rs] > upper) return false;
-		if (preorder[rs] < rv) continue;
-		break;
-	}
-	if (rs > ps) {
-		if (!dfs(preorder, preorder[ps], ps + 1, rs - 1, lower, rv)) return false;
-	}
-	if (rs <= pe) {
-		if (!dfs(preorder, preorder[rs], rs + 1, pe, rv, upper)) return false;
+	int curMin = INT_MIN;
+
+	for (const auto& v : preorder) {
+		if (v < curMin) return false;
+		while (!st.empty() && st.top() < v) {
+			curMin = st.top();
+			st.pop();
+		}
+		st.emplace(v);
 	}
 	return true;
-}
-
-bool Solution::verifyPreorder(vector<int> &preorder) {
-	return dfs(preorder, preorder[0], 1, preorder.size() - 1, 0, INT_MAX);
 }
