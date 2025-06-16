ee6d770159aaf55f9342ad5f931dc5abe46b1344
neilhuang <neilhuang@synology.com>
2025-06-16 13:55:09 +0800

    <LeetCode202109> 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance

--- 1334_Find_The_City_With_The_Smallest_Number_Of_Neighbors_At_A_Threshold_Distance/Solution.cpp
+++ 1334_Find_The_City_With_The_Smallest_Number_Of_Neighbors_At_A_Threshold_Distance/Solution.cpp
@@ -1,62 +1,40 @@
 #include <Solution.h>
-#include <iostream>
-#include <algorithm>
-#include <queue>
-
-using std::max;
-using std::pair;
-using std::priority_queue;
-
-void dijkstra(int n, int start, vector<vector<pair<int, int>>> &od, vector<vector<int>> &minCost) {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, std::greater<pair<int, int>>> pq;
-
-	pq.emplace(pair<int, int>(0, start));
-
-	while (!pq.empty()) {
-		int curCost = pq.top().first;
-		int curNode = pq.top().second;
-		pq.pop();
-
-		if (curCost > minCost[start][curNode])
-			continue;
-
-		for (const auto &next : od[curNode]) {
-			if (minCost[start][next.first] <= (curCost + next.second))
-				continue;
-			minCost[start][next.first] = curCost + next.second;
-			pq.emplace(pair<int, int>(minCost[start][next.first], next.first));
-		}
-	}
-}
 
 int Solution::findTheCity(int n, vector<vector<int>> &edges, int distanceThreshold) {
-	vector<vector<int>> minCost(n, vector<int>(n, distanceThreshold + 1));
-	vector<vector<pair<int, int>>> od(n, vector<pair<int, int>>());
+	vector<vector<int>> minCost(n, vector<int>(n, INT_MAX/2));
 
-	for (const auto &e : edges) {
-		od[e[0]].emplace_back(pair<int, int>(e[1], e[2]));
-		od[e[1]].emplace_back(pair<int, int>(e[0], e[2]));
+	for (const auto& e : edges) {
+		minCost[e[0]][e[1]] = e[2];
+		minCost[e[1]][e[0]] = e[2];
 	}
+	for (int i = 0 ; i < n ; ++i) minCost[i][i] = 0;
 
-	for (int i = 0; i < n; ++i) {
-		minCost[i][i] = 0;
-		dijkstra(n, i, od, minCost);
-	}
-
-	int minReach = n;
-	int result = -1;
-	for (int i = 0; i < n; ++i) {
-		int cnt = 0;
-		for (int j = 0; j < n; ++j) {
-			if (i == j || minCost[i][j] == (distanceThreshold + 1))
-				continue;
-			++cnt;
+	vector<int> cnt(n, 0);
+	int minCnt = INT_MAX;
+	int result = 0;
+	for (int k = 0 ; k < n ; ++k) {
+		for (int i = 0 ; i < n ; ++i) {
+			for (int j = 0 ; j < n ; ++j) {
+				if (i == j) continue;
+				if (minCost[i][j] > (minCost[i][k] + minCost[k][j])) {
+					minCost[i][j] = minCost[i][k] + minCost[k][j];
+				}
+			}
 		}
-		if (minReach > cnt) {
-			minReach = cnt;
+	}
+	for(int i = 0 ; i < n ; ++i) {
+		for (int j = 0 ; j < n ; ++j) {
+			if (minCost[i][j] <= distanceThreshold) {
+				++cnt[i];
+			}
+		}
+	}
+	for (int i = 0 ; i < n ; ++i) {
+		if (minCnt == cnt[i]) {
 			result = i;
-		} else if (minReach == cnt) {
-			result = max(result, i);
+		} else if (minCnt > cnt[i]) {
+			minCnt = cnt[i];
+			result = i;
 		}
 	}
 	return result;
