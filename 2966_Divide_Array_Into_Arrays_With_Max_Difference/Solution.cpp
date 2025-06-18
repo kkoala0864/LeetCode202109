b53ac810c76584f49091c5d0295043dd17477af3
neilhuang <neilhuang@synology.com>
2025-06-18 09:57:24 +0800

    <LeetCode202109> 2966. Divide Array Into Arrays With Max Difference

--- 2966_Divide_Array_Into_Arrays_With_Max_Difference/Solution.cpp
+++ 2966_Divide_Array_Into_Arrays_With_Max_Difference/Solution.cpp
@@ -1,25 +1,19 @@
 #include <Solution.h>
-#include <iostream>
-#include <algorithm>
 
-using std::cout;
-using std::endl;
-using std::sort;
-
+// the value is vector is in order or out of order
+// what's the length range of the vector
+// we can sort the vector and get each k value in order to form the vector
+// every time we get eack k value we need to check the first value and kth value differen is small or equal than k
 vector<vector<int>> Solution::divideArray(vector<int> &nums, int k) {
-	sort(nums.begin(), nums.end());
-
 	vector<vector<int>> result;
-	for (int i = 0; i < nums.size(); ++i) {
-		int mod = i % 3;
-		if (mod == 0) {
-			result.emplace_back(vector<int>());
-			result.back().emplace_back(nums[i]);
-		} else if (mod == 1 && ((nums[i] - nums[i - 1]) <= k) || mod == 2 && ((nums[i] - nums[i - 2]) <= k)) {
-			result.back().emplace_back(nums[i]);
-		} else {
-			return vector<vector<int>>();
-		}
+	if (nums.size() % 3) return result;
+
+	sort(nums.begin(), nums.end());
+	for (int i = 0 ; i < nums.size() ; i += 3) {
+		if ((nums[i + 2] - nums[i]) > k) return {};
+		result.push_back({nums[i], nums[i+1], nums[i+2]});
 	}
 	return result;
 }
+// TC : O(nlogn)
+// SC : O(n);
--- 2966_Divide_Array_Into_Arrays_With_Max_Difference/main.cpp
+++ 2966_Divide_Array_Into_Arrays_With_Max_Difference/main.cpp
@@ -1,8 +1,4 @@
 #include <Solution.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 int main(void) {
 	vector<int> input = {1, 3, 4, 8, 7, 9, 3, 5, 1};
