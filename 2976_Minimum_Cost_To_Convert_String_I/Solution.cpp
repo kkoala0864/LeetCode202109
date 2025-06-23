bb13dc7ab2ecf2462ff3e14dc86d39b1bfa354d7
neilhuang <neilhuang@synology.com>
2025-06-23 10:13:47 +0800

    <LeetCode202109> 2976. Minimum Cost to Convert String I

--- 2976_Minimum_Cost_To_Convert_String_I/Solution.cpp
+++ 2976_Minimum_Cost_To_Convert_String_I/Solution.cpp
@@ -1,51 +1,27 @@
 #include <Solution.h>
-#include <iostream>
-#include <queue>
-
-using std::pair;
-using std::priority_queue;
-
-void dijkstra(vector<vector<pair<int, int>>> &od, vector<int> &minCost, int start) {
-	priority_queue<pair<int, int>, vector<pair<int, int>>, std::greater<pair<int, int>>> pq;
-	minCost[start] = 0;
-	pq.emplace(pair<int, int>(0, start));
-
-	while (!pq.empty()) {
-		int cost = pq.top().first;
-		int node = pq.top().second;
-		pq.pop();
-
-		if (cost > minCost[node])
-			continue;
-
-		for (const auto &next : od[node]) {
-			if (minCost[next.first] <= minCost[node] + next.second)
-				continue;
-			minCost[next.first] = minCost[node] + next.second;
-			pq.emplace(pair<int, int>(minCost[next.first], next.first));
-		}
-	}
-}
 
 long long Solution::minimumCost(string source, string target, vector<char> &original, vector<char> &changed, vector<int> &cost) {
-	vector<vector<pair<int, int>>> od(26, vector<pair<int, int>>());
+	vector<vector<long long>> minCost(26, vector<long long>(26, LLONG_MAX/2));
 
-	for (int i = 0; i < original.size(); ++i) {
-		od[original[i] - 'a'].emplace_back(pair<int, int>(changed[i] - 'a', cost[i]));
+	for (int i = 0 ; i < 26 ; ++i) minCost[i][i] = 0;
+	for (int i = 0 ; i < original.size() ; ++i) {
+		int oi = original[i] - 'a';
+		int ci = changed[i] - 'a';
+		minCost[oi][ci] = min(minCost[oi][ci], (long long)cost[i]);
 	}
-	vector<vector<int>> minCost(26, vector<int>(26, INT_MAX));
 
+	for (int k = 0 ; k < 26 ; ++k) {
+		for (int i = 0 ; i < 26 ; ++i) {
+			for (int j = 0 ; j < 26 ; ++j) {
+				minCost[i][j] = min(minCost[i][j], minCost[i][k] + minCost[k][j]);
+			}
+		}
+	}
 	long long result = 0;
-	for (int i = 0; i < source.size(); ++i) {
-		if (source[i] == target[i])
-			continue;
+	for (int i = 0 ; i < source.size() ; ++i) {
 		int si = source[i] - 'a';
 		int ti = target[i] - 'a';
-		if (minCost[si][si] == INT_MAX) {
-			dijkstra(od, minCost[si], si);
-		}
-		if (minCost[si][ti] == INT_MAX)
-			return -1;
+		if (minCost[si][ti] == LLONG_MAX/2) return -1;
 		result += minCost[si][ti];
 	}
 	return result;
