87e0642f0e8e9244ccf84c843d36ce0e40e142f5
neilhuang <neilhuang@synology.com>
2025-06-25 11:06:46 +0800

    <LeetCode202109> 2040. Kth Smallest Product of Two Sorted Arrays

--- 2040_Kth_Smallest_Product_of_Two_Sorted_Arrays/Solution.cpp
+++ 2040_Kth_Smallest_Product_of_Two_Sorted_Arrays/Solution.cpp
@@ -1,23 +1,21 @@
 #include <Solution.h>
-#include <iostream>
 #include <cmath>
-
-// n1 * n2 <= golden
-// if n1 > 0 : n2 <= golden/n1;
-// if n1 == 0 : golden > 0 ? n2.size() : 0;
-// if n1 < 0 : n2 >= golden/n1;
 
 long long getCnt(vector<int>& n1, vector<int>& n2, long long golden) {
 	long long cnt = 0;
 	for (const auto& v : n1) {
-		if (v > 0) {
-			long long target = floor(golden*1.0 / v);
+		if (v == 0) {
+			// if golden < 0 then v * n2 == 0 should not be included
+			cnt += (golden >= 0) ? (long long)n2.size() : 0;
+		} else if (v > 0) {
+			long long target = floor(golden * 1.0/(long long) v);
 			long long idx = upper_bound(n2.begin(), n2.end(), target) - n2.begin();
 			cnt += idx;
-		} else if (v == 0) {
-			cnt += (golden >= 0) ? (long long)n2.size() : 0;
 		} else {
-			long long target = ceil(golden*1.0 / v);
+			// 100 / -10 => -10, -9, -8...
+			// -100 / -10 => 10, 11, 12...
+			// so is v is negative, we need to include curIdx to the end
+			long long target = ceil(golden * 1.0/ (long long) v);
 			long long idx = lower_bound(n2.begin(), n2.end(), target) - n2.begin();
 			cnt += ((long long)n2.size() - idx);
 		}
@@ -26,26 +24,26 @@ long long getCnt(vector<int>& n1, vector<int>& n2, long long golden) {
 }
 
 long long Solution::kthSmallestProduct(vector<int>& nums1, vector<int>& nums2, long long k) {
-	long long l = min({(long long)nums1[0] * (long long)nums2[0],
-			     (long long)nums1[0] * (long long)nums2.back(),
-			     (long long)nums1.back() * (long long)nums2[0],
-			     (long long)nums1.back() * (long long)nums2.back()});
-
-
-	long long r = max({(long long)nums1[0] * (long long)nums2[0],
-			     (long long)nums1[0] * (long long)nums2.back(),
-			     (long long)nums1.back() * (long long)nums2[0],
-			     (long long)nums1.back() * (long long)nums2.back()});
-
+	vector<long long> n1 = {nums1[0], nums1.back()};
+	vector<long long> n2 = {nums2[0], nums2.back()};
+	long long l = LLONG_MAX;
+	long long r = LLONG_MIN;
+	for (const auto& v1 : n1) {
+		for (const auto& v2 : n2) {
+			l = min(l, v1 * v2);
+			r = max(r, v1 * v2);
+		}
+	}
 
 	long long mid = 0;
+
 	while (l < r) {
-		mid = l + ((r - l) >> 1);
+		mid = l + ((r-l) >> 1);
 		long long cnt = getCnt(nums1, nums2, mid);
-		if (cnt < k) {
-			l = mid + 1;
-		} else {
+		if (cnt >= k) {
 			r = mid;
+		} else {
+			l = mid + 1;
 		}
 	}
 	return l;
--- 2040_Kth_Smallest_Product_of_Two_Sorted_Arrays/main.cpp
+++ 2040_Kth_Smallest_Product_of_Two_Sorted_Arrays/main.cpp
@@ -1,13 +1,9 @@
 #include <Solution.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 int main(void) {
-	vector<int> n1 = {-2,-1,0,1,2};
-	vector<int> n2 = {-3,-1,2,4,5};
-	int k = 3;
+	vector<int> n1 = {-9,-4,1,6,8,8,9,10};
+	vector<int> n2 = {-10,-10,-8,-8,1,3,6,6,8,10};
+	int k = 29;
 	Solution *test = new Solution();
 	cout << test->kthSmallestProduct(n1, n2, k) << endl;
 	return 0;
