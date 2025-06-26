cc2342b73aa580dfb50476a3113ee74c03adc6a6
neilhuang <neilhuang@synology.com>
2025-06-26 12:02:58 +0800

    <LeetCode202109> add range add and lazy propogate segment tree

--- code_template/segment_tree/Solution.h
+++ code_template/segment_tree/Solution.h
@@ -1,6 +1,4 @@
-#include <vector>
-
-using namespace std;
+#include <data_type.h>
 
 class SegTree {
 public:
@@ -30,4 +28,49 @@ public:
 	}
 private:
 	vector<int> tree;
+};
+
+class RangeTree {
+	public :
+		RangeTree(int size) {
+			tree = vector<pair<int, int>>(4 * size + 4, pair<int, int>());
+		}
+
+		void apply(int ti, int tl, int tr, int v) {
+			tree[ti].first += (v * (tr - tl + 1));
+			tree[ti].second += v;
+		}
+
+		void push(int ti, int tl, int tr) {
+			if (tree[ti].second == 0 || tl == tr) return;
+			int m = tl + ((tr - tl) >> 1);
+			apply(ti << 1, tl, m, tree[ti].second);
+			apply((ti << 1) + 1, m + 1, tr, tree[ti].second);
+			tree[ti].second = 0;
+		}
+
+		void rangeAdd(int vl, int vr, int v, int ti, int tl, int tr) {
+			if ((vr < tl) || (tr < vl)) return;
+			if (vl <= tl && tr <= vr) {
+				apply(ti, tl, tr, v);
+				return;
+			}
+			push(ti, tl, tr);
+			int mid = tl + ((tr - tl) >> 1);
+			rangeAdd(vl, vr, v, ti << 1, tl, mid);
+			rangeAdd(vl, vr, v, (ti << 1) + 1, mid + 1, tr);
+			tree[ti].first = tree[ti << 1].first + tree[(ti << 1) + 1].first;
+		}
+
+		int rangeSum(int vl, int vr, int ti, int tl, int tr) {
+			if ((vr < tl) || (tr < vl)) return 0;
+			if (vl <= tl && tr <= vr) {
+				return tree[ti].first;
+			}
+			push(ti, tl, tr);
+			int mid = tl + ((tr - tl) >> 1);
+			return rangeSum(vl, vr, ti << 1, tl, mid) + rangeSum(vl, vr, (ti << 1) + 1, mid + 1, tr);
+		}
+	private:
+		vector<pair<int, int>> tree;
 };
--- code_template/segment_tree/main.cpp
+++ code_template/segment_tree/main.cpp
@@ -1,8 +1,4 @@
 #include <Solution.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 int main(void) {
 	SegTree tree(10);
@@ -14,5 +10,11 @@ int main(void) {
 	for (int i = 0; i < 10; ++i) {
 		cout << tree.query(0, i, 1, 0, 10) << endl;
 	}
+
+	RangeTree Rtree(8);
+	Rtree.rangeAdd(0, 7, 1, 1, 0, 8);
+	Rtree.rangeAdd(2, 5, 3, 1, 0, 8);
+	cout << "range sum : " << Rtree.rangeSum(0, 7, 1, 0, 8) << endl;
+	cout << "range sum : " << Rtree.rangeSum(3, 4, 1, 0, 8) << endl;
 	return 0;
 }
