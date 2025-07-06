055717f253cd18da8801ebd359b240b58dc6bcce
neilhuang <neilhuang@synology.com>
2025-07-06 20:35:01 +0800

    <LeetCode202109> 987. Vertical Order Traversal of a Binary Tree

--- 0987_Vertical_Order_Traversal_Of_A_Binary_Tree/Solution.cpp
+++ 0987_Vertical_Order_Traversal_Of_A_Binary_Tree/Solution.cpp
@@ -1,46 +1,26 @@
 #include <Solution.h>
-#include <iostream>
-#include <map>
-#include <queue>
 
-using std::map;
-using std::pair;
-using std::queue;
+void dfs(TreeNode* node, int x, int y, map<int, map<int, multiset<int>>>& value) {
+	if (!node) return;
+	value[y][x].emplace(node->val);
+
+	dfs(node->left, x + 1, y - 1, value);
+	dfs(node->right, x + 1, y + 1, value);
+}
 
 vector<vector<int>> Solution::verticalTraversal(TreeNode *root) {
+	map<int, map<int, multiset<int>>> value;
+	dfs(root, 0, 0, value);
+
 	vector<vector<int>> result;
-	if (!root)
-		return result;
-
-	map<int, vector<int>> oMap;
-
-	queue<pair<TreeNode *, int>> que, next;
-	que.emplace(pair<TreeNode *, int>(root, 0));
-
-	map<int, vector<int>> rowMap;
-	while (!que.empty()) {
-		auto cur = que.front();
-		que.pop();
-
-		rowMap[cur.second].emplace_back(cur.first->val);
-
-		if (cur.first->left)
-			next.emplace(pair<TreeNode *, int>(cur.first->left, cur.second - 1));
-		if (cur.first->right)
-			next.emplace(pair<TreeNode *, int>(cur.first->right, cur.second + 1));
-
-		if (que.empty()) {
-			que = move(next);
-			for (auto &e : rowMap) {
-				sort(e.second.begin(), e.second.end(), std::greater<int>());
-				while (!e.second.empty()) {
-					oMap[e.first].emplace_back(e.second.back());
-					e.second.pop_back();
-				}
+	for (const auto& sm : value) {
+		vector<int> list;
+		for (const auto& e : sm.second) {
+			for (const auto& s : e.second) {
+				list.emplace_back(s);
 			}
 		}
+		result.emplace_back(list);
 	}
-	for (auto &e : oMap)
-		result.emplace_back(e.second);
 	return result;
 }
--- 0987_Vertical_Order_Traversal_Of_A_Binary_Tree/main.cpp
+++ 0987_Vertical_Order_Traversal_Of_A_Binary_Tree/main.cpp
@@ -1,10 +1,19 @@
 #include <Solution.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 int main(void) {
+	// [3,9,20,null,null,15,7]
+	TreeNode *root = new TreeNode(3);
+	root->left = new TreeNode(9);
+	root->right = new TreeNode(20);
+	root->right->left = new TreeNode(15);
+	root->right->right = new TreeNode(7);
 	Solution *test = new Solution();
+	auto result = test->verticalTraversal(root);
+	for (const auto &vec : result) {
+		for (const auto &val : vec) {
+			cout << val << " ";
+		}
+		cout << endl;
+	}
 	return 0;
 }
