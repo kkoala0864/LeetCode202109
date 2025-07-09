69379b1391f35bd0ca57550e5dd12d2f9734f969
neilhuang <neilhuang@synology.com>
2025-07-09 19:36:17 +0800

    <LeetCode202109> 3439. Reschedule Meetings for Maximum Free Time I

--- 3439_Reschedule_Meetings_for_Maximum_Free_Time_I/Solution.cpp
+++ 3439_Reschedule_Meetings_for_Maximum_Free_Time_I/Solution.cpp
@@ -1,27 +1,24 @@
 #include <Solution.h>
-#include <iostream>
-#include <algorithm>
-
-using std::max;
 
 int Solution::maxFreeTime(int eventTime, int k, vector<int> &startTime, vector<int> &endTime) {
-	vector<int> free;
-
-	int cur = 0;
-	for (int i = 0; i < startTime.size(); ++i) {
-		free.emplace_back(startTime[i] - cur);
-		cur = endTime[i];
+	vector<int> freeTime;
+	int lastEnd = 0;
+	for (int i = 0 ; i < startTime.size() ; ++i) {
+		freeTime.emplace_back(startTime[i] - lastEnd);
+		lastEnd = endTime[i];
 	}
-	free.emplace_back(eventTime - cur);
+	freeTime.emplace_back(eventTime - lastEnd);
+
+	++k;
+	int cnt = 0;
 	int result = 0;
-	cur = 0;
-	for (int i = 0; i < free.size(); ++i) {
-		cur += free[i];
-		if (i >= k + 1) {
-			cur -= free[i - (k + 1)];
-		}
+	for (int i = 0 ; i < freeTime.size() ; ++i) {
+		cnt += freeTime[i];
 		if (i >= k) {
-			result = max(result, cur);
+			cnt -= freeTime[i-k];
+		}
+		if (i >= k-1) {
+			result = max(result, cnt);
 		}
 	}
 	return result;
