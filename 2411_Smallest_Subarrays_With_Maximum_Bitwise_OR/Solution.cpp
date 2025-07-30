5e6595f3961852a8fc5cf1eb3b8d47a3d7a872f6
neilhuang <neilhuang@synology.com>
2025-07-30 13:03:03 +0800

    <LeetCode202109> 2411. Smallest Subarrays With Maximum Bitwise OR

--- 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR/Solution.cpp
+++ 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR/Solution.cpp
@@ -1,59 +1,26 @@
 #include <Solution.h>
 
-vector<int> sub(vector<int>& a, vector<int>& b) {
-	vector<int> result(32, 0);
-	for (int i = 0 ; i < 32 ; ++i) result[i] = a[i] - b[i];
-	return result;
-}
-
-bool check(int mx, vector<vector<int>>& preORBits, int mid) {
-	auto bits = sub(preORBits.back(), preORBits[mid]);
-	for (int i = 0 ; i < 32 ; ++i) {
-		int mb = (1 << i) & mx ? 1 : 0;
-		int bb = bits[i] > 0 ? 1 : 0;
-		if (mb != bb) return false;
-	}
-	return true;
-}
-
-void doOper(int v, vector<int>& bits, int oper) {
-	for (int i = 0 ; i < 32 ; ++i) {
-		if ((1 << i) & v) {
-			if (oper == 0) ++bits[i];
-			else --bits[i];
-		}
-	}
-}
-
-int getLen(vector<vector<int>>& preORBits, int curMax) {
-	int l = 0, r = preORBits.size() - 1;
-	int mid = 0;
-
-	while (l < r) {
-		mid = r - ((r - l) >> 1);
-		bool ret = check(curMax, preORBits, mid);
-		if (ret) {
-			l = mid;
-		} else {
-			r = mid - 1;
-		}
-	}
-	return !check(curMax, preORBits, l) ? preORBits.size() : max(((int)preORBits.size() - l - 1), 1);
-}
-
 vector<int> Solution::smallestSubarrays(vector<int>& nums) {
 	int size = nums.size();
 
-	vector<vector<int>> preORBits;
-	vector<int> cur(32, 0);
-	int v = 0;
 	vector<int> result(size, INT_MAX);
 
+	int mx = 0;
+	vector<int> cur(32, size-1);
 	for (int i = size - 1 ; i >= 0 ; --i) {
-		v |= nums[i];
-		doOper(nums[i], cur, 0);
-		preORBits.emplace_back(cur);
-		result[i] = getLen(preORBits, v);
+		int far = -1;
+		mx |= nums[i];
+		for (int j = 0 ; j < 32 ; ++j) {
+			if ((1 << j) & nums[i]) {
+				cur[j] = i;
+			}
+		}
+		for (int j = 0 ; j < 32 ; ++j) {
+			if ((1 << j) & mx) {
+				far = max(far, cur[j]);
+			}
+		}
+		result[i] = far == -1 ? 1 : far - i + 1;
 	}
 	return result;
 }
--- 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR/main.cpp
+++ 2411_Smallest_Subarrays_With_Maximum_Bitwise_OR/main.cpp
@@ -1,7 +1,7 @@
 #include <Solution.h>
 
 int main(void) {
-	vector<int> input = {1,0,2,1,3};
+	vector<int> input = {0,0,0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,32,2,0,0,0,0,0,0,28,16,32,0,0,0,0,0,0,0,1,0,8,0,0,0,0,0,0,0,0,0,32,0,0,0,0,0,0,0,0,16,0,0,0,0,2,0,0,32,0,0,0,4,16,0,4,0,32,0,0,8,0,1,1,0,0,0,0,0};
 	Solution *test = new Solution();
 	auto ret = test->smallestSubarrays(input);
 	for (const auto& v : ret) cout << v << " ";
