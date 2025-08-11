6cca8de0e338569ca70d46e422a11ae825a3eaf7
neilhuang <neilhuang@synology.com>
2025-08-11 09:52:04 +0800

    <LeetCode202109> 869. Reordered Power of 2

--- 0869_Reordered_Power_Of_2/Solution.cpp
+++ 0869_Reordered_Power_Of_2/Solution.cpp
@@ -1,35 +1,27 @@
 #include <Solution.h>
-#include <iostream>
-#include <vector>
 
-using std::vector;
+vector<int> getCnt(int v) {
+	vector<int> dCnt(10, 0);
+	while (v > 0) {
+		++dCnt[v % 10];
+		v /= 10;
+	}
+	return dCnt;
+}
+
+bool check(vector<int>& lhs, vector<int>& rhs) {
+	for (int i = 0 ; i < 10 ; ++i) {
+		if (lhs[i] != rhs[i]) return false;
+	}
+	return true;
+}
 
 bool Solution::reorderedPowerOf2(int n) {
-	vector<vector<int>> cnt(32, vector<int>(10, 0));
+	auto nCnt = getCnt(n);
 
-	int base = 1;
-	for (int i = 0; i < 32; ++i) {
-		int val = base << i;
-		while (val > 0) {
-			++cnt[i][val % 10];
-			val /= 10;
-		}
-	}
-
-	vector<int> target(10, 0);
-	while (n > 0) {
-		++target[n % 10];
-		n /= 10;
-	}
-
-	for (int i = 0; i < 32; ++i) {
-		int j = 0;
-		for (; j < 10; ++j) {
-			if (target[j] != cnt[i][j])
-				break;
-		}
-		if (j == 10)
-			return true;
+	for (int i = 0 ; i < 32 ; ++i) {
+		auto cCnt = getCnt(1 << i);
+		if (check(nCnt, cCnt)) return true;
 	}
 	return false;
 }
