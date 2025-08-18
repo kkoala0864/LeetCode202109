759c079bdeea3c5c875fef117d2a9eb7ed92e87d
neilhuang <neilhuang@synology.com>
2025-08-18 16:18:45 +0800

    <LeetCode202109> 1323. Maximum 69 Number

--- 1323_Maximum_69_Number/Solution.cpp
+++ 1323_Maximum_69_Number/Solution.cpp
@@ -1,17 +1,12 @@
 #include <Solution.h>
-#include <iostream>
-#include <string>
-
-using std::string;
-using std::to_string;
 
 int Solution::maximum69Number(int num) {
-	string numStr = to_string(num);
-	for (int i = 0; i < numStr.size(); ++i) {
-		if (numStr[i] == '6') {
-			numStr[i] = '9';
+	string s = to_string(num);
+	for (int i = 0 ; i < s.size() ; ++i) {
+		if (s[i] == '6') {
+			s[i] = '9';
 			break;
 		}
 	}
-	return stoi(numStr);
+	return stoi(s);
 }
