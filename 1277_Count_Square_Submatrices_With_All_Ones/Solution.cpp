de33ed31a4ffa5d2b85063f2044e7d4b167bceae
neilhuang <neilhuang@synology.com>
2025-08-20 10:10:38 +0800

    <LeetCode202109> 1277. Count Square Submatrices with All Ones

--- 1277_Count_Square_Submatrices_With_All_Ones/Solution.cpp
+++ 1277_Count_Square_Submatrices_With_All_Ones/Solution.cpp
@@ -1,26 +1,16 @@
 #include <Solution.h>
-#include <iostream>
-#include <algorithm>
-
-using std::min;
 
 int Solution::countSquares(vector<vector<int>> &matrix) {
-
-	int m = matrix.size();
-	int n = matrix[0].size();
-
-	vector<vector<int>> dp = vector<vector<int>>(m, vector<int>(n, 0));
 	int result = 0;
-	for (int i = 0; i < m; ++i) {
-		for (int j = 0; j < n; ++j) {
-			if (matrix[i][j] == 0)
+	for (int i = 0 ; i < matrix.size() ; ++i) {
+		for (int j = 0 ; j < matrix[0].size() ; ++j) {
+			if (matrix[i][j] == 0) {
 				continue;
-			if (i == 0 || j == 0)
-				dp[i][j] = 1;
-			else {
-				dp[i][j] = min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]}) + 1;
 			}
-			result += dp[i][j];
+			if (i > 0 && j > 0) {
+				matrix[i][j] = min({matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1]}) + 1;
+			}
+			result += matrix[i][j];
 		}
 	}
 	return result;
