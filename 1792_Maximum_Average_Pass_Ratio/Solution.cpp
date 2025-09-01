43caa1931534fae59585bc6fb4c492e924dbc748
neilhuang <neilhuang@synology.com>
2025-09-01 11:29:53 +0800

    <LeetCode202109> 1792. Maximum Average Pass Ratio

--- 1792_Maximum_Average_Pass_Ratio/Solution.cpp
+++ 1792_Maximum_Average_Pass_Ratio/Solution.cpp
@@ -1,40 +1,32 @@
 #include <Solution.h>
-#include <iostream>
-#include <queue>
-
-using std::cout;
-using std::endl;
-using std::pair;
-using std::priority_queue;
 
 double Solution::maxAverageRatio(vector<vector<int>> &classes, int extraStudents) {
 	priority_queue<pair<double, int>> pq;
-	double size = classes.size();
 
-	for (int i = 0; i < classes.size(); ++i) {
-		if (classes[i][0] == classes[i][1])
-			continue;
-		double c = (double)classes[i][0] / (double)classes[i][1];
-		double n = (double)(classes[i][0] + 1) / (double)(classes[i][1] + 1);
-		pq.emplace(pair<double, int>(n - c, i));
+	for (int i = 0 ; i < classes.size() ; ++i) {
+		if (classes[i][0] == classes[i][1]) continue;
+		double r = (classes[i][0] * 1.0) / classes[i][1];
+		double nr = ((classes[i][0] + 1) * 1.0) / (classes[i][1] + 1);
+		pq.emplace(pair<double, int>(nr - r, i));
 	}
-	if (pq.empty())
-		return 1;
+	if (pq.empty()) return 1;
 
 	while (extraStudents-- > 0) {
-		int i = pq.top().second;
-		int r = pq.top().first;
+		int ci = pq.top().second;
 		pq.pop();
 
-		++classes[i][0];
-		++classes[i][1];
-		double c = (double)classes[i][0] / (double)classes[i][1];
-		double n = (double)(classes[i][0] + 1) / (double)(classes[i][1] + 1);
-		pq.emplace(pair<double, int>(n - c, i));
+		++classes[ci][0];
+		++classes[ci][1];
+		double r = (classes[ci][0] * 1.0) / classes[ci][1];
+		double nr = ((classes[ci][0] + 1) * 1.0) / (classes[ci][1] + 1);
+		pq.emplace(pair<double, int>(nr - r, ci));
 	}
-	double rSum = 0;
-	for (const auto &v : classes) {
-		rSum += ((double)v[0] / (double)v[1]);
+
+	double sum = 0;
+	for (const auto& c : classes) {
+		double r = (c[0] * 1.0) / c[1];
+		sum += r;
 	}
-	return rSum / size;
+	return sum / classes.size();
+
 }
--- 1792_Maximum_Average_Pass_Ratio/main.cpp
+++ 1792_Maximum_Average_Pass_Ratio/main.cpp
@@ -1,11 +1,7 @@
 #include <Solution.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 int main(void) {
-	vector<vector<int>> input = {{2, 4}, {3, 9}, {4, 5}, {2, 10}};
+	vector<vector<int>> input = {{2,4},{3,9},{4,5},{2,10}};
 	int k = 4;
 	Solution *test = new Solution();
 	cout << test->maxAverageRatio(input, k) << endl;
