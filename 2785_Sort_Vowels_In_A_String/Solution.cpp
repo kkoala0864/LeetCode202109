35634d2b81ad49285d6546f837cdc79ce3706b32
neilhuang <neilhuang@synology.com>
2025-09-11 12:36:02 +0800

    <LeetCode202109> 2785. Sort Vowels in a String

--- 2785_Sort_Vowels_In_A_String/Solution.cpp
+++ 2785_Sort_Vowels_In_A_String/Solution.cpp
@@ -1,42 +1,23 @@
 #include <Solution.h>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
 
-using std::unordered_map;
-using std::vector;
-
-bool isVowel(char c) {
-	char t = tolower(c);
-	return t == 'a' || t == 'e' || t == 'i' || t == 'o' || t == 'u';
-}
-
-void getVowelStr(unordered_map<char, int> &m, vector<char> &vt, string &result) {
-	for (int i = vt.size() - 1; i >= 0; --i) {
-		if (m.count(vt[i]) > 0) {
-			result += string(m[vt[i]], vt[i]);
-		}
-	}
+bool isVowels(char c) {
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+		c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
 }
 
 string Solution::sortVowels(string s) {
-	vector<char> VT = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
-	unordered_map<char, int> m;
-	vector<int> vi;
-	for (int i = 0; i < s.size(); ++i) {
-		if (isVowel(s[i])) {
-			++m[s[i]];
-			vi.emplace_back(i);
+	string vs;
+	for (const auto& c : s) {
+		if (isVowels(c)) {
+			vs.push_back(c);
 		}
 	}
-
-	string vs;
-	getVowelStr(m, VT, vs);
-
-	for (int i = 0; i < vi.size(); ++i) {
-		s[vi[i]] = vs.back();
-		vs.pop_back();
+	sort(vs.begin(), vs.end());
+	int vi = 0;
+	for (int i = 0 ; i < s.size() ; ++i) {
+		if (isVowels(s[i])) {
+			s[i] = vs[vi++];
+		}
 	}
 	return s;
 }
