5cc3ca359a5281e3f3ba3ec63390f804e8eed3b2
neilhuang <neilhuang@synology.com>
2025-09-22 13:09:12 +0800

    <LeetCode202109> 3005. Count Elements With Maximum Frequency

--- 3005_Count_Elements_With_Maximum_Frequency/Solution.cpp
+++ 3005_Count_Elements_With_Maximum_Frequency/Solution.cpp
@@ -1,26 +1,16 @@
 #include <Solution.h>
-#include <iostream>
-#include <unordered_map>
-#include <algorithm>
-
-using std::max;
-using std::unordered_map;
 
 int Solution::maxFrequencyElements(vector<int> &nums) {
-	vector<int> cnt(101, 0);
-	for (const auto &v : nums)
-		++cnt[v];
+	unordered_map<int, int> cnt;
+	int mxCnt = 0;
 
-	vector<int> freq(101, 0);
-	for (const auto &c : cnt) {
-		if (c == 0)
-			continue;
-		++freq[c];
+	for (const auto& v : nums) {
+		++cnt[v];
+		mxCnt = max(mxCnt, cnt[v]);
 	}
-	for (int i = 100; i >= 1; --i) {
-		if (freq[i] == 0)
-			continue;
-		return freq[i] * i;
+	int result = 0;
+	for (const auto& e : cnt) {
+		if (e.second == mxCnt) result += mxCnt;
 	}
-	return 0;
+	return result;
 }
