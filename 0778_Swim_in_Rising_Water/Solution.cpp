bbd80f4f1efe83deacadea02e39c2f3f8899b553
neilhuang <neilhuang@synology.com>
2025-10-06 04:58:55 +0000

    <LeetCode202109> 778. Swim in Rising Water

--- 0778_Swim_in_Rising_Water/Solution.cpp
+++ 0778_Swim_in_Rising_Water/Solution.cpp
@@ -1,39 +1,41 @@
 #include <Solution.h>
-#include <iostream>
-#include <queue>
-#include <climits>
+
+int find(vector<int>& root, int idx) {
+	if (root[idx] == idx) return idx;
+	return root[idx] = find(root, root[idx]);
+}
 
 int Solution::swimInWater(vector<vector<int>>& grid) {
-	priority_queue<vector<int>> que, next;
+	priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
 	int m = grid.size();
 	int n = grid[0].size();
-	vector<vector<int>> minCost(m, vector<int>(n, INT_MAX));
 
-	minCost[0][0] = grid[0][0];
-	que.emplace(vector<int>({grid[0][0], 0, 0}));
+	for (int i = 0 ; i < m ; ++i) {
+		for (int j = 0 ; j < n ; ++j) {
+			pq.emplace(vector<int>({grid[i][j], i, j}));
+		}
+	}
+
+	vector<int> root;
+	for (int i = 0 ; i < (m * n) ; ++i) root.emplace_back(i);
 
 	vector<int> dir = {1, 0, -1, 0, 1};
-	while (!que.empty()) {
-		int cost = que.top()[0];
-		int x = que.top()[1];
-		int y = que.top()[2];
-		que.pop();
+	while (!pq.empty()) {
+		int curDepth = pq.top()[0];
+		int x = pq.top()[1];
+		int y = pq.top()[2];
+		pq.pop();
 
-		if (cost > minCost[x][y]) continue;
-
-		int hCnt = 0;
 		for (int i = 0 ; i < 4 ; ++i) {
 			int nx = x + dir[i];
 			int ny = y + dir[i+1];
-			if (nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-			int nCost = max(cost, grid[nx][ny]);
-			if (nCost >= minCost[nx][ny]) continue;
-			minCost[nx][ny] = nCost;
-			next.emplace(vector<int>({nCost, nx, ny}));
+			if (nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] > curDepth) continue;
+			int lr = find(root, x * n + y);
+			int rr = find(root, nx * n + ny);
+			if (lr == rr) continue;
+			root[lr] = root[rr] = min(lr, rr);
 		}
-		if (que.empty()) {
-			que = move(next);
-		}
+		if (find(root, 0) == find(root, m * n - 1)) return curDepth;
 	}
-	return minCost[m-1][n-1];
+	return -1;
 }
