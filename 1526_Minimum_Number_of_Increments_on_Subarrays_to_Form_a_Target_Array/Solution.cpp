01129558cccf32edde2e5e32f9b860641be387a4
neilhuang <neilhuang@synology.com>
2025-10-31 09:25:06 +0000

    <LeetCode202109> 1526. Minimum Number of Increments on Subarrays to Form a Target Array

--- 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/Solution.cpp
+++ 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/Solution.cpp
@@ -1,24 +1,19 @@
 #include <Solution.h>
-#include <iostream>
-#include <stack>
-
-using std::stack;
 
 int Solution::minNumberOperations(vector<int> &target) {
-	stack<int> st;
-
 	int result = 0;
+
+	stack<int> st;
 	st.emplace(0);
-	for (const auto &v : target) {
+
+	for (const auto& v : target) {
 		bool check = false;
-		while (!st.empty() && st.top() > v) {
+		while (!st.empty() && st.top() >= v) {
 			check = true;
 			st.pop();
 		}
-		if (!check)
-			result += (v - st.top());
-		if (st.top() != v)
-			st.emplace(v);
+		if (!check) result += (v - st.top());
+		st.emplace(v);
 	}
 	return result;
 }
--- 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/main.cpp
+++ 1526_Minimum_Number_of_Increments_on_Subarrays_to_Form_a_Target_Array/main.cpp
@@ -1,11 +1,7 @@
 #include <Solution.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 int main(void) {
-	vector<int> input = {3, 1, 5, 4, 2};
+	vector<int> input = {3,4,2,5,6};
 	Solution *test = new Solution();
 	cout << test->minNumberOperations(input) << endl;
 	return 0;
