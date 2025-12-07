52ad709c82c5653c87374817f3502ae6b5be67fc
neilhuang <neilhuang@synology.com>
2025-12-07 12:55:34 +0800

    <LeetCode202109> 1214. Two Sum BSTs

--- 1214_Two_Sum_BSTs/Solution.cpp
+++ 1214_Two_Sum_BSTs/Solution.cpp
@@ -1,37 +1,19 @@
 #include <Solution.h>
-#include <iostream>
-#include <vector>
 
-using std::vector;
-
-void serialize(TreeNode *node, vector<int> &vec) {
-	if (!node)
-		return;
-	if (node->left)
-		serialize(node->left, vec);
-	vec.emplace_back(node->val);
-	if (node->right)
-		serialize(node->right, vec);
+bool search(TreeNode* node, int target) {
+	if (!node) return false;
+	if (node->val == target) return true;
+	if (target > node->val) {
+		return search(node->right, target);
+	} else {
+		return search(node->left, target);
+	}
 }
 
 bool Solution::twoSumBSTs(TreeNode *root1, TreeNode *root2, int target) {
-	vector<int> v1, v2;
-	serialize(root1, v1);
-	serialize(root2, v2);
+	if (!root1) return false;
+	int rest = target - root1->val;
+	if (search(root2, rest)) return true;
 
-	int i1 = 0, i2 = v2.size() - 1;
-
-	int sum = 0;
-	while (i1 < v1.size() && i2 >= 0) {
-		sum = v1[i1] + v2[i2];
-
-		if (sum == target) {
-			return true;
-		} else if (sum < target) {
-			++i1;
-		} else {
-			--i2;
-		}
-	}
-	return false;
+	return twoSumBSTs(root1->left, root2, target) || twoSumBSTs(root1->right, root2, target);
 }
--- 1214_Two_Sum_BSTs/Solution.h
+++ 1214_Two_Sum_BSTs/Solution.h
@@ -1,18 +1,4 @@
-struct TreeNode
-{
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode()
-	    : val(0), left(nullptr), right(nullptr) {
-	}
-	TreeNode(int x)
-	    : val(x), left(nullptr), right(nullptr) {
-	}
-	TreeNode(int x, TreeNode *left, TreeNode *right)
-	    : val(x), left(left), right(right) {
-	}
-};
+#include <data_type.h>
 
 class Solution {
 public:
